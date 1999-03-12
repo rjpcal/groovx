0e2f4053efde395d627e9eb40f5b94d40af33fa2
Robert J. Peters <rjpcal@gmail.com>
1999-03-12 19:08:24 +0000

    this is a development file for a new way to control experiments: through ScaledObj's, Trial's, Tlist's, and Expt's. The individual classes will eventually be split out of this file.

--- src/visx/trial.h
+++ src/visx/trial.h
@@ -1,18 +1,53 @@
+///////////////////////////////////////////////////////////////////////
 // trial.h
+// Rob Peters
+// created: Mar-99
+// written: Fri Mar 12 11:07:18 1999
+static const char vcid[] = "$Id$";
+///////////////////////////////////////////////////////////////////////
 
-// class GrObj : public TrialEvent {};
+class Tlist {
+private:
+  vector<Position *> itsPositions;
+  vector<GrObj *> itsObjs;
+  vector<Trial *> itsTrials;
+  int itsCurGroup;
+  bool itsVisibility;
+public:
+  void showTrial(int trial) const;
+  void drawCurTrial() const;
+  void setVisibile(bool vis);
+  void setCurGroup(int group);
+  void addToTrial(int trial, int objid, int posid);
+}
+
+
+class Expt {
+  vec<Trial *> itsTrials; // each is unique
+  vec<int> itsTrialSequence; // randomly ordered list of indexes into itsTrials
+										    // (may contain repeats)
+  int itsRandSeed;
+  int itsCurstim;        // index into itsTrialList
+  
+public:
+  void beginNextTrial();
+  void recordResponse(int resp);
+  int lastResponse() { 
+	 return itsTrials[itsTrialList[itsCurstim-1]]->lastResponse();
+  }
+}
 
 class Trial {
   typedef vec<ScaledObj *> ObjGrp;
   ObjGrp itsObjs;
   list<int> itsResponses;
   int itsType;
-
+  
   class ScaledObj : public GrObj {
 	 GrObj *itsObj;
 	 Scaleable *itsScale;
   public:
-	 ScaledObj(GrObj *obj, Scaleable *scl) : itsObj(obj), itsScaleable(scl) {}
+	 ScaledObj(int objid, int posid) : itsObj(obj), itsScaleable(scl) {}
 	 ~ScaledObj() {} // someone else owns the GrObj and Scaleable
 	 void render() {
 		glPushMatrix(GL_MODELVIEW);
@@ -23,18 +58,18 @@ class Trial {
 		glPopMatrix();
 	 }
   };
-
+  
 public:
   Trial () : itsObjs(), itsResponses() {
   }
-
+  
   ~Trial () {
 	 for (ObjGrp::iterator itr = itsObjs.begin(); itr != itsObjs.end(); itr++) {
 		delete *itr;
 		*itr = NULL;
 	 }
   }
-
+  
   void add(GrObj *obj, Scaleable *scl) { 
 	 itsObjs.push_back(new ScaledObj(obj, scl));
   }
@@ -43,7 +78,10 @@ public:
 		(*itr)->action();
 	 }
   }
-  virtual int trialType() { return -1; }
+
+  virtual int trialType() { return -1; } 
+  // returns some info about relationship between objects in trial
+
   void recordResponse(int resp) { itsResponses.push_back(resp); }
   int lastResponse() { return itsResponses.back(); }
 };
