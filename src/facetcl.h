04c0ffddd7784779f095c0ff55c44c6a244531c2
Robert J. Peters <rjpcal@gmail.com>
1999-03-13 20:52:45 +0000

    fixed version control string (again)

--- src/facetcl.h
+++ src/facetcl.h
@@ -2,8 +2,7 @@
 // facetcl.h
 // Rob Peters
 // created: Jan-99
-// written: Fri Mar 12 12:57:55 1999
-static const char vcid_facetcl_h[] = "$Id$";
+// written: Sat Mar 13 12:52:37 1999
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef FACETCL_H_DEFINED
@@ -16,4 +15,5 @@ namespace FaceTcl {
   Tcl_PackageInitProc Face_Init;
 }
 
+static const char vcid_facetcl_h[] = "$Id$";
 #endif // !FACETCL_H_DEFINED
