2824910a1a274a96d16922de9e7556554d207c67
Robert J. Peters <rjpcal@gmail.com>
1999-03-15 03:32:26 +0000

    added serialize/deserialize and readFromObjidsOnly

--- src/visx/trial.cc
+++ src/visx/trial.cc
@@ -2,7 +2,7 @@
 // trial.cc
 // Rob Peters
 // created: Fri Mar 12 17:43:21 1999
-// written: Sat Mar 13 15:41:13 1999
+// written: Sun Mar 14 19:31:39 1999
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef TRIAL_CC_DEFINED
@@ -11,6 +11,7 @@
 #include "trial.h"
 
 #include <iostream.h>
+#include <strstream.h>
 #include <string>
 #include <typeinfo>
 #include <GL/gl.h>
@@ -21,24 +22,34 @@
 #include "grobj.h"
 #include "position.h"
 
-#define NO_TRACE
 #include "trace.h"
+#define LOCAL_DEBUG
 #include "debug.h"
 
 ///////////////////////////////////////////////////////////////////////
-// Trial class declaration
+// Trial member functions
 ///////////////////////////////////////////////////////////////////////
 
-IOResult Trial::serialize(ostream &os, IOFlag flag = NO_FLAGS) const {
+Trial::Trial(istream &is, IOFlag flag, 
+				 const ObjList& olist, const PosList &plist) :
+  itsObjList(olist), itsPosList(plist), 
+  itsObjs(), itsResponses(), itsType(-1)
+{
+DOTRACE("Trial::Trial");
+  deserialize(is, flag);
+}
+
+
+IOResult Trial::serialize(ostream &os, IOFlag flag) const {
 DOTRACE("Trial::serialize");
   if (flag & IO::BASES) { /* there are no bases to deserialize */ }
 
   char sep = ' ';
-  if (flag & IO::TYPENAME) { os << typeid(Position).name() << sep; }
+  if (flag & IO::TYPENAME) { os << typeid(Trial).name() << sep; }
 
   // itsObjs
   os << itsObjs.size() << sep;
-  for (ObjGrp::iterator ii = itsObjs.begin(); ii != itsObjs.end(); ii++) {
+  for (ObjGrp::const_iterator ii = itsObjs.begin(); ii != itsObjs.end(); ii++) {
 	 os << (*ii).first << sep << (*ii).second << sep << sep;
   }
   // itsResponses
@@ -49,7 +60,7 @@ DOTRACE("Trial::serialize");
   return checkStream(os);
 }
 
-IOResult Trial::deserialize(istream &is, IOFlag flag = NO_FLAGS) {
+IOResult Trial::deserialize(istream &is, IOFlag flag) {
 DOTRACE("Trial::deserialize");
   if (flag & IO::BASES) { /* there are no bases to deserialize */ }
   if (flag & IO::TYPENAME) {
@@ -65,13 +76,46 @@ DOTRACE("Trial::deserialize");
   int objid, posid;
   for (int i = 0; i < size; i++) {
 	 is >> objid >> posid;
-	 itsObjs.push_back(ObjPos(objid, posid));
+	 add(objid, posid);
   }
   // itsResponses
   deserializeVecInt(is, itsResponses);
   // itsType
   is >> itsType;
   return checkStream(is);
+}
+
+bool Trial::readFromObjidsOnly(istrstream &is) {
+DOTRACE("Trial::readFromObjidsOnly");
+  int posid = 0;
+  int objid;
+  while (ist >> objid) {
+	 add(objid, posid);
+	 posid++;
+  }
+  return posid;					  // return the number of objid's read
+}
+
+const char* Trial::description() const {
+DOTRACE("Trial::description");
+  const int BUF_SIZE = 200;
+  static char buf[BUF_SIZE];
+
+  ostrstream ost(buf, BUF_SIZE);
+  
+  ost << "trial type == " << trialType()
+		<< ", objs ==";
+  for (int i = 0; i < itsObjs.size(); i++) {
+	 ost << " " << itsObjs[i].first;
+  }
+  ost << ", categories ==";
+  for (int j = 0; j < itsObjs.size(); j++) {
+	 GrObj *obj = itsObjList.getObj(itsObjs[j].first);
+	 ost << " " << obj->getCategory();
+  }
+  ost << '\0';
+
+  return buf;
 }
 
 void Trial::action() const {
@@ -94,4 +138,5 @@ DOTRACE("Trial::action");
   }
 }
 
+static const char vcid_trial_cc[] = "$Header$";
 #endif // !TRIAL_CC_DEFINED
