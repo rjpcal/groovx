fb18f6ff43fd726894abee0f2e84fea4ea61a3f8
Robert J. Peters <rjpcal@gmail.com>
1999-05-25 22:40:20 +0000

    Added a version of err_message that takes a const char* argument to indicate who generated the error. This is intended to be used from procedures that are removed from the Tcl command-line interface and therfore do not have an argv[] or objv[] array to pass to the err_message function. In particular, this is true of event-triggered callbacks that occur asynchronously in the background.

--- src/errmsg.h
+++ src/errmsg.h
@@ -2,30 +2,41 @@
 // errmsg.h
 // Rob Peters
 // created: Dec-98
-// written: Wed Apr 14 20:22:55 1999
+// written: Wed May 12 14:02:47 1999
 // $Id$
 //
-// this provides a simple inlined function that appends a Tcl function name
-// and an error message to the interpreters result
+// This file provides several simple inlined functions that append a
+// Tcl function name and a diagnostic error message to the
+// interpreters result. There are several ways that the function name
+// can be specified: either the entire objv[] or argv[] array from a
+// Tcl_CmdProc can be passed to err_message, in which case the value
+// of objv[0] or argv[0] is taken to be the function name;
+// alternatively a string literal can be passed in as the function
+// name.
 //
-// this is provided as a function so that the Tcl library calls can be
-// modified when the API changes
 ///////////////////////////////////////////////////////////////////////
 
 namespace {
 
-  inline void err_message(Tcl_Interp *interp, Tcl_Obj *const objv[],
-                          const char * err_msg) {
+  inline void err_message(Tcl_Interp* interp, Tcl_Obj* const objv[],
+                          const char* err_msg) {
 
 	 Tcl_AppendObjToObj(Tcl_GetObjResult(interp), objv[0]);
     Tcl_AppendStringsToObj(Tcl_GetObjResult(interp), 
 									": ", err_msg, (char *) NULL); 
   }
 
-  inline void err_message(Tcl_Interp *interp, char *argv[],
-                          const char * err_msg) {
+  inline void err_message(Tcl_Interp* interp, char* argv[],
+                          const char* err_msg) {
 
     Tcl_AppendStringsToObj(Tcl_GetObjResult(interp), argv[0], 
+									": ", err_msg, (char *) NULL); 
+  }
+
+  inline void err_message(Tcl_Interp* interp, const char* arg,
+                          const char* err_msg) {
+
+    Tcl_AppendStringsToObj(Tcl_GetObjResult(interp), arg, 
 									": ", err_msg, (char *) NULL); 
   }
 
