0130092748dd080d533489a3eb5101b4fe46f011
Robert J. Peters <rjpcal@gmail.com>
1999-05-27 18:49:04 +0000

    Removed getObjList since ObjList has now been changed to a singleton class, with the instance accessible through ObjList::theObjList().

--- src/objlisttcl.h
+++ src/objlisttcl.h
@@ -1,31 +1,34 @@
 ///////////////////////////////////////////////////////////////////////
+//
 // objlisttcl.h
 // Rob Peters
 // created: Jan-99
-// written: Mon Apr 19 17:05:35 1999
+// written: Thu May 27 11:29:48 1999
 // $Id$
+//
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef OBJLISTTCL_H_DEFINED
 #define OBJLISTTCL_H_DEFINED
 
 ///////////////////////////////////////////////////////////////////////
+//
 // ObjList Tcl package declarations
+//
 ///////////////////////////////////////////////////////////////////////
 
 struct Tcl_Interp;
-typedef int (Tcl_PackageInitProc) (Tcl_Interp *interp);
+typedef int (Tcl_PackageInitProc) (Tcl_Interp* interp);
 struct Tcl_Obj;
 class ObjList;
 class GrObj;
 
 namespace ObjlistTcl {
   typedef int (ObjlistTcl_SubcmdProc) 
-    (const ObjList& olist, Tcl_Interp *interp,
-     int objc, Tcl_Obj *const objv[]);
+    (const ObjList& olist, Tcl_Interp* interp,
+     int objc, Tcl_Obj* const objv[]);
 
-  ObjList& getObjList();
-  GrObj* getObjFromArg(Tcl_Interp *interp, Tcl_Obj *const objv[], 
+  GrObj* getObjFromArg(Tcl_Interp* interp, Tcl_Obj* const objv[], 
                        const ObjList& olist, int argn);
 
   const char* const bad_objid_msg = "objid out of range";
