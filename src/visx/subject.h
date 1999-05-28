bff50373717e6b716fb7de75fea6f111c67942dd
Robert J. Peters <rjpcal@gmail.com>
1999-05-28 03:16:13 +0000

    Implemented the charCount() now required by the IO interface.

--- src/visx/subject.h
+++ src/visx/subject.h
@@ -2,14 +2,14 @@
 // subject.h
 // Rob Peters
 // created: Dec-98
-// written: Sun Apr 25 14:12:53 1999
+// written: Thu May 20 09:47:00 1999
 // $Id$
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef SUBJECT_H_DEFINED
 #define SUBJECT_H_DEFINED
 
-#ifndef IO_H_INCLUDED
+#ifndef IO_H_DEFINED
 #include "io.h"
 #endif
 
@@ -19,20 +19,21 @@
 
 class Subject : public virtual IO {
 public:
-  Subject(const char *name, const char* dir);
+  Subject(const char* name, const char* dir);
   Subject(istream &is, IOFlag flag);
   virtual ~Subject();
 
   virtual void serialize(ostream &os, IOFlag flag) const;
   virtual void deserialize(istream &is, IOFlag flag);
+  virtual int charCount() const;
 
   const char* getName() const { return itsName; }
   void setName(const char* name);
   const char* getDirectory() const { return itsDirectory; }
   void setDirectory(const char* dir);
 private:
-  char *itsName;
-  char *itsDirectory;
+  char* itsName;
+  char* itsDirectory;
 };
 
 static const char vcid_subject_h[] = "$Header$";
