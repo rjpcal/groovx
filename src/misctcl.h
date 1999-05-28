b0f5034b4cb8381752c86b1ef03e453710471d18
Robert J. Peters <rjpcal@gmail.com>
1999-05-28 02:57:24 +0000

    Added two empty comment lines... outasight, man!

--- src/misctcl.h
+++ src/misctcl.h
@@ -1,19 +1,21 @@
 ///////////////////////////////////////////////////////////////////////
+//
 // misctcl.h
 // Rob Peters
 // created: Nov-98
-// written: Mon Mar 15 15:54:05 1999
+// written: Thu May 27 19:57:04 1999
 // $Id$
 //
 // this package provides miscellaneous helper procedures for Tcl
 // including rand(), sleep(), and usleep()
+//
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef MISCTCL_H_DEFINED
 #define MISCTCL_H_DEFINED
 
 struct Tcl_Interp;
-typedef int (Tcl_PackageInitProc) (Tcl_Interp *interp);
+typedef int (Tcl_PackageInitProc) (Tcl_Interp* interp);
 
 namespace MiscTcl {
   Tcl_PackageInitProc Misc_Init;
