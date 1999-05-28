4fe644cbbc69d5bc6e89575eb2ec43971cae2f32
Robert J. Peters <rjpcal@gmail.com>
1999-05-28 02:56:09 +0000

    Added go() and rego() methods to override those in Position. These methods allows a Jitter to re-do the same translation that it did last. This is important if objects are to be erased from the screen in exactly the same location as they are drawn. For Jitter's, a new position is chosen whenever go() is called, after which all subsequent calls to rego() will do the same translation until the next call to go().

--- src/visx/jitter.h
+++ src/visx/jitter.h
@@ -1,15 +1,25 @@
 ///////////////////////////////////////////////////////////////////////
+//
 // jitter.h
 // Rob Peters
 // created: Wed Apr  7 13:46:40 1999
-// written: Mon Apr 26 21:21:44 1999
+// written: Thu May 27 19:54:06 1999
 // $Id$
+//
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef JITTER_H_DEFINED
 #define JITTER_H_DEFINED
 
+#ifndef POSITION_H_DEFINED
 #include "position.h"
+#endif
+
+///////////////////////////////////////////////////////////////////////
+//
+// Jitter class definition
+//
+///////////////////////////////////////////////////////////////////////
 
 class Jitter : public Position, public virtual IO {
 public:
@@ -23,6 +33,7 @@ public:
 
   virtual void serialize(ostream &os, IOFlag flag) const;
   virtual void deserialize(istream &is, IOFlag flag);
+  virtual int charCount() const;
 
   //////////////////
   // manipulators //
@@ -36,16 +47,30 @@ public:
   // actions //
   /////////////
 
+protected:
   virtual void translate() const;
   virtual void rotate() const;
 
+public:
+  virtual void go() const;
+  virtual void rego() const;
+  // The go() inherited is fine to reuse for Jitter, but rego must be
+  // redefined for Jitter so that we duplicate the previous jittering,
+  // rather than do a new jittering.
+
 private:
+  void rejitter() const;
+
   // These specify the amount of jitter that will be effected by the
   // translate() or rotate() calls; the actual value used will be in
   // the range of the specified value +/- jitter value.
   float itsXJitter;
   float itsYJitter;
   float itsRJitter;
+
+  mutable float itsXShift;
+  mutable float itsYShift;
+  mutable float itsRShift;
 };
 
 static const char vcid_jitter_h_[] = "$Header$";
