86c67f3525637be618e6b6bcc3320e3b1d66a439
Robert J. Peters <rjpcal@gmail.com>
1999-05-28 02:56:48 +0000

    Added two empty comment lines... yippee!

--- src/jittertcl.h
+++ src/jittertcl.h
@@ -1,16 +1,18 @@
 ///////////////////////////////////////////////////////////////////////
+//
 // jittertcl.h
 // Rob Peters
 // created: Wed Apr  7 14:58:07 1999
-// written: Wed Apr  7 15:22:13 1999
+// written: Thu May 27 19:56:33 1999
 // $Id$
+//
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef JITTERTCL_H_DEFINED
 #define JITTERTCL_H_DEFINED
 
 struct Tcl_Interp;
-typedef int (Tcl_PackageInitProc) (Tcl_Interp *interp);
+typedef int (Tcl_PackageInitProc) (Tcl_Interp* interp);
 
 namespace JitterTcl {
   Tcl_PackageInitProc Jitter_Init;
