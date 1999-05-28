d9b6fb605d2fafdab5ab9965fbf409cf83cf74ab
Robert J. Peters <rjpcal@gmail.com>
1999-05-28 03:22:26 +0000

    Exposed write_responsesProc in public interface.

--- src/tlisttcl.h
+++ src/tlisttcl.h
@@ -1,24 +1,34 @@
 ///////////////////////////////////////////////////////////////////////
+//
 // tlisttcl.h
 // Rob Peters
 // created: Sat Mar 13 12:37:22 1999
-// written: Tue Mar 16 19:22:53 1999
+// written: Thu May 27 13:49:50 1999
 // $Id$
+//
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef TLISTTCL_H_DEFINED
 #define TLISTTCL_H_DEFINED
 
 ///////////////////////////////////////////////////////////////////////
+//
 // Tlist Tcl package declarations
+//
 ///////////////////////////////////////////////////////////////////////
 
 struct Tcl_Interp;
-typedef int (Tcl_PackageInitProc) (Tcl_Interp *interp);
+typedef int (Tcl_PackageInitProc) (Tcl_Interp* interp);
+struct Tcl_Obj;
 class Tlist;
 
 namespace TlistTcl {
   Tlist& getTlist();
+
+  // Exported procedures
+  int write_responsesProc(Tcl_Interp* interp, Tcl_Obj* const objv[],
+								  const char* filename);
+
   Tcl_PackageInitProc Tlist_Init;
 }
 
