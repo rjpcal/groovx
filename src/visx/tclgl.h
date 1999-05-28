825bb5ce1456a61b90f335141d43d98215ec499b
Robert J. Peters <rjpcal@gmail.com>
1999-05-28 03:27:59 +0000

    The last two empty comment lines I'll be adding for a while. Sigh...

--- src/visx/tclgl.h
+++ src/visx/tclgl.h
@@ -1,21 +1,23 @@
 ///////////////////////////////////////////////////////////////////////
+//
 // tclgl.h
 // Rob Peters
 // created: Nov-98
-// written: Tue Mar 16 19:26:21 1999
+// written: Thu May 27 20:27:41 1999
 // $Id$
 //
 // This package provides some simple Tcl functions that are wrappers
 // for C OpenGL functions. The function names, argument lists, and
 // symbolic constants for the Tcl functions are identical to those in
 // the analagous C functions.
+//
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef TCLGL_H_DEFINED
 #define TCLGL_H_DEFINED
 
 struct Tcl_Interp;
-typedef int (Tcl_PackageInitProc) (Tcl_Interp *interp);
+typedef int (Tcl_PackageInitProc) (Tcl_Interp* interp);
 
 namespace TclGL {
   // initializes the Tclgl package when called from Tcl_AppInit
