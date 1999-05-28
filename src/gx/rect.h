5ca26925a435140b72d81e40bc1e4a8e1a779459
Robert J. Peters <rjpcal@gmail.com>
1999-05-28 03:10:36 +0000

    Changed Rect to template class rather than assuming float always. Of course all of the clients are still using float as far as I know, but someday things might change... (also added _four_ empty comment lines!!!)

--- src/gx/rect.h
+++ src/gx/rect.h
@@ -1,29 +1,38 @@
 ///////////////////////////////////////////////////////////////////////
+//
 // rect.h
 // Rob Peters
 // created: Jan-99
-// written: Fri Apr  2 16:27:02 1999
+// written: Thu May 27 20:10:24 1999
 // $Id$
+//
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef RECT_H_DEFINED
 #define RECT_H_DEFINED
 
+///////////////////////////////////////////////////////////////////////
+//
+// Rect template class definition
+//
+///////////////////////////////////////////////////////////////////////
+
+template<class V>
 class Rect {
 public:
   Rect() : l(0.0), t(0.0), r(0.0), b(0.0) {}
-  Rect(const Rect& i) : l(i.l), t(i.t), r(i.r), b(i.b) {}
+  Rect(const Rect<V>& i) : l(i.l), t(i.t), r(i.r), b(i.b) {}
   
-  float l, t, r, b;
-  void setRectLTRB(float L, float T, float R, float B)
+  V l, t, r, b;
+  void setRectLTRB(V L, V T, V R, V B)
         { l = L; t = T; r = R; b = B; }
-  void setRectLRBT(float L, float R, float B, float T)
+  void setRectLRBT(V L, V R, V B, V T)
         { l = L; r = R; b = B; t = T; }
-  float width() const { return (r-l); }
-  float height() const { return (t-b); }
-  float aspect() const { return width()/height(); }
-  void widen(float f) { l *= f; r *=f; }
-  void heighten(float f) { t *=f; b *=f; }
+  V width() const { return (r-l); }
+  V height() const { return (t-b); }
+  V aspect() const { return width()/height(); }
+  void widen(V f) { l *= f; r *=f; }
+  void heighten(V f) { t *=f; b *=f; }
 };
 
 static const char vcid_rect_h[] = "$Header$";
