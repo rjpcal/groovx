4394b120632fb3e7e779f74d4e3908dc4987fbb4
Robert J. Peters <rjpcal@gmail.com>
1999-05-28 03:03:10 +0000

    Yes!!! Two more lines of empty comments!

--- src/positiontcl.h
+++ src/positiontcl.h
@@ -1,16 +1,18 @@
 ///////////////////////////////////////////////////////////////////////
+//
 // positiontcl.h
 // Rob Peters
 // created: Sat Mar 13 12:52:55 1999
-// written: Tue Mar 16 19:35:05 1999
+// written: Thu May 27 20:02:57 1999
 // $Id$
+//
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef POSITIONTCL_H_DEFINED
 #define POSITIONTCL_H_DEFINED
 
 struct Tcl_Interp;
-typedef int (Tcl_PackageInitProc) (Tcl_Interp *interp);
+typedef int (Tcl_PackageInitProc) (Tcl_Interp* interp);
 
 namespace PositionTcl {
   Tcl_PackageInitProc Position_Init;
