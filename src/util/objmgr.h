9455f0c423644fe78215e007113d59259931080b
Robert J. Peters <rjpcal@gmail.com>
1999-06-07 21:21:56 +0000

    Initial revision

---
+++ src/util/objmgr.h
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////////////////////////////
+// iomgr.h
+// Rob Peters
+// created: Fri Apr 23 01:12:37 1999
+// written: Mon Jun  7 14:21:48 1999
+// $Id$
+///////////////////////////////////////////////////////////////////////
+
+#ifndef IOMGR_H_DEFINED
+#define IOMGR_H_DEFINED
+
+#ifndef IO_H_DEFINED
+#include "io.h"
+#endif
+
+class istream;
+
+namespace IoMgr {
+  IO* newIO(const char* type);
+  IO* newIO(istream &is, IO::IOFlag flag);
+  IO* newIO(const char* type, istream &is, IO::IOFlag flag);
+}
+
+static const char vcid_iomgr_h[] = "$Header$";
+#endif // !IOMGR_H_DEFINED
