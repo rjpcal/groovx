f74c5e721fb13346160cdd2ad625df1a47dba333
Robert J. Peters <rjpcal@gmail.com>
1999-06-11 01:53:51 +0000

    Removed Tcl_Interp* argument from getExpt() since ExptDriver, which contains the Expt, is now a singleton.

--- src/expttcl.h
+++ src/expttcl.h
@@ -2,7 +2,7 @@
 // expttcl.h
 // Rob Peters
 // created: Mon Mar  8 03:18:35 1999
-// written: Thu May 20 11:44:36 1999
+// written: Thu Jun 10 18:52:39 1999
 // $Id$
 ///////////////////////////////////////////////////////////////////////
 
@@ -15,7 +15,7 @@ typedef int (Tcl_PackageInitProc) (Tcl_Interp* interp);
 class Expt;
 
 namespace ExptTcl {
-  Expt& getExpt(Tcl_Interp* interp);
+  Expt& getExpt();
 
   Tcl_PackageInitProc Expt_Init;
 }
