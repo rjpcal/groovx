874e0610fd51acca4d714a88fa3a15de73e38bf1
Robert J. Peters <rjpcal@gmail.com>
1999-06-14 21:26:23 +0000

    The PosList package is now implemented as a TclListPkg, with the exception of the posType commands... this may be added to the TclListPkg base class at a later time.

--- src/poslisttcl.cc
+++ src/poslisttcl.cc
@@ -3,7 +3,7 @@
 // poslisttcl.cc
 // Rob Peters
 // created: Sat Mar 13 12:46:09 1999
-// written: Mon Jun  7 11:41:53 1999
+// written: Mon Jun 14 14:26:03 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -20,6 +20,7 @@
 #include "errmsg.h"
 #include "position.h"
 #include "poslist.h"
+#include "tcllistpkg.h"
 
 #define NO_TRACE
 #include "trace.h"
@@ -33,11 +34,7 @@
 ///////////////////////////////////////////////////////////////////////
 
 namespace PoslistTcl {
-  Tcl_ObjCmdProc posCountCmd;
-  Tcl_ObjCmdProc resetPosListCmd;
   Tcl_ObjCmdProc posTypeCmd;
-  Tcl_ObjCmdProc stringifyCmd;
-  Tcl_ObjCmdProc destringifyCmd;
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -66,34 +63,6 @@ DOTRACE("PoslistTcl::getPosFromArg");
   return posid.get();
 }
 
-// return the number of objects in thePosList
-int PoslistTcl::posCountCmd(ClientData, Tcl_Interp* interp,
-                           int objc, Tcl_Obj* const objv[]) {
-DOTRACE("PoslistTcl::posCountCmd");
-  if (objc > 1) {
-    Tcl_WrongNumArgs(interp, 1, objv, NULL);
-    return TCL_ERROR;
-  }
-
-  const PosList& plist = PosList::thePosList();
-  Tcl_SetObjResult(interp, Tcl_NewIntObj(plist.posCount()));
-  return TCL_OK;
-}
-
-// clear the ObjList of all GrObj's by calling ObjList::clearObjs()
-int PoslistTcl::resetPosListCmd(ClientData, Tcl_Interp* interp,
-                                int objc, Tcl_Obj* const objv[]) {
-DOTRACE("PoslistTcl::resetPosListCmd");
-  if (objc > 1) {
-    Tcl_WrongNumArgs(interp, 1, objv, NULL);
-    return TCL_ERROR;
-  }
-
-  PosList::thePosList().clearPos();
-
-  return TCL_OK;
-}
-
 // return an object's name
 int PoslistTcl::posTypeCmd(ClientData, Tcl_Interp* interp,
                            int objc, Tcl_Obj* const objv[]) {
@@ -114,83 +83,52 @@ DOTRACE("PoslistTcl::posTypeCmd");
   return TCL_OK;
 }
 
-int PoslistTcl::stringifyCmd(ClientData, Tcl_Interp* interp,
-                                     int objc, Tcl_Obj* const objv[]) {
-DOTRACE("PoslistTcl::stringifyCmd");
-  if (objc != 1)  {
-    Tcl_WrongNumArgs(interp, 1, objv, NULL);
-    return TCL_ERROR;
+///////////////////////////////////////////////////////////////////////
+//
+// PosListPkg class definition
+//
+///////////////////////////////////////////////////////////////////////
+
+class PosListPkg : public TclListPkg {
+public:
+  PosListPkg(Tcl_Interp* interp) :
+	 TclListPkg(interp, "PosList", "2.5")
+  {
+	 Tcl_CreateObjCommand(interp, "PosList::posType", PoslistTcl::posTypeCmd,
+								 (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
+
+	 Tcl_Eval(interp, 
+				 "namespace eval PosList {\n"
+				 "  proc resetPosList {} { PosList::reset }\n"
+				 "  proc posCount {} { return [PosList::count] }\n"
+				 "  namespace export posType\n"
+				 "  namespace export resetPosList\n"
+				 "  namespace export posCount\n"
+				 "}");
+	 Tcl_Eval(interp,
+				 "namespace import PosList::posType\n"
+				 "namespace import PosList::resetPosList\n"
+				 "namespace import PosList::posCount\n");
   }
 
-  const PosList& plist = PosList::thePosList();
+  virtual IO& getList() { return PosList::thePosList(); }
+  virtual int getBufSize() { return 200; }
 
-  const int BUF_SIZE = 200;
-  char buf[BUF_SIZE];
-  ostrstream ost(buf, BUF_SIZE);
+  virtual void reset() { PosList::thePosList().clearPos(); }
+  virtual int count() { return PosList::thePosList().posCount(); }
+};
 
-  try {
-	 plist.serialize(ost, IO::BASES|IO::TYPENAME);
-	 ost << '\0';
-  }
-  catch (IoError& err) {
-	 err_message(interp, objv, err.info().c_str());
-	 return TCL_ERROR;
-  }
-  Tcl_SetObjResult(interp, Tcl_NewStringObj(buf, -1));
-  return TCL_OK;
-}
-
-int PoslistTcl::destringifyCmd(ClientData, Tcl_Interp* interp,
-                                     int objc, Tcl_Obj* const objv[]) {
-DOTRACE("PoslistTcl::destringifyCmd");
-  if (objc != 2)  {
-    Tcl_WrongNumArgs(interp, 1, objv, "string");
-    return TCL_ERROR;
-  }
-
-  PosList& plist = PosList::thePosList();
-
-  const char* buf = Tcl_GetString(objv[1]);
-  Assert(buf);
-
-  istrstream ist(buf);
-
-  try {
-	 plist.deserialize(ist, IO::BASES|IO::TYPENAME);
-  }
-  catch (IoError& err) {
-	 err_message(interp, objv, err.info().c_str());
-	 return TCL_ERROR;
-  }
-  return TCL_OK;
-}
+//---------------------------------------------------------------------
+//
+// PoslistTcl::Poslist_Init --
+//
+//---------------------------------------------------------------------
 
 int PoslistTcl::Poslist_Init(Tcl_Interp* interp) {
 DOTRACE("PoslistTcl::Poslist_Init");
-  // Add all commands to the ::PosList namespace
-  Tcl_CreateObjCommand(interp, "PosList::posCount", posCountCmd,
-                       (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
-  Tcl_CreateObjCommand(interp, "PosList::resetPosList", resetPosListCmd,
-                       (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
-  Tcl_CreateObjCommand(interp, "PosList::posType", posTypeCmd,
-                       (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
-  Tcl_CreateObjCommand(interp, "PosList::stringify", stringifyCmd,
-                       (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
-  Tcl_CreateObjCommand(interp, "PosList::destringify", destringifyCmd,
-                       (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
 
-  Tcl_Eval(interp, 
-			  "namespace eval PosList {\n"
-			  "  namespace export posType\n"
-			  "  namespace export resetPosList\n"
-			  "  namespace export posCount\n"
-			  "}");
-  Tcl_Eval(interp,
-			  "namespace import PosList::posType\n"
-			  "namespace import PosList::resetPosList\n"
-			  "namespace import PosList::posCount\n");
+  new PosListPkg(interp);
 
-  Tcl_PkgProvide(interp, "Poslist", "2.1");
   return TCL_OK;
 }
 
