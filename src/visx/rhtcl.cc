bb366101d8c1870c906bbd0d02b7670d1b9fa5ae
Robert J. Peters <rjpcal@gmail.com>
1999-06-15 21:53:14 +0000

    RhPkg now derives from TclItemPkg and uses the attributes features of that class to take care of "useFeedback" and "keyRespPairs", so that commands for these attributes do not have to be explicitly declared in this file.

--- src/visx/rhtcl.cc
+++ src/visx/rhtcl.cc
@@ -3,7 +3,7 @@
 // rhtcl.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Wed Jun  9 20:39:46 1999
-// written: Mon Jun 14 14:08:37 1999
+// written: Tue Jun 15 14:52:06 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -16,49 +16,14 @@
 
 #include "rhlist.h"
 #include "responsehandler.h"
-#include "tclcmd.h"
-#include "attribcommand.h"
-#include "tclpkg.h"
+#include "attribcmd.h"
+#include "tclitempkg.h"
 #include "tcllistpkg.h"
 
 #define NO_TRACE
 #include "trace.h"
 #define LOCAL_ASSERT
 #include "debug.h"
-
-//---------------------------------------------------------------------
-//
-// RhAttribCommand class
-//
-// This is a base class for other command classes that get/set
-// attributes of ResponseHandler's.
-//
-//---------------------------------------------------------------------
-
-class RhAttribCommand : public AttribCommand {
-public:
-  RhAttribCommand(Tcl_Interp* interp, const char* cmd_name) :
-	 AttribCommand(interp, cmd_name, "rhid ?new_value?", 2, 3), itsRh(NULL) {}
-protected:
-  virtual void get() = 0;
-  virtual void set() = 0;
-
-  ResponseHandler* itsRh;
-private:
-  
-  virtual void invoke() {
-	 int id = getIntFromArg(1);
-	 
-	 static const char* const bad_rhid_msg = "invalid response handler id";
-	 if ( !RhList::theRhList().isValidId(id) ) {
-		throw TclError(bad_rhid_msg);
-	 }
-
-	 itsRh = RhList::theRhList().getPtr(id);
-	 
-	 AttribCommand::invoke();
-  }
-};
 
 //--------------------------------------------------------------------
 //
@@ -79,54 +44,6 @@ protected:
   }
 };
 
-//--------------------------------------------------------------------
-//
-// KeyRespPairsCmd --
-//
-//
-//--------------------------------------------------------------------
-
-class KeyRespPairsCmd : public RhAttribCommand {
-public:
-  KeyRespPairsCmd(Tcl_Interp* interp, const char* cmd_name) :
-	 RhAttribCommand(interp, cmd_name) {}
-protected:
-  virtual void get() { returnString(itsRh->getKeyRespPairs().c_str()); }
-  virtual void set() { itsRh->setKeyRespPairs(getStringFromArg(2)); }
-};
-
-//--------------------------------------------------------------------
-//
-// UseFeedbackCmd --
-//
-// Retrieve the current value or set a new value for whether feedback
-// is given in a specified ResponseHandler. If feedback is turned on,
-// then a user-defined function named "feedback" must be defined to
-// take a single argument (the trial response). This function will be
-// called upon each successful trial completion, and the function
-// should provide feedback as to whether the response was correct or
-// incorrect. (However, note that the response does not need to deal
-// with *invalid* responses; these are filtered out before "feedback"
-// is called.)
-//
-// Results: 
-// If the current value is being retrieved, returns a boolean
-// indicating whether feedback is used.
-//
-// Side effects:
-// If a new value is provided, feedback may be turned on or off.
-//
-//--------------------------------------------------------------------
-
-class UseFeedbackCmd : public RhAttribCommand {
-public:
-  UseFeedbackCmd(Tcl_Interp* interp, const char* cmd_name) :
-	 RhAttribCommand(interp, cmd_name) {}
-protected:
-  virtual void get() { returnBool(itsRh->getUseFeedback()); }
-  virtual void set() { itsRh->setUseFeedback(getBoolFromArg(2)); }
-};
-
 ///////////////////////////////////////////////////////////////////////
 //
 // RhListPkg class definition
@@ -136,7 +53,7 @@ protected:
 class RhListPkg : public TclListPkg {
 public:
   RhListPkg(Tcl_Interp* interp) :
-	 TclListPkg(interp, "RhList", "1.1")
+	 TclListPkg(interp, "RhList", "1.4")
   {
 	 RhList::theRhList().setInterp(interp);
 	 RhList::theRhList().insertAt(0, new ResponseHandler());
@@ -155,15 +72,29 @@ public:
 //
 ///////////////////////////////////////////////////////////////////////
 
-class RhPkg : public TclPkg {
+class RhPkg : public TclItemPkg {
 public:
   RhPkg(Tcl_Interp* interp) :
-	 TclPkg(interp, "Rh", "1.1") 
+	 TclItemPkg(interp, "Rh", "1.4")
   {
 	 addCommand( new ResponseHandlerCmd(interp, "Rh::responseHandler") );
-	 addCommand( new UseFeedbackCmd(interp, "Rh::useFeedback") );
-	 addCommand( new KeyRespPairsCmd(interp, "Rh::keyRespPairs") );	 
+	 declareBoolAttrib("useFeedback",
+							 new TBoolAttrib<ResponseHandler> (
+										&ResponseHandler::getUseFeedback,
+										&ResponseHandler::setUseFeedback));
+	 declareStringAttrib("keyRespPairs",
+								new TStringAttrib<ResponseHandler> (
+									   &ResponseHandler::getKeyRespPairs,
+										&ResponseHandler::setKeyRespPairs));
   }
+
+  virtual void* getItemFromId(int id) {
+	 if ( !RhList::theRhList().isValidId(id) ) {
+		throw TclError("invalid response handler id");
+	 }
+	 return static_cast<void*>(RhList::theRhList().getPtr(id));
+  }
+  
 };
 
 //--------------------------------------------------------------------
