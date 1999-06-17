9123fb37b1c0ef23d8ca5c6514cfaaeff862a47f
Robert J. Peters <rjpcal@gmail.com>
1999-06-17 19:11:35 +0000

    Initial revision

---
+++ src/tcl/tclcode.h
@@ -0,0 +1,42 @@
+///////////////////////////////////////////////////////////////////////
+//
+// tclevalcmd.h
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Thu Jun 17 10:38:13 1999
+// written: Thu Jun 17 10:48:39 1999
+// $Id$
+//
+// TclEvalCmd serves as a wrapper for a Tcl command string that is to
+// be TclEval'd. It creates an object from the string, locks the
+// object to ensure that it is not garbage collected, and provides a
+// function to invoke the command in a Tcl_Interp. The command is
+// byte-compiled by Tcl upon its first invocation.
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef TCLEVALCMD_H_DEFINED
+#define TCLEVALCMD_H_DEFINED
+
+#ifndef TCLOBJLOCK_H_DEFINED
+#include "tclobjlock.h"
+#endif
+
+class TclEvalCmd {
+public:
+  TclEvalCmd(const char* tcl_cmd, int flags = TCL_EVAL_GLOBAL) :
+	 itsCmdObj(Tcl_NewStringObj(tcl_cmd, -1)),
+	 itsLock(itsCmdObj),
+	 itsFlags(flags) {}
+
+  int invoke(Tcl_Interp* interp) { 
+	 return Tcl_EvalObjEx(interp, itsCmdObj, itsFlags);
+  }
+
+private:
+  Tcl_Obj* itsCmdObj;
+  TclObjLock itsLock;
+  int itsFlags;
+};
+
+static const char vcid_tclevalcmd_h[] = "$Header$";
+#endif // !TCLEVALCMD_H_DEFINED
