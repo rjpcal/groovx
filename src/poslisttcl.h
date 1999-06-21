4aaa08f60bd2378d4ac6da2d3e66b0b4967af99b
Robert J. Peters <rjpcal@gmail.com>
1999-06-21 18:45:26 +0000

    Removed PoslistTcl_SubcmdProc and getPosFromArg declarations since nobody was using them anymore.

--- src/poslisttcl.h
+++ src/poslisttcl.h
@@ -3,7 +3,7 @@
 // poslisttcl.h
 // Rob Peters
 // created: Sat Mar 13 12:43:01 1999
-// written: Sun Jun 20 18:05:56 1999
+// written: Mon Jun 21 11:42:07 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -11,28 +11,12 @@
 #ifndef POSLISTTCL_H_DEFINED
 #define POSLISTTCL_H_DEFINED
 
-///////////////////////////////////////////////////////////////////////
-//
-// PosList Tcl package declarations
-//
-///////////////////////////////////////////////////////////////////////
+namespace PoslistTcl {
+  const char* const bad_posid_msg = "posid out of range";
+}
 
 struct Tcl_Interp;
 typedef int (Tcl_PackageInitProc) (Tcl_Interp* interp);
-struct Tcl_Obj;
-class PosList;
-class Position;
-
-namespace PoslistTcl {
-  typedef int (PoslistTcl_SubcmdProc) 
-    (PosList* plist, Tcl_Interp* interp,
-     int objc, Tcl_Obj* const objv[]);
-
-  Position* getPosFromArg(Tcl_Interp* interp, Tcl_Obj* const objv[], 
-                          const PosList& plist, int argn);
-
-  const char* const bad_posid_msg = "posid out of range";
-}
 
 extern "C" Tcl_PackageInitProc Poslist_Init;
 
