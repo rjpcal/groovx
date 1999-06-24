54a11be913ec27dcda0a90b990c4e0f5b61f9540
Robert J. Peters <rjpcal@gmail.com>
1999-06-24 23:23:37 +0000

    Removed the entire class specialization of TAttribCmd for T=const string&, and instead use just a specialization of TAttribCmd::invoke.

--- src/tcl/tclpkg.cc
+++ src/tcl/tclpkg.cc
@@ -3,7 +3,7 @@
 // tclitempkg.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Jun 15 12:33:54 1999
-// written: Wed Jun 23 14:18:51 1999
+// written: Thu Jun 24 16:23:35 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -31,9 +31,9 @@ template <class T>
 class TGetterCmd : public TclCmd {
 public:
   TGetterCmd(TclItemPkg* pkg, const char* cmd_name, Getter<T>* getter,
-				 int item_argn) :
+				 const char* usage, int item_argn) :
 	 TclCmd(pkg->interp(), cmd_name, 
-			  item_argn ? "item_id" : NULL, 
+			  usage ? usage : (item_argn ? "item_id" : NULL), 
 			  item_argn+1, item_argn+1),
 	 itsPkg(pkg),
 	 itsGetter(getter), 
@@ -55,9 +55,9 @@ template <class T>
 class TSetterCmd : public TclCmd {
 public:
   TSetterCmd(TclItemPkg* pkg, const char* cmd_name, Setter<T>* setter,
-				 int item_argn) :
+				 const char* usage, int item_argn) :
 	 TclCmd(pkg->interp(), cmd_name, 
-			  item_argn ? "item_id new_value" : "new_value", 
+			  usage ? usage : (item_argn ? "item_id new_value" : "new_value"), 
 			  item_argn+2, item_argn+2),
 	 itsPkg(pkg),
 	 itsSetter(setter),
@@ -89,10 +89,10 @@ private:
 template <class T>
 class TAttribCmd : public TclCmd {
 public:
-  TAttribCmd(TclItemPkg* pkg, const char* cmd_name,
-					 Attrib<T>* attrib, int item_argn) :
+  TAttribCmd(TclItemPkg* pkg, const char* cmd_name, Attrib<T>* attrib,
+				 const char* usage, int item_argn) :
 	 TclCmd(pkg->interp(), cmd_name,
-			  item_argn ? "item_id ?new_value?" : "?new_value?",
+			  usage ? usage : (item_argn ? "item_id ?new_value?" : "?new_value?"),
 			  item_argn+1, item_argn+2),
 	 itsPkg(pkg),
 	 itsAttrib(attrib),
@@ -127,50 +127,71 @@ private:
   int itsObjcSet;
 };
 
+// // We need a specialization of TAttribCmd to handle C++ string's. This
+// // is because the Attrib itself should deal with 'const string&' for
+// // efficiency reasons, yet in the set() function we cannot obtain the
+// // value by passing a reference to getValFromArg() as for the other
+// // types, since we can't have a reference to a reference.
+// template <>
+// class TAttribCmd<const string&> : public TclCmd {
+// public:
+//   TAttribCmd(TclItemPkg* pkg, const char* cmd_name,
+// 				 Attrib<const string&>* attrib, const char* usage, int item_argn) :
+// 	 TclCmd(pkg->interp(), cmd_name,
+// 			  usage ? usage : (item_argn ? "item_id ?new_value?" : "?new_value?"),
+// 			  item_argn+1, item_argn+2),
+// 	 itsPkg(pkg),
+// 	 itsAttrib(attrib),
+// 	 itsItemArgn(item_argn),
+// 	 itsValArgn(item_argn+1), 
+// 	 itsObjcGet(item_argn+1), 
+// 	 itsObjcSet(item_argn+2) {}
+
+// protected:
+//   virtual void invoke() {
+// 	 int id = itsItemArgn ? getIntFromArg(itsItemArgn) : -1;
+// 	 void* item = itsPkg->getItemFromId(id);
+
+// 	 if (objc() == itsObjcGet) {
+// 		returnVal(itsAttrib->get(item));
+// 	 }
+// 	 else if (objc() == itsObjcSet) {
+// 		string val = getStringFromArg(itsValArgn);
+// 		itsAttrib->set(item, val);
+// 		returnVoid();
+// 	 }
+// 	 else { Assert(0); }
+//   }
+
+// private:
+//   TclItemPkg* itsPkg;
+//   auto_ptr< Attrib<const string&> > itsAttrib;
+//   int itsItemArgn;
+//   int itsValArgn;
+//   int itsObjcGet;
+//   int itsObjcSet;
+// };
+
 // We need a specialization of TAttribCmd to handle C++ string's. This
 // is because the Attrib itself should deal with 'const string&' for
 // efficiency reasons, yet in the set() function we cannot obtain the
 // value by passing a reference to getValFromArg() as for the other
 // types, since we can't have a reference to a reference.
 template <>
-class TAttribCmd<const string&> : public TclCmd {
-public:
-  TAttribCmd(TclItemPkg* pkg, const char* cmd_name,
-					 Attrib<const string&>* attrib, int item_argn) :
-	 TclCmd(pkg->interp(), cmd_name,
-			  item_argn ? "item_id ?new_value?" : "?new_value?",
-			  item_argn+1, item_argn+2),
-	 itsPkg(pkg),
-	 itsAttrib(attrib),
-	 itsItemArgn(item_argn),
-	 itsValArgn(item_argn+1), 
-	 itsObjcGet(item_argn+1), 
-	 itsObjcSet(item_argn+2) {}
-
-protected:
-  virtual void invoke() {
-	 int id = itsItemArgn ? getIntFromArg(itsItemArgn) : -1;
-	 void* item = itsPkg->getItemFromId(id);
-
-	 if (objc() == itsObjcGet) {
-		returnVal(itsAttrib->get(item));
-	 }
-	 else if (objc() == itsObjcSet) {
-		string val = getStringFromArg(itsValArgn);
-		itsAttrib->set(item, val);
-		returnVoid();
-	 }
-	 else { Assert(0); }
+void TAttribCmd<const string&>::invoke() {
+  int id = itsItemArgn ? getIntFromArg(itsItemArgn) : -1;
+  void* item = itsPkg->getItemFromId(id);
+  
+  if (objc() == itsObjcGet) {
+	 returnVal(itsAttrib->get(item));
   }
-
-private:
-  TclItemPkg* itsPkg;
-  auto_ptr< Attrib<const string&> > itsAttrib;
-  int itsItemArgn;
-  int itsValArgn;
-  int itsObjcGet;
-  int itsObjcSet;
-};
+  else if (objc() == itsObjcSet) {
+	 string val = getStringFromArg(itsValArgn);
+	 itsAttrib->set(item, val);
+	 returnVoid();
+  }
+  else { Assert(0); }
+}
 
 ///////////////////////////////////////////////////////////////////////
 //
@@ -181,9 +202,9 @@ private:
 class ActionCmd : public TclCmd {
 public:
   ActionCmd(TclItemPkg* pkg, const char* cmd_name, Action* action,
-				int item_argn) :
+				const char* usage, int item_argn) :
 	 TclCmd(pkg->interp(), cmd_name, 
-			  item_argn ? "item_id" : NULL, 
+			  usage ? usage : (item_argn ? "item_id" : NULL),
 			  item_argn+1, item_argn+1),
 	 itsPkg(pkg),
 	 itsAction(action),
@@ -209,123 +230,50 @@ private:
 ///////////////////////////////////////////////////////////////////////
 
 template <class T>
-void TclItemPkg::declareGetter_(TclItemPkg* pkg, 
-										  const char* cmd_name, Getter<T>* getter) {
+void TclItemPkg::declareGetter_(TclItemPkg* pkg, const char* cmd_name,
+										  Getter<T>* getter, const char* usage) {
   pkg->addCommand( new TGetterCmd<T>(pkg, pkg->makePkgCmdName(cmd_name), 
-												 getter, pkg->itsItemArgn) );
+												 getter, usage, pkg->itsItemArgn) );
 }
 
 template <class T>
-void TclItemPkg::declareSetter_(TclItemPkg* pkg,
-										  const char* cmd_name, Setter<T>* setter) {
+void TclItemPkg::declareSetter_(TclItemPkg* pkg, const char* cmd_name,
+										  Setter<T>* setter, const char* usage) {
   pkg->addCommand( new TSetterCmd<T>(pkg, pkg->makePkgCmdName(cmd_name),
-												 setter, pkg->itsItemArgn) );
+												 setter, usage, pkg->itsItemArgn) );
 }
 
 template <class T>
-void TclItemPkg::declareAttrib_(TclItemPkg* pkg,
-										  const char* attrib_name, Attrib<T>* attrib) {
+void TclItemPkg::declareAttrib_(TclItemPkg* pkg, const char* attrib_name,
+										  Attrib<T>* attrib, const char* usage) {
   pkg->addCommand( new TAttribCmd<T>(pkg, pkg->makePkgCmdName(attrib_name),
-												 attrib, pkg->itsItemArgn) );
+												 attrib, usage, pkg->itsItemArgn) );
 }
 
 void TclItemPkg::instantiate() {
-  Getter<int>* gi=0;             declareGetter_(this, 0, gi);
-  Getter<bool>* gb=0;            declareGetter_(this, 0, gb);
-  Getter<double>* gd=0;          declareGetter_(this, 0, gd);
-  Getter<const char*>* gcc=0;    declareGetter_(this, 0, gcc);
-  Getter<const string&>* gcs=0;  declareGetter_(this, 0, gcs);
+  Getter<int>* gi=0;             declareGetter_(this, 0, gi, 0);
+  Getter<bool>* gb=0;            declareGetter_(this, 0, gb, 0);
+  Getter<double>* gd=0;          declareGetter_(this, 0, gd, 0);
+  Getter<const char*>* gcc=0;    declareGetter_(this, 0, gcc, 0);
+  Getter<const string&>* gcs=0;  declareGetter_(this, 0, gcs, 0);
 
-  Setter<int>* si=0;             declareSetter_(this, 0, si);
-  Setter<bool>* sb=0;            declareSetter_(this, 0, sb);
-  Setter<double>* sd=0;          declareSetter_(this, 0, sd);
-  Setter<const char*>* scc=0;    declareSetter_(this, 0, scc);
-//   Setter<const string&>* scs=0;  declareSetter_(this, 0, scs);
+  Setter<int>* si=0;             declareSetter_(this, 0, si, 0);
+  Setter<bool>* sb=0;            declareSetter_(this, 0, sb, 0);
+  Setter<double>* sd=0;          declareSetter_(this, 0, sd, 0);
+  Setter<const char*>* scc=0;    declareSetter_(this, 0, scc, 0);
+//   Setter<const string&>* scs=0;  declareSetter_(this, 0, scs, 0);
 
-  Attrib<int>* ai=0;             declareAttrib_(this, 0, ai);
-  Attrib<bool>* ab=0;            declareAttrib_(this, 0, ab);
-  Attrib<double>* ad=0;          declareAttrib_(this, 0, ad);
-  Attrib<const char*>* acc=0;    declareAttrib_(this, 0, acc);
-  Attrib<const string&>* acs=0;  declareAttrib_(this, 0, acs);
+  Attrib<int>* ai=0;             declareAttrib_(this, 0, ai, 0);
+  Attrib<bool>* ab=0;            declareAttrib_(this, 0, ab, 0);
+  Attrib<double>* ad=0;          declareAttrib_(this, 0, ad, 0);
+  Attrib<const char*>* acc=0;    declareAttrib_(this, 0, acc, 0);
+  Attrib<const string&>* acs=0;  declareAttrib_(this, 0, acs, 0);
 }
 
-// void TclItemPkg::declareGetter(const char* cmd_name, Getter<int>* getter) {
-//   addCommand( new TGetterCmd<int>(this, makePkgCmdName(cmd_name), 
-// 											 getter, itsItemArgn) );
-// }
-// void TclItemPkg::declareGetter(const char* cmd_name, Getter<bool>* getter) {
-//   addCommand( new TGetterCmd<bool>(this, makePkgCmdName(cmd_name),
-// 											  getter, itsItemArgn) );
-// }
-// void TclItemPkg::declareGetter(const char* cmd_name, Getter<double>* getter) {
-//   addCommand( new TGetterCmd<double>(this, makePkgCmdName(cmd_name),
-// 												 getter, itsItemArgn) );
-// }
-// void TclItemPkg::declareGetter(const char* cmd_name, 
-// 										 Getter<const char*>* getter) {
-//   addCommand( new TGetterCmd<const char*>(this, makePkgCmdName(cmd_name),
-// 														getter, itsItemArgn) );
-// }
-// void TclItemPkg::declareGetter(const char* cmd_name, 
-// 										 Getter<const string&>* getter) {
-//   addCommand( new TGetterCmd<const string&>(this, makePkgCmdName(cmd_name),
-// 														  getter, itsItemArgn) );
-// }
-
-// void TclItemPkg::declareSetter(const char* cmd_name, Setter<int>* setter) {
-//   addCommand( new TSetterCmd<int>(this, makePkgCmdName(cmd_name),
-// 											 setter, itsItemArgn) );
-// }
-// void TclItemPkg::declareSetter(const char* cmd_name, Setter<bool>* setter) {
-//   addCommand( new TSetterCmd<bool>(this, makePkgCmdName(cmd_name),
-// 											  setter, itsItemArgn) );
-// }
-// void TclItemPkg::declareSetter(const char* cmd_name, Setter<double>* setter) {
-//   addCommand( new TSetterCmd<double>(this, makePkgCmdName(cmd_name),
-// 												 setter, itsItemArgn) );
-// }
-// void TclItemPkg::declareSetter(const char* cmd_name, 
-// 										 Setter<const char*>* setter) {
-//   addCommand( new TSetterCmd<const char*>(this, makePkgCmdName(cmd_name), 
-// 														setter, itsItemArgn) );
-// }
-// // void TclItemPkg::declareAttrib(const char* attrib_name, 
-// // 										 Attrib<const string&>* attrib) {
-// //   addCommand( new TAttribCmd<const string&>(
-// // 								  this, makePkgCmdName(attrib_name), attrib) );
-// // }
-
-// void TclItemPkg::declareAttrib(const char* attrib_name, Attrib<int>* attrib) {
-//   addCommand( new TAttribCmd<int>(this, makePkgCmdName(attrib_name),
-// 												 attrib, itsItemArgn) );
-// }
-
-// void TclItemPkg::declareAttrib(const char* attrib_name, Attrib<bool>* attrib) {
-//   addCommand ( new TAttribCmd<bool>(this, makePkgCmdName(attrib_name),
-// 													attrib, itsItemArgn) );
-// }
-
-// void TclItemPkg::declareAttrib(const char* attrib_name, 
-// 										 Attrib<double>* attrib) {
-//   addCommand( new TAttribCmd<double>(this, makePkgCmdName(attrib_name),
-// 													 attrib, itsItemArgn) );
-// }
-
-// void TclItemPkg::declareAttrib(const char* attrib_name, 
-// 										 Attrib<const char*>* attrib) {
-//   addCommand( new TAttribCmd<const char*>(this, makePkgCmdName(attrib_name),
-// 															attrib, itsItemArgn) );
-// }
-
-// void TclItemPkg::declareAttrib(const char* attrib_name, 
-// 										 Attrib<const string&>* attrib) {
-//   addCommand( new TAttribCmd<const string&>(this, makePkgCmdName(attrib_name),
-// 														  attrib, itsItemArgn) );
-// }
-
-void TclItemPkg::declareAction(const char* action_name, Action* action) {
+void TclItemPkg::declareAction(const char* action_name, Action* action,
+										 const char* usage) {
   addCommand( new ActionCmd(this, makePkgCmdName(action_name),
-									 action, itsItemArgn) );
+									 action, usage, itsItemArgn) );
 }
 
 
