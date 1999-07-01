601e8f189f204e85aba33a28c9e8f6c9952e0655
Robert J. Peters <rjpcal@gmail.com>
1999-07-01 19:43:10 +0000

    Initial revision

---
+++ src/visx/gtext.h
@@ -0,0 +1,57 @@
+///////////////////////////////////////////////////////////////////////
+//
+// gtext.h
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Thu Jul  1 11:54:47 1999
+// written: Thu Jul  1 12:26:30 1999
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef GTEXT_H_DEFINED
+#define GTEXT_H_DEFINED
+
+#ifndef GROBJ_H_DEFINED
+#include "grobj.h"
+#endif
+
+#ifndef ERROR_H_DEFINED
+#include "error.h"
+#endif
+
+#ifndef STRING_DEFINED
+#include <string>
+#endif
+
+class GtextError : public ErrorWithMsg {
+public:
+  GtextError(const string& str = "") : ErrorWithMsg(str) {}
+}
+
+class Gtext : public GrObj {
+public:
+  Gtext(const char* text=0);
+  Gtext(istream& is, IOFlag flag);
+  virtual ~Gtext();
+
+  virtual void serialize(ostream &os, IOFlag flag) const;
+  virtual void deserialize(istream &is, IOFlag flag);
+  virtual int charCount() const;
+
+  virtual void setCategory(int) {}
+  virtual int getCategory() const { return -1; }
+
+  void setText(const char* text);
+  const char* getText() const;
+
+  static void loadFont(const char* fontname);
+
+protected:
+  virtual void grRecompile() const;
+
+private:
+  string itsText;
+};
+
+static const char vcid_gtext_h[] = "$Header$";
+#endif // !GTEXT_H_DEFINED
