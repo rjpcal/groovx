f65b4d630a0db966cecd4b8fe5b8b1bc6986109b
Robert J. Peters <rjpcal@gmail.com>
1999-07-02 00:47:45 +0000

    Added T* dummy parameter to getSequenceFromArg in order to help the poor old compiler out with template type deduction when necessary.

--- src/tcl/tclcmd.h
+++ src/tcl/tclcmd.h
@@ -3,7 +3,7 @@
 // tclcmd.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Fri Jun 11 14:50:43 1999
-// written: Mon Jun 14 12:19:34 1999
+// written: Thu Jul  1 17:46:00 1999
 // $Id$
 //
 // This file defines the abstract class TclCmd, which provides
@@ -12,9 +12,9 @@
 // and for common behavior to be placed in base classes. The
 // TclCmd class itself takes care of such things as checking
 // the argument count, and provides utility functions such as
-// getIntFromArg() and getStringFromArg() for getting basic types from
+// getIntFromArg() and getCstringFromArg() for getting basic types from
 // a command argument, as well as functions such as returnInt() and
-// returnString() for returning basic types to Tcl throught the
+// returnCstring() for returning basic types to Tcl throught the
 // Tcl_ObjResult.
 //
 ///////////////////////////////////////////////////////////////////////
@@ -32,23 +32,17 @@
 #define STRING_DEFINED
 #endif
 
-class TclError {
-public:
-  TclError(const string& msg="") : itsMsg(msg) {}
-  virtual ~TclError();
-  const string& msg() { return itsMsg; }
-private:
-  const string itsMsg;
-};
+#ifndef TCLERROR_H_DEFINED
+#include "tclerror.h"
+#endif
 
 class TclCmd {
 public:
   // If exact_objc is true, then the objc of a command invocation is
   // required to exactly equal either objc_min or objc_max; if it is
   // false, then objc must be between objc_min and objc_max, inclusive.
-  TclCmd(Tcl_Interp* interp, const char* cmd_name, 
-					 const char* usage, int objc_min=0, int objc_max=100000,
-					 bool exact_objc=false);
+  TclCmd(Tcl_Interp* interp, const char* cmd_name, const char* usage, 
+			int objc_min=0, int objc_max=100000, bool exact_objc=false);
   virtual ~TclCmd();
 
   static int dummyInvoke(ClientData clientData, Tcl_Interp* interp,
@@ -66,21 +60,96 @@ protected:
   int    getIntFromArg(int argn);
   bool   getBoolFromArg(int argn);
   double getDoubleFromArg(int argn);
-  char*  getStringFromArg(int argn);
+  const char*  getCstringFromArg(int argn);
+  string getStringFromArg(int argn) { return string(getCstringFromArg(argn)); }
+
+  template <class T>
+  void getValFromArg(int argn, T& val)
+	 { getValFromObj(itsObjv[argn], val); }
+
+  template <class T, class Iterator>
+  void getSequenceFromArg(int argn, Iterator itr, T* dummy) {
+	 Tcl_Obj** elements;
+	 int count;
+	 if ( Tcl_ListObjGetElements(itsInterp, itsObjv[argn], &count, &elements)
+			!= TCL_OK) {
+		throw TclError();
+	 }
+	 for (int i = 0; i < count; ++i) {
+		T val;
+		getValFromObj(elements[i], val);
+		*itr = val;
+	 }
+  }
 
   void returnVoid();
   void returnError();
   void returnInt(int val);
   void returnBool(bool val);
   void returnDouble(double val);
-  void returnString(const char* val);
+  void returnCstring(const char* val);
+  void returnString(const string& val);
+
+  void returnVal(int val) { returnInt(val); }
+  void returnVal(bool val) { returnBool(val); }
+  void returnVal(double val) { returnDouble(val); }
+  void returnVal(const char* val) { returnCstring(val); }
+  void returnVal(const string& val) { returnString(val); }
+
+  template <class Itr>
+  void returnSequence(Itr begin, Itr end) {
+	 while (begin != end) {
+		lappendVal(*begin);
+		++begin;
+	 }
+  }
+
+  void lappendVal(int val);
+  void lappendVal(bool val);
+  void lappendVal(double val);
+  void lappendVal(const char* val);
+  void lappendVal(const string& val);
 
 private:
+  template <class T>
+  void getValFromObj(Tcl_Obj* obj, T* val);
+
+  template <>
+  void getValFromObj<int>(Tcl_Obj* obj, int& val) {
+	 if ( Tcl_GetIntFromObj(itsInterp, obj, &val) != TCL_OK ) throw TclError();
+  }
+
+  template <>
+  void getValFromObj<bool>(Tcl_Obj* obj, bool& val) {
+	 int int_val;
+	 if ( Tcl_GetBooleanFromObj(itsInterp, obj, &int_val) != TCL_OK )
+		throw TclError();
+	 val = bool(int_val);
+  }
+
+  template <>
+  void getValFromObj<double>(Tcl_Obj* obj, double& val) {
+	 if ( Tcl_GetDoubleFromObj(itsInterp, obj, &val) != TCL_OK )
+		throw TclError();
+  }
+
+  template <>
+  void getValFromObj<int>(Tcl_Obj* obj, const char*& val) {
+	 val = Tcl_GetString(obj);
+  }
+
+  template <>
+  void getValFromObj<int>(Tcl_Obj* obj, string& val) {
+	 val = Tcl_GetString(obj);
+  }
+
+  // These are set once per command object
   const char* const itsUsage;
   const int itsObjcMin;
   const int itsObjcMax;
   const bool itsExactObjc;
 
+  // These are set once per invocation of the command object
   Tcl_Interp* itsInterp;
   int itsObjc;
   Tcl_Obj* const* itsObjv;
