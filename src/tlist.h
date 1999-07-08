93a448dc86ae1118cc4b3eb02649eba132f2c78f
Robert J. Peters <rjpcal@gmail.com>
1999-07-08 17:41:40 +0000

    Last check-in before getTrial() and getTrialConst() will be removed (since their functionality is already provided by base class functions PtrList<Trial>::getPtr etc.).

--- src/tlist.h
+++ src/tlist.h
@@ -3,7 +3,7 @@
 // tlist.h
 // Rob Peters
 // created: Fri Mar 12 13:23:02 1999
-// written: Wed Jun  9 15:08:09 1999
+// written: Sat Jul  3 14:44:31 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -11,9 +11,8 @@
 #ifndef TLIST_H_DEFINED
 #define TLIST_H_DEFINED
 
-#ifndef VECTOR_DEFINED
-#include <vector>
-#define VECTOR_DEFINED
+#ifndef PTRLIST_H_DEFINED
+#include "ptrlist.h"
 #endif
 
 #ifndef IO_H_DEFINED
@@ -30,9 +29,8 @@ class PosId;
 //
 ///////////////////////////////////////////////////////////////////////
 
-class Tlist : public virtual IO {
+class Tlist : public PtrList<Trial>, public virtual IO {
 private:
-  vector<Trial *> itsTrials;
   int itsCurTrial;
   bool itsVisibility;
 
@@ -59,51 +57,58 @@ public:
 
   virtual int charCount() const;
 
-  int readFromObjidsOnly(istream &is, int num_trials, int offset = 0);
   // Reads a list of 'simple' trial descriptions which contain objid's
   // only; posid's are inferred from position int the list. If
   // num_trials is passed as < 0, the function will read to the end of
   // the istream. If offset is non-zero, it will be added to each
   // incoming objid before it is inserted into the Trial. Returns the
   // number of trials that were loaded.
+  int readFromObjidsOnly(istream &is, int num_trials, int offset = 0);
 
   ///////////////
   // accessors //
   ///////////////
 
-  int trialCount() const;
-  // Return the current number of valid trials
-
-  bool isValidTrial(int trial) const;
-
-  Trial& getTrial(int trial);
   // Precondition: trial > 0
   // This function will grow the Tlist if trial is beyond the current capacity,
   // and it will create a new Trial if the Trial* at trial is currently NULL.
+  Trial& getTrial(int trial);
 
-  Trial& getTrial(int trial) const;
   // Precondition: trial is a valid trialid 
   //
   // This accessor for const Tlist's will not create a new Trial or
   // expand the Tlist
-
-  void getValidTrials(vector<int>& vec) const;
-  // Puts a list of all valid (i.e. within-range and non-null) trial
-  // ids into the vector<int> that is passed in by reference
+  Trial& getTrialConst(int trial) const;
 
   //////////////////
   // manipulators //
   //////////////////
 
+  // Change the global visibility of a Tlist. This determines whether
+  // anything will be displayed by a "redraw" command, or by remap
+  // events sent to the screen window.
   void setVisible(bool vis);
+
   void setCurTrial(int trial);
   void addToTrial(int trial, ObjId objid, PosId posid);
-  void clearTrial(int trial);
-  void clearAllTrials();
+  void clear();
 
   /////////////
   // actions //
   /////////////
+
+  // Redraws the Tlist's current trial to the OpenGL window. If the
+  // Tlist's visibility is false, nothing will be displayed.
+  void redraw() const;
+
+  // Erase the Tlist's current trial from the OpenGL window. The
+  // Tlist's visibility is set to false, so redraw events sent to the
+  // screen window will not cause the Tlist to draw anything.
+  void undraw();
+
+  // This command sets the Tlist's visiblity to zero and clears the
+  // OpenGL window.
+  void clearscreen();
 
   void drawTrial(int trial);
   void undrawTrial(int trial);
