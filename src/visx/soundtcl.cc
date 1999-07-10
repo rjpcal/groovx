e2a09c78d9bb1b4364d9f98ea4cb970e25646861
Robert J. Peters <rjpcal@gmail.com>
1999-07-10 02:20:20 +0000

    There are now two implementations of SoundTcl in the file; the new one is a more standard ListPkg and ListItemPkg implementation of sound functionality. After this check-in, the old version will be removed.

--- src/visx/soundtcl.cc
+++ src/visx/soundtcl.cc
@@ -2,7 +2,7 @@
 // soundtcl.cc
 // Rob Peters
 // created: Tue Apr 13 14:09:59 1999
-// written: Tue Apr 27 13:29:29 1999
+// written: Fri Jul  9 19:12:54 1999
 // $Id$
 ///////////////////////////////////////////////////////////////////////
 
@@ -18,56 +18,57 @@
 #include <fstream.h>				  // to check if files exist
 
 #include "errmsg.h"
+#include "soundlist.h"
+#include "sound.h"
+#include "listpkg.h"
+#include "listitempkg.h"
+#include "tcllink.h"
 
-#define LOCAL_PROF
+#define NO_TRACE
 #include "trace.h"
 #define LOCAL_ASSERT
 #include "debug.h"
 
 ///////////////////////////////////////////////////////////////////////
-// Sound class declaration
+// HpAudioSound class declaration
 ///////////////////////////////////////////////////////////////////////
 
-namespace {
-  class SoundError {
-  public:
-	 SoundError() : itsInfo() {}
-	 SoundError(const string& filename) { setInfo(filename); }
-	 const string& info() { return itsInfo; }
-  protected:
-	 void setInfo(const string& filename) {
-		itsInfo = string("SoundError: bad or nonexistent file '") + filename + "'";
-	 }
-  private:
-	 string itsInfo;
-  };
+class SoundFilenameError : public SoundError {
+public:
+  SoundFilenameError() : SoundError() {}
+  SoundFilenameError(const string& filename) :
+	 SoundError(string("bad or nonexistent file '") + filename + "'") {}
+};
 
-  class Sound {
-  public:
-	 Sound(Audio* audio, const string& filename);
-	 virtual ~Sound();
-	 void play();
-	 void setFile(const string& filename);
-	 const string& getFile() const { return itsFilename; }
-  private:
-	 Audio* itsAudio;
-	 string itsFilename;
-	 SBucket* itsSBucket;
-	 SBPlayParams itsPlayParams;
-  };
-}
+class HpAudioSound : public Sound {
+public:
+  HpAudioSound(Audio* audio, const string& filename);
+  virtual ~HpAudioSound();
+
+  virtual void serialize(ostream& os, IOFlag flag) const {}
+  virtual void deserialize(istream& is, IOFlag flag) {}
+  virtual int charCount() const { return 0; }
+  
+  virtual void play();
+  virtual void setFile(const string& filename);
+  virtual const string& getFile() const { return itsFilename; }
+
+private:
+  Audio* itsAudio;
+  string itsFilename;
+  SBucket* itsSBucket;
+  SBPlayParams itsPlayParams;
+};
 
 ///////////////////////////////////////////////////////////////////////
-// Sound member definitions
+// HpAudioSound member definitions
 ///////////////////////////////////////////////////////////////////////
 
-Sound::Sound(Audio* audio, const string& filename) :
+HpAudioSound::HpAudioSound(Audio* audio, const string& filename) :
   itsAudio(audio), itsSBucket(NULL)
 {
-DOTRACE("Sound::Sound");
-  Assert(itsAudio);
-
-  setFile(filename);
+DOTRACE("HpAudioSound::HpAudioSound");
+  if ( !itsAudio ) { throw SoundError("invalid audio server connection"); }
 
   itsPlayParams.pause_first = 0; 
   itsPlayParams.start_offset.type = ATTSamples; 
@@ -79,26 +80,29 @@ DOTRACE("Sound::Sound");
   itsPlayParams.play_volume = AUnityGain;
   itsPlayParams.duration.type = ATTFullLength;
   itsPlayParams.event_mask = 0;
+
+  setFile(filename);
 }
 
-Sound::~Sound() {
-DOTRACE("Sound::~Sound");
+HpAudioSound::~HpAudioSound() {
+DOTRACE("HpAudioSound::~HpAudioSound");
   ADestroySBucket( itsAudio, itsSBucket, NULL );
 }
 
-void Sound::play() {
-DOTRACE("Sound::play");
-  Assert(itsAudio);
+void HpAudioSound::play() {
+DOTRACE("HpAudioSound::play");
+  if ( !itsAudio ) { throw SoundError("invalid audio server connection"); }
+
   ATransID xid = APlaySBucket( itsAudio, itsSBucket, &itsPlayParams, NULL );
 }
 	
-void Sound::setFile(const string& filename) {
-DOTRACE("Sound::setFile");
-  Assert(itsAudio);
+void HpAudioSound::setFile(const string& filename) {
+DOTRACE("HpAudioSound::setFile");
+  if ( !itsAudio ) { throw SoundError("invalid audio server connection"); }
 
   ifstream ifs(filename.c_str());
   if (ifs.fail()) {
-	 throw SoundError(filename);
+	 throw SoundFilenameError(filename);
   }
   ifs.close();
   itsFilename = filename;
@@ -121,15 +125,14 @@ DOTRACE("Sound::setFile");
 ///////////////////////////////////////////////////////////////////////
 
 namespace {
-  map< string, Sound* > theSoundMap;
+  map< string, HpAudioSound* > theSoundMap;
 
   string ok_sound_file = "/cit/rjpeters/face/audio/saw50_500Hz_300ms.au";
   string err_sound_file = "/cit/rjpeters/face/audio/saw50_350Hz_300ms.au";
 
-  Audio* audio;
+  Audio* theAudio;
 
   // Error messages
-  const char* const bad_filename_msg = "sound file does not exist";
   const char* const bad_sound_msg = "sound does not exist";
   const char* const no_audio_msg = "audio is not enabled";
 }
@@ -138,121 +141,107 @@ namespace {
 // Sound Tcl package definitions
 ///////////////////////////////////////////////////////////////////////
 
-int SoundTcl::setCmd(ClientData, Tcl_Interp *interp,
-							int objc, Tcl_Obj *const objv[]) {
+int SoundTcl::setCmd(ClientData, Tcl_Interp* interp,
+							int objc, Tcl_Obj* const objv[]) {
 DOTRACE("SoundTcl::setCmd");
-  if ( !audio ) {
+  if ( !theAudio ) {
 	 err_message(interp, objv, no_audio_msg);
 	 return TCL_OK;
   }
 
   if (objc < 2 || objc > 3) {
-	 Tcl_WrongNumArgs(interp, 1, objv, "sound_name [new_filename]");
+	 Tcl_WrongNumArgs(interp, 1, objv, "sound_name ?new_filename?");
 	 return TCL_ERROR;
   }
   
-  const char* sound = Tcl_GetString(objv[1]);
+  string snd_name = Tcl_GetString(objv[1]);
 
+  // Retrieve current filename
   if (objc == 2) {
-	 if (!theSoundMap[sound]) {
+	 if (!theSoundMap[snd_name]) {
 		err_message(interp, objv, bad_sound_msg);
 		return TCL_ERROR;
 	 }
 	 Tcl_SetObjResult(interp,
-		    Tcl_NewStringObj(theSoundMap[sound]->getFile().c_str(), -1));
+		    Tcl_NewStringObj(theSoundMap[snd_name]->getFile().c_str(), -1));
   }
+
+  // Set new filename
   if (objc == 3) {
 	 try {
-		if (!theSoundMap[sound]) {
-		  theSoundMap[sound] = new Sound(audio, Tcl_GetString(objv[2]));
+		if (!theSoundMap[snd_name]) {
+		  theSoundMap[snd_name] = new HpAudioSound(theAudio, Tcl_GetString(objv[2]));
 		}	 
 		else {
-		  theSoundMap[sound]->setFile(Tcl_GetString(objv[2]));
+		  theSoundMap[snd_name]->setFile(Tcl_GetString(objv[2]));
 		}
 	 }
 	 catch (SoundError& err) {
- 		err_message(interp, objv, err.info().c_str());
+ 		err_message(interp, objv, err.msg().c_str());
 		return TCL_ERROR;
 	 }
   }
   return TCL_OK;
 }
 
-int SoundTcl::playCmd(ClientData, Tcl_Interp *interp,
-							 int objc, Tcl_Obj *const objv[]) {
+int SoundTcl::playCmd(ClientData, Tcl_Interp* interp,
+							 int objc, Tcl_Obj* const objv[]) {
 DOTRACE("SoundTcl::playCmd");
-  if ( !audio ) {
-	 err_message(interp, objv, no_audio_msg);
-	 return TCL_OK;
-  }
-
   if (objc != 2) {
 	 Tcl_WrongNumArgs(interp, 1, objv, "sound_name");
 	 return TCL_ERROR;
   }
   
-  const char* snd_name = Tcl_GetString(objv[1]);
+  return playProc(interp, objv, Tcl_GetString(objv[1]));
+}
 
-  if (!theSoundMap[snd_name]) {
-	 err_message(interp, objv, bad_sound_msg);
+int SoundTcl::playProc(Tcl_Interp* interp, Tcl_Obj* const objv[],
+							  const char* snd_name) {
+DOTRACE("SoundTcl::playProc");
+  const char* err_info = 
+	 (objv != NULL) ? Tcl_GetString(objv[0]) : "SoundTcl::playProc";
+
+  if ( !theAudio ) {
+	 err_message(interp, err_info, no_audio_msg);
+	 return TCL_OK;
+  }
+
+  HpAudioSound* theSound = theSoundMap[snd_name];
+
+  if (!theSound) {
+	 if (objv != NULL)
+	 err_message(interp, err_info, bad_sound_msg);
 	 return TCL_ERROR;
   }
 
-  theSoundMap[snd_name]->play();
+  theSound->play();
   return TCL_OK;
 }
 
-int SoundTcl::haveAudioCmd(ClientData, Tcl_Interp *interp,
-									int objc, Tcl_Obj *const objv[]) {
-DOTRACE("SoundTcl::haveAudioCmd");
-  if ( !audio ) {
-	 Tcl_SetObjResult(interp, Tcl_NewIntObj(0));
-  }
-  else {
-	 Tcl_SetObjResult(interp, Tcl_NewIntObj(1));
-  }
-  return TCL_OK;
-}
-
-SoundTcl::SoundTcl(Tcl_Interp *interp) : itsStatus(TCL_OK) {
+SoundTcl::SoundTcl(Tcl_Interp* interp) : itsStatus(TCL_OK) {
 DOTRACE("SoundTcl::SoundTcl");
-  // Open an audio connection to the default audio server, then check
-  // to make sure connection succeeded. If the connection fails,
-  // 'audio' is set to NULL. In this case, all procedures in the
-  // Sound:: namespace will have no effect but will return without
-  // error.
-  const char* ServerName = "";
-  long status = 0;
-  audio = AOpenAudio( const_cast<char *>(ServerName), &status );
-  if ( status != 0 ) {
-	 audio = NULL;
-  }
-
   // Add commands to ::Sound namespace.
   Tcl_CreateObjCommand(interp, "Sound::set", setCmd,
                        (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
   Tcl_CreateObjCommand(interp, "Sound::play", playCmd,
                        (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
-  Tcl_CreateObjCommand(interp, "Sound::haveAudio", haveAudioCmd,
-                       (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
 
   // If we had a successfull audio connection, set up some initial
   // sounds.
-  if ( audio ) {
-	 ASetCloseDownMode( audio, AKeepTransactions, NULL );
-#ifdef LOCAL_DEBUG
-	 DUMP_VAL2(AAudioString(audio));
-#endif
+  if ( theAudio ) {
+	 ASetCloseDownMode( theAudio, AKeepTransactions, NULL );
+
+	 DebugEvalNL(AAudioString(theAudio));
 
 	 // Try to initialize the default 'ok' and 'err' sounds.
 	 // These will fail if the default sound files are missing.
 	 try {
-		theSoundMap["ok"] = new Sound(audio, ok_sound_file);
-		theSoundMap["err"] = new Sound(audio, err_sound_file);
+		theSoundMap["ok"] = new HpAudioSound(theAudio, ok_sound_file);
+		theSoundMap["err"] = new HpAudioSound(theAudio, err_sound_file);
 	 }
 	 catch (SoundError& err) {
 		Tcl_AppendStringsToObj(Tcl_GetObjResult(interp),
-									  "Sound_Init: ", err.info().c_str(), NULL);
+									  "Sound_Init: ", err.msg().c_str(), NULL);
 		itsStatus = TCL_ERROR;
 	 }
   }
@@ -263,9 +252,130 @@ DOTRACE("SoundTcl::SoundTcl");
 
 SoundTcl::~SoundTcl() {
 DOTRACE("SoundTcl::~SoundTcl");
-  if ( audio ) {
-	 ACloseAudio( audio, NULL );
+  if ( theAudio ) {
+	 ACloseAudio( theAudio, NULL );
+	 theAudio = 0;
   }
+}
+
+//---------------------------------------------------------------------
+//
+// SoundPkg --
+//
+//---------------------------------------------------------------------
+
+namespace SoundTcl_ {
+  class SoundCmd;
+  class HaveAudioCmd;
+  class SoundPkg;
+}
+
+class SoundTcl_::SoundCmd : public TclCmd {
+public:
+  SoundCmd(Tcl_Interp* interp, const char* cmd_name) :
+	 TclCmd(interp, cmd_name, "filename", 2, 2) {}
+protected:
+  virtual void invoke() {
+	 const char* filename = getCstringFromArg(1);
+
+	 Sound* p = new HpAudioSound(theAudio, filename);
+	 int id = SoundList::theSoundList().insert(p);
+	 returnInt(id);
+  }
+};
+
+class SoundTcl_::HaveAudioCmd : public TclCmd {
+public:
+  HaveAudioCmd(Tcl_Interp* interp, const char* cmd_name) :
+	 TclCmd(interp, cmd_name, NULL, 1, 1) {}
+protected:
+  virtual void invoke() { returnBool( theAudio != 0 ); }
+};
+
+class SoundTcl_::SoundPkg :
+  public AbstractListItemPkg<HpAudioSound, SoundList> {
+public:
+  SoundPkg(Tcl_Interp* interp) :
+	 AbstractListItemPkg<HpAudioSound, SoundList>(
+			 interp, SoundList::theSoundList(), "Sound", "1.3")
+  {
+	 // Open an audio connection to the default audio server, then
+	 // check to make sure connection succeeded. If the connection
+	 // fails, 'audio' is set to NULL. In this case, all procedures in
+	 // the HpAudioSound:: namespace will have no effect but will
+	 // return without error.
+	 const char* ServerName = "";
+	 long status = 0;
+	 theAudio = AOpenAudio( const_cast<char *>(ServerName), &status );
+	 if ( status != 0 ) {
+		theAudio = NULL;
+	 }
+
+	 if ( theAudio ) {
+		ASetCloseDownMode( theAudio, AKeepTransactions, NULL );
+
+		try {
+		  static int OK = 0;
+		  theList().insertAt(OK, new HpAudioSound(theAudio, ok_sound_file));
+		  Tcl_LinkInt(interp, "Sound::ok", &OK, TCL_LINK_READ_ONLY);
+
+		  static int ERR = 1;
+		  theList().insertAt(ERR, new HpAudioSound(theAudio, err_sound_file));
+		  Tcl_LinkInt(interp, "Sound::err", &ERR, TCL_LINK_READ_ONLY);
+		}
+		catch (SoundError& err) {
+		  Tcl_AppendStringsToObj(Tcl_GetObjResult(interp),
+										 "SoundPkg: ", err.msg().c_str(), NULL);
+		}
+
+	 }
+	 addCommand( new SoundCmd(interp, "Sound::Sound") );
+	 addCommand( new HaveAudioCmd(interp, "Sound::haveAudio") );
+	 declareAction("play", new CAction<Sound>(&Sound::play));
+	 declareAttrib("file", new CAttrib<Sound, const string&>(&Sound::getFile,
+																				&Sound::setFile));
+
+  }
+
+  ~SoundPkg() {
+	 if ( theAudio ) {
+		ACloseAudio( theAudio, NULL );
+		theAudio = 0;
+	 }
+  }
+};
+
+//---------------------------------------------------------------------
+//
+// SoundListPkg --
+//
+//---------------------------------------------------------------------
+
+namespace SoundListTcl {
+  class SoundListPkg;
+}
+
+class SoundListTcl::SoundListPkg : public ListPkg<SoundList> {
+public:
+  SoundListPkg(Tcl_Interp* interp) :
+	 ListPkg<SoundList>(interp, SoundList::theSoundList(),
+							  "SoundList", "1.3") {}
+};
+
+//---------------------------------------------------------------------
+//
+// Sound_Init --
+//
+//---------------------------------------------------------------------
+
+int Sound_Init(Tcl_Interp* interp) {
+DOTRACE("Sound_Init");
+  static SoundTcl aSoundTcl(interp);
+
+  new SoundTcl_::SoundPkg(interp);
+  new SoundListTcl::SoundListPkg(interp);
+
+  return TCL_OK;
 }
 
 static const char vcid_soundtcl_cc[] = "$Header$";
