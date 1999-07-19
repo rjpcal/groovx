48a6630ccb9dccc851a0301966d72832727e98db
Robert J. Peters <rjpcal@gmail.com>
1999-07-19 23:47:42 +0000

    Added class TclObjPtr, which like TclObjLock provides a lock on a Tcl_Obj by incrementing the ref count in the constructor, decrementing it in the destructor, but which also can be converted to a Tcl_Obj*, therefore acting as a just-above-dumb pointer.

--- src/tcl/tclobjptr.h
+++ src/tcl/tclobjptr.h
@@ -2,12 +2,14 @@
 // tclobjlock.h
 // Rob Peters
 // created: Tue May 11 13:44:19 1999
-// written: Tue May 11 13:44:38 1999
+// written: Mon Jul 19 16:46:24 1999
 // $Id$
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef TCLOBJLOCK_H_DEFINED
 #define TCLOBJLOCK_H_DEFINED
+
+struct Tcl_Obj;
 
 namespace {
 
@@ -22,6 +24,18 @@ namespace {
     ~TclObjLock() { Tcl_DecrRefCount(itsObj); }
   private:
     Tcl_Obj* itsObj;
+  };
+
+  class TclObjPtr {
+  public:
+	 TclObjPtr(Tcl_Obj* obj) : itsObj(obj) { Tcl_IncrRefCount(itsObj); }
+	 ~TclObjPtr() { Tcl_DecrRefCount(itsObj); }
+	 
+	 typedef Tcl_Obj* Tcl_ObjPtr;
+	 operator Tcl_ObjPtr() { return itsObj; }
+	 
+  private:
+	 Tcl_Obj* itsObj;
   };
 
 }
