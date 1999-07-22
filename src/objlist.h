fda4157e1f26fc2ee769d4ae30052c3f62ed5379
Robert J. Peters <rjpcal@gmail.com>
1999-07-22 19:55:48 +0000

    Removed unnecessary functions getObj, addObj, addObjAt... these all duplicated functionality already provided by PtrList.

--- src/objlist.h
+++ src/objlist.h
@@ -3,7 +3,7 @@
 // objlist.h 
 // Rob Peters
 // Created: Nov-98
-// written: Wed Jun 30 13:55:10 1999
+// written: Thu Jul 22 12:55:03 1999
 // $Id$
 //
 //
@@ -37,10 +37,6 @@ class ObjList : public PtrList<GrObj>, public virtual IO {
 private:
   typedef PtrList<GrObj> Base;
 
-  //////////////
-  // creators //
-  //////////////
-
 protected:
   // Construct a list of 'size' GrObj*'s, initialized to NULL
   ObjList (int size) : Base(size) {}
@@ -48,37 +44,6 @@ protected:
 public:
   // Returns a reference to the singleton instance of ObjList
   static ObjList& theObjList();
-
-  ///////////////
-  // accessors //
-  ///////////////
-
-  // returns true if 'objid' is a valid index into a non-NULL GrObj* in
-  // the ObjList, given its current size
-  bool isValidObjid(ObjId objid) const { return Base::isValidId(objid.toInt()); }
-
-  // both functions return the GrObj* at the index given by 'objid'.
-  // There is no range-checking--this must be done by the client with
-  // isValidObjid().
-  GrObj* getObj (ObjId objid) const { return Base::getPtr(objid.toInt()); }
-
-  //////////////////
-  // manipulators //
-  //////////////////
-
-  // add obj at the next available location, and return the index
-  // where it was inserted. If necessary, the list will be expanded to
-  // make room for the obj
-  ObjId addObj(GrObj* obj) { return ObjId(Base::insert(obj)); }
-
-  // add obj at index 'objid', destroying any GrObj that previously
-  // occupied that location. The list will be expanded if 'objid' exceeds
-  // the size of the list. If objid is < 0, the function returns without
-  // effect.
-  void addObjAt(ObjId objid, GrObj* obj) { Base::insertAt(objid.toInt(), obj); }
-
-  // delete the GrObj at index 'i', and reset the GrObj* to NULL
-  void removeObj(ObjId objid) { Base::remove(objid.toInt()); }
 };
 
 static const char vcid_objlist_h[] = "$Header$";
