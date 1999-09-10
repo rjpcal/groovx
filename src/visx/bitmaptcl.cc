2bda0ed3f0ddbe714f4e3430c6bf56a087ddef19
Robert J. Peters <rjpcal@gmail.com>
1999-09-10 21:32:13 +0000

    Bitmap now serves as an abstract base class for subclass GLBitmap and XBitmap. This file now registers the generic base class commands in Bitmap, and registers creation commands for GLBitmap and XBitmap.

--- src/visx/bitmaptcl.cc
+++ src/visx/bitmaptcl.cc
@@ -3,7 +3,7 @@
 // bitmaptcl.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Jun 15 11:43:45 1999
-// written: Mon Jul 12 12:58:04 1999
+// written: Wed Sep  8 13:19:17 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -14,6 +14,8 @@
 #include <tcl.h>
 
 #include "bitmap.h"
+#include "glbitmap.h"
+#include "xbitmap.h"
 #include "iomgr.h"
 #include "objlist.h"
 #include "listitempkg.h"
@@ -27,6 +29,14 @@
 namespace BitmapTcl {
   class LoadPbmCmd;
   class BitmapPkg;
+}
+
+namespace GLBitmapTcl {
+  class GLBitmapPkg;
+}
+
+namespace XBitmapTcl {
+  class XBitmapPkg;
 }
 
 //---------------------------------------------------------------------
@@ -53,22 +63,42 @@ protected:
 //
 //---------------------------------------------------------------------
 
-class BitmapTcl::BitmapPkg : public ListItemPkg<Bitmap, ObjList> {
+class BitmapTcl::BitmapPkg : public AbstractListItemPkg<Bitmap, ObjList> {
 public:
   BitmapPkg(Tcl_Interp* interp) :
-	 ListItemPkg<Bitmap, ObjList>(interp, ObjList::theObjList(), "Bitmap", "1.1")
+	 AbstractListItemPkg<Bitmap, ObjList>(interp, ObjList::theObjList(),
+													  "Bitmap", "1.1")
   {
 	 addCommand( new LoadPbmCmd(this, "Bitmap::loadPbm") );
 	 declareCAction("flipContrast", &Bitmap::flipContrast);
 	 declareCAction("flipVertical", &Bitmap::flipVertical);
 	 declareCAction("center", &Bitmap::center);
+	 declareCGetter("width", &Bitmap::width);
+	 declareCGetter("height", &Bitmap::height);
 	 declareCAttrib("rasterX", &Bitmap::getRasterX, &Bitmap::setRasterX);
 	 declareCAttrib("rasterY", &Bitmap::getRasterY, &Bitmap::setRasterY);
 	 declareCAttrib("zoomX", &Bitmap::getZoomX, &Bitmap::setZoomX);
 	 declareCAttrib("zoomY", &Bitmap::getZoomY, &Bitmap::setZoomY);
-	 declareCAttrib("usingGlBitmap",
-						 &Bitmap::getUsingGlBitmap, &Bitmap::setUsingGlBitmap);
   }
+};
+
+class GLBitmapTcl::GLBitmapPkg : public ListItemPkg<GLBitmap, ObjList> {
+public:
+  GLBitmapPkg(Tcl_Interp* interp) :
+	 ListItemPkg<GLBitmap, ObjList>(interp, ObjList::theObjList(),
+											  "GLBitmap", "1.1")
+  {
+	 declareCAttrib("usingGlBitmap",
+						 &GLBitmap::getUsingGlBitmap, &GLBitmap::setUsingGlBitmap);
+  }
+};
+
+class XBitmapTcl::XBitmapPkg : public ListItemPkg<XBitmap, ObjList> {
+public:
+  XBitmapPkg(Tcl_Interp* interp) :
+	 ListItemPkg<XBitmap, ObjList>(interp, ObjList::theObjList(),
+											  "XBitmap", "1.1")
+  {}
 };
 
 //---------------------------------------------------------------------
@@ -83,8 +113,11 @@ int Bitmap_Init(Tcl_Interp* interp) {
 DOTRACE("Bitmap_Init");
 
   new BitmapTcl::BitmapPkg(interp);
+  new GLBitmapTcl::GLBitmapPkg(interp);
+  new XBitmapTcl::XBitmapPkg(interp);
 
-  FactoryRegistrar<IO, Bitmap> registrar(IoFactory::theOne());
+  FactoryRegistrar<IO, GLBitmap> registrar1(IoFactory::theOne());
+  FactoryRegistrar<IO, XBitmap>  registrar2(IoFactory::theOne());
 
   return TCL_OK;
 }
