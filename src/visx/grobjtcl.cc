e5f6e3c2a6f335bfa9191dfabad3b7be66e85787
Robert J. Peters <rjpcal@gmail.com>
1999-09-30 05:07:43 +0000

    Added attributes and commands for dealing with the new GrObj functionality, such as bounding boxes, and rendering, alignment, and scaling modes.

--- src/visx/grobjtcl.cc
+++ src/visx/grobjtcl.cc
@@ -3,7 +3,7 @@
 // grobjtcl.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Thu Jul  1 14:01:18 1999
-// written: Tue Sep 21 11:30:19 1999
+// written: Tue Sep 21 18:19:20 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -26,8 +26,32 @@
 ///////////////////////////////////////////////////////////////////////
 
 namespace GrobjTcl {
+  class BoundingBoxCmd;
   class TypeCmd;
   class GrObjPkg;
+};
+
+//---------------------------------------------------------------------
+//
+// BoundingBoxCmd
+//
+//---------------------------------------------------------------------
+
+class GrobjTcl::BoundingBoxCmd : public TclItemCmd<GrObj> {
+public:
+  BoundingBoxCmd(TclItemPkg* pkg, const char* cmd_name) :
+	 TclItemCmd<GrObj>(pkg, cmd_name, "objid", 2, 2) {}
+protected:
+  virtual void invoke() {
+	 double left, top, right, bottom;
+	 bool have_box = getItem()->getBoundingBox(left, top, right, bottom);
+	 if (have_box) {
+		lappendVal(left);
+		lappendVal(top);
+		lappendVal(right);
+		lappendVal(bottom);
+	 }
+  }
 };
 
 //---------------------------------------------------------------------
@@ -59,16 +83,48 @@ public:
 	 AbstractListItemPkg<GrObj, ObjList>(interp, ObjList::theObjList(),
 													 "GrObj", "2.5")
   {
+	 addCommand( new BoundingBoxCmd(this, "GrObj::boundingBox") );
 	 addCommand( new TypeCmd(this, "GrObj::type") );
 
+	 declareCAttrib("alignmentMode",
+						 &GrObj::getAlignmentMode, &GrObj::setAlignmentMode);
+	 declareCAttrib("aspectRatio",
+						 &GrObj::getAspectRatio, &GrObj::setAspectRatio);
 	 declareCAttrib("bbVisibility",
 						 &GrObj::getBBVisibility, &GrObj::setBBVisibility);
 	 declareCAttrib("category", &GrObj::getCategory, &GrObj::setCategory);
+	 declareCAttrib("centerX", &GrObj::getCenterX, &GrObj::setCenterX);
+	 declareCAttrib("centerY", &GrObj::getCenterY, &GrObj::setCenterY);
+	 declareCAttrib("height", &GrObj::getHeight, &GrObj::setHeight);
+	 declareCAttrib("maxDimension",
+						 &GrObj::getMaxDimension, &GrObj::setMaxDimension);
 	 declareCAttrib("renderMode",
 						 &GrObj::getRenderMode, &GrObj::setRenderMode);
+	 declareCAttrib("scalingMode",
+						 &GrObj::getScalingMode, &GrObj::setScalingMode);
 	 declareCAttrib("unRenderMode",
 						 &GrObj::getUnRenderMode, &GrObj::setUnRenderMode);
 	 declareCAction("update", &GrObj::update);
+	 declareCAttrib("width", &GrObj::getWidth, &GrObj::setWidth);
+
+	 linkVarCopy("GrObj::GROBJ_DIRECT_RENDER", GrObj::GROBJ_DIRECT_RENDER);
+	 linkVarCopy("GrObj::GROBJ_GL_COMPILE", GrObj::GROBJ_GL_COMPILE);
+	 linkVarCopy("GrObj::GROBJ_GL_BITMAP_CACHE", GrObj::GROBJ_GL_BITMAP_CACHE);
+	 linkVarCopy("GrObj::GROBJ_X11_BITMAP_CACHE", GrObj::GROBJ_X11_BITMAP_CACHE);
+	 linkVarCopy("GrObj::GROBJ_SWAP_FORE_BACK", GrObj::GROBJ_SWAP_FORE_BACK);
+	 linkVarCopy("GrObj::GROBJ_CLEAR_BOUNDING_BOX", GrObj::GROBJ_CLEAR_BOUNDING_BOX);
+
+	 linkVarCopy("GrObj::NATIVE_SCALING", GrObj::NATIVE_SCALING);
+	 linkVarCopy("GrObj::MAINTAIN_ASPECT_SCALING", GrObj::MAINTAIN_ASPECT_SCALING);
+	 linkVarCopy("GrObj::FREE_SCALING", GrObj::FREE_SCALING);
+
+	 linkVarCopy("GrObj::NATIVE_ALIGNMENT", GrObj::NATIVE_ALIGNMENT);
+	 linkVarCopy("GrObj::CENTER_ON_CENTER", GrObj::CENTER_ON_CENTER);
+	 linkVarCopy("GrObj::NW_ON_CENTER", GrObj::NW_ON_CENTER);
+	 linkVarCopy("GrObj::NE_ON_CENTER", GrObj::NE_ON_CENTER);
+	 linkVarCopy("GrObj::SW_ON_CENTER", GrObj::SW_ON_CENTER);
+	 linkVarCopy("GrObj::SE_ON_CENTER", GrObj::SE_ON_CENTER);
+	 linkVarCopy("GrObj::ARBITRARY_ON_CENTER", GrObj::ARBITRARY_ON_CENTER);
   }
 };
 
