03f9de704d01774267057bbb30e7b024a1f55058
Robert J. Peters <rjpcal@gmail.com>
1999-10-04 22:55:17 +0000

    Initial revision

---
+++ src/propitempkg.h
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////
+//
+// propitempkg.h
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Mon Oct  4 15:42:59 1999
+// written: Mon Oct  4 15:54:39 1999
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef PROPITEMPKG_H_DEFINED
+#define PROPITEMPKG_H_DEFINED
+
+#ifndef LISTITEMPKG_H_DEFINED
+#include "listitempkg.h"
+#endif
+
+#ifndef IOMGR_H_DEFINED
+#include "iomgr.h"
+#endif
+
+template <class C, class List>
+class PropertyListItemPkg : public ListItemPkg<C, List> {
+public:
+  PropertyListItemPkg(Tcl_Interp* interp, List& aList,
+							 const char* name, const char* version) :
+	 ListItemPkg<C, List>(interp, aList, name, version)
+  {
+	 CTclIoItemPkg<C>::declareAllProperties();
+	 FactoryRegistrar<IO, C> registrar(IoFactory::theOne());
+  }
+};
+
+
+static const char vcid_propitempkg_h[] = "$Header$";
+#endif // !PROPITEMPKG_H_DEFINED
