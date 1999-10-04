8ac37fc9061a9dd663196a1181a5fc228581e0a9
Robert J. Peters <rjpcal@gmail.com>
1999-10-04 02:56:47 +0000

    Removed Poslist_Init declaration.

--- src/poslisttcl.h
+++ src/poslisttcl.h
@@ -3,7 +3,7 @@
 // poslisttcl.h
 // Rob Peters
 // created: Sat Mar 13 12:43:01 1999
-// written: Mon Jun 21 11:42:07 1999
+// written: Sun Oct  3 19:56:25 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -14,11 +14,6 @@
 namespace PoslistTcl {
   const char* const bad_posid_msg = "posid out of range";
 }
-
-struct Tcl_Interp;
-typedef int (Tcl_PackageInitProc) (Tcl_Interp* interp);
-
-extern "C" Tcl_PackageInitProc Poslist_Init;
 
 static const char vcid_poslisttcl_h[] = "$Header$";
 #endif // !POSLISTTCL_H_DEFINED
