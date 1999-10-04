3a42129ebdd904f173721e385c786096f945e193
Robert J. Peters <rjpcal@gmail.com>
1999-10-04 02:57:47 +0000

    Removed Objlist_Init declaration.

--- src/objlisttcl.h
+++ src/objlisttcl.h
@@ -3,7 +3,7 @@
 // objlisttcl.h
 // Rob Peters
 // created: Jan-99
-// written: Mon Jun 21 11:09:44 1999
+// written: Sun Oct  3 19:57:14 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -15,11 +15,6 @@ namespace ObjlistTcl {
   const char* const bad_objid_msg = "objid out of range";
   const char* const wrong_type_msg = "object not of correct type";
 }
-
-struct Tcl_Interp;
-typedef int (Tcl_PackageInitProc) (Tcl_Interp* interp);
-
-extern "C" Tcl_PackageInitProc Objlist_Init;
 
 static const char vcid_objlisttcl_h[] = "$Header$";
 #endif // !OBJLISTTCL_H_DEFINED
