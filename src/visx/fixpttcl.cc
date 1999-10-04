1f12d4b844c642099469488ecc2a19a13adde835
Robert J. Peters <rjpcal@gmail.com>
1999-10-04 23:23:18 +0000

    FixPt is now implemented using CTProperty's, so the Fixpt Tcl package is just a PropertyListItemPkg.

--- src/visx/fixpttcl.cc
+++ src/visx/fixpttcl.cc
@@ -3,7 +3,7 @@
 // fixpttcl.cc
 // Rob Peters
 // created: Jan-99
-// written: Mon Jul 12 13:04:29 1999
+// written: Mon Oct  4 16:22:39 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -12,51 +12,22 @@
 #define FIXPTTCL_CC_DEFINED
 
 #include <tcl.h>
-#include <cstring>
 
-#include "iomgr.h"
-#include "objlist.h"
 #include "fixpt.h"
-#include "listitempkg.h"
+#include "objlist.h"
+#include "propitempkg.h"
 
 #define NO_TRACE
 #include "trace.h"
-#include "debug.h"
 
-///////////////////////////////////////////////////////////////////////
-//
-// FixptPkg class definition
-//
-///////////////////////////////////////////////////////////////////////
-
-namespace FixptTcl {
-  class FixptPkg;
-}
-
-class FixptTcl::FixptPkg : public ListItemPkg<FixPt, ObjList> {
-public:
-  FixptPkg(Tcl_Interp* interp) :
-	 ListItemPkg<FixPt, ObjList>(interp, ObjList::theObjList(), "Fixpt", "2.6")
-  {
-	 declareCAttrib("length", &FixPt::getLength, &FixPt::setLength);
-	 declareCAttrib("width", &FixPt::getWidth, &FixPt::setWidth);
-  }
-};
-
-//---------------------------------------------------------------------
-//
-// Fixpt_Init --
-//
-//---------------------------------------------------------------------
 
 extern "C" Tcl_PackageInitProc Fixpt_Init;
 
 int Fixpt_Init(Tcl_Interp* interp) {
 DOTRACE("Fixpt_Init");
 
-  new FixptTcl::FixptPkg(interp);
-
-  FactoryRegistrar<IO, FixPt> registrar(IoFactory::theOne());
+  new PropertyListItemPkg<FixPt, ObjList>(interp, ObjList::theObjList(),
+														"Fixpt", "1.2");
 
   return TCL_OK;
 }
