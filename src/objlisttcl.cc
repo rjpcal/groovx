3ab1180724ece93ee79254253f6d6f67931dfedd
Robert J. Peters <rjpcal@gmail.com>
1999-10-05 21:30:15 +0000

    Added LoadObjectsCmd, which works similarly to LoadFaces in FaceTcl, but for any type of GrObj. The typename of the objects in the file can be specified on the command-line, but if not, the command assumes the typenames are written in the file as part of normal serialized objects. This command may eventually be genericized and moved to the ListPkg template.

--- src/objlisttcl.cc
+++ src/objlisttcl.cc
@@ -3,7 +3,7 @@
 // objlisttcl.cc
 // Rob Peters
 // created: Jan-99
-// written: Sun Oct  3 19:29:45 1999
+// written: Tue Oct  5 14:28:25 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -11,6 +11,11 @@
 #ifndef OBJLISTTCL_CC_DEFINED
 #define OBJLISTTCL_CC_DEFINED
 
+#include <fstream.h>
+#include <cctype>
+#include <string>
+
+#include "iomgr.h"
 #include "objlist.h"
 #include "listpkg.h"
 
@@ -20,7 +25,86 @@
 #include "debug.h"
 
 namespace ObjlistTcl {
-  class ObjListPkg;
+  class LoadObjectsCmd;
+  class ObjListPkg;  
+
+  void eatWhitespace(istream& is) {
+	 while ( isspace(is.peek()) ) {
+		is.get();
+	 }
+  }
+}
+
+//---------------------------------------------------------------------
+//
+// LoadFacesCmd --
+//
+//---------------------------------------------------------------------
+
+class ObjlistTcl::LoadObjectsCmd : public TclCmd {
+public:
+  LoadObjectsCmd(Tcl_Interp* interp, const char* cmd_name) :
+	 TclCmd(interp, cmd_name,
+			  "filename ?num_to_read? ?typename?", 2, 4, false)
+  {}
+
+protected:
+  virtual void invoke();
+};
+
+void ObjlistTcl::LoadObjectsCmd::invoke() {
+  const char* file = getCstringFromArg(1);
+
+  int num_to_read = -1;			  // -1 indicates to read to eof
+  if (objc() >= 3) { num_to_read = getIntFromArg(2); }
+
+  const char* given_type = 0;
+  if (objc() >= 4) { given_type = getCstringFromArg(3); }
+
+  ifstream ifs(file);
+  if (ifs.fail()) { throw TclError("unable to open file"); }
+
+  
+  ObjList& olist = ObjList::theObjList();
+
+  string dummy;
+
+  int num_read = 0;
+  vector<int> ids;
+
+  eatWhitespace(ifs);
+
+  while ( (num_to_read < 0 || num_read < num_to_read)
+			 && (ifs.peek() != EOF) ) {
+
+	 // allow for whole-line comments between objects beginning with '#'
+	 if (ifs.peek() == '#') {
+		getline(ifs, dummy, '\n');
+		continue;
+	 }
+	 
+	 string type;
+	 if (given_type == 0) { ifs >> type; } 
+	 else                 { type = given_type; }
+
+	 IO* io = IoMgr::newIO(type.c_str());
+	 
+	 GrObj* p = dynamic_cast<GrObj*>(io);
+	 if (!p) {
+		throw InputError("ObjlistTcl::loadObjects");
+	 }
+	 
+	 io->deserialize(ifs, IO::NO_FLAGS);
+	 
+	 int objid = olist.insert(p);
+	 ids.push_back(objid);
+	 ++num_read;
+
+	 eatWhitespace(ifs);
+  }
+  
+  // Return the ids of all the faces created
+  returnSequence(ids.begin(), ids.end());
 }
 
 //---------------------------------------------------------------------
@@ -32,7 +116,10 @@ namespace ObjlistTcl {
 class ObjlistTcl::ObjListPkg : public ListPkg<ObjList> {
 public:
   ObjListPkg(Tcl_Interp* interp) :
-	 ListPkg<ObjList>(interp, ObjList::theObjList(), "ObjList", "3.0") {}
+	 ListPkg<ObjList>(interp, ObjList::theObjList(), "ObjList", "3.0")
+  {
+	 addCommand( new LoadObjectsCmd(interp, "ObjList::loadObjects") );
+  }
 };
 
 //---------------------------------------------------------------------
