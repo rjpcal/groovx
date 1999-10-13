5884065c6f3abe405492dfdd1863b11aa201996e
Robert J. Peters <rjpcal@gmail.com>
1999-10-13 18:54:49 +0000

    Added more detailed reporting of to-implement tests, known bugs, and intermittent bugs.

--- testing/test.tcl
+++ testing/test.tcl
@@ -20,10 +20,10 @@ set DATE [clock format [clock seconds] -format %H%M%d%b%Y]
 
 set num_tests 0
 set num_success 0
-set num_to_implement 0
-set num_known_bugs 0
-set num_intermittent_bugs 0
 
+set TO_IMPLEMENT {}
+set KNOWN_BUGS {}
+set INTERMITTENT_BUGS {}
 
 # Flags to pass into 'flags' argument of test.
 set no_test 0
@@ -35,9 +35,15 @@ set test_serialize 0
 
 # If the 'flags' argument is 0, the test is skipped.
 proc test {package_name test_name script expected_result_regexp {flags 1}} {
-	 if { $flags == $::must_implement } { incr ::num_to_implement }
-	 if { $flags == $::skip_known_bug } { incr ::num_known_bugs }
-	 if { $flags == $::intermittent_bug } { incr :: num_intermittent_bugs }
+	 if { $flags == $::must_implement } {
+		  lappend ::TO_IMPLEMENT "$package_name $test_name"
+	 }
+	 if { $flags == $::skip_known_bug } {
+		  lappend ::KNOWN_BUGS "$package_name $test_name"
+	 }
+	 if { $flags == $::intermittent_bug } {
+		  lappend ::INTERMITTENT_BUGS "$package_name $test_name"
+	 }
 	 if {$flags <= 0} { return }
 	 if {$::VERBOSE != 0} {
 		  puts "==== $package_name $test_name"
@@ -69,12 +75,31 @@ proc finish {} {
 	 set ::END_CLICKS [clock clicks]
 	 set elapsed_ms [expr ($::END_CLICKS-$::START_CLICKS) / $clicks_per_ms]
 	 set per_test [expr $::num_tests ? ($elapsed_ms/$::num_tests) : 0]
-	 set msg "$::num_success tests succeeded of $::num_tests tests attempted.
-Testing lasted $elapsed_ms msec, or $per_test msec per test.
-$::num_to_implement tests remain to be implemented.
-$::num_known_bugs known bugs were reported.
-$::num_intermittent_bugs intermittent bugs were reported.
-Testing complete."
+	 set msg "$::num_success tests succeeded of $::num_tests tests attempted.\n"
+	 append msg "Testing lasted $elapsed_ms msec, or $per_test msec per test.\n"
+
+	 if { [llength $::TO_IMPLEMENT] > 0 } {
+		  append msg "[llength $::TO_IMPLEMENT] tests remain to be implemented:\n"
+		  foreach item $::TO_IMPLEMENT {
+				append msg "\t${item}\n"
+		  }
+	 }
+
+    if { [llength $::KNOWN_BUGS] > 0 } {
+		  append msg "[llength $::KNOWN_BUGS] known bugs were reported:\n"
+		  foreach bug $::KNOWN_BUGS {
+				append msg "\t${bug}\n"
+		  }
+	 }
+
+	 if { [llength $::INTERMITTENT_BUGS] > 0 } {
+		  append msg "[llength $::INTERMITTENT_BUGS] intermittent bugs were reported:\n"
+		  foreach bug $::INTERMITTENT_BUGS {
+				append msg "\t${bug}\n"
+		  }
+	 }
+
+	 append msg "Testing complete."
     puts $msg
     after 10000 { event generate . <KeyPress> -keysym Return }
 	 tk_messageBox -icon info -type ok -message $msg
