516a2f6d7930e9d371dd0322d97a8b1176e28926
Robert J. Peters <rjpcal@gmail.com>
1999-10-14 19:54:36 +0000

    Changed the expected error strings as necessary due to FixPt's new use of Property's. (The error strings actually now come from TclValue's, rather than straight from the Tcl_Interp).

--- testing/fixpt_test.tcl
+++ testing/fixpt_test.tcl
@@ -7,6 +7,9 @@
 ###
 ##############################################################################
 
+package require Fixpt
+package require Objlist
+
 ### Fixpt::FixptCmd ###
 test "FixptTcl-Fixpt::Fixpt" "no args limit" {} $BLANK $no_test
 test "FixptTcl-Fixpt::Fixpt" "normal create" {
@@ -27,7 +30,7 @@ test "FixptTcl-length" "normal set" {
 } {^0$}
 test "FixptTcl-length" "error from non-numeric input" {
     Fixpt::length $::fix junk
-} {^expected floating-point number but got "junk"$}
+} {^Fixpt::length: expected floating-point number but got "junk"$}
 test "FixptTcl-length" "error from bad objid" {
     Fixpt::length -1 1.0
 } {^Fixpt::length: an error of type InvalidIdError occurred$}
@@ -48,17 +51,17 @@ test "FixptTcl-width" "normal set" {
 } {^0$}
 test "FixptTcl-width" "error from non-numeric input" {
     Fixpt::width $::fix junk
-} {expected integer but got "junk"}
+} {^Fixpt::width: expected integer but got "junk"$}
 test "FixptTcl-width" "error from bad objid" {
     Fixpt::width -1 5
-} {Fixpt::width: an error of type InvalidIdError occurred}
+} {^Fixpt::width: an error of type InvalidIdError occurred$}
 test "FixptTcl-width" "error from wrong type" {
     set f [Face::Face]
 	 Fixpt::width $f 5
-} {Fixpt::width: object not of correct type}
+} {^Fixpt::width: object not of correct type$}
 test "FixptTcl-width" "error from non-integral number" {
     Fixpt::width $::fix 1.5
-} {expected integer but got "1\.5"}
+} {^Fixpt::width: expected integer but got "1\.5"$}
 
 ObjList::reset
 unset fix
