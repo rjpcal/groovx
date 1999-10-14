969aa7dc27c52194a2f671a453e59d9878a21726
Robert J. Peters <rjpcal@gmail.com>
1999-10-14 19:26:34 +0000

    Added several static functions to Sound, most notably newPlatformSound(), which returns a pointer to an object of some platform-appropriate Sound subclass. The other routines are provided to allow clients to initialize, release, and inquire the status of, any platform-dependent sound resources.

--- src/visx/sound.h
+++ src/visx/sound.h
@@ -3,13 +3,18 @@
 // sound.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Thu Jul  8 11:43:07 1999
-// written: Thu Jul  8 12:11:15 1999
+// written: Thu Oct 14 11:16:35 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef SOUND_H_DEFINED
 #define SOUND_H_DEFINED
+
+#ifndef STRING_DEFINED
+#include <string>
+#define STRING_DEFINED
+#endif
 
 #ifndef ERROR_H_DEFINED
 #include "error.h"
@@ -26,6 +31,24 @@ public:
 
 class Sound : public virtual IO {
 public:
+  // This function will attempt to do any platform-dependent
+  // initialization that is necessary to use Sound's, returning true
+  // on success, or false on failure.
+  static bool initSound();
+
+  // This function returns true if the necessary initSound() has been
+  // previously called with success, and returns false if initSound()
+  // has not been called, or if initSound() failed.
+  static bool haveSound();
+
+  // This function shuts down sound capability in a
+  // platform-appropriate way.
+  static void closeSound();
+
+  // Returns a pointer to a new platform-appropriate Sound object. The
+  // caller is responsible for destroying the Sound object.
+  static Sound* newPlatformSound(const string& soundfile);
+
   virtual ~Sound();
 
   virtual void serialize(ostream& os, IOFlag flag) const = 0;
