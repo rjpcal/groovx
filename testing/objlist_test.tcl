5068d4faf59a9b25fbd5a30fdb4b268e34819395
Robert J. Peters <rjpcal@gmail.com>
1999-10-21 19:55:01 +0000

    Switched to using the generic list test framework provided in list_test.tcl, via the command List::testList.

--- testing/objlist_test.tcl
+++ testing/objlist_test.tcl
@@ -10,40 +10,10 @@
 ### regexp to match ObjList in serializations
 set OBJLIST {ObjList|PtrList<GrObj>}
 	 
-### ObjList::resetCmd ###
-test "ObjlistTcl-ObjList::reset" "too many args" {
-    ObjList::reset junk
-} {wrong \# args: should be "ObjList::reset"}
-test "ObjlistTcl-ObjList::reset" "check number of objects -> 0" {
-	 Face::Face
-	 Fixpt::Fixpt
-	 ObjList::reset
-	 ObjList::count
-} {^0$}
-test "ObjlistTcl-ObjList::reset" "check first vacant -> 0" {
-	 Face::Face
-	 Fixpt::Fixpt
-	 ObjList::reset
-	 Face::Face
-} {^0$}
-test "ObjlistTcl-ObjList::reset" "no error" {} $BLANK $no_test
-
-### ObjList::countCmd ###
-test "ObjlistTcl-ObjList::count" "too many args" {
-    ObjList::count junk
-} {wrong \# args: should be "ObjList::count"}
-test "ObjlistTcl-ObjList::count" "normal use" {
-	 ObjList::reset
-	 Face::Face
-	 Fixpt::Fixpt
-	 ObjList::count
-} {^2$}
-test "ObjlistTcl-ObjList::count" "no error" {} $BLANK $no_test
+source ${::TEST_DIR}/list_test.tcl
+List::testList ObjlistTcl ObjList GrObj Face Fish
 
 ### ObjList::stringifyCmd ###
-test "ObjlistTcl-ObjList::stringify" "too many args" {
-    ObjList::stringify junk
-} {wrong \# args: should be "ObjList::stringify"}
 test "ObjlistTcl-ObjList::stringify" "use on empty list" {
     ObjList::reset
 	 ObjList::stringify
@@ -58,22 +28,8 @@ test "ObjlistTcl-ObjList::stringify" "use on filled list" {
 0 Face .*
 1 FixPt .*
 2"
-test "ObjlistTcl-ObjList::stringify" "no error" {} $BLANK $no_test
 	 
 ### ObjList::destringifyCmd ###
-test "ObjlistTcl-ObjList::destringify" "args" {
-    ObjList::destringify
-} {wrong \# args: should be "ObjList::destringify string"}
-test "ObjlistTcl-ObjList::destringify" "use with filled list" {
-	 ObjList::reset
-	 Face::Face
-	 Fixpt::Fixpt
-	 set a [ObjList::stringify]
-	 ObjList::destringify $a
-	 expr [ObjList::count]==2 \
-				&& ([string compare [GrObj::type 0] Face] == 0) \
-				&& ([string compare [GrObj::type 1] FixPt] == 0)
-} {^1$}
 test "ObjlistTcl-ObjList::destringify" "stringify, destringify, restringify" {
 	 ObjList::reset
 	 Face::Face
@@ -83,17 +39,3 @@ test "ObjlistTcl-ObjList::destringify" "stringify, destringify, restringify" {
 	 set b [ObjList::stringify]
 	 expr [string compare $a $b] == 0
 } {^1$}
-test "ObjlistTcl-ObjList::destringify" "use with empty list" {
-	 ObjList::reset
-	 set a [ObjList::stringify]
-	 ObjList::destringify $a
-	 expr [ObjList::count]==0
-} {^1$}
-test "ObjlistTcl-ObjList::destringify" "error on incomplete input" {
-    ObjList::destringify "ObjList"
-} {ObjList::destringify: InputError: ObjList}
-test "ObjlistTcl-ObjList::destringify" "error on bad input" {
-    ObjList::destringify "this is a bunch of bs"
-} {ObjList::destringify: InputError: ObjList}
-
-
--- testing/poslist_test.tcl
+++ testing/poslist_test.tcl
@@ -10,40 +10,10 @@
 ### regexp to match PosList in serializations
 set POSLIST {PosList|PtrList<Position>}
 
-### PosList::resetCmd ###
-test "PoslistTcl-PosList::reset" "too many args" {
-    PosList::reset junk
-} {wrong \# args: should be "PosList::reset"}
-test "PoslistTcl-PosList::reset" "check number of positions -> 0" {
-	 Pos::Pos
-	 Jitter::Jitter
-	 PosList::reset
-	 PosList::count
-} {^0$}
-test "PoslistTcl-PosList::reset" "check first vacant -> 0" {
-	 Pos::Pos
-	 Jitter::Jitter
-	 PosList::reset
-	 Pos::Pos
-} {^0$}
-test "PoslistTcl-PosList::reset" "no error" {} $BLANK $no_test
-
-### PosList::countCmd ###
-test "PoslistTcl-PosList::count" "args" {
-    PosList::count junk
-} {wrong \# args: should be "PosList::count"}
-test "PoslistTcl-PosList::count" "normal use" { 
-	 PosList::reset
-	 Pos::Pos
-	 Jitter::Jitter
-	 PosList::count
-} {^2$}
-test "PoslistTcl-PosList::count" "no error" {} $BLANK $no_test
+source ${::TEST_DIR}/list_test.tcl
+List::testList PoslistTcl PosList Pos Pos Jitter
 
 ### PosList::stringifyCmd ###
-test "PoslistTcl-PosList::stringify" "args" {
-    PosList::stringify junk
-} {wrong \# args: should be "PosList::stringify"}
 test "PoslistTcl-PosList::stringify" "stringify a non-empty PosList" {
 	 PosList::reset
 	 set p [Pos::Pos]
@@ -61,38 +31,3 @@ test "PoslistTcl-PosList::stringify" "stringify a non-empty PosList" {
 1 Jitter 1\.5 3 4\.5
 Position 5\.3 10\.6 15\.9 1 2\.5 4 1\.5 0 -2\.2 3
 2"
-
-test "PoslistTcl-PosList::stringify" "stringify an empty PosList" {
-	 PosList::reset
-	 PosList::stringify
-} "$POSLIST $INT 0
-0"
-test "PoslistTcl-PosList::stringify" "no error" {} $BLANK $no_test
-
-### PosList::destringifyCmd ###
-test "PoslistTcl-PosList::destringify" "args" {
-    PosList::destringify
-} {wrong \# args: should be "PosList::destringify string"}
-test "PoslistTcl-PosList::destringify" "normal use" {
-	 PosList::reset
-	 set p [Pos::Pos]
-	 Pos::rotate $p 3.0 1.5 0.0 -2.2
-	 Pos::scale $p 1.0 2.5 4.0
-	 Pos::translate $p 5.3 10.6 15.9
-	 set j [Jitter::Jitter]
-	 Pos::rotate $j 3.0 1.5 0.0 -2.2
-	 Pos::scale $j 1.0 2.5 4.0
-	 Pos::translate $j 5.3 10.6 15.9
-	 Jitter::setJitter $j 1.5 3.0 4.5
-	 set str [PosList::stringify]
-	 PosList::reset
-	 PosList::destringify $str
-	 expr [PosList::count]==2 \
-				&& ([string compare [Pos::type 0] Position] == 0) \
-				&& ([string compare [Pos::type 1] Jitter] == 0)
-} {^1$}
-test "PoslistTcl-PosList::destringify" "error" {
-    PosList::destringify "this is all just a bunch of bunk"
-} {PosList::destringify: InputError: PosList}
-
-
