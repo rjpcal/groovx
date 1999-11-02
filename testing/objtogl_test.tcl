e87bcef4647de912e674f6ce5b4abd846ca2865e
Robert J. Peters <rjpcal@gmail.com>
1999-11-02 19:16:19 +0000

    Moved 'update' after Togl::init up inside the conditional on Togl::inited.

--- testing/objtogl_test.tcl
+++ testing/objtogl_test.tcl
@@ -14,8 +14,7 @@ set PACKAGE ObjTogl
 
 ### ObjTogl::InitCmd ###
 test "ObjTogl-Togl::Init" "test widget create" {
-	 if { ![Togl::inited] } { Togl::init "-rgba false"}
-	 update
+	 if { ![Togl::inited] } { Togl::init "-rgba false"; update }
 } {^$}
 
 ### Togl::loadFontCmd ###
