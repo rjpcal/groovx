edd0a2baa0636813bb2554ec8d5db70b666f5de0
Robert J. Peters <rjpcal@gmail.com>
1999-11-02 21:38:44 +0000

    Initial revision

---
+++ testing/gabor_test.tcl
@@ -0,0 +1,14 @@
+##############################################################################
+###
+### gabor_test.tcl
+### Rob Peters
+### Nov-1999
+### $Id$
+###
+##############################################################################
+
+package require Gabor
+
+source ${::TEST_DIR}/grobj_test.tcl
+
+GrObj::testSubclass Gabor Gabor
