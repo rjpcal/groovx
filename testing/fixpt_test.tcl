3ae9de9fd75ffa9827bfee8b74fadd4d5b272d38
Robert J. Peters <rjpcal@gmail.com>
1999-11-02 18:23:47 +0000

    Changed Fixpt to FixPt.

--- testing/fixpt_test.tcl
+++ testing/fixpt_test.tcl
@@ -14,58 +14,58 @@ source ${::TEST_DIR}/grobj_test.tcl
 
 GrObj::testSubclass Fixpt FixPt
 
-### Fixpt::FixptCmd ###
-test "FixptTcl-Fixpt::Fixpt" "no args limit" {} $BLANK $no_test
-test "FixptTcl-Fixpt::Fixpt" "normal create" {
-	 Fixpt::Fixpt
+### FixPt::FixPtCmd ###
+test "FixptTcl-FixPt::FixPt" "no args limit" {} $BLANK $no_test
+test "FixptTcl-FixPt::FixPt" "normal create" {
+	 FixPt::FixPt
 } {^[0-9]+$}
 
-set fix [Fixpt::Fixpt]
+set fix [FixPt::FixPt]
 
 ### lengthCmd ###
 test "FixptTcl-length" "too few args" {
-    Fixpt::length
-} {^wrong \# args: should be "Fixpt::length item_id\(s\) \?new_value\(s\)\?"$}
+    FixPt::length
+} {^wrong \# args: should be "FixPt::length item_id\(s\) \?new_value\(s\)\?"$}
 test "FixptTcl-length" "too many args" {
-    Fixpt::length j u n
-} {^wrong \# args: should be "Fixpt::length item_id\(s\) \?new_value\(s\)\?"$}
+    FixPt::length j u n
+} {^wrong \# args: should be "FixPt::length item_id\(s\) \?new_value\(s\)\?"$}
 test "FixptTcl-length" "normal set" {
-	 catch {Fixpt::length $::fix 1.0}
+	 catch {FixPt::length $::fix 1.0}
 } {^0$}
 test "FixptTcl-length" "error from non-numeric input" {
-    Fixpt::length $::fix junk
-} {^Fixpt::length: expected floating-point number but got "junk"$}
+    FixPt::length $::fix junk
+} {^FixPt::length: expected floating-point number but got "junk"$}
 test "FixptTcl-length" "error from bad objid" {
-    Fixpt::length -1 1.0
-} {^Fixpt::length: an error of type InvalidIdError occurred$}
+    FixPt::length -1 1.0
+} {^FixPt::length: an error of type InvalidIdError occurred$}
 test "FixptTcl-length" "error from wrong type" {
     set f [Face::Face]
-	 Fixpt::length $f 1.0
-} {^Fixpt::length: object not of correct type$}
+	 FixPt::length $f 1.0
+} {^FixPt::length: object not of correct type$}
 
 ### widthCmd ###
 test "FixptTcl-width" "too few args" {
-    Fixpt::width
-} {wrong \# args: should be "Fixpt::width item_id\(s\) \?new_value\(s\)\?"}
+    FixPt::width
+} {wrong \# args: should be "FixPt::width item_id\(s\) \?new_value\(s\)\?"}
 test "FixptTcl-width" "too many args" {
-    Fixpt::width j u n
-} {wrong \# args: should be "Fixpt::width item_id\(s\) \?new_value\(s\)\?"}
+    FixPt::width j u n
+} {wrong \# args: should be "FixPt::width item_id\(s\) \?new_value\(s\)\?"}
 test "FixptTcl-width" "normal set" {
-	 catch {Fixpt::width $::fix 2}
+	 catch {FixPt::width $::fix 2}
 } {^0$}
 test "FixptTcl-width" "error from non-numeric input" {
-    Fixpt::width $::fix junk
-} {^Fixpt::width: expected integer but got "junk"$}
+    FixPt::width $::fix junk
+} {^FixPt::width: expected integer but got "junk"$}
 test "FixptTcl-width" "error from bad objid" {
-    Fixpt::width -1 5
-} {^Fixpt::width: an error of type InvalidIdError occurred$}
+    FixPt::width -1 5
+} {^FixPt::width: an error of type InvalidIdError occurred$}
 test "FixptTcl-width" "error from wrong type" {
     set f [Face::Face]
-	 Fixpt::width $f 5
-} {^Fixpt::width: object not of correct type$}
+	 FixPt::width $f 5
+} {^FixPt::width: object not of correct type$}
 test "FixptTcl-width" "error from non-integral number" {
-    Fixpt::width $::fix 1.5
-} {^Fixpt::width: expected integer but got "1\.5"$}
+    FixPt::width $::fix 1.5
+} {^FixPt::width: expected integer but got "1\.5"$}
 
 ObjList::reset
 unset fix
