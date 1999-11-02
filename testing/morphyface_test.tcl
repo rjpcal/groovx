db5cf3e6d742ff7c6730c72c0933f932026d797e
Robert J. Peters <rjpcal@gmail.com>
1999-11-02 18:32:01 +0000

    Initial revision

---
+++ testing/morphyface_test.tcl
@@ -0,0 +1,14 @@
+##############################################################################
+###
+### morphyface_test.tcl
+### Rob Peters
+### Nov-1999
+### $Id$
+###
+##############################################################################
+
+package require Morphyface
+
+source ${::TEST_DIR}/grobj_test.tcl
+
+GrObj::testSubclass Morphyface MorphyFace
