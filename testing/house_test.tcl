2a77401746d93132013717be40afe0a342147a6b
Robert J. Peters <rjpcal@gmail.com>
1999-11-03 05:48:04 +0000

    Initial revision

---
+++ testing/house_test.tcl
@@ -0,0 +1,14 @@
+##############################################################################
+###
+### house_test.tcl
+### Rob Peters
+### Nov-1999
+### $Id$
+###
+##############################################################################
+
+package require House
+
+source ${::TEST_DIR}/grobj_test.tcl
+
+GrObj::testSubclass House House
---
+++ testing/maskhatch_test.tcl
@@ -0,0 +1,14 @@
+##############################################################################
+###
+### maskhatch_test.tcl
+### Rob Peters
+### Nov-1999
+### $Id$
+###
+##############################################################################
+
+package require Maskhatch
+
+source ${::TEST_DIR}/grobj_test.tcl
+
+GrObj::testSubclass Maskhatch MaskHatch
