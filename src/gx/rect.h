68835628d391a027b2ea8d761fe62d894a53b3ce
Robert J. Peters <rjpcal@gmail.com>
1999-11-04 17:20:03 +0000

    Added getters (getRectLTRB, getRectLRBT) for Rect that allow all members to be retrieved at once, analogously to the setters setRectLTRB, setRectLRBT. Also added centerX() and centerY() accessors.

--- src/gx/rect.h
+++ src/gx/rect.h
@@ -3,7 +3,7 @@
 // rect.h
 // Rob Peters
 // created: Jan-99
-// written: Wed Oct 13 09:09:55 1999
+// written: Thu Nov  4 09:12:56 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -20,20 +20,36 @@
 template<class V>
 class Rect {
 public:
+  // Creators
   Rect() : l(), t(), r(), b() {}	// everything inits to zero
 
   Rect(V L, V T, V R, V B) : l(L), t(T), r(R), b(B) {}
 
   Rect(const Rect<V>& i) : l(i.l), t(i.t), r(i.r), b(i.b) {}
   
+  // Data members
   V l, t, r, b;
+
+
+  // Accessors
+  void getRectLTRB(V& L, V& T, V& R, V& B) const
+        { L = l; T = t; R = R; B = b; }
+  void getRectLRBT(V& L, V& R, V& B, V& T) const
+        { L = l; R = r; B = b; T = t; }
+
+  V width() const { return (r-l); }
+  V height() const { return (t-b); }
+  V aspect() const { return width()/height(); }
+
+  V centerX() const { return (r+l)/V(2); }
+  V centerY() const { return (t+b)/V(2); }
+
+  // Manipulators
   void setRectLTRB(V L, V T, V R, V B)
         { l = L; t = T; r = R; b = B; }
   void setRectLRBT(V L, V R, V B, V T)
         { l = L; r = R; b = B; t = T; }
-  V width() const { return (r-l); }
-  V height() const { return (t-b); }
-  V aspect() const { return width()/height(); }
+
   void widen(V f) { l *= f; r *=f; }
   void heighten(V f) { t *=f; b *=f; }
 };
