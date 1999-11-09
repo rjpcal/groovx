de6724a00e0242e3805c16b125b62d3ca0af6306
Robert J. Peters <rjpcal@gmail.com>
1999-11-09 01:15:25 +0000

    Moved data and private functions out of main body of KbdResponseHdlr class and into nested implementation class Impl. Further refactoring will then take place within the Impl class.

--- src/visx/kbdresponsehdlr.h
+++ src/visx/kbdresponsehdlr.h
@@ -3,23 +3,13 @@
 // kbdresponsehdlr.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Mon Jun 21 18:09:11 1999
-// written: Thu Oct 21 18:11:26 1999
+// written: Mon Nov  8 16:23:43 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef KBDRESPONSEHDLR_H_DEFINED
 #define KBDRESPONSEHDLR_H_DEFINED
-
-#ifndef TCL_H_DEFINED
-#include <tcl.h>
-#define TCL_H_DEFINED
-#endif
-
-#ifndef VECTOR_DEFINED
-#include <vector>
-#define VECTOR_DEFINED
-#endif
 
 #ifndef STRING_DEFINED
 #include <string>
@@ -28,10 +18,6 @@
 
 #ifndef RESPONSEHANDLER_H_DEFINED
 #include "responsehandler.h"
-#endif
-
-#ifndef TCLOBJLOCK_H_DEFINED
-#include "tclobjlock.h"
 #endif
 
 
@@ -57,14 +43,11 @@ public:
   // manipulators/accessors
   virtual void setInterp(Tcl_Interp* interp);
 
-  void setKeyRespPairs(const string& s) { 
-	 itsKeyRespPairs = s; 
-	 updateRegexps();
-  }
-  const string& getKeyRespPairs() const { return itsKeyRespPairs; }
+  void setKeyRespPairs(const string& s);
+  const string& getKeyRespPairs() const;
 
-  bool getUseFeedback() const { return itsUseFeedback; }
-  void setUseFeedback(bool val) { itsUseFeedback = val; }
+  bool getUseFeedback() const;
+  void setUseFeedback(bool val);
 
   const char* getFeedbackPairs() const;
   void setFeedbackPairs(const char* feedback_string);
@@ -79,41 +62,9 @@ protected:
   virtual void ignore() const;
 
 private:
-  static Tcl_ObjCmdProc handleCmd;
-  int handleResponse(const char* keysym) const;
-  virtual int getRespFromKeysym(const char* keysym) const;
-  int feedback(int response) const;
-  void updateFeedbacks();
-  void updateRegexps();
-  struct RegExp_ResponseVal {
-    RegExp_ResponseVal(Tcl_RegExp rx, int rv) : regexp(rx), resp_val(rv) {}
-    Tcl_RegExp regexp;
-    int resp_val;
-  };
-
-  string itsKeyRespPairs;
-  mutable vector<RegExp_ResponseVal> itsRegexps;
-
-  string itsFeedbackPairs;
-  bool itsUseFeedback;
-
-//    struct Condition_Feedback {
-//    	 Condition_Feedback(Tcl_Obj* cond, Tcl_Obj* res);
-//  	 Condition_Feedback(const Condition_Feedback& rhs);
-//  	 Condition_Feedback& operator=(const Condition_Feedback& rhs);
-
-//    	 TclObjPtr condition;
-//    	 TclObjPtr result; 
-//    };
-  struct Condition_Feedback {
-  	 Condition_Feedback(Tcl_Obj* cond, Tcl_Obj* res) :
-  		condition(cond), result(res) {}		
-  	 Tcl_Obj* condition;
-  	 Tcl_Obj* result; 
-  };
-  mutable vector<Condition_Feedback> itsFeedbacks;
-
-  mutable const char* itsRawResponse;
+  class Impl;
+  friend class KbdResponseHdlr::Impl;
+  Impl* const itsImpl;
 };
 
 static const char vcid_kbdresponsehdlr_h[] = "$Header$";
