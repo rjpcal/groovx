540d24647b1dd5b18f80ccc8c5445ad740ecb853
Robert J. Peters <rjpcal@gmail.com>
1999-11-10 20:43:38 +0000

    Reformatted comments for Doc++.

--- src/io/io.h
+++ src/io/io.h
@@ -3,15 +3,8 @@
 // io.h
 // Rob Peters 
 // created: Jan-99
-// written: Thu Oct 21 18:00:51 1999
+// written: Wed Nov 10 12:43:17 1999
 // $Id$
-//
-// This file defines the IO abstract interface. This interface
-// provides the framwork necessary for object I/O. Classes which need
-// to be serialized/deserialized should inherit virtually from
-// IO. Also defined in this file are the various exception types that
-// may be thrown in serialize and deserialize methods. All IO-related
-// exception classes are derived from IoError.
 //
 ///////////////////////////////////////////////////////////////////////
 
@@ -41,60 +34,87 @@ class Writer;
 class type_info;
 
 ///////////////////////////////////////////////////////////////////////
-//
-// IO class -- abstract interface definition
-//
-///////////////////////////////////////////////////////////////////////
-
+/**
+ *
+ * The IO class defines the abstract interface for object
+ * input/output. Classes which need to be read/written
+ * should inherit virtually from IO.
+ *
+ * @memo The IO class defines the abstract interface for object
+ * input/output.
+ **/
 class IO {
 public:
-  // The following flags may be OR'ed together and passed to the flag
-  // argument of serialize/deserialize to control different aspects of
-  // the formatting used to read and write objects. In general, the
-  // same flags must be used to read an object as were used to write
-  // it.
-  typedef int IOFlag;
-  static const IOFlag NO_FLAGS   = 0;
-  static const IOFlag TYPENAME   = 1 << 0; // The class's name is written/read
-  static const IOFlag BASES      = 1 << 1; // The class's bases is written/read
 
-  // A default separator to be used between elements in a serialized object.
+  /**@name IO flags
+	*
+   * The following flags may be OR'ed together and passed to the flag
+   * argument of serialize/deserialize to control different aspects of
+   * the formatting used to read and write objects. In general, the
+   * same flags must be used to read an object as were used to write
+   * it.
+	*
+	**/
+  //@{  
+  ///
+  typedef int IOFlag;
+  ///
+  static const IOFlag NO_FLAGS   = 0;
+  /// The class's name is written/read
+  static const IOFlag TYPENAME   = 1 << 0;
+  /// The class's bases are written/read
+  static const IOFlag BASES      = 1 << 1;
+  //@}
+
+  /// A default separator to be used between elements in a serialized object.
   static const char SEP = ' ';
 
+  /// Default constructor
   IO();
+
+  ///
   virtual ~IO() = 0;
 
-  // "Old" IO functions
+  /**@name  "Old" IO functions  */
+  //@{
+  ///
   virtual void serialize(ostream& os, IOFlag flag) const;
+  ///
   virtual void deserialize(istream& is, IOFlag flag);
-
+  ///
   virtual int charCount() const;
+  //@}
 
-  // "New" IO functions
+  /**@name  "New" IO functions  */
+  //@{
+  ///
   virtual void readFrom(Reader* reader) = 0;
+  ///
   virtual void writeTo(Writer* writer) const = 0;
-
-  // Returns a unique id for this object
+  //@}
+  
+  /// Returns a unique id for this object
   unsigned long id() const;
 
-  // Returns a string specifying the typename of the actual
-  // object. The implementation provided by IO returns a demangled
-  // version of typeid(*this).name(), which should very closely
-  // resemble the way the object was declared in source code.
+  /** Returns a string specifying the typename of the actual
+		object. The implementation provided by IO returns a demangled
+		version of typeid(*this).name(), which should very closely
+		resemble the way the object was declared in source code. */
   virtual string ioTypename() const;
 
-  // This function removes all whitespace characters from the istream,
-  // until the first non-whitespace character is seen. It returns the
-  // number of whitespace characters that were removed from the stream.
+  /** This function removes all whitespace characters from the
+		istream, until the first non-whitespace character is seen. It
+		returns the number of whitespace characters that were removed
+		from the stream. */
   static int eatWhitespace(istream& is);
 
-  // This function reads the next word from the istream is, and
-  // compares it to the correct names in correctNames. correctNames
-  // should consist of one or more space-separated words. If the word
-  // read from the stream does not match any of these words, and
-  // doCheck is true, an InputError is thrown with the last word in
-  // correctNames as its info. If doCheck is false, the function does
-  // nothing except read a word from the istream.
+  /** This function reads the next word from the istream is, and
+      compares it to the correct names in correctNames. correctNames
+      should consist of one or more space-separated words. If the word
+      read from the stream does not match any of these words, and
+      doCheck is true, an InputError is thrown with the last word in
+      correctNames as its info. If doCheck is false, the function does
+      nothing except read a word from the istream. */
   static void readTypename(istream& is,
 									const string& correctNames,
 									bool doCheck = true);
@@ -103,61 +123,100 @@ private:
   unsigned long itsId;
 };
 
-// This template function returns the number of characters needed to
-// write a type using the << insertion operator. It is instantiated
-// for the basic types.
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * This template function returns the number of characters needed to
+ * write a type using the << insertion operator. It is instantiated
+ * for the basic types.
+ *
+ * @memo Gives the number of characters for a string representation of
+ * a basic type.
+ **/
 template<class T>
 int gCharCount(T val);
 
 ///////////////////////////////////////////////////////////////////////
-//
-// IO exception classes
-//
-///////////////////////////////////////////////////////////////////////
+/**
+ *
+ *@name  IO exception classes
+ *
+ * Defined here are the various exception types that may be thrown in
+ * IO's input and output methods. All IO-related exception classes are
+ * derived from IoError.
+ *
+ **/
+//@{
 
+///
 class IoError : public ErrorWithMsg {
 public:
+  ///
   IoError();
+  ///
   IoError(const string& str);
+  ///
   IoError(const type_info& ti);
 protected:
+  ///
   virtual void setMsg(const string& str);
+  ///
   virtual void setMsg(const type_info& ti);
 };
 
+///
 class InputError : public IoError {
 public:
+  ///
   InputError() : IoError() {}
+  ///
   InputError(const string& str) { setMsg(str); }
+  ///
   InputError(const type_info& ti) { setMsg(ti); }
 };
 
+///
 class OutputError : public IoError {
 public:
+  ///
   OutputError() : IoError() {}
+  ///
   OutputError(const string& str) { setMsg(str); }
+  ///
   OutputError(const type_info& ti) { setMsg(ti); }
 };
 
+///
 class IoLogicError : public IoError {
 public:
+  ///
   IoLogicError() : IoError() {}
+  ///
   IoLogicError(const string& str) { setMsg(str); }
+  ///
   IoLogicError(const type_info& ti) { setMsg(ti); }
 };
 
+///
 class IoValueError : public IoError {
 public:
+  ///
   IoValueError() : IoError() {}
+  ///
   IoValueError(const string& str) { setMsg(str); }
+  ///
   IoValueError(const type_info& ti) { setMsg(ti); }
 };
 
+///
 class IoFilenameError : public IoError {
 public:
+  ///
   IoFilenameError() : IoError() {}
+  ///
   IoFilenameError(const string& filename) { setMsg(filename); }
 };
+//@}
 
 static const char vcid_io_h[] = "$Header$";
 #endif // !IO_H_DEFINED
--- src/visx/grobj.h
+++ src/visx/grobj.h
@@ -3,16 +3,8 @@
 // grobj.h
 // Rob Peters 
 // created: Dec-98
-// written: Tue Nov  2 14:18:09 1999
+// written: Wed Nov 10 12:41:51 1999
 // $Id$
-//
-// This is the abstract base class for graphic objects. GrObj*'s may
-// be stored and manipulated in ObjList's. Subclasses derived from
-// GrObj must specify the details of how their objects will be drawn,
-// by overriding the virtual functions grRender() and grUnRender(),
-// and/or by choosing appropriate render/unrender modes with
-// setRenderMode() and setUnRenderMode(). Public clients call draw()
-// or undraw() to display or hide the object.
 //
 ///////////////////////////////////////////////////////////////////////
 
@@ -34,8 +26,23 @@
 class GrObjImpl;
 
 ///////////////////////////////////////////////////////////////////////
-// GrObj abstract class definition
-///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * This is the abstract base class for graphic objects. GrObj*'s may
+ * be stored and manipulated in ObjList's. Subclasses derived from
+ * GrObj must specify the details of how their objects will be drawn,
+ * by overriding the virtual functions grRender() and grUnRender(),
+ * and/or by choosing appropriate render/unrender modes with
+ * setRenderMode() and setUnRenderMode(). Public clients call draw()
+ * or undraw() to display or hide the object. The caches that mediate
+ * the various drawing modes are kept up to date by using the
+ * Observable interface. Thus, whenever a manipulator changes a
+ * parameter in a derived class, it should also call
+ * Observable::sendStateChangeMsg() to indicate that an update is
+ * needed.
+ *
+ * @memo Abstract base class for graphic objects.
+ **/
 
 class GrObj : public virtual Observable, 
 				  public virtual Observer,
@@ -43,172 +50,235 @@ class GrObj : public virtual Observable,
 {
 public:
 
-  /////////////////////
-  // rendering modes //
-  /////////////////////
+  /**@name   Rendering modes
+	*
+	* These symbolic constants can be used to get/set the rendering or
+	* unrendering modes with get/setRenderMode() or
+	* get/setUnRednerMode(). The default rendering mode is
+	* GROBJ_GL_COMPILE, and the default unrendering mode is
+	* GROBJ_SWAP_FORE_BACK.
+	*
+	**/
+  //@{
+  ///
   typedef int GrObjRenderMode;
 
+  /** In this mode, grRender() will be called every time the object is
+		drawn, and grUnRender() will be called every time the object is
+		undrawn.  */
   static const GrObjRenderMode GROBJ_DIRECT_RENDER = 1;
-  // In this mode, grRender() will be called every time the object is
-  // drawn, and grUnRender() will be called every time the object is
-  // undrawn.
 
+  /**@name   Caching rendering modes
+	*
+   * In these modes, grRender() is called on an draw request only if
+   * the object's state has changed since the last draw request. If it
+   * has not changed, then the object is instead rendered from either
+   * 1) a cached OpenGL display list, 2) a cached OpenGL bitmap, or 3)
+   * a cached X11 bitmap. The GROBJ_GL_COMPILE will function exactly
+   * the same as GROBJ_DIRECT_RENDER, except that drawing should be
+   * faster when the object's state is unchanged. The bitmap cache
+   * modes may also yield a speed increase, but carry the following
+   * caveats: 1) the object's size will not respond to OpenGL scaling
+   * or rotation requests, but will be fixed at the screen size at
+   * which the object was most recently direct-rendered; note that
+   * OpenGL translation requests will still work properly, and 2) in
+   * the X11 bitmap mode, the object will always be rendered into the
+   * front buffer, regardless of whether OpenGL double-buffering is
+   * being used.
+	**/
+  //@{
+  ///
   static const GrObjRenderMode GROBJ_GL_COMPILE = 2;
+  ///
   static const GrObjRenderMode GROBJ_GL_BITMAP_CACHE = 3;
+  ///
   static const GrObjRenderMode GROBJ_X11_BITMAP_CACHE = 4;
-  // In these mode, grRender() is called on an draw request only if
-  // the object's state has changed since the last draw request. If it
-  // has not changed, then the object is instead rendered from either
-  // 1) a cached OpenGL display list, 2) a cached OpenGL bitmap, or 3)
-  // a cached X11 bitmap. The GROBJ_GL_COMPILE will function exactly
-  // the same as GROBJ_DIRECT_RENDER, except that drawing should be
-  // faster when the object's state is unchanged. The bitmap cache
-  // modes may also yield a speed increase, but carry the following
-  // caveats: 1) the object's size will not respond to OpenGL scaling
-  // or rotation requests, but will be fixed at the screen size at
-  // which the object was most recently direct-rendered; note that
-  // OpenGL translation requests will still work properly, and 2) in
-  // the X11 bitmap mode, the object will always be rendered into the
-  // front buffer, regardless of whether OpenGL double-buffering is
-  // being used.
+  //@}
 
+  /** This mode may be used only as an unrendering mode. If selected,
+		unrendering will be done by performing a normal render except
+		with the foreground and background colors swapped. */
   static const GrObjRenderMode GROBJ_SWAP_FORE_BACK = 5;
-  // This mode may be used only as an unrendering mode. If selected,
-  // unrendering will be done by performing a normal render except
-  // with the foreground and background colors swapped.
 
+  /** This mode may be used only as an unrendering mode. If selected,
+		unrendering will be done by clearing to the background color the
+		region enclosed by the object's bounding box. */
   static const GrObjRenderMode GROBJ_CLEAR_BOUNDING_BOX = 6;
-  // This mode may be used only as an unrendering mode. If selected,
-  // unrendering will be done by clearing to the background color the
-  // region enclosed by the object's bounding box.
+
+  //@}
 
 
-  ///////////////////
-  // scaling modes //
-  ///////////////////
+  /**@name  Scaling modes   */
+  //@{
 
+  ///
   typedef int ScalingMode;
 
-  static const int NATIVE_SCALING           = 1; // default
+  /// This is the default mode
+  static const int NATIVE_SCALING           = 1;
+  ///
   static const int MAINTAIN_ASPECT_SCALING  = 2;
+  ///
   static const int FREE_SCALING             = 3;
 
+  //@}
 
-  /////////////////////
-  // alignment modes //
-  /////////////////////
+  /**@name  Alignment modes   */
+  //@{
 
+  ///
   typedef int AlignmentMode;
 
+  ///  
   static const int NATIVE_ALIGNMENT      = 1; // default
+  ///  
   static const int CENTER_ON_CENTER      = 2;
+  ///  
   static const int NW_ON_CENTER          = 3;
+  ///  
   static const int NE_ON_CENTER          = 4;
+  ///  
   static const int SW_ON_CENTER          = 5;
+  ///  
   static const int SE_ON_CENTER          = 6;
+  ///  
   static const int ARBITRARY_ON_CENTER   = 7;
-
+  //@}
 
   //////////////
   // creators //
   //////////////
 
+  /// Default constructor
   GrObj(GrObjRenderMode render_mode = GROBJ_GL_COMPILE,
 		  GrObjRenderMode unrender_mode = GROBJ_SWAP_FORE_BACK);
-  // Default constructor.
 
+  /// Construct from an istream by using deserialize.
   GrObj(istream &is, IOFlag flag);
-  // Construct from an istream by using deserialize.
 
+  ///
   virtual ~GrObj();
-  // Frees the OpenGL display list.
 
+  ///
   virtual void serialize(ostream &os, IOFlag flag) const;
+  ///
   virtual void deserialize(istream &is, IOFlag flag);
-  // These functions write the object's state from/to an output/input
-  // stream. Both functions are defined, but are no-ops for GrObj.
 
+  ///
   virtual int charCount() const;
 
+  ///
   virtual void readFrom(Reader* reader);
+  ///
   virtual void writeTo(Writer* writer) const;
 
   ///////////////
   // accessors //
   ///////////////
 
+  ///
   bool getBBVisibility() const;
 
+  /** This will return the bounding box given by grGetBoundingBox(),
+		except that those values will be modified to reflect internal
+		scaling, translation, and pixel border values. */
   bool getBoundingBox(double& left, double& top,
 							 double& right, double& bottom) const;
-  // This will return the bounding box given by grGetBoundingBox(),
-  // except that those values will be modified to reflect internal
-  // scaling, translation, and pixel border values.
 
+  /** Subclasses may override this function to fill in the parameters
+		with the bounding box in GL coordinates for the object's onscreen
+		image. The function returns true if a bounding box has provided,
+		or false if no bounding box is available. The default
+		implementation provided by GrObj returns false. */
   protected: virtual bool grGetBoundingBox(double& left, double& top,
 														 double& right, double& bottom,
 														 int& border_pixels) const;
-  // Subclasses may override this function to fill in the parameters
-  // with the bounding box in GL coordinates for the object's onscreen
-  // image. The function returns true if a bounding box has provided,
-  // or false if no bounding box is available. The default
-  // implementation provided by GrObj returns false.
 
 public:
+  ///
   ScalingMode getScalingMode() const;
+  ///
   double getWidth() const;
+  ///
   double getHeight() const;
+  ///
   double getAspectRatio() const;
+  ///
   double getMaxDimension() const;
 
+  ///
   AlignmentMode getAlignmentMode() const;
+  ///
   double getCenterX() const;
+  ///
   double getCenterY() const;
 
+  /** getCategory and setCategory are used to manipulate some
+		user-defined categories that can be used to classify different
+		GrObj objects. */
   virtual int getCategory() const;
-  // getCategory and setCategory are used to manipulate some
-  // user-defined categories that can be used to classify different
-  // GrObj objects.
 
+  ///
   GrObjRenderMode getRenderMode() const;
+  ///
   GrObjRenderMode getUnRenderMode() const;
 
+  /**@name Screen-World coordinate conversion
+	*
+   * These two functions convert between world coordinates (OpenGL
+   * object coordinates) and screen/window coordinates. Normally the
+   * OpenGL state (modelview matrix, projection matrix, and viewport)
+   * is re-queried each time, but if it is known that the OpenGL state
+   * has not changed between calls to these functions, then some speed
+   * can be gained by passing recalculate_state as false.
+	*/
+  //@{
+  ///
   static void getScreenFromWorld(double world_x, double world_y,
 											int& screen_x, int& screen_y,
 											bool recalculate_state = true);
+  ///
   static void getWorldFromScreen(int screen_x, int screen_y,
 											double& world_x, double& world_y,
 											bool recalculate_state = true);
-  // These two functions convert between world coordinates (OpenGL
-  // object coordinates) and screen/window coordinates. Normally the
-  // OpenGL state (modelview matrix, projection matrix, and viewport)
-  // is re-queried each time, but if it is known that the OpenGL state
-  // has not changed between calls to these functions, then some speed
-  // can be gained by passing recalculate_state as false.
-
+  //@}
 
   //////////////////
   // manipulators //
   //////////////////
 
+  ///
   void setBBVisibility(bool visibility);
 
+  ///
   void setScalingMode(ScalingMode val);
+  ///
   void setWidth(double val);
+  ///
   void setHeight(double val);
+  ///
   void setAspectRatio(double val);
+  ///
   void setMaxDimension(double val);
 
+  ///
   void setAlignmentMode(AlignmentMode val);
+  ///
   void setCenterX(double val);
+  ///
   void setCenterY(double val);
 
+  ///
   virtual void setCategory(int val);
   
+  ///
   void setRenderMode(GrObjRenderMode mode);
+  ///
   void setUnRenderMode(GrObjRenderMode mode);
 
+  ///
   virtual void receiveStateChangeMsg(const Observable* obj);
+  ///
   virtual void receiveDestroyMsg(const Observable* obj);
 
 
@@ -216,52 +286,48 @@ public:
   // actions //
   /////////////
 
+  /** Swaps the foreground and background colors, in a way that is
+      appropriate to color-index/RGBA modes. In order to be able to
+      unswap the colors, this function should normally be preceded by
+      glPushAttrib(GL_CURRENT_BIT | GL_COLOR_BUFFER_BIT), so that a
+      call to glPopAttrib() will then unswap the colors. */
   static void swapForeBack();
-  // Swaps the foreground and background colors, in a way that is
-  // appropriate to color-index/RGBA modes. In order to be able to
-  // unswap the colors, this function should normally be preceded by
-  // glPushAttrib(GL_CURRENT_BIT | GL_COLOR_BUFFER_BIT), so that a
-  // call to glPopAttrib() will then unswap the colors.
 
+  /** Recompiles the OpenGL display list or bitmap cache, if there are
+		changes pending, otherwise does nothing. */
   void update() const;
-  // Recompiles the OpenGL display list or bitmap cache, if there are
-  // changes pending, otherwise does nothing.
 
+  /** This function draws the object according to the GrRenderMode
+		selected with setRenderMode(). */
   void draw() const;
-  // This function draws the object according to the GrRenderMode
-  // selected with setRenderMode().
 
+  /** This function undraws the object according to the GrRenderMode
+		selected with setUnRenderMode(). */
   void undraw() const;
-  // This function undraws the object according to the GrRenderMode
-  // selected with setUnRenderMode().
 
 protected:
+  ///
   void grDrawBoundingBox() const;
 
+  /** This function must be overridden in derived classes to execute
+		the actual OpenGL commands that render the object. */
   virtual void grRender() const = 0;
-  // This function must be overridden in derived classes to execute
-  // the actual OpenGL commands that render the object.
 
+  /** This function will be called if the unrendering mode is set to
+		GROBJ_DIRECT_RENDER or to any of the compile or cache modes. The
+		default implementation provided by GrObj does nothing. */
   virtual void grUnRender() const;
-  // This function will be called if the unrendering mode is set to
-  // GROBJ_DIRECT_RENDER or to any of the compile or cache modes. The
-  // default implementation provided by GrObj does nothing.
 
+  /** This function queries whether the GrObj's cached representation
+		is in correspondence with all of its parameters. */
   bool grIsCurrent() const;
-  void grPostUpdated() const;
-  // These functions manipulate whether the GrObj is considered
-  // current: the GrObj is current if its cached OpenGL display list
-  // or bitmap is in correspondence with all of its parameters. Thus,
-  // whenever a manipulator changes a parameter in a derived class, it
-  // should also call sendStateChangeMsg to indicate that an update is
-  // needed.
 
+  /** This function returns the id of the OpenGL display list that
+		should be compiled in grRecompile. */
   int grDisplayList() const;
-  // This function returns the id of the OpenGL display list that
-  // should be compiled in grRecompile.
 
-  friend class GrObjImpl;
 private:
+  friend class GrObjImpl;
   GrObjImpl* const itsImpl;	  // opaque pointer to implementation
 };
 
