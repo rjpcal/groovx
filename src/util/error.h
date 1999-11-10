30aae9da0be0c3c293c312ebd8d5c3de5fed48f6
Robert J. Peters <rjpcal@gmail.com>
1999-11-10 20:15:29 +0000

    Reformatted comments for Doc++.

--- src/util/error.h
+++ src/util/error.h
@@ -3,7 +3,7 @@
 // error.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Jun 22 14:59:47 1999
-// written: Thu Jun 24 11:21:19 1999
+// written: Wed Nov 10 12:15:08 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -16,18 +16,26 @@
 #define STRING_DEFINED
 #endif
 
+/// The base class for all error classes
 class Error {
 public:
+  ///
   Error();
+  ///
   virtual ~Error();
 };
 
+/// A simple error class that carries a string message describing the error.
 class ErrorWithMsg : public virtual Error {
 public:
+  ///
   ErrorWithMsg() : Error() {}
+  ///
   ErrorWithMsg(const string& str);
+  ///
   virtual const string& msg() const;
 protected:
+  ///
   virtual void setMsg(const string& str);
 private:
   string itsInfo;
