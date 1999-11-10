fcb5e6ed1d97fb47751cb37c13168d75de6bdd58
Robert J. Peters <rjpcal@gmail.com>
1999-11-10 23:59:25 +0000

    Reformatted comments for Doc++.

--- src/property.h
+++ src/property.h
@@ -3,7 +3,7 @@
 // property.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Wed Sep 29 10:24:22 1999
-// written: Thu Oct 21 18:07:33 1999
+// written: Wed Nov 10 15:59:17 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -42,15 +42,20 @@
 #endif
 
 ///////////////////////////////////////////////////////////////////////
-//
-// Property abstract base class definition
-//
-///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * Property defines an interface for getting and setting Value's whose
+ * type is not statically known.
+ *
+ * @memo Abstract base class for get/set dynamically-typed Value's.
+ **/
 
 class Property {
 public:
+  ///
   template <class C> friend class PropFriend;
 
+  ///
   virtual ~Property();
 
 #ifndef GCC_COMPILER
@@ -59,34 +64,54 @@ protected:
 public:
 #endif
 
+  ///
   virtual void set(const Value& val) = 0;
+  ///
   virtual const Value& get() const = 0;
 };
 
+
 ///////////////////////////////////////////////////////////////////////
-//
-// TProperty template concrete class defintion
-//
-///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * TProperty provides a simple implementation of Property using the
+ * template class TValue. This provides the benefit that the owner of
+ * the TProperty will know the static type represented by its TValue,
+ * and can therefor treat the value efficiently as a native type. On
+ * the other hand, clients who do not want or need to know the real
+ * static type of the TValue can access the value through the more
+ * generic Property and Value interfaces. The native-type related
+ * functions are protected, but CTProperty allows friendship to be
+ * granted to one class (probably the owner of the property).
+ *
+ * @memo Simple implementation of Property using TValue template.
+ **/
 
 template <class T>
 class TProperty : public Property, public virtual IO {
 public:
+  ///
   TProperty(T init=T()) : itsVal(init) {}
 
+  ///
   template <class C> friend class PropFriend;
 
+  ///
   virtual void serialize(ostream& os, IOFlag) const 
 	 { os << itsVal.itsVal << ' '; }
+  ///
   virtual void deserialize(istream& is, IOFlag)
 	 { is >> itsVal.itsVal; }
 
+  ///
   virtual int charCount() const
 	 { return gCharCount<T>(itsVal.itsVal); }
 
+  ///
   virtual void readFrom(Reader* reader)
 	 { reader->readValue("value", itsVal.itsVal); }
 
+  ///
   virtual void writeTo(Writer* writer) const
 	 { writer->writeValue("value", itsVal.itsVal); }
   
@@ -96,15 +121,22 @@ protected:
 public:
 #endif
 
+  ///
   virtual void set(const Value& new_val) { new_val.get(itsVal.itsVal); }
+  ///
   virtual const Value& get() const { return itsVal; }
 
+  ///
   virtual void setNative(T new_val) { itsVal.itsVal = new_val; }
+  ///
   virtual T getNative() const { return itsVal.itsVal; }
 
+  /// Returns a reference to the stored value
   T& operator()() { return itsVal.itsVal; }
+  /// Returns a reference to the stored value
   const T& operator()() const { return itsVal.itsVal; }
 
+  /// The stored value
   TValue<T> itsVal;
 };
 
@@ -113,26 +145,44 @@ void TProperty<bool>::deserialize(istream& is, IOFlag) {
   int temp; is >> temp; itsVal.itsVal = bool(temp);
 }
 
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * CTProperty simply injects a friend declaration into the TProperty
+ * interface, so that the class that owns a TProperty can access its
+ * native-type interface.
+ *
+ * @memo Allows an owner class to have friendship of a TProperty.
+ **/
 template <class C, class T>
 class CTProperty : public TProperty<T> { 
 public:
+  ///
   CTProperty(T init = T()) : TProperty<T>(init) {}
 
 #ifndef GCC_COMPILER
+  ///
   friend class C;
 #endif
 
 };
 
 ///////////////////////////////////////////////////////////////////////
-//
-// TBoundedProperty template concrete class definition
-//
-///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * TBoundedProperty is just like TProperty except that it places
+ * limits its stored value to a specified range. The range is
+ * specified through the template arguments min, max and div, such
+ * that the acceptable range will be [min/div, max/div].
+ *
+ * @memo Like TProperty, but limits the range of the stored value.
+ **/
 
 template <class T, int min, int max, int div>
 class TBoundedProperty : public TProperty<T> {
 public:
+  ///
   TBoundedProperty(T init=T()) : TProperty<T>(init) {}
 
 #ifndef GCC_COMPILER
@@ -141,44 +191,67 @@ protected:
 public:
 #endif
 
+  ///
   virtual void set(const Value& new_val)
 	 {
 		T temp; new_val.get(temp);
 		if (temp >= min/T(div) && temp <= max/T(div))
 		  TProperty<T>::itsVal.itsVal = temp;
 	 }
+  ///
   virtual const Value& get() const { return TProperty<T>::itsVal; }
   
+  ///
   virtual void setNative(T new_val)
 	 {
 		if (new_val >= min/T(div) && new_val <= max/T(div))
 		  TProperty<T>::itsVal.itsVal = new_val;
 	 }
+  ///
   T virtual getNative() const { return TProperty<T>::itsVal.itsVal; }
 };
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * CTBoundedProperty simply injects a friend declaration into the
+ * TBoundedProperty interface, so that the class that owns a TProperty
+ * can access its native-type interface.
+ *
+ * @memo Allows an owner class to have friendship of a TBoundedProperty.
+ **/
 
 template <class C, class T, int min, int max, int div>
 class CTBoundedProperty : public TBoundedProperty<T, min, max, div> { 
 public:
+  ///
   CTBoundedProperty(T init = T()) : TBoundedProperty<T, min, max, div>(init) {}
 
 #ifndef GCC_COMPILER
+  ///
   friend class C;
 #endif
 
 };
 
+
 ///////////////////////////////////////////////////////////////////////
-//
-// TPtrProperty template concrete class definition
-//
-///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * TPtrProperty implements the property interface using a TValuePtr as
+ * its stored value. The TValuePtr can be reseated to point at
+ * different objects of the same native type.
+ *
+ * @memo A property whose stored value is a reseatable reference.
+ **/
 
 template <class T>
 class TPtrProperty : public Property, public virtual IO {
 public:
+  ///
   TPtrProperty(T& valRef) : itsVal(valRef) {}
 
+  ///
   template <class C> friend class PropFriend;
 
 #ifndef GCC_COMPILER
@@ -187,39 +260,62 @@ protected:
 public:
 #endif
 
+  ///
   void reseat(T& valRef) { itsVal.reseat(valRef); }
 
+  ///
   virtual void serialize(ostream& os, IOFlag) const 
 	 { os << itsVal() << ' '; }
+  ///
   virtual void deserialize(istream& is, IOFlag)
 	 { is >> itsVal(); }
 
+  ///
   virtual int charCount() const
 	 { return gCharCount<T>(itsVal()); }
 
+  ///
   virtual void readFrom(Reader* reader)
 	 { reader->readValue("value", itsVal()); }
 
+  ///
   virtual void writeTo(Writer* writer) const
 	 { writer->writeValue("value", itsVal()); }
   
 
+  ///
   virtual void set(const Value& new_val) { new_val.get(itsVal()); }
+  ///
   virtual const Value& get() const { return itsVal; }
 
+  ///
   virtual void setNative(T new_val) { itsVal() = new_val; }
+  ///
   virtual T getNative() const { return itsVal(); }
 
+  ///
   T& operator()() { return itsVal(); }
+  ///
   const T& operator()() const { return itsVal(); }
 
 private:
   TValuePtr<T> itsVal;
 };
 
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * CTPtrProperty simply injects a friend declaration into the
+ * TPtrProperty interface, so that the class that owns a TProperty can
+ * access its native-type interface.
+ *
+ * @memo Allows an owner class to have friendship of a TPtrProperty.
+ **/
+
 template <class C, class T>
 class CTPtrProperty : public TPtrProperty<T> {
 public:
+  ///
   CTPtrProperty(T& valRef) : TPtrProperty<T>(valRef) {}
 
 #ifndef GCC_COMPILER
@@ -229,40 +325,60 @@ public:
 };
 
 ///////////////////////////////////////////////////////////////////////
-//
-// PropFriend template class definition
-//
-///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * PropFriend is a mixin class that provides get/set functions for
+ * classes that own Property's. Additionally, there are get/setNative
+ * functions that can be used on TProperty's.
+ *
+ * @memo Mixin class to provide get/set functions for classes that own
+ * Property's.
+ **/
 
 template <class C>
 class PropFriend : public virtual Observable {
 public:
+  ///
   const Value& get(const Property C::* prop) const
 	 { return (static_cast<const C*>(this)->*prop).get(); }
 
+  ///
   void set(Property C::* prop, const Value& new_val)
 	 { (static_cast<C*>(this)->*prop).set(new_val); sendStateChangeMsg(); }
 
+  ///
   template <class T>
   T getNative(const TProperty<T> C::* prop) const
 	 { return (static_cast<const C*>(this)->*prop).getNative(); }
 
+  ///
   template <class T>
   void setNative(TProperty<T> C::* prop, T new_val)
 	 { (static_cast<C*>(this)->*prop).setNative(new_val); sendStateChangeMsg(); }
 };
 
+
 ///////////////////////////////////////////////////////////////////////
-//
-// PropertyInfo template class definition
-//
-///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * PropertyInfo stores ancillary information about a property, that
+ * might be useful, for example, in constructing a user interface to
+ * manipulate various Property's. It stores a human-readable name for
+ * the property, a pointer-to-member that refers to the property, as
+ * will as suggested minimum and maximum values for the Property's
+ * value. Finally, a startNewGroup member provides a simple way to
+ * segregate a list of Property's into groups.
+ *
+ * @memo Stores ancillary information about a property.
+ **/
 
 template <class C>
 class PropertyInfo {
 public:
+  ///
   typedef Property C::* PropPtr;
 
+  ///
   template <class T>
   PropertyInfo(const string& name_, PropPtr property_,
 					T min_, T max_, T res_, bool new_group = false) :
@@ -274,6 +390,7 @@ public:
 	 startNewGroup(new_group) 
   {}
   
+  ///
   PropertyInfo(const PropertyInfo& rhs) :
 	 name(rhs.name),
 	 property(rhs.property),
@@ -283,6 +400,7 @@ public:
 	 startNewGroup(rhs.startNewGroup)
   {}
 
+  ///
   PropertyInfo& operator=(const PropertyInfo& rhs) {
 	 name = rhs.name;
 	 property = rhs.property;
@@ -292,12 +410,18 @@ public:
 	 startNewGroup = rhs.startNewGroup;
   }
 
+  /// User-friendly name of the property
   string name;
+  /// Pointer-to-member that refers to the property
   PropPtr property;
-  auto_ptr<Value> min;			  // suggested minimum value
-  auto_ptr<Value> max;			  // suggested maximum value
-  auto_ptr<Value> res;			  // suggested resolution
-  bool startNewGroup;			  // whether this is the first of a new group
+  /// Suggested minimum value
+  auto_ptr<Value> min;
+  /// Suggested maximum value
+  auto_ptr<Value> max;
+  /// Suggested resolution
+  auto_ptr<Value> res;
+  /// True if this is the first in a new group of properties
+  bool startNewGroup;
 };
 
 static const char vcid_property_h[] = "$Header$";
