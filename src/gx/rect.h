5ebc505b113a6e375177fcd9fea11dfbb1701712
Robert J. Peters <rjpcal@gmail.com>
1999-11-15 22:08:03 +0000

    Made data members private, and added inline accessors for the data members.

--- src/gx/rect.h
+++ src/gx/rect.h
@@ -3,7 +3,7 @@
 // rect.h
 // Rob Peters
 // created: Jan-99
-// written: Thu Nov  4 09:12:56 1999
+// written: Mon Nov 15 14:07:31 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -30,10 +30,6 @@ public:
   Rect<V>& operator=(const Rect<V>& i)
 	 { l=i.l; t=i.t; r=i.r; b=i.b; return *this; }
 
-  // Data members
-  V l, t, r, b;
-
-
   // Accessors
   void getRectLTRB(V& L, V& T, V& R, V& B) const
         { L = l; T = t; R = r; B = b; }
@@ -58,6 +54,20 @@ public:
 
   void widenByStep(V step) { l -= step; r += step; }
   void heightenByStep(V step) { b -= step; t += step; }
+
+  V& left() { return l; }
+  V& right() { return r; }
+  V& bottom() { return b; }
+  V& top() { return t; }
+
+  const V& left() const { return l; }
+  const V& right() const { return r; }
+  const V& bottom() const { return b; }
+  const V& top() const { return t; }
+
+private:
+  // Data members
+  V l, t, r, b;
 };
 
 static const char vcid_rect_h[] = "$Header$";
