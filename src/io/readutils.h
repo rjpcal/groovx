7b7adee546fb60dc6f170f6f0af446bc6c3bb549
Robert J. Peters <rjpcal@gmail.com>
1999-11-16 22:41:58 +0000

    Initial revision

---
+++ src/io/readutils.h
@@ -0,0 +1,78 @@
+///////////////////////////////////////////////////////////////////////
+//
+// readutils.h
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Tue Nov 16 14:25:40 1999
+// written: Tue Nov 16 14:36:21 1999
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef READUTILS_H_DEFINED
+#define READUTILS_H_DEFINED
+
+#ifndef READER_H_DEFINED
+#include "reader.h"
+#endif
+
+class ReadUtils {
+public:
+
+  /** Provides a generic interface for handling containers, sequences,
+		etc. of value types. */
+  template <class Inserter, class T>
+  static void readValueSeq(Reader* reader,
+									const string& name, Inserter inserter, T* = 0) {
+	 int count = reader->readInt(name+"Count");
+
+	 for (int i = 0; i < count; ++i) {
+		T temp;
+		reader->readValue(name+makeNumberString(i), temp);
+		*inserter = temp;
+		++inserter;
+	 }
+  }
+
+  /** Provides a generic interface for handling containers, sequences,
+		etc. of Value subtypes. */
+  template <class Inserter, class T>
+  static void readValueObjSeq(Reader* reader,
+										const string& name, Inserter inserter, T* = 0) {
+	 int count = reader->readInt(name+"Count");
+
+	 for (int i = 0; i < count; ++i) {
+		T temp;
+		reader->readValueObj(name+makeNumberString(i), temp);
+		*inserter = temp;
+		++inserter;
+	 }
+  }
+
+  /** Provides a generic interface for handling containers, sequences,
+      etc. of IO objects */
+  template <class Inserter, class C>
+  static void readObjectSeq(Reader* reader,
+									 const string& name, Inserter inserter, C* /*dummy*/) {
+	 int count = reader->readInt(name+"Count");
+
+	 for (int i = 0; i < count; ++i) {
+		IO* io = reader->readObject(name+makeNumberString(i));
+
+		if (io == 0) {
+		  *inserter = static_cast<C*>(0);
+		}
+
+		else {
+		  C* obj = dynamic_cast<C*>(io);
+		  if (obj == 0) throw ReadError("failed cast in readObjectSeq");
+		  *inserter = obj;
+		}
+
+	 }
+  }
+  
+  static string makeNumberString(int number);
+};
+
+static const char vcid_readutils_h[] = "$Header$";
+#endif // !READUTILS_H_DEFINED
