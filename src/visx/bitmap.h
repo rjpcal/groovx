ae4524561fdd58943c52340ad64ffed3bc77288e
Robert J. Peters <rjpcal@gmail.com>
1999-11-21 04:03:07 +0000

    Reformatted comments for Doc++ documentation.

--- src/visx/bitmap.h
+++ src/visx/bitmap.h
@@ -3,7 +3,7 @@
 // bitmap.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Jun 15 11:30:24 1999
-// written: Mon Nov 15 15:27:00 1999
+// written: Sat Nov 20 20:00:51 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -29,59 +29,119 @@
 #define VECTOR_DEFINED
 #endif
 
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * Bitmap is an abstract subclass of GrObj that manages bitmap data,
+ * but does not implement a method for rendering the bitmap data to
+ * the screen, since there is typically more than one way to do
+ * this. Subclasses of Bitmap can implement a particular rendering
+ * method by overriding doRender() and doUndraw() appropriately.
+ *
+ * @memo Abstract subclass of GrObj for rendering bitmaps.
+ **/
+///////////////////////////////////////////////////////////////////////
+
 class Bitmap : public GrObj {
   // Creators
 public:
+  ///
   Bitmap();
+  /** Create a bitmap from a graphics file. Currently the only
+		supported file format is PBM (Portable BitMap). */
   Bitmap(const char* filename);
+  /// Construct a Bitmap by deserializing from a stream.
   Bitmap(istream& is, IOFlag flag);
 
   private: void init();
 
 public:
+  ///
   virtual ~Bitmap();
 
+  ///
   virtual void serialize(ostream& os, IOFlag flag) const;
+  ///
   virtual void deserialize(istream& is, IOFlag flag);
 
+  ///
   virtual int charCount() const;
 
+  ///
   virtual void readFrom(Reader* reader);
+  ///
   virtual void writeTo(Writer* writer) const;
 
   /////////////
   // actions //
   /////////////
 
-  public:    void loadPbmFile(const char* filename);
-  public:    void writePbmFile(const char* filename) const;
+public:
+  ///
+  void loadPbmFile(const char* filename);
+  ///
+  void writePbmFile(const char* filename) const;
 
-  public:    void grabScreenRect(int left, int top, int right, int bottom);
-  public:    void grabScreenRect(const Rect<int>& rect);
-  public:    void grabWorldRect(double left, double top,
-										  double right, double bottom);
-  public:    void grabWorldRect(const Rect<double>& rect);
-  // These routines grab pixels from a rectanglur area of the screen
-  // buffer into the Bitmap's pixel array. The coordinates of the
-  // rectangle may be specified in pixel values (ScreenRect) or in
-  // OpenGL coordinate values (WorldRect).
+  /** These routines grab pixels from a rectanglur area of the screen
+		buffer into the Bitmap's pixel array. The coordinates of the
+		rectangle may be specified in pixel values (ScreenRect) or in
+		OpenGL coordinate values (WorldRect). */
+  //@{
+  ///
+  void grabScreenRect(int left, int top, int right, int bottom);
+  ///
+  void grabScreenRect(const Rect<int>& rect);
+  ///
+  void grabWorldRect(double left, double top,
+							double right, double bottom);
+  ///
+  void grabWorldRect(const Rect<double>& rect);
+  //@}
 
-  protected: virtual void bytesChangeHook(unsigned char* /* theBytes */,
-														int /* width */,
-														int /* height */,
-														int /* bits_per_pixel */,
-														int /* byte_alignment */) {}
+  protected:
+  /** This operation is a hook that is called after every time that
+      the bitmap data change. The default implementation here is a
+      no-op, but subclasses may override if they need to perform
+      specific actions when that bitmap data change. */
+  virtual void bytesChangeHook(unsigned char* /* theBytes */,
+										 int /* width */,
+										 int /* height */,
+										 int /* bits_per_pixel */,
+										 int /* byte_alignment */) {}
 
-  public:    void flipContrast();
-  protected: void doFlipContrast();
+  public:
+  /** Flips the luminance contrast of the bitmap data, in a way that
+      may depend on the format of the bitmap data. */
+  void flipContrast();
 
-  public:    void flipVertical();
-  protected: void doFlipVertical();
+  protected:
+  void doFlipContrast();
 
-  public:    void center();
+  public:
+  /// Vertically inverts the image.
+  void flipVertical();
 
-  protected: virtual void grRender() const;
-  protected: virtual void doRender(unsigned char* /* bytes */,
+  protected:
+  void doFlipVertical();
+
+  public:
+  /** Centers the image so that its center coincides with the origin
+      in the graphics environment when it is rendered. */
+  void center();
+
+  protected:
+  /** Implements the GrObj rendering operation. This function
+      delegates the work to doRender(); therefore, subclasses of
+      Bitmap should not override grRender(), but should instead
+      override doRender(). */
+  virtual void grRender() const;
+
+  protected:
+  /** This is the operation that subclasses must implement to do the
+		actual rendering of the bitmap data. All of the information
+		needed to do the rendering is passed as arguments to the
+		function. */
+  virtual void doRender(unsigned char* /* bytes */,
 											  double /* x_pos */,
 											  double /* y_pos */,
 											  int /* width */,
@@ -91,8 +151,18 @@ public:
 											  double /* zoom_x */,
 											  double /* zoom_y */) const = 0;
 
-  public:    virtual void grUnRender() const;
-  protected: virtual void doUndraw(
+  public:
+  /** Implements the GrObj undrawing operation. This function
+      delegates the work to doUndraw(); therefore, subclasses of
+      Bitmap should not override grUnRender(), but should instead
+      override doUndraw(). */
+  virtual void grUnRender() const;
+
+  protected:
+  /** This is the operation that subclasses should override if they
+      need to provide a special unrendering method. A default no-op
+      implementation is provided. */
+  virtual void doUndraw(
 					  int /*winRasterX*/, int /*winRasterY*/,
 					  int /*winWidthX*/, int /*winHeightY*/) const;
 
@@ -100,39 +170,59 @@ public:
   // accessors //
   ///////////////
 
-  protected: virtual bool grGetBoundingBox(Rect<double>& bounding_box,
+  protected:
+  ///
+  virtual bool grGetBoundingBox(Rect<double>& bounding_box,
 														 int& border_pixels) const;
 
 public:
-  virtual int getCategory() const { return -1; }
-
+  /// Get the number of bytes of image data.
   int byteCount() const;
+  /// Get the number of bytes per row in the image data.
   int bytesPerRow() const;
 
+  /// Get the image's width in pixels.
   int width() const;
+  /// Get the image's height in pixels.
   int height() const;
 
+  /** Manipulate the coordinates of the image's raster position (this
+      specifies where the lower left corner of the image will
+      fall). */
+  //@{
+  ///
   double getRasterX() const;
+  ///
   double getRasterY() const;
-  double getZoomX() const;
-  double getZoomY() const;
-
-  bool getUsingZoom() const;
-
-  //////////////////
-  // manipulators //
-  //////////////////
-
-  virtual void setCategory(int) {}
-
+  ///
   void setRasterX(double val);
+  ///
   void setRasterY(double val);
-  void setZoomX(double val);
-  void setZoomY(double val);
+  //@}
 
+  /** Manipulate the zoom factors of the image. (NOTE: not all
+      subclasses may be able to support zooming.) */
+  //@{
+  ///
+  double getZoomX() const;
+  ///
+  double getZoomY() const;
+  ///
+  void setZoomX(double val);
+  ///
+  void setZoomY(double val);
+  //@}
+
+  /// Manipulate whether zooming should be used.
+  //@{
+  ///
+  bool getUsingZoom() const;
+  ///
   void setUsingZoom(bool val);
-  
+  //@}
+
 protected:
+  /// Return a pointer to the raw bitmap data.
   unsigned char* theBytes() const
 	 { return const_cast<unsigned char*>(&(itsBytes[0])); }
 
