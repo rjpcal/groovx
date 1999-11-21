f52a0e980bc032c0c6549f56fa811ecbc6e91347
Robert J. Peters <rjpcal@gmail.com>
1999-11-21 09:15:17 +0000

    Initial revision

---
+++ src/util/objdb.cc
@@ -0,0 +1,185 @@
+///////////////////////////////////////////////////////////////////////
+//
+// ioptrlist.cc
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Sun Nov 21 00:26:29 1999
+// written: Sun Nov 21 01:13:30 1999
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef IOPTRLIST_CC_DEFINED
+#define IOPTRLIST_CC_DEFINED
+
+#include "ioptrlist.h"
+
+#include <iostream.h>
+#include <typeinfo>
+#include <string>
+
+#include "demangle.h"
+#include "iomgr.h"
+#include "reader.h"
+#include "writer.h"
+
+#include "trace.h"
+#include "debug.h"
+
+IoPtrList::IoPtrList(int size) :
+  VoidPtrList(size)
+{
+DOTRACE("IoPtrList::IoPtrList");
+}
+
+void IoPtrList::serialize(ostream &os, IOFlag flag) const {
+DOTRACE("IoPtrList::serialize");
+  string ioTag = IO::ioTypename();
+
+  if (flag & BASES) { /* there are no bases to deserialize */ }
+
+  char sep = ' ';
+  if (flag & TYPENAME) { os << ioTag << sep; }
+
+  const vector<void*>& theVec = VoidPtrList::vec();
+
+  // itsVec: we will serialize only the non-null T*'s in
+  // itsVec. In order to correctly deserialize the object later, we
+  // must write both the size of itsVec (in order to correctly
+  // resize later), as well as the number of non-null objects that we
+  // serialize (so that deserialize knows when to stop reading).
+  os << theVec.size() << sep;
+
+  int num_non_null = VoidPtrList::count();
+  os << num_non_null << endl;
+
+  // Serialize all non-null ptr's.
+  int c = 0;
+  for (int i = 0; i < theVec.size(); ++i) {
+    if (theVec[i] != NULL) {
+      os << i << sep;
+      // we must serialize the typename since deserialize requires a
+      // typename in order to call the virtual constructor
+		IO* obj = fromVoidToIO(theVec[i]);
+      obj->serialize(os, flag|TYPENAME);
+      ++c;
+    }
+  }
+
+  if (c != num_non_null) {
+	 throw IoLogicError(ioTag);
+  }
+
+  // itsFirstVacant
+  os << firstVacant() << endl;
+  if (os.fail()) throw OutputError(ioTag);
+}
+
+
+void IoPtrList::deserialize(istream &is, IOFlag flag) {
+DOTRACE("IoPtrList::deserialize");
+ string ioTag = IO::ioTypename();
+
+  if (flag & BASES) { /* there are no bases to deserialize */ }
+  if (flag & TYPENAME) { 
+	 IO::readTypename(is, ioTag);
+  }
+
+  vector<void*>& theVec = VoidPtrList::vec();
+
+  // theVec
+  clear();
+  int size, num_non_null;
+  is >> size >> num_non_null;
+  // We must check if the istream has failed in order to avoid
+  // attempting to resize theVec to some crazy size.
+  if (is.fail()) throw InputError(ioTag);
+  if ( size < 0 || num_non_null < 0 || num_non_null > size ) {
+	 throw IoValueError(ioTag);
+  }
+  theVec.resize(size, NULL);
+  int ptrid;
+  string type;
+  for (int i = 0; i < num_non_null; ++i) {
+    is >> ptrid;
+	 if (ptrid < 0 || ptrid >= size) {
+		throw IoValueError(ioTag);
+	 }
+
+	 is >> type;
+
+	 IO* obj = IoMgr::newIO(type.c_str());
+	 if (!obj) throw InputError(ioTag);
+
+	 insertVoidPtrAt(ptrid, static_cast<void*>(obj));
+
+	 obj->deserialize(is, flag & ~IO::TYPENAME);
+  }
+  // itsFirstVacant
+  is >> VoidPtrList::firstVacant();
+  if (firstVacant() < 0) {
+	 throw IoValueError(ioTag);
+  }
+
+  // The next character after itsFirstVacant had better be a newline,
+  // and we need to remove it from the stream.
+  if ( is.get() != '\n' ) throw IoLogicError(ioTag);
+
+  if (is.fail()) throw InputError(ioTag);
+}
+
+
+int IoPtrList::charCount() const {
+DOTRACE("IoPtrList::charCount");
+  string ioTag = IO::ioTypename();
+  int ch_count = ioTag.size() + 1
+	 + gCharCount<int>(vec().size()) + 1;
+  int num_non_null = VoidPtrList::count();
+  ch_count += 
+	 gCharCount<int>(num_non_null) + 1;
+  
+  for (int i = 0; i < vec().size(); ++i) {
+	 if (vec()[i] != NULL) {
+		ch_count += gCharCount<int>(i) + 1;
+
+		IO* obj = fromVoidToIO(vec()[i]);
+		ch_count += obj->charCount() + 1;
+	 }
+  }
+
+  ch_count += gCharCount<int>(firstVacant()) + 1;
+  return ch_count + 5;
+}
+
+
+void IoPtrList::readFrom(Reader* reader) {
+DOTRACE("IoPtrList::readFrom");
+  firstVacant() = reader->readInt("itsFirstVacant");
+
+  vector<IO*> tempVec;
+
+  reader->readObjectSeq("itsVec", back_inserter(tempVec), (IO*) 0);
+
+  vec().clear();
+  vec().resize(tempVec.size());
+
+  for (int i = 0; i < tempVec.size(); ++i)
+	 if (tempVec[i] != 0)
+		vec()[i] = static_cast<void*>(tempVec[i]);
+}
+
+
+void IoPtrList::writeTo(Writer* writer) const {
+DOTRACE("IoPtrList::writeTo");
+  writer->writeInt("itsFirstVacant", firstVacant());
+
+  vector<IO*> ioVec(vec().size());
+
+  for (int i = 0; i < vec().size(); ++i)
+	 if ( vec()[i] != 0 )
+		ioVec[i] = fromVoidToIO(vec()[i]);
+
+  writer->writeObjectSeq("itsVec", ioVec.begin(), ioVec.end());
+}
+
+static const char vcid_ioptrlist_cc[] = "$Header$";
+#endif // !IOPTRLIST_CC_DEFINED
---
+++ src/util/objdb.h
@@ -0,0 +1,58 @@
+///////////////////////////////////////////////////////////////////////
+//
+// ioptrlist.h
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Sun Nov 21 00:26:29 1999
+// written: Sun Nov 21 01:15:14 1999
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef IOPTRLIST_H_DEFINED
+#define IOPTRLIST_H_DEFINED
+
+#ifndef VOIDPTRLIST_H_DEFINED
+#include "voidptrlist.h"
+#endif
+
+#ifndef IO_H_DEFINED
+#include "io.h"
+#endif
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * IoPtrList extends VoidPtrList by adding and implementing the IO
+ * interface. This class remains abstract, however, because the cast
+ * from void* to IO* is delegated to subclasses by way of the pure
+ * virtual function fromVoidToIO().
+ *
+ * @memo A subclass of VoidPtrList that adds IO capabilities.
+ **/
+///////////////////////////////////////////////////////////////////////
+
+class IoPtrList : public VoidPtrList, public virtual IO {
+public:
+  ///
+  IoPtrList(int size);
+
+  ///
+  virtual void serialize(ostream &os, IOFlag flag) const;
+  ///
+  virtual void deserialize(istream &is, IOFlag flag);
+
+  ///
+  virtual int charCount() const;
+
+  ///
+  virtual void readFrom(Reader* reader);
+  ///
+  virtual void writeTo(Writer* writer) const;
+
+protected:
+  /// Subclasses must override this to cast the void* to an IO*.
+  virtual IO* fromVoidToIO(void* ptr) const = 0;
+};
+
+static const char vcid_ioptrlist_h[] = "$Header$";
+#endif // !IOPTRLIST_H_DEFINED
