6e7f284b8eeb780501200bb74801e0a13e77ec4c
Robert J. Peters <rjpcal@gmail.com>
1999-11-21 06:03:34 +0000

    Reformatted comments for Doc++.

--- src/gfxattribs.h
+++ src/gfxattribs.h
@@ -1,39 +1,52 @@
 ///////////////////////////////////////////////////////////////////////
+//
 // gfxattribs.h
 // Rob Peters
 // created: Tue Mar  9 18:28:13 1999
-// written: Tue Jun 22 12:16:52 1999
+// written: Sat Nov 20 21:59:59 1999
 // $Id$
 //
-// This class containing only static members serves as a
-// platform-independent and low-level means for different parts of a
-// program to communicate about various properties of the local
-// graphics environment,
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef GFXATTRIBS_H_DEFINED
 #define GFXATTRIBS_H_DEFINED
 
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * The GfxAttribs class serves as a global repository for information
+ * about the graphics environment.
+ *
+ **/
+///////////////////////////////////////////////////////////////////////
+
 class GfxAttribs {
 public:
+  ///
   enum GfxFlag { NO_FLAGS               = 0,
                  RGBA_FLAG              = 1 << 0,
                  PRIVATE_CMAP_FLAG      = 1 << 1,
 					  DOUBLE_BUFFER_FLAG     = 1 << 2 };
 
+  ///
   static bool usingRgba() { return isTrue(RGBA_FLAG); }
+  ///
   static bool usingDoubleBuffer() { return isTrue(DOUBLE_BUFFER_FLAG); }
 
+  ///
   static void setFlagsIf(GfxFlag flags, bool cond) {
     if (cond) theFlags = GfxFlag(theFlags | flags);
     else theFlags = GfxFlag(theFlags & ~flags);
   }
 
+  ///
   static bool isTrue(GfxFlag flag) {
     return ((theFlags & flag) != 0);
   }
 
+  ///
   static double getScreenPpi() { return theScreenPpi; }
+  ///
   static void setScreenPpi(double f) { theScreenPpi = f; }
 
 private:
--- src/visx/glbitmap.h
+++ src/visx/glbitmap.h
@@ -3,7 +3,7 @@
 // glbitmap.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Wed Sep  8 11:02:30 1999
-// written: Wed Oct 20 10:23:38 1999
+// written: Sat Nov 20 22:03:29 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -20,41 +20,63 @@
 #define STRING_DEFINED
 #endif
 
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * GLBitmap is a subclass of Bitmap that renders the bitmap data using
+ * OpenGL's glDrawPixels() or glBitmap().
+ *
+ * @memo Bitmap subclass for rendering using OpenGL's glDrawPixels().
+ **/
+///////////////////////////////////////////////////////////////////////
+
 class GLBitmap : public Bitmap {
 public:
   // Creators
 
+  ///
   GLBitmap();
+  ///
   GLBitmap(const char* filename);
+  ///
   GLBitmap(istream& is, IOFlag flag);
 
   private: void init();
 
 public:
+  ///
   virtual ~GLBitmap();
 
+  ///
   virtual void serialize(ostream& os, IOFlag flag) const;
+  ///
   virtual void deserialize(istream& is, IOFlag flag);
 
+  ///
   virtual int charCount() const;
 
+  ///
   virtual void readFrom(Reader* reader);
+  ///
   virtual void writeTo(Writer* writer) const;
 
-  // Actions
-  protected: virtual void doRender(unsigned char* /* bytes */,
-											  double /* x_pos */,
-											  double /* y_pos */,
-											  int /* width */,
-											  int /* height */,
-											  int /* bits_per_pixel */,
-											  int /* byte_alignment */,
-											  double /* zoom_x */,
-											  double /* zoom_y */) const;
-
-  // Accessors/Manipulators
-  public:    bool getUsingGlBitmap() const;
-  public:    void setUsingGlBitmap(bool val);
+  protected:
+  ///
+  virtual void doRender(unsigned char* /* bytes */,
+								double /* x_pos */,
+								double /* y_pos */,
+								int /* width */,
+								int /* height */,
+								int /* bits_per_pixel */,
+								int /* byte_alignment */,
+								double /* zoom_x */,
+								double /* zoom_y */) const;
+  
+  public:
+  ///
+  bool getUsingGlBitmap() const;
+  ///
+  void setUsingGlBitmap(bool val);
   
 private:
   bool itsUsingGlBitmap;
