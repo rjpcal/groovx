06e55ba2ef74b65c4908d0cd21e40fbaf71a4008
Robert J. Peters <rjpcal@gmail.com>
1999-11-21 03:35:31 +0000

    Reformatted comments for Doc++ documentation.

--- src/visx/bezier.h
+++ src/visx/bezier.h
@@ -3,7 +3,7 @@
 // bezier.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Sep 21 09:51:40 1999
-// written: Wed Sep 22 12:01:36 1999
+// written: Sat Nov 20 19:25:53 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -22,105 +22,13 @@
 #endif
 
 ///////////////////////////////////////////////////////////////////////
-//
-// Bezier4 class -- specialized for four control points
-//
-///////////////////////////////////////////////////////////////////////
-
-class Bezier4 {
-private:
-  vector<double> R;				  // control points
-
-  // coefficients of the Bezier polynomial
-  // p(u) = c0[0] + c0[1]*u + c0[2]*u^2 + c0[3]*u^3
-  vector<double> c0;
-
-  // coefficients of the derivative polynomial
-  // p'(u) = c1[0] + c1[1]*u + c1[2]*u^2
-  vector<double> c1;
-
-  double b2_4ac;
-
-  double extremum1;
-  double extremum2;
-
-public:
-  Bezier4(double R0, double R1, double R2, double R3);
-
-  void setCtrlPnts(double R0, double R1, double R2, double R3);
-
-  double eval(double u);
-  double evalDeriv(double u);
-
-  double evalMax();
-  double evalMin();
-};
-
-Bezier4::Bezier4(double R0, double R1, double R2, double R3) :
-  R(4),
-  c0(4),
-  c1(3)
-{
-  setCtrlPnts(R0, R1, R2, R3);
-}
-
-void Bezier4::setCtrlPnts(double R0, double R1, double R2, double R3) {
-  R[0] = R0;
-  R[1] = R1;
-  R[2] = R2;
-  R[3] = R3;
-
-  c0[0] =    R[0];
-  c0[1] = -3*R[0] + 3*R[1];
-  c0[2] =  3*R[0] - 6*R[1] + 3*R[2];
-  c0[3] =   -R[0] + 3*R[1] - 3*R[2] + R[3];
-
-  for (int i = 0; i < c1.size(); ++i) {
-	 c1[i] = (i+1) * c0[i+1];
-  }
-
-  b2_4ac = c1[1]*c1[1] - 4*c1[0]*c1[2];
-
-  if (b2_4ac >= 0.0) {
-	 double part1 = -c1[1] / (2*c1[2]);
-	 double part2 = sqrt(b2_4ac) / (2*c1[2]);
-
-	 extremum1 = part1 + part2;
-	 if (extremum1 < 0.0) extremum1 = 0.0;
-	 if (extremum1 > 1.0) extremum1 = 1.0;
-
-	 extremum2 = part1 - part2;
-	 if (extremum2 < 0.0) extremum2 = 0.0;
-	 if (extremum2 > 1.0) extremum2 = 1.0;
-  }
-  else {
-	 extremum1 = 0.0;
-	 extremum2 = 1.0;
-  }
-}
-
-double Bezier4::eval(double u) {
-  return (c0[0] + c0[1]*u + c0[2]*u*u + c0[3]*u*u*u);
-}
-
-double Bezier4::evalDeriv(double u) {
-  return (c1[0] + c1[1]*u + c1[2]*u*u);
-}
-
-double Bezier4::evalMax() {
-  return max( max(eval(0.0), eval(1.0)),
-				  max(eval(extremum1), eval(extremum2)) );
-}
-
-double Bezier4::evalMin() {
-  return min( min(eval(0.0), eval(1.0)),
-				  min(eval(extremum1), eval(extremum2)) );
-}
-
-///////////////////////////////////////////////////////////////////////
-//
-// Bezier class --
-//
+/**
+ *
+ * Bezier is a general class for evaluating Bezier curves, their
+ * derivatives, and their minimum and maximum values.
+ *
+ * @memo General class for evaluating Bezier curves.
+ **/
 ///////////////////////////////////////////////////////////////////////
 
 class Bezier {
@@ -142,7 +50,7 @@ private:
   double valMin_;
   double valMax_;
 
-  int choose(int n, int k) {
+  static int choose(int n, int k) {
 	 // result = n! / [k!(n-k)!]
 	 int result = 1;
 
@@ -162,17 +70,34 @@ private:
   }
 
 public:
-  // If extrema_res is < 0, the default resolution will be used
+  /** Construct from a vector of control points. The argument
+      extrema_res is used to specify the number of subdivisions that
+      will be examined when searching for the extrema. If extrema_res
+      is < 0 (the default value), a default number of subdivisions
+      will be used. */
   Bezier(const vector<double>& RR, int extrema_res=-1);
 
+  /// Reset the control points,
   void setCtrlPnts(const vector<double>& RR, int extrema_res=-1);
 
+  
+  /// Evaluate the Bezier curve for a given u in [0, 1]
   double eval(double u);
+  /// Evaluate the derivative of the Bezier curve for a given u in [0, 1]
   double evalDeriv(double u);
 
+  /** Returns the value of u at which the Bezier curve takes its
+      maximum value. */
   double uMin() { return uMin_; }
+  /** Returns the value of u at which the Bezier curve takes its
+      minimum value. */
   double uMax() { return uMax_; }
+
+  /** Returns the maximum value that the Bezier curve will take in the
+      range u in [0, 1] */
   double valMin() { return valMin_; }
+  /** Returns the minimum value that the Bezier curve will take in the
+      range u in [0, 1] */
   double valMax() { return valMax_; }
 };
 
@@ -260,6 +185,115 @@ double Bezier::evalDeriv(double u) {
 	 result += c1[i] * pow(u,i);
   }
   return result;
+}
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * The Bezier4 class is specialized for evaluating Bezier curves with
+ * four control points.
+ *
+ * @memo Specialized class for evaluating Bezier curves with four
+ * control points.
+ **/
+///////////////////////////////////////////////////////////////////////
+
+class Bezier4 {
+private:
+  vector<double> R;				  // control points
+
+  // coefficients of the Bezier polynomial
+  // p(u) = c0[0] + c0[1]*u + c0[2]*u^2 + c0[3]*u^3
+  vector<double> c0;
+
+  // coefficients of the derivative polynomial
+  // p'(u) = c1[0] + c1[1]*u + c1[2]*u^2
+  vector<double> c1;
+
+  double b2_4ac;
+
+  double extremum1;
+  double extremum2;
+
+public:
+  /// Construct from four control points.
+  Bezier4(double R0, double R1, double R2, double R3);
+
+  /// Reset the control points.
+  void setCtrlPnts(double R0, double R1, double R2, double R3);
+
+  /// Evaluate the Bezier curve for a given u in [0, 1]
+  double eval(double u);
+  /// Evaluate the derivative of the Bezier curve for a given u in [0, 1]
+  double evalDeriv(double u);
+
+  /** Returns the maximum value that the Bezier curve will take in the
+      range u in [0, 1] */
+  double evalMax();
+  /** Returns the minimum value that the Bezier curve will take in the
+      range u in [0, 1] */
+  double evalMin();
+};
+
+Bezier4::Bezier4(double R0, double R1, double R2, double R3) :
+  R(4),
+  c0(4),
+  c1(3)
+{
+  setCtrlPnts(R0, R1, R2, R3);
+}
+
+void Bezier4::setCtrlPnts(double R0, double R1, double R2, double R3) {
+  R[0] = R0;
+  R[1] = R1;
+  R[2] = R2;
+  R[3] = R3;
+
+  c0[0] =    R[0];
+  c0[1] = -3*R[0] + 3*R[1];
+  c0[2] =  3*R[0] - 6*R[1] + 3*R[2];
+  c0[3] =   -R[0] + 3*R[1] - 3*R[2] + R[3];
+
+  for (int i = 0; i < c1.size(); ++i) {
+	 c1[i] = (i+1) * c0[i+1];
+  }
+
+  b2_4ac = c1[1]*c1[1] - 4*c1[0]*c1[2];
+
+  if (b2_4ac >= 0.0) {
+	 double part1 = -c1[1] / (2*c1[2]);
+	 double part2 = sqrt(b2_4ac) / (2*c1[2]);
+
+	 extremum1 = part1 + part2;
+	 if (extremum1 < 0.0) extremum1 = 0.0;
+	 if (extremum1 > 1.0) extremum1 = 1.0;
+
+	 extremum2 = part1 - part2;
+	 if (extremum2 < 0.0) extremum2 = 0.0;
+	 if (extremum2 > 1.0) extremum2 = 1.0;
+  }
+  else {
+	 extremum1 = 0.0;
+	 extremum2 = 1.0;
+  }
+}
+
+double Bezier4::eval(double u) {
+  return (c0[0] + c0[1]*u + c0[2]*u*u + c0[3]*u*u*u);
+}
+
+double Bezier4::evalDeriv(double u) {
+  return (c1[0] + c1[1]*u + c1[2]*u*u);
+}
+
+double Bezier4::evalMax() {
+  return max( max(eval(0.0), eval(1.0)),
+				  max(eval(extremum1), eval(extremum2)) );
+}
+
+double Bezier4::evalMin() {
+  return min( min(eval(0.0), eval(1.0)),
+				  min(eval(extremum1), eval(extremum2)) );
 }
 
 static const char vcid_bezier_h[] = "$Header$";
