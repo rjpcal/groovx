aeef9be723deedc75be8f59759d1f269a9a53189
Robert J. Peters <rjpcal@gmail.com>
1999-12-01 18:58:13 +0000

    Initial revision

---
+++ src/visx/experiment.h
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////
+//
+// experiment.h
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Wed Dec  1 10:52:17 1999
+// written: Wed Dec  1 10:56:47 1999
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef EXPERIMENT_H_DEFINED
+#define EXPERIMENT_H_DEFINED
+
+class Experiment {
+public:
+  virtual void draw() = 0;
+  virtual void undraw() = 0;
+  virtual void edSwapBuffers() = 0;
+
+  virtual void edBeginExpt() = 0;
+  virtual void edBeginTrial() = 0;
+  virtual void edResponseSeen() = 0;
+  virtual void edProcessResponse(int response) = 0;
+  virtual void edAbortTrial() = 0;
+  virtual void edEndTrial() = 0;
+  virtual void edHaltExpt() const = 0;
+  virtual void edResetExpt() = 0;
+};
+
+static const char vcid_experiment_h[] = "$Header$";
+#endif // !EXPERIMENT_H_DEFINED
