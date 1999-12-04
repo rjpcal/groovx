594ba97df84627f2c65c18815bfb81745e5293e3
Robert J. Peters <rjpcal@gmail.com>
1999-12-04 20:53:12 +0000

    Initial revision

---
+++ src/gwt/widget.cc
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////
+//
+// widget.cc
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Sat Dec  4 12:52:59 1999
+// written: Sat Dec  4 12:53:10 1999
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef WIDGET_CC_DEFINED
+#define WIDGET_CC_DEFINED
+
+#include "widget.h"
+
+Widget::~Widget() {}
+
+static const char vcid_widget_cc[] = "$Header$";
+#endif // !WIDGET_CC_DEFINED
---
+++ src/visx/experiment.cc
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////
+//
+// experiment.cc
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Sat Dec  4 12:50:51 1999
+// written: Sat Dec  4 12:51:20 1999
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef EXPERIMENT_CC_DEFINED
+#define EXPERIMENT_CC_DEFINED
+
+#include "experiment.h"
+
+Experiment::~Experiment() {}
+
+static const char vcid_experiment_cc[] = "$Header$";
+#endif // !EXPERIMENT_CC_DEFINED
