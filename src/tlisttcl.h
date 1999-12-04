f85923f4f7e506840a7f38d497b99fa84d23335d
Robert J. Peters <rjpcal@gmail.com>
1999-12-04 11:50:09 +0000

    writeResponsesProc has been removed (see TlistUtils for similar functionality).

--- src/tlisttcl.h
+++ src/tlisttcl.h
@@ -3,23 +3,13 @@
 // tlisttcl.h
 // Rob Peters
 // created: Sat Mar 13 12:37:22 1999
-// written: Thu Jun 24 20:15:08 1999
+// written: Sat Dec  4 03:49:47 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef TLISTTCL_H_DEFINED
 #define TLISTTCL_H_DEFINED
-
-struct Tcl_Interp;
-typedef int (Tcl_PackageInitProc) (Tcl_Interp* interp);
-struct Tcl_Obj;
-class Tlist;
-
-namespace TlistTcl {
-  // Exported procedures
-  void writeResponsesProc(const char* filename);
-}
 
 extern "C" Tcl_PackageInitProc Tlist_Init;
 
