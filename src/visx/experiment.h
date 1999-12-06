f20c6e3617f166bf1ef27c8c9b9e2ceb70d2e84d
Robert J. Peters <rjpcal@gmail.com>
1999-12-06 21:17:49 +0000

    Added comments formatted for Doxygen.

--- src/visx/experiment.h
+++ src/visx/experiment.h
@@ -3,7 +3,7 @@
 // experiment.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Wed Dec  1 10:52:17 1999
-// written: Sat Dec  4 12:51:32 1999
+// written: Mon Dec  6 13:14:41 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -13,26 +13,52 @@
 
 class Widget;
 
+/// Protocol class that represents psychophysical experiments.
 class Experiment {
 public:
   virtual ~Experiment();
 
   virtual Widget* getWidget() = 0;
+  ///< Return the Widget in which the experiment is running.
 
   virtual void edDraw() = 0;
+  ///< Draws the current trial onto the screen.
+
   virtual void edUndraw() = 0;
+  ///< Undraws the current trial from the screen.
+
   virtual void edSwapBuffers() = 0;
+  ///< Swaps the buffers in the Experiment's Widget.
 
   virtual void edBeginExpt() = 0;
+  ///< Begin the experiment.
+
   virtual void edBeginTrial() = 0;
+  ///< Begin the next trial.
+
   virtual void edResponseSeen() = 0;
+  ///< Tell the Experiment that a response has occurred.
+
   virtual void edProcessResponse(int response) = 0;
+  /**< Tell the Experiment to process a response.
+	    @param response An experiment-specific value representing the response. */
+
   virtual void edAbortTrial() = 0;
+  ///< Abort the current trial, and move on to the next trial.
+
   virtual void edEndTrial() = 0;
+  ///< End the current trial normally, and move on to the next trial.
+
   virtual void edHaltExpt() const = 0;
+  ///< Halt the experiment. No more trials will be begun.
+
   virtual void edResetExpt() = 0;
+  /**< Reset the Experiment, restoring it to a state in which no
+	    trials have been run. */
 
   virtual void edSetCurrentTrial(int trial) = 0;
+  /**< Changes the current trial that should be redrawn in the case
+       that a redraw or refresh event is sent to its Widget. */
 };
 
 static const char vcid_experiment_h[] = "$Header$";
