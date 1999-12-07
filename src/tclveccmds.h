4d2c679c9c1807c1c28fc78035062c09fbde5916
Robert J. Peters <rjpcal@gmail.com>
1999-12-07 20:53:34 +0000

    Moved the implementations over into the .cc file.

--- src/tclveccmds.h
+++ src/tclveccmds.h
@@ -3,7 +3,7 @@
 // tclveccmds.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Dec  7 12:11:41 1999
-// written: Tue Dec  7 12:15:38 1999
+// written: Tue Dec  7 12:29:47 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -17,6 +17,12 @@
 
 class TclItemPkg;
 
+template <class T> class Getter;
+template <class T> class Setter;
+template <class T> class Attrib;
+
+class Action;
+
 ///////////////////////////////////////////////////////////////////////
 /**
  *
@@ -29,44 +35,16 @@ template <class ValType>
 class TVecGetterCmd : public virtual TclCmd {
 public:
   TVecGetterCmd(TclItemPkg* pkg, const char* cmd_name, Getter<ValType>* getter,
-                const char* usage, int item_argn) :
-    TclCmd(pkg->interp(), cmd_name, 
-           usage ? usage : (item_argn ? "item_id(s)" : NULL), 
-           item_argn+1, item_argn+1),
-    itsPkg(pkg),
-    itsGetter(getter), 
-    itsItemArgn(item_argn) {}
+                const char* usage, int item_argn);
 
 protected:
-  virtual void invoke() {
-    vector<int> ids;
-    if (itsItemArgn) {
-      getSequenceFromArg(itsItemArgn, back_inserter(ids), (int*) 0);
-    }
-    else {
-      ids.push_back(-1);
-    }
+  virtual void invoke();
 
-    void* item = itsPkg->getItemFromId(ids[0]);
-    returnVal(itsGetter->get(item));
-
-    for (size_t i = 1; i < ids.size(); ++i) {
-      void* item = itsPkg->getItemFromId(ids[i]);
-      lappendVal(itsGetter->get(item));
-    }
-  }
 private:
   TclItemPkg* itsPkg;
   auto_ptr< Getter<ValType> > itsGetter;
   int itsItemArgn;
 };
-
-template<>
-void TVecGetterCmd<const string&>::invoke() {
-  int id = itsItemArgn ? getIntFromArg(itsItemArgn) : -1;
-  void* item = itsPkg->getItemFromId(id);
-  returnVal(itsGetter->get(item));
-}
 
 ///////////////////////////////////////////////////////////////////////
 /**
@@ -80,46 +58,10 @@ template <class T>
 class TVecSetterCmd : public virtual TclCmd {
 public:
   TVecSetterCmd(TclItemPkg* pkg, const char* cmd_name, Setter<T>* setter,
-                const char* usage, int item_argn) :
-    TclCmd(pkg->interp(), cmd_name, 
-           usage ? usage : (item_argn ? 
-                            "item_id(s) new_value(s)" : "new_value"), 
-           item_argn+2, item_argn+2),
-    itsPkg(pkg),
-    itsSetter(setter),
-    itsItemArgn(item_argn),
-    itsValArgn(item_argn+1) {}
+                const char* usage, int item_argn);
 
 protected:
-  virtual void invoke() {
-    vector<int> ids;
-    if (itsItemArgn) {
-      getSequenceFromArg(itsItemArgn, back_inserter(ids), (int*) 0);
-    }
-    else {
-      ids.push_back(-1);
-    }
-
-    vector<T> vals;
-	 if (ids.size() == 1) {
-		T val;
-		getValFromArg(itsValArgn, val);
-		vals.push_back(val);
-	 }
-	 else {
-		getSequenceFromArg(itsValArgn, back_inserter(vals), (T*) 0);
-	 }
-
-    size_t max_valn = vals.size()-1;
-
-    for (size_t i = 0, valn = 0; i < ids.size(); ++i) {
-      void* item = itsPkg->getItemFromId(ids[i]);
-      itsSetter->set(item, vals[valn]);
-      if (valn < max_valn) ++valn;
-    }
-
-    returnVoid();
-  }
+  virtual void invoke();
 
 private:
   TclItemPkg* itsPkg;
@@ -127,39 +69,6 @@ private:
   int itsItemArgn;
   int itsValArgn;
 };
-
-// Specialization for T=const string&, since we must declare 'vals' as
-// type 'vector<string>' rather than 'vector<const string&>'.
-template<>
-void TVecSetterCmd<const string&>::invoke() {
-    vector<int> ids;
-    if (itsItemArgn) {
-      getSequenceFromArg(itsItemArgn, back_inserter(ids), (int*) 0);
-    }
-    else {
-      ids.push_back(-1);
-    }
-
-    vector<string> vals;
-	 if (ids.size() == 1) {
-		string val;
-		getValFromArg(itsValArgn, val);
-		vals.push_back(val);
-	 }
-	 else {
-		getSequenceFromArg(itsValArgn, back_inserter(vals), (string*) 0);
-	 }
-
-    size_t max_valn = vals.size()-1;
-
-    for (size_t i = 0, valn = 0; i < ids.size(); ++i) {
-      void* item = itsPkg->getItemFromId(ids[i]);
-      itsSetter->set(item, vals[valn]);
-      if (valn < max_valn) ++valn;
-    }
-
-    returnVoid();
-}
 
 ///////////////////////////////////////////////////////////////////////
 /**
@@ -173,24 +82,12 @@ template <class T>
 class TVecAttribCmd : public TVecGetterCmd<T>, public TVecSetterCmd<T> {
 public:
   TVecAttribCmd(TclItemPkg* pkg, const char* cmd_name, Attrib<T>* attrib,
-                const char* usage, int item_argn) :
-    TVecGetterCmd<T>(pkg, 0, attrib, 0, item_argn),
-    TVecSetterCmd<T>(pkg, 0, attrib, 0, item_argn),
-    TclCmd(pkg->interp(), cmd_name,
-           usage ? usage : (item_argn ?
-                            "item_id(s) ?new_value(s)?" : "?new_value?"),
-           item_argn+1, item_argn+2, false),
-    itsObjcGet(item_argn+1),
-    itsObjcSet(item_argn+2) {}
+                const char* usage, int item_argn);
 
-  ~TVecAttribCmd() { DOTRACE("TVecAttribCmd::~TVecAttribCmd"); }
+  ~TVecAttribCmd();
 
 protected:
-  virtual void invoke() {
-    if      (TclCmd::objc() == itsObjcGet) { TVecGetterCmd<T>::invoke(); }
-    else if (TclCmd::objc() == itsObjcSet) { TVecSetterCmd<T>::invoke(); }
-    else    /* "can't happen" */           { Assert(0); }
-  }
+  virtual void invoke();
 
 private:
   int itsObjcGet;
@@ -208,29 +105,10 @@ private:
 class VecActionCmd : public TclCmd {
 public:
   VecActionCmd(TclItemPkg* pkg, const char* cmd_name, Action* action,
-               const char* usage, int item_argn) :
-    TclCmd(pkg->interp(), cmd_name, 
-           usage ? usage : (item_argn ? "item_id(s)" : NULL),
-           item_argn+1, item_argn+1),
-    itsPkg(pkg),
-    itsAction(action),
-    itsItemArgn(item_argn) {}
+               const char* usage, int item_argn);
 
 protected:
-  virtual void invoke() {
-    vector<int> ids;
-    if (itsItemArgn) {
-      getSequenceFromArg(itsItemArgn, back_inserter(ids), (int*) 0);
-    }
-    else {
-      ids.push_back(-1);
-    }
-
-    for (size_t i = 0; i < ids.size(); ++i) {
-      void* item = itsPkg->getItemFromId(ids[i]);
-      itsAction->action(item);
-    }
-  }
+  virtual void invoke();
 
 private:
   TclItemPkg* itsPkg;
