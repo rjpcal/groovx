916e094e13f138da7ed7b869ae928809c2735a88
Robert J. Peters <rjpcal@gmail.com>
1999-12-16 01:39:52 +0000

    Initial revision

---
+++ src/listpkg.cc
@@ -0,0 +1,90 @@
+///////////////////////////////////////////////////////////////////////
+//
+// listpkg.cc
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Wed Dec 15 17:27:51 1999
+// written: Wed Dec 15 17:36:48 1999
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef LISTPKG_CC_DEFINED
+#define LISTPKG_CC_DEFINED
+
+#include "listpkg.h"
+
+#include <vector>
+
+#include "trace.h"
+
+namespace Tcl {
+
+//---------------------------------------------------------------------
+//
+// GetValidIdsCmd --
+//
+//---------------------------------------------------------------------
+
+class GetValidIdsCmd : public TclItemCmd<IoPtrList> {
+public:
+  GetValidIdsCmd(TclItemPkg* pkg, const char* cmd_name) :
+	 TclItemCmd<IoPtrList>(pkg, cmd_name, NULL, 1, 1) {}
+protected:
+  virtual void invoke() {
+	 vector<int> ids;
+	 IoPtrList* theList = TclItemCmd<IoPtrList>::getItem();
+	 theList->insertValidIds(back_inserter(ids));
+	 TclCmd::returnSequence(ids.begin(), ids.end());
+  }
+};
+
+//---------------------------------------------------------------------
+//
+// IsValidIdCmd --
+//
+//---------------------------------------------------------------------
+
+class IsValidIdCmd : public TclItemCmd<IoPtrList> {
+public:
+  IsValidIdCmd(TclItemPkg* pkg, const char* cmd_name) :
+	 TclItemCmd<IoPtrList>(pkg, cmd_name, "item_id", 2, 2) {}
+protected:
+  virtual void invoke() {
+	 int id = TclCmd::getIntFromArg(1);
+	 IoPtrList* theList = TclItemCmd<IoPtrList>::getItem();
+	 TclCmd::returnBool(theList->isValidId(id));
+  }
+};
+
+} // end namespace Tcl
+
+Tcl::IoPtrListPkg::IoPtrListPkg(Tcl_Interp* interp, IoPtrList& aList,
+										  const char* pkg_name, const char* version) :
+  CTclIoItemPkg<IoPtrList>(interp, pkg_name, version, 0),
+  itsList(aList)
+{
+DOTRACE("Tcl::IoPtrListPkg::IoPtrListPkg");
+  declareGetter("count",
+					 new CGetter<IoPtrList, int>(&IoPtrList::count));
+  declareAction("reset",
+					 new CAction<IoPtrList>(&IoPtrList::clear));
+  declareSetter("remove",
+					 new CSetter<IoPtrList, int>(&IoPtrList::remove), "item_id");
+  addCommand( new GetValidIdsCmd(this, 
+											TclPkg::makePkgCmdName("getValidIds")) );
+  addCommand( new IsValidIdCmd(this,
+										 TclPkg::makePkgCmdName("isValidId")) );
+}	 
+
+IO& Tcl::IoPtrListPkg::getIoFromId(int) {
+DOTRACE("Tcl::IoPtrListPkg::getIoFromId");
+  return dynamic_cast<IO&>(itsList);
+}
+
+IoPtrList* Tcl::IoPtrListPkg::getCItemFromId(int) {
+DOTRACE("Tcl::IoPtrListPkg::getCItemFromId");
+  return &itsList;
+}
+
+static const char vcid_listpkg_cc[] = "$Header$";
+#endif // !LISTPKG_CC_DEFINED
