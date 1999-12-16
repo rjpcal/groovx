23b899d48bb6a2b321777556b9bd5f22f2661ecc
Robert J. Peters <rjpcal@gmail.com>
1999-12-16 01:53:39 +0000

    Removed unneeded PosListPkg... we can just use an IoPtrListPkg.

--- src/poslisttcl.cc
+++ src/poslisttcl.cc
@@ -3,7 +3,7 @@
 // poslisttcl.cc
 // Rob Peters
 // created: Sat Mar 13 12:46:09 1999
-// written: Wed Dec 15 17:43:40 1999
+// written: Wed Dec 15 17:52:55 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -11,37 +11,11 @@
 #ifndef POSLISTTCL_CC_DEFINED
 #define POSLISTTCL_CC_DEFINED
 
-#include <tcl.h>
-
 #include "poslist.h"
 #include "listpkg.h"
 
 #define NO_TRACE
 #include "trace.h"
-#define LOCAL_ASSERT
-#include "debug.h"
-
-///////////////////////////////////////////////////////////////////////
-//
-// Poslist Tcl package declarations
-//
-///////////////////////////////////////////////////////////////////////
-
-namespace PoslistTcl {
-  class PosListPkg;
-}
-
-///////////////////////////////////////////////////////////////////////
-//
-// PosListPkg class definition
-//
-///////////////////////////////////////////////////////////////////////
-
-class PoslistTcl::PosListPkg : public Tcl::IoPtrListPkg {
-public:
-  PosListPkg(Tcl_Interp* interp) :
-	 Tcl::IoPtrListPkg(interp, PosList::thePosList(), "PosList", "3.0") {}
-};
 
 //---------------------------------------------------------------------
 //
@@ -54,7 +28,8 @@ extern "C" Tcl_PackageInitProc Poslist_Init;
 int Poslist_Init(Tcl_Interp* interp) {
 DOTRACE("Poslist_Init");
 
-  new PoslistTcl::PosListPkg(interp);
+  new Tcl::IoPtrListPkg(interp, PosList::thePosList(),
+								"PosList", "$Revision$");
 
   return TCL_OK;
 }
