8159f455b6189f975ab5336ba7c2a51dba3ca92a
Robert J. Peters <rjpcal@gmail.com>
1999-12-16 23:42:49 +0000

    Moved the definition of TclItemPkg from here back to tclitempkg.  Now we define TclItemPkgBase, which declares only getItemFromId(). This avoids the previous cyclical dependency between tclitempkg and tclveccmds.

--- src/tclitempkgbase.h
+++ src/tclitempkgbase.h
@@ -3,7 +3,7 @@
 // tclitempkgbase.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Thu Dec 16 15:25:06 1999
-// written: Thu Dec 16 15:26:54 1999
+// written: Thu Dec 16 15:37:00 1999
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -46,54 +46,19 @@ public:
 namespace Tcl {
 
 ///////////////////////////////////////////////////////////////////////
-//
-// TclItemPkg class definition
-//
+/**
+ *
+ * TclItemPkgBase class definition
+ *
+ **/
 ///////////////////////////////////////////////////////////////////////
 
-class TclItemPkg : public TclPkg {
+class TclItemPkgBase : public TclPkg {
 public:
-  TclItemPkg(Tcl_Interp* interp, const char* name, const char* version,
-				 int item_argn=1) :
-	 TclPkg(interp, name, version), 
-	 itsItemArgn(item_argn) {}
+  TclItemPkgBase(Tcl_Interp* interp, const char* name, const char* version) :
+	 TclPkg(interp, name, version) {}
 
   virtual void* getItemFromId(int id) = 0;
-
-  int itemArgn() const { return itsItemArgn; }
-  
-protected:
-  template <class T>
-  void declareGetter(const char* cmd_name, Getter<T>* getter,
-							const char* usage = 0)
-  { declareGetter_(this, cmd_name, getter, usage); }
-  
-  template <class T>
-  void declareSetter(const char* cmd_name, Setter<T>* setter,
-							const char* usage = 0)
-  { declareSetter_(this, cmd_name, setter, usage); }
-
-  template <class T>
-  void declareAttrib(const char* attrib_name, Attrib<T>* attrib,
-							const char* usage = 0)
-  { declareAttrib_(this, attrib_name, attrib, usage); }
-
-  void declareAction(const char* action_name, Action* action,
-							const char* usage = 0);
-
-private:
-  template <class T>
-  static void declareGetter_(TclItemPkg*, const char*, Getter<T>*, const char*);
-
-  template <class T>
-  static void declareSetter_(TclItemPkg*, const char*, Setter<T>*, const char*);
-
-  template <class T>
-  static void declareAttrib_(TclItemPkg*, const char*, Attrib<T>*, const char*);
-
-  void instantiate();
-
-  int itsItemArgn;
 };
 
 } // end namespace Tcl
