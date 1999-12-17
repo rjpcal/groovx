0e98a08e96e497fc4f7caf592c979a0d5ff76b7e
Robert J. Peters <rjpcal@gmail.com>
1999-12-17 01:55:08 +0000

    Replaced KbdRh error messages with the EventRh:: error messages that they now trigger due to delegation.

--- testing/kbdrh_test.tcl
+++ testing/kbdrh_test.tcl
@@ -20,10 +20,10 @@ RhList::reset
 set rhid [KbdRh::KbdRh]
 test "KbdRhTcl-KbdRh::useFeedback" "too few args" {
 	 KbdRh::useFeedback
-} {^wrong \# args: should be "KbdRh::useFeedback item_id\(s\) \?new_value\(s\)\?"$}
+} {^wrong \# args: should be "EventRh::useFeedback item_id\(s\) \?new_value\(s\)\?"$}
 test "KbdRhTcl-KbdRh::useFeedback" "too many args" {
 	 KbdRh::useFeedback junk junk junk
-} {^wrong \# args: should be "KbdRh::useFeedback item_id\(s\) \?new_value\(s\)\?"$}
+} {^wrong \# args: should be "EventRh::useFeedback item_id\(s\) \?new_value\(s\)\?"$}
 test "KbdRhTcl-KbdRh::useFeedback" "normal use" {
 	 KbdRh::useFeedback $::rhid true
 	 KbdRh::useFeedback $::rhid
@@ -57,18 +57,18 @@ test "KbdRhTcl-KbdRh::useFeedback" "error on non-boolean input" {
 } {^expected boolean value but got "FLASE"$}
 test "KbdRhTcl-KbdRh::useFeedback" "error on bad rhid" {
 	 KbdRh::useFeedback -1
-} {^KbdRh::useFeedback: .*$}
+} {^EventRh::useFeedback: .*$}
 
 ### KbdRh::keyRespPairs ###
 test "KbdRhTcl-KbdRh::keyRespPairs" "too few args" {
 	 KbdRh::keyRespPairs
-} {^wrong \# args: should be "KbdRh::keyRespPairs item_id\(s\) \?new_value\(s\)\?"$}
+} {^wrong \# args: should be "EventRh::inputResponseMap item_id\(s\) \?new_value\(s\)\?"$}
 test "KbdRhTcl-KbdRh::keyRespPairs" "too many args" {
 	 KbdRh::keyRespPairs junk junk junk
-} {^wrong \# args: should be "KbdRh::keyRespPairs item_id\(s\) \?new_value\(s\)\?"$}
+} {^wrong \# args: should be "EventRh::inputResponseMap item_id\(s\) \?new_value\(s\)\?"$}
 test "KbdRhTcl-KbdRh::keyRespPairs" "error on bad rhid" {
 	 KbdRh::keyRespPairs -1
-} {^KbdRh::keyRespPairs: .*$}
+} {^EventRh::inputResponseMap: .*$}
 test "KbdRhTcl-KbdRh::keyRespPairs" "normal use" {
 	 KbdRh::keyRespPairs $::rhid { {{^[aA]$} 0} {{^[lL]$} 1} }
 	 KbdRh::keyRespPairs $::rhid
@@ -77,13 +77,13 @@ test "KbdRhTcl-KbdRh::keyRespPairs" "normal use" {
 ### KbdRh::feedbackPairs ###
 test "KbdRhTcl-KbdRh::feedbackPairs" "too few args" {
 	 KbdRh::feedbackPairs
-} {^wrong \# args: should be "KbdRh::feedbackPairs item_id\(s\) \?new_value\(s\)\?"$}
+} {^wrong \# args: should be "EventRh::feedbackMap item_id\(s\) \?new_value\(s\)\?"$}
 test "KbdRhTcl-KbdRh::feedbackPairs" "too many args" {
 	 KbdRh::feedbackPairs junk junk junk
-} {^wrong \# args: should be "KbdRh::feedbackPairs item_id\(s\) \?new_value\(s\)\?"$}
+} {^wrong \# args: should be "EventRh::feedbackMap item_id\(s\) \?new_value\(s\)\?"$}
 test "KbdRhTcl-KbdRh::feedbackPairs" "error on bad rhid" {
 	 KbdRh::feedbackPairs -1
-} {^KbdRh::feedbackPairs: .*$}
+} {^EventRh::feedbackMap: .*$}
 test "KbdRhTcl-KbdRh::feedbackPairs" "normal use" {
 	 KbdRh::feedbackPairs $::rhid { {x 5} {z 9} }
 	 KbdRh::feedbackPairs $::rhid
