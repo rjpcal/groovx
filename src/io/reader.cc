605274857d86ffa2afeb8ea7900589191f56242c
Robert J. Peters <rjpcal@gmail.com>
2000-02-15 18:43:08 +0000

    Removed makeNumberString() (this is now provided by ReadUtils).

--- src/io/reader.cc
+++ src/io/reader.cc
@@ -3,7 +3,7 @@
 // reader.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Mon Jun  7 12:47:00 1999
-// written: Tue Nov 30 17:24:42 1999
+// written: Tue Feb 15 10:42:55 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -55,14 +55,6 @@ void Reader::readValue<const char*>(const string& name, const char*& return_valu
 template <>
 void Reader::readValue<Value>(const string& name, Value& return_value) {
   readValueObj(name, return_value);
-}
-
-
-string Reader::makeNumberString(int number) {
-  char buf[16];
-  ostrstream ost(buf, 16);
-  ost << number << '\0';
-  return buf;
 }
 
 static const char vcid_reader_cc[] = "$Header$";
