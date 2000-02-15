82d59c66ec66d465aa74a76a38e093e9a355a10e
Robert J. Peters <rjpcal@gmail.com>
2000-02-15 18:30:27 +0000

    Added Doxygen docs for Getter, Setter, Attrib, and Action.

--- src/tclitempkgbase.h
+++ src/tclitempkgbase.h
@@ -3,7 +3,7 @@
 // tclitempkgbase.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Thu Dec 16 15:25:06 1999
-// written: Thu Dec 16 15:37:00 1999
+// written: Tue Feb 15 10:29:44 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -23,23 +23,42 @@ struct Tcl_Interp;
 //
 ///////////////////////////////////////////////////////////////////////
 
+/** A templated abstract base class that represents getters (const
+    member functions that take no arguments and return a value of type
+    \c T.) */
 template <class T>
 class Getter {
 public:
+  /** Should be overridden by subclasses to cast \a item to the
+      appropriate type, then call the appropriate member and return
+      its result. */
   virtual T get(void* item) const = 0;
 };
 
+
+/** A templated abstract base class that represents setters (non-const
+    member functions that take a single argument of type \c T.*/
 template <class T>
 class Setter {
 public:
+  /** Should be overridden by subclasses to cast \a item to the
+      appropriate type, then call the appropriate member with \a val
+      as the lone argument. */
   virtual void set(void* item, T val) = 0;
 };
 
+
+/** An abstract base class that represent an read-write attribute. */
 template <class T>
 class Attrib : public virtual Getter<T>, public virtual Setter<T> {};
 
+
+/** An abstract base class for representing actions (member functions
+    with no argument and no return value). */
 class Action {
 public:
+  /** Should be overridden by subclasses to cast \a item to the
+      appropriate type, then perform an appropriate action on the result. */
   virtual void action(void* item) = 0;
 };
 
