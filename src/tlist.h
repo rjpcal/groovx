bb597c4e631d63c7d1b233b1062eb186f1b21243
Robert J. Peters <rjpcal@gmail.com>
2000-02-16 16:03:44 +0000

    Finished Doxygen documentation, removed destructor (PtrList<Trial>'s destructor handles everything now), added int size argument to constructor.

--- src/tlist.h
+++ src/tlist.h
@@ -3,7 +3,7 @@
 // tlist.h
 // Rob Peters
 // created: Fri Mar 12 13:23:02 1999
-// written: Sat Dec  4 03:54:51 1999
+// written: Wed Feb 16 08:02:44 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -20,37 +20,35 @@
 #endif
 
 class Trial;
-class ObjId;
-class PosId;
 
 ///////////////////////////////////////////////////////////////////////
-//
-// Tlist class definition
-//
+/**
+ *
+ * Tlist is a singleton wrapper for \c PtrList<Trial>.
+ *
+ **/
 ///////////////////////////////////////////////////////////////////////
 
-class Tlist : public PtrList<Trial>, public virtual IO {
-  //////////////
-  // creators //
-  //////////////
-
-protected:
-  Tlist();
-
+class Tlist : public PtrList<Trial> {
 private:
   static Tlist theInstance;
 
+protected:
+  /// Construct and reserve space for \a size objects.
+  Tlist(int size);
+
 public:
 
-  // Returns a reference to the singleton instance of Tlist
+  // Returns a reference to the singleton instance.
   static Tlist& theTlist();
 
-  virtual ~Tlist();
-
-  // These I/O functions write/read the entire state of the Tlist
+  /// Overridden from PtrList<Trial> to provide compatibility with old Tlist.
   virtual void serialize(ostream &os, IOFlag flag) const;
+
+  /// Overridden from PtrList<Trial> to provide compatibility with old Tlist.
   virtual void deserialize(istream &is, IOFlag flag);
 
+  /// Overridden from PtrList<Trial> to provide compatibility with old Tlist.
   virtual int charCount() const;
 };
 
