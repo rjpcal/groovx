a15d868f41075d6a47fedab206c73e245b769fc0
Robert J. Peters <rjpcal@gmail.com>
2000-02-16 15:33:29 +0000

    Finished Doxygen documentation.

--- src/bitmaprep.h
+++ src/bitmaprep.h
@@ -3,7 +3,7 @@
 // bitmaprep.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Wed Dec  1 20:18:32 1999
-// written: Tue Feb  8 15:28:33 2000
+// written: Wed Feb 16 07:33:10 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -36,20 +36,25 @@ template <class V> class Rect;
 ///////////////////////////////////////////////////////////////////////
 /**
  *
- * BitmapRep
+ * \c BitmapRep maintains data associated with a bitmap. \c BitmapRep
+ * loosely implements the \c GrObj and \c IO interfaces, but does not
+ * actually inherit from these classes. In fact, \c BitmapRep is used
+ * as an implementation class for \c Bitmap, which delegates most of
+ * its functionality to \c BitmapRep. \c BitmapRep objects must be
+ * initialized with a \c BmapRenderer object, which handles the actual
+ * rendering of the bitmao data onto the screen.
  *
- * @short A class that maintains data associated with a bitmap.
  **/
 ///////////////////////////////////////////////////////////////////////
 
 class BitmapRep {
 public:
-  ///
+  /// Construct with empty bitmap data.
   BitmapRep(BmapRenderer* renderer) :
 	 itsRenderer(renderer)
 	 { init(); }
 
-  ///
+  /// Construct and load bitmap data from \a filename.
   BitmapRep(BmapRenderer* renderer, const char* filename) :
 	 itsRenderer(renderer),
 	 itsFilename(filename)
@@ -58,7 +63,7 @@ public:
 	 loadPbmFile(filename);
   }
 
-  ///
+  /// Non-virtual destructor implies this class is not polymorphic.
   ~BitmapRep() {}
 
 private:
@@ -66,17 +71,16 @@ private:
 
 public:
 
-  ///
+  /// Conforms to the \c IO interface.
   void serialize(ostream& os, int flag) const;
-  ///
+  /// Conforms to the \c IO interface.
   void deserialize(istream& is, int flag);
-
-  ///
+  /// Conforms to the \c IO interface.
   int charCount() const;
 
-  ///
+  /// Conforms to the \c IO interface.
   void readFrom(Reader* reader);
-  ///
+  /// Conforms to the \c IO interface.
   void writeTo(Writer* writer) const;
 
   /////////////
@@ -84,41 +88,56 @@ public:
   /////////////
 
 public:
-  ///
+  /// Loads PBM bitmap data from the PBM file \a filename.
   void loadPbmFile(const char* filename);
-  /** Queues a pbm file for loading. The file will not actually be
-      loaded until such time as the data from the file is needed. */
+
+  /** Queues the PBM file \a filename for loading. The PBM bitmap data
+      will not actually be retrieved from the file until it is
+      needed. */
   void queuePbmFile(const char* filename);
-  ///
+
+  /// Loads PBM bitmap data from an istream.
   void loadPbmFile(istream& is);
-  ///
+
+  /// Writes PBM bitmap data to the file \a filename.
   void writePbmFile(const char* filename) const;
 
-  /** These routines grab pixels from a rectanglur area of the screen
-		buffer into the Bitmap's pixel array. The coordinates of the
-		rectangle may be specified in pixel values (ScreenRect) or in
-		OpenGL coordinate values (WorldRect). */
-  //@{
-  ///
+  /** Grabs pixels from a rectangulur area of the screen buffer into
+		the Bitmap's pixel array. The coordinates of the rectangle are
+		specified in pixel values. */
   void grabScreenRect(int left, int top, int right, int bottom);
-  ///
+
+  /** Grabs pixels from a rectangulur area of the screen buffer into
+		the Bitmap's pixel array. The coordinates of the rectangle are
+		specified in pixel values. */
   void grabScreenRect(const Rect<int>& rect);
-  ///
+
+  /** Grabs pixels from a rectangulur area of the screen buffer into
+		the Bitmap's pixel array. The coordinates of the rectangle are
+		specified in OpenGL coordinates. */
   void grabWorldRect(double left, double top,
 							double right, double bottom);
-  ///
+
+  /** Grabs pixels from a rectangulur area of the screen buffer into
+		the Bitmap's pixel array. The coordinates of the rectangle are
+		specified in OpenGL coordinates. */
   void grabWorldRect(const Rect<double>& rect);
-  //@}
 
   /** Flips the luminance contrast of the bitmap data, in a way that
-      may depend on the format of the bitmap data. */
+      may depend on the format of the bitmap data. The polarity of the
+      contrast relative to its original value is computed and stored,
+      so that it can be maintained throughout IO operations. */
   void flipContrast();
 
+  /// Does the actual bit-twiddling to flip the contrast.
   void doFlipContrast() { itsData.flipVertical(); }
 
-  /// Vertically inverts the image.
+  /** Vertically inverts the image. The polarity of the orientation
+      relative to its original value is computed and stored, so that
+      it can be maintained throughout IO operations. */
   void flipVertical();
 
+  /// Does the actual bit-twiddling to vertically flip the image.
   void doFlipVertical() { itsData.flipVertical(); }
 
   /** Centers the image so that its center coincides with the origin
@@ -137,15 +156,17 @@ public:
   // accessors //
   ///////////////
 
-  ///
+  /// Conforms to the \c GrObj interface.
   void grGetBoundingBox(Rect<double>& bounding_box,
 								int& border_pixels) const;
 
+  /// Conforms to the \c GrObj interface.
   bool grHasBoundingBox() const;
 
   /// Get the number of bytes of image data.
-  int byteCount() const
+  int byteCount() const 
 	 { return itsData.byteCount(); }
+
   /// Get the number of bytes per row in the image data.
   int bytesPerRow() const
 	 { return itsData.bytesPerRow(); }
@@ -153,44 +174,44 @@ public:
   /// Get the image's width in pixels.
   int width() const
 	 { return itsData.width(); }
+
   /// Get the image's height in pixels.
   int height() const
 	 { return itsData.height(); }
 
-  /** Manipulate the coordinates of the image's raster position (this
-      specifies where the lower left corner of the image will
-      fall). */
-  //@{
-  ///
+  /// Get the x value of the location of the image's lower left corner.
   double getRasterX() const;
-  ///
+
+  /// Get the y value of the location of the image's lower left corner.
   double getRasterY() const;
-  ///
+
+  /** Change the x value of the location of the image's lower left
+		corner to \a val. */
   void setRasterX(double val);
-  ///
+
+  /** Change the y value of the location of the image's lower left
+		corner to \a val. */
   void setRasterY(double val);
-  //@}
 
-  /** Manipulate the zoom factors of the image. (NOTE: not all
-      subclasses may be able to support zooming.) */
-  //@{
-  ///
+  /// Get the factor by which the bitmap will be scaled in the x direction.
   double getZoomX() const;
-  ///
-  double getZoomY() const;
-  ///
-  void setZoomX(double val);
-  ///
-  void setZoomY(double val);
-  //@}
 
-  /// Manipulate whether zooming should be used.
-  //@{
-  ///
+  /// Get the factor by which the bitmap will be scaled in the y direction.
+  double getZoomY() const;
+
+  /** Change the factor by which the bitmap will be scaled in the x
+      direction to \a val. */
+  void setZoomX(double val);
+
+  /** Change the factor by which the bitmap will be scaled in the y
+      direction to \a val. */
+  void setZoomY(double val);
+
+  /// Query whether zooming is currently to be used.
   bool getUsingZoom() const;
-  ///
+
+  /// Change whether zooming will be used.
   void setUsingZoom(bool val);
-  //@}
 
 private:
   void clearBytes();
--- src/visx/bitmap.h
+++ src/visx/bitmap.h
@@ -3,7 +3,7 @@
 // bitmap.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Jun 15 11:30:24 1999
-// written: Tue Feb  8 15:26:36 2000
+// written: Wed Feb 16 07:30:37 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -40,36 +40,31 @@ class BmapRenderer;
  * this. Subclasses of Bitmap can implement a particular rendering
  * method by overriding doRender() and doUndraw() appropriately.
  *
- * @short Abstract subclass of GrObj for rendering bitmaps.
  **/
 ///////////////////////////////////////////////////////////////////////
 
 class Bitmap : public GrObj {
   // Creators
 public:
-  ///
+  /// Create an empty bitmap.
   Bitmap(BmapRenderer* renderer);
+
   /** Create a bitmap from a graphics file. Currently the only
 		supported file format is PBM (Portable BitMap). */
   Bitmap(BmapRenderer* renderer, const char* filename);
+
   /// Construct a Bitmap by deserializing from a stream.
   Bitmap(BmapRenderer* renderer, istream& is, IOFlag flag);
 
 public:
-  ///
+  /// Virtual destructor ensures proper destruction of subclasses.
   virtual ~Bitmap();
 
-  ///
   virtual void serialize(ostream& os, IOFlag flag) const;
-  ///
   virtual void deserialize(istream& is, IOFlag flag);
-
-  ///
   virtual int charCount() const;
 
-  ///
   virtual void readFrom(Reader* reader);
-  ///
   virtual void writeTo(Writer* writer) const;
 
   /////////////
@@ -77,62 +72,78 @@ public:
   /////////////
 
 public:
-  ///
+  /// Loads PBM bitmap data from the PBM file at \a filename.
   void loadPbmFile(const char* filename);
-  ///
-  void loadPbmFile(istream& is);
-  ///
+
+  /// Loads PBM bitmap data from the stream \a input.
+  void loadPbmFile(istream& input);
+
+  /// Loads PBM bitmap data from the gzip'ed PBM file at \a filename.
   void loadPbmGzFile(const char* filename);
-  ///
+
+  /// Writes PBM bitmap data to the file \a filename.
   void writePbmFile(const char* filename) const;
 
-  /** These routines grab pixels from a rectanglur area of the screen
-		buffer into the Bitmap's pixel array. The coordinates of the
-		rectangle may be specified in pixel values (ScreenRect) or in
-		OpenGL coordinate values (WorldRect). */
-  //@{
-  ///
+  /** Grabs pixels from a rectangulur area of the screen buffer into
+		the Bitmap's pixel array. The coordinates of the rectangle are
+		specified in pixel values. */
   void grabScreenRect(int left, int top, int right, int bottom);
-  ///
+
+  /** Grabs pixels from a rectangulur area of the screen buffer into
+		the Bitmap's pixel array. The coordinates of the rectangle are
+		specified in pixel values. */
   void grabScreenRect(const Rect<int>& rect);
-  ///
+
+  /** Grabs pixels from a rectangulur area of the screen buffer into
+		the Bitmap's pixel array. The coordinates of the rectangle are
+		specified in OpenGL coordinates. */
   void grabWorldRect(double left, double top,
 							double right, double bottom);
-  ///
-  void grabWorldRect(const Rect<double>& rect);
-  //@}
 
-  public:
+  /** Grabs pixels from a rectangulur area of the screen buffer into
+		the Bitmap's pixel array. The coordinates of the rectangle are
+		specified in OpenGL coordinates. */
+  void grabWorldRect(const Rect<double>& rect);
+
+public:
   /** Flips the luminance contrast of the bitmap data, in a way that
-      may depend on the format of the bitmap data. */
+      may depend on the format of the bitmap data. The polarity of the
+      contrast relative to its original value is computed and stored,
+      so that it can be maintained throughout IO operations. */
   void flipContrast();
 
-  protected:
+protected:
+  /// Does the actual bit-twiddling to flip the contrast.
   void doFlipContrast();
 
-  public:
-  /// Vertically inverts the image.
+public:
+  /** Vertically inverts the image. The polarity of the orientation
+      relative to its original value is computed and stored, so that
+      it can be maintained throughout IO operations. */
   void flipVertical();
 
-  protected:
+protected:
+  /// Does the actual bit-twiddling to vertically flip the image.
   void doFlipVertical();
 
-  public:
+public:
   /** Centers the image so that its center coincides with the origin
       in the graphics environment when it is rendered. */
   void center();
 
   protected:
-  /** Implements the GrObj rendering operation. This function
-      delegates the work to itsRenderer; therefore, subclasses of
-      Bitmap should not override grRender(), but should instead
-      provide a specialized renderer. */
+  /** Implements the \c GrObj rendering operation. The rendering is
+      delegated to the \c BmapRenderer passed to the
+      constructor. Therefore, subclasses of \c Bitmap should not
+      override \c grRender(), but should instead provide a specialized
+      \c BmapRenderer. */
   virtual void grRender(Canvas& canvas) const;
 
-  /** Implements the GrObj undrawing operation. This function
-      delegates the work to itsRenderer; therefore, subclasses of
-      Bitmap should not override grUnRender(), but should instead
-      provide a specialized renderer. */
+  /** Implements the \c GrObj rendering operation. The rendering is
+      delegated to the \c BmapRenderer passed to the
+      constructor. Therefore, subclasses of \c Bitmap should not
+      override \c grUnRender(), but should instead provide a
+      specialized \c BmapRenderer. */
   virtual void grUnRender(Canvas& canvas) const;
 
   ///////////////
@@ -140,58 +151,57 @@ public:
   ///////////////
 
   protected:
-  ///
   virtual void grGetBoundingBox(Rect<double>& bounding_box,
 										  int& border_pixels) const;
 
-  ///
   virtual bool grHasBoundingBox() const;
 
 public:
   /// Get the number of bytes of image data.
   int byteCount() const;
+
   /// Get the number of bytes per row in the image data.
   int bytesPerRow() const;
 
   /// Get the image's width in pixels.
   int width() const;
+
   /// Get the image's height in pixels.
   int height() const;
 
-  /** Manipulate the coordinates of the image's raster position (this
-      specifies where the lower left corner of the image will
-      fall). */
-  //@{
-  ///
+  /// Get the x value of the location of the image's lower left corner.
   double getRasterX() const;
-  ///
+
+  /// Get the y value of the location of the image's lower left corner.
   double getRasterY() const;
-  ///
+
+  /** Change the x value of the location of the image's lower left
+		corner to \a val. */
   void setRasterX(double val);
-  ///
+
+  /** Change the y value of the location of the image's lower left
+		corner to \a val. */
   void setRasterY(double val);
-  //@}
 
-  /** Manipulate the zoom factors of the image. (NOTE: not all
-      subclasses may be able to support zooming.) */
-  //@{
-  ///
+  /// Get the factor by which the bitmap will be scaled in the x direction.
   double getZoomX() const;
-  ///
-  double getZoomY() const;
-  ///
-  void setZoomX(double val);
-  ///
-  void setZoomY(double val);
-  //@}
 
-  /// Manipulate whether zooming should be used.
-  //@{
-  ///
+  /// Get the factor by which the bitmap will be scaled in the y direction.
+  double getZoomY() const;
+
+  /** Change the factor by which the bitmap will be scaled in the x
+      direction to \a val. */
+  void setZoomX(double val);
+
+  /** Change the factor by which the bitmap will be scaled in the y
+      direction to \a val. */
+  void setZoomY(double val);
+
+  /// Query whether zooming is currently to be used.
   bool getUsingZoom() const;
-  ///
+
+  /// Change whether zooming will be used.
   void setUsingZoom(bool val);
-  //@}
 
 private:
   BitmapRep* const itsImpl;
