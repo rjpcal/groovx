1320f27eb3dec1a49de613f18726bf5ed7d3c1d3
Robert J. Peters <rjpcal@gmail.com>
2000-02-16 16:17:44 +0000

    Added some Doxygen documentation.

--- src/ptrlist.h
+++ src/ptrlist.h
@@ -3,7 +3,7 @@
 // ptrlist.h
 // Rob Peters
 // created: Fri Apr 23 00:35:31 1999
-// written: Tue Feb  1 18:15:55 2000
+// written: Wed Feb 16 08:17:00 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -22,22 +22,28 @@
 ///////////////////////////////////////////////////////////////////////
 /**
  *
- * PtrList is templatized container for pointers. PtrList is
- * responsible for the memory management of all the objects to which
- * it holds pointers. The pointers are accessed by integer indices
- * into the PtrList. There are no operations on the capacity of
- * PtrList; any necessary resizing is done when necessary in an insert
- * call.
+ * PtrList is templatized container that stores pointers and owns
+ * (memory-manages) the objects to which the pointers refer. PtrList
+ * is responsible for the memory management of all the objects to
+ * which it holds pointers. The pointers are accessed by integer
+ * indices into the PtrList. There are no operations on the capacity
+ * of PtrList; any necessary resizing is done when necessary in an
+ * insert call.
  *
- * @short A template container that stores and owns pointers.
  **/
 ///////////////////////////////////////////////////////////////////////
 
 template <class T>
 class PtrList : public IoPtrList {
 public:
-  ///
+  /// Construct and reserve space for \a size objects.
   PtrList (int size);
+
+  /** Virtual destructor calls \c clear(), according to the
+      requirement of \c VoidPtrList, to ensure no memory
+      leaks. Further subclasses of \c PtrList do not need to call \c
+      clear() from their destructors. */
+  virtual ~PtrList();
 
   //////////////
   // pointers //
@@ -143,11 +149,17 @@ public:
   ///////////////
 
 public:
-  ///
+  /** Return the object at index \a id. No range check is performed on
+      \a id, so the behavior is undefined if \a id is not a valid
+      index into the list. If \a id is out of range, a segmentation
+      fault may occur. If \a is in range, but is not an index for a
+      valid object, the pointer returned may be null. */
   Ptr getPtr(int id) const throw () 
 	 { return DumbPtr<T>(castToT(VoidPtrList::getVoidPtr(id))); }
 
-  ///
+  /** Returns the object at index \a id, after a check is performed to
+      ensure that \a id is in range, and refers to a valid object. If
+      the check fails, an \c InvalidIdError exception is thrown. */
   Ptr getCheckedPtr(int id) const throw (InvalidIdError)
 	 { return DumbPtr<T>(castToT(VoidPtrList::getCheckedVoidPtr(id))); }
 
@@ -162,30 +174,27 @@ public:
   // manipulators //
   //////////////////
 
-  ///
+  /// Insert \a ptr into the list, and return its id.
   int insert(Ptr ptr)
 	 { return VoidPtrList::insertVoidPtr(castFromT(ptr.get())); }
 
-  ///
+  /** Insert \a ptr into the list at index \a id. If an object
+      previously existed at index \a id, that object will be properly
+      destroyed. */
   void insertAt(int id, Ptr ptr)
 	 { VoidPtrList::insertVoidPtrAt(id, castFromT(ptr.get())); }
 
 protected:
-  /**
-	* Casts
-	**/
-  //@{
-  ///
+  /// Safely cast \a ptr to the correct type for this list.
   T* castToT(void* ptr) const;
-  ///
-  void* castFromT(T* ptr) const;
-  ///
-  virtual IO* fromVoidToIO(void* ptr) const;
-  ///
-  virtual void* fromIOToVoid(IO* ptr) const;
-  //@}
 
-  ///
+  /// Cast \a ptr back to \c void*.
+  void* castFromT(T* ptr) const;
+
+  virtual IO* fromVoidToIO(void* ptr) const;
+  virtual void* fromIOToVoid(IO* ptr) const;
+
+  /// Casts \a ptr to the correct type, then \c delete's it.
   virtual void destroyPtr(void* ptr);
 };
 
