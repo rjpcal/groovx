bd172ff15358be4a2c12d6773595c211e46dd2d6
Robert J. Peters <rjpcal@gmail.com>
2000-02-18 17:18:14 +0000

    Finished Doxygen documentation.

--- src/visx/xbitmap.h
+++ src/visx/xbitmap.h
@@ -3,7 +3,7 @@
 // xitmap.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Sep  7 14:39:09 1999
-// written: Wed Dec  1 20:59:21 1999
+// written: Fri Feb 18 09:13:42 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -17,21 +17,37 @@
 
 class XBmapRenderer;
 
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * \c XBitmap is a subclass of \c Bitmap that renders the bitmap data
+ * using X11's drawing API's.
+ *
+ **/
+///////////////////////////////////////////////////////////////////////
+
 class XBitmap : public Bitmap {
 public:
   // Creators
+
+  /// Construct an empty bitmap.
   XBitmap();
+
+  /** Create a bitmap from a graphics file. See documentation for \c
+      Bitmap for a list of supported file types. */
   XBitmap(const char* filename);
+
+  /// Construct from an \c istream by calling \c deserialize().
   XBitmap(istream& is, IOFlag flag);
 
   private: void init();
   
 public:
+  /// Virtual destructor.
   virtual ~XBitmap();
 
   virtual void serialize(ostream& os, IOFlag flag) const;
   virtual void deserialize(istream& is, IOFlag flag);
-
   virtual int charCount() const;
 
 private:
--- src/visx/xbmaprenderer.h
+++ src/visx/xbmaprenderer.h
@@ -3,7 +3,7 @@
 // xbmaprenderer.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Wed Dec  1 17:19:23 1999
-// written: Mon Dec  6 21:58:46 1999
+// written: Fri Feb 18 09:18:09 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -21,11 +21,25 @@ typedef _XImage XImage;
 struct Tk_Window_;
 typedef Tk_Window_* Tk_Window;
 
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * \c XBmapRenderer is an implementation of \c BmapRenderer that
+ * renders bitmap data using X11's drawing API's.
+ *
+ **/
+///////////////////////////////////////////////////////////////////////
+
 class XBmapRenderer : public BmapRenderer {
 public:
+  /** Initializes some X11 structures. Must be called before any \c
+      XBmapRenderer objects are rendered. */
   static void initClass(Tk_Window win);
 
+  /// Default constructor.
   XBmapRenderer();
+
+  /// Virtual destructor.
   virtual ~XBmapRenderer();
 
   virtual void doRender(Canvas& canvas,
@@ -43,6 +57,8 @@ public:
 					  int winRasterX, int winRasterY,
 					  int winWidthX, int winHeightY) const;
 
+  /** Reimplemented from \c BmapRenderer to keep the \c XImage
+      up-to-date when the bitmap data change. */
   virtual void notifyBytesChanged() const;
 
 private:
