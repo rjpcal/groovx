7ba24c58295895d50b2459f73d817078dcb4fda0
Robert J. Peters <rjpcal@gmail.com>
2000-02-21 06:02:42 +0000

    Added Doxygen documentation.

--- src/gx/pbm.h
+++ src/gx/pbm.h
@@ -3,7 +3,7 @@
 // pbm.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Jun 15 16:41:06 1999
-// written: Thu Jan 20 00:30:32 2000
+// written: Sun Feb 20 22:02:25 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -31,33 +31,57 @@
 
 class istream;
 
+/** This exception class will be thrown by \c Pbm if there is an error
+    during parsing of the bitmap data. */
 class PbmError : public ErrorWithMsg {
 public:
-  PbmError(const string& str="");
+  /// Construct with an appropriate error message \a msg.
+  PbmError(const string& msg="");
 };
 
 class Pbm {
 public:
+  /** Construct from the specified parameters. This has the same
+      effect as calling \c setBytes() with the same arguments. */
   Pbm(const vector<unsigned char>& bytes,
 		int width, int height, int bits_per_pixel);
+
+  /** Construct by copying from of \a data. This has the same effect
+      as calling \c setBytes() with the same argument. */
   Pbm(const BmapData& data);
+
+  /// Construct by reading PBM format data from the \c istream \a is.
   Pbm(istream& is);
+
+  /// Construct by reading PBM format data from the file \a filename.
   Pbm(const char* filename);
+
+  /// Virtual destructor.
   virtual ~Pbm();
 
-  // This function transfers ownership of itsBytes to the caller of
-  // the function.
+  /** This function allows an external caller to take ownership of the
+      bitmap data help by the \c Pbm object. The internal bitmap data
+      are swapped into \a bytes, and the other reference parameters
+      are set appropriately. The \c Pbm's internal state is then
+      reinitialized, to contain an empty bitmap. */
   void grabBytes(vector<unsigned char>& bytes,
  					  int& width, int& height, int& bits_per_pixel);					  
 
+  /** The data are copied from \a bytes into the \c Pbm object. */
   void setBytes(const vector<unsigned char>& bytes,
 					 int width, int height, int bits_per_pixel);
 
+  /** Bitmap data are copied from \a data into the \c Pbm object. */
   void setBytes(const BmapData& data);
 
+  /** The \c Pbm object's internal representation is swapped with that
+      of \a data. */
   void swapInto(BmapData& data);
 
+  /// Write PBM format data to the \c ostream \a os.
   void write(ostream& os) const;
+
+  /// Write PBM format data to the file \a filename.
   void write(const char* filename) const;
 
 private:
