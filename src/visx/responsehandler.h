079e29f17cf08dae263fd9315f313e67ea69210a
Robert J. Peters <rjpcal@gmail.com>
2000-02-21 16:56:39 +0000

    Finished Doxygen documentation.

--- src/visx/responsehandler.h
+++ src/visx/responsehandler.h
@@ -3,7 +3,7 @@
 // responsehandler.h
 // Rob Peters
 // created: Tue May 18 16:21:09 1999
-// written: Mon Dec  6 14:02:50 1999
+// written: Mon Feb 21 08:54:17 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -22,14 +22,12 @@ class Experiment;
 ///////////////////////////////////////////////////////////////////////
 /**
  *
- * ResponseHandler provides an abstract interface for collecting
+ * \c ResponseHandler defines an abstract interface for collecting
  * responses from a user during an experiment. However, it says
  * nothing about which modality will provide those responses. The key
- * virtual functions are the actions rhXxx(); these are called by
- * ExptDriver at appropriate points in the trial sequence.
+ * virtual functions are the actions \c rhXxx(); these are called by
+ * \c ExptDriver at appropriate points in the trial sequence.
  *
- * @short Defines the interface for collecting responses from a user
- * during an experiment.
  **/
 ///////////////////////////////////////////////////////////////////////
 
@@ -37,44 +35,45 @@ class ResponseHandler : public virtual IO {
 public:
   /// Default constructor.
   ResponseHandler();
-  ///
+
+  /// Virtual destructor.
   virtual ~ResponseHandler();
 
-  ///
   virtual void serialize(ostream& os, IOFlag flag) const = 0;
-  ///
   virtual void deserialize(istream& is, IOFlag flag) = 0;
-
-  ///
   virtual int charCount() const = 0;
 
-  ///
   virtual void readFrom(Reader* reader) = 0;
-  ///
   virtual void writeTo(Writer* writer) const = 0;
 
-  ///
+  /** This symbolic is used in place of an actual response value to
+      indicate that the response was invalid or could not be
+      determined. */
   static const int INVALID_RESPONSE = -1;
 
-  ///
+  /** Use \a interp as the Tcl interpreter for any Tcl-related
+      processing that may be required in handling responses. */
   virtual void setInterp(Tcl_Interp* interp) = 0;
 
-  /** @name  Experiment event sequence actions
-	*
-	* These functions will be called by ExptDriver at appropriate
-	* points in the experiment sequence.
-	*
-	**/
-  //@{
-  ///
+  /** Will by called by an \c Experiment at the beginning of a
+      trial. \c ResponseHandler subclasses implement this function to
+      prepare listening for responses, etc. */
   virtual void rhBeginTrial(Experiment* expt) const = 0;
-  ///
+
+  /** Will by called by an \c Experiment if a trial is aborted. \c
+      ResponseHandler subclasses implement this function to quit
+      listening for responses, etc. */
   virtual void rhAbortTrial(Experiment* expt) const = 0;
-  ///
+
+  /** Will by called by an \c Experiment when a trial is complete. \c
+      ResponseHandler subclasses implement this function to quit
+      listening for responses, etc. */
   virtual void rhEndTrial(Experiment* expt) const = 0;
-  ///
+
+  /** Will by called by an \c Experiment when the sequence of trials
+      is halted suddenly. \c ResponseHandler subclasses implement this
+      function to quit listening for responses, etc. */
   virtual void rhHaltExpt(Experiment* expt) const = 0;
-  //@}
 };
 
 static const char vcid_responsehandler_h[] = "$Header$";
