747fdbe425f04d2a651c3cf25750ce0ab982bc5c
Robert J. Peters <rjpcal@gmail.com>
2000-03-07 02:16:58 +0000

    Added fixed_block and dynamic_block classes.

--- src/util/arrays.h
+++ src/util/arrays.h
@@ -3,7 +3,7 @@
 // arrays.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Mon Mar  6 15:56:36 2000
-// written: Mon Mar  6 16:30:44 2000
+// written: Mon Mar  6 17:18:10 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -74,6 +74,159 @@ struct static_block {
   size_type size() const { return N; }
 
   T data[N];
+};
+
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * \c fixed_block is a simple wrapper for a dynamically allocated
+ * array whose size is fixed on instantiation. Copying and assignment
+ * are not allowed.
+ *
+ **/
+///////////////////////////////////////////////////////////////////////
+
+template <class T>
+class fixed_block {
+private:
+  fixed_block(const fixed_block<T>& other);
+  fixed_block<T>& operator=(const fixed_block<T>& other);
+
+public:
+
+  typedef T value_type;
+
+  typedef T* pointer;
+  typedef const T* const_pointer;
+  typedef T& reference;
+  typedef const T& const_reference;
+
+  typedef pointer iterator;
+  typedef const_pointer const_iterator;
+
+  typedef size_t size_type;
+  typedef ptrdiff_t difference_type;
+
+  fixed_block(size_type n) : N(n), data(new T[N]) {}
+  ~fixed_block() { delete [] data; }
+
+  iterator begin() { return data; }
+  iterator end() { return data+N; }
+
+  const_iterator begin() const { return data; }
+  const_iterator end() const { return data+N; }
+
+  reference operator[](size_type n) { return data[n]; }
+  const_reference operator[](size_type n) const { return data[n]; }
+
+  reference at(size_type n)
+	 {
+		if ( n >= N )
+		  throw Util::out_of_range();
+		return data[n];
+	 }
+
+  const_reference at(size_type n) const
+	 {
+		if ( n >= N )
+		  throw Util::out_of_range();
+		return data[n];
+	 }
+
+  size_type size() const { return N; }
+
+private:
+  size_type N;
+  T* data;
+};
+
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * \c dynamic_block is a simple wrapper for a dynamically allocated
+ * array whose size is fixed on instantiation. Copying and assignment
+ * are not allowed.
+ *
+ **/
+///////////////////////////////////////////////////////////////////////
+
+template <class T>
+class dynamic_block {
+private:
+  dynamic_block(const dynamic_block<T>& other);
+  dynamic_block<T>& operator=(const dynamic_block<T>& other);
+
+public:
+
+  typedef T value_type;
+
+  typedef T* pointer;
+  typedef const T* const_pointer;
+  typedef T& reference;
+  typedef const T& const_reference;
+
+  typedef pointer iterator;
+  typedef const_pointer const_iterator;
+
+  typedef size_t size_type;
+  typedef ptrdiff_t difference_type;
+
+  dynamic_block(size_type n) : N(n), data(new T[N]) {}
+  ~dynamic_block() { delete [] data; }
+
+  void swap(dynamic_block& other)
+	 {
+		size_type other_size = other.N;
+		other.N = this->N;
+		this->N = other_size;
+
+		T* other_data = other.data;
+		other.data = this->data;
+		this->data = other_data;
+	 }
+
+  iterator begin() { return data; }
+  iterator end() { return data+N; }
+
+  const_iterator begin() const { return data; }
+  const_iterator end() const { return data+N; }
+
+  reference operator[](size_type n) { return data[n]; }
+  const_reference operator[](size_type n) const { return data[n]; }
+
+  reference at(size_type n)
+	 {
+		if ( n >= N )
+		  throw Util::out_of_range();
+		return data[n];
+	 }
+
+  const_reference at(size_type n) const
+	 {
+		if ( n >= N )
+		  throw Util::out_of_range();
+		return data[n];
+	 }
+
+  size_type size() const { return N; }
+
+  void resize(size_type new_size)
+	 {
+		dynamic_block temp(new_size);
+		for (iterator from = this->begin(), to = temp.begin();
+			  from != this->end() && to != temp.end();
+			  ++from, ++to)
+		  {
+			 *to = *from;
+		  }
+		this->swap(temp);
+	 }
+
+private:
+  size_type N;
+  T* data;
 };
 
 
