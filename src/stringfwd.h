afcbafda059eb30252be78dce394f51380987941
Robert J. Peters <rjpcal@gmail.com>
2000-03-07 21:49:27 +0000

    Added forward declaration of std::string for gcc compiler/STL.

--- src/stringfwd.h
+++ src/stringfwd.h
@@ -3,7 +3,7 @@
 // stringfwd.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Fri Mar  3 12:32:58 2000
-// written: Mon Mar  6 16:21:06 2000
+// written: Tue Mar  7 13:28:16 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -13,10 +13,27 @@
 
 #if defined(ACC_COMPILER)
 
+template< class charT, class traits, class Allocator > class basic_string;
+
 class allocator;
 template<class charT> class string_char_traits;
-template< class charT, class traits, class Allocator > class basic_string;
 typedef basic_string<char, string_char_traits<char>, allocator> string;
+
+#elif defined(GCC_COMPILER)
+
+template <class charT, class traits, class Allocator> class basic_string;
+
+// template <int __inst> class __malloc_alloc_template;
+// typedef __malloc_alloc_template<0> malloc_alloc;
+// typedef malloc_alloc alloc;
+
+template <bool threads, int inst> class __default_alloc_template;
+typedef __default_alloc_template<false, 0> alloc;
+
+template<class charT> class string_char_traits;
+
+typedef basic_string<char, string_char_traits<char>, alloc> string;
+
 
 #else
 #  include <string>
