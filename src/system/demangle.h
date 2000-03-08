d2d5c0f87ae57ebfeb977cc313c0b2a6810015e0
Robert J. Peters <rjpcal@gmail.com>
2000-03-08 23:53:50 +0000

    Removed string-returning version of demangle().

--- src/system/demangle.h
+++ src/system/demangle.h
@@ -3,19 +3,13 @@
 // demangle.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Wed Oct 13 10:41:03 1999
-// written: Sat Mar  4 02:59:58 2000
+// written: Wed Mar  8 15:53:41 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef DEMANGLE_H_DEFINED
 #define DEMANGLE_H_DEFINED
-
-#ifndef STRINGFWD_H_DEFINED
-#include "stringfwd.h"
-#endif
-
-string demangle(const string& in);
 
 // Warning: the result of this function is only valid until the next
 // call to the function.
