dcf283bb6a27c1226f21e86762d9440e374aa64c
Robert J. Peters <rjpcal@gmail.com>
2000-03-08 03:13:08 +0000

    Added shared_ptr.

--- src/util/pointers.h
+++ src/util/pointers.h
@@ -3,8 +3,19 @@
 // pointers.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Mar  7 14:52:52 2000
-// written: Tue Mar  7 15:07:38 2000
+// written: Tue Mar  7 15:22:43 2000
 // $Id$
+//
+// -------------------------------------------------------------------
+//
+// Borrowed and modified from boost.org smart_ptr.hpp
+// Original copyright notice:
+//
+//  (C) Copyright Greg Colvin and Beman Dawes 1998, 1999. Permission
+//  to copy, use, modify, sell and distribute this software is granted
+//  provided this copyright notice appears in all copies. This
+//  software is provided "as is" without express or implied warranty,
+//  and with no claim as to its suitability for any purpose.
 //
 ///////////////////////////////////////////////////////////////////////
 
@@ -30,7 +41,7 @@ public:
 	 {}
 
   template <class TT>
-  explicit scoped_ptr( TT* p=0 ) throw() :
+  explicit scoped_ptr( TT* p ) throw() :
 	 ptr(p)
 	 {}
 
@@ -58,6 +69,124 @@ private:
 
   T* ptr;
 };
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ *  An enhanced relative of scoped_ptr with reference counted copy semantics.
+ *  The object pointed to is deleted when the last shared_ptr pointing to it
+ *  is destroyed or reset.
+ *
+ **/
+///////////////////////////////////////////////////////////////////////
+
+template<class T>
+class shared_ptr {
+public:
+  typedef T element_type;
+
+  explicit shared_ptr(T* p =0) :
+	 px(p)
+	 {
+		try { pn = new long(1); }  // fix: prevent leak if new throws
+		catch (...) { delete p; throw; } 
+	 }
+
+  shared_ptr(const shared_ptr& r) throw() : px(r.px) { ++*(pn = r.pn); }
+
+  ~shared_ptr() { dispose(); }
+
+  shared_ptr& operator=(const shared_ptr& r)
+	 {
+		share(r.px,r.pn);
+		return *this;
+	 }
+
+  template<class TT>
+  shared_ptr(const shared_ptr<TT>& r) throw() :
+	 px(r.px)
+	 { 
+		++*(pn = r.pn); 
+	 }
+
+  template<class TT>
+  shared_ptr& operator=(const shared_ptr<TT>& r)
+	 {
+		share(r.px,r.pn);
+		return *this;
+	 }
+
+  void reset(T* p=0)
+	 {
+		if ( px == p ) return;  // fix: self-assignment safe
+		if (--*pn == 0) { delete px; }
+		else { // allocate new reference counter
+		  try { pn = new long; }  // fix: prevent leak if new throws
+		  catch (...) {
+			 ++*pn;  // undo effect of --*pn above to meet effects guarantee 
+			 delete p;
+			 throw;
+		  } // catch
+		} // allocate new reference counter
+		*pn = 1;
+		px = p;
+	 }
+
+  T& operator*() const throw()
+	 { return *px; }
+
+  T* operator->() const throw()
+	 { return px; }
+
+  T* get() const throw()
+	 { return px; }
+
+  long use_count() const throw()
+	 { return *pn; }
+
+  bool unique() const throw()
+	 { return *pn == 1; }
+
+  void swap(shared_ptr<T>& other) throw()
+	 {
+		T* other_px = other.px;
+		other.px = this->px;
+		this->px = other_px;
+		long* other_pn = other.pn;
+		other.pn = this->pn;
+		this->pn = other_pn;
+	 }
+
+private:
+
+  T*     px;     // contained pointer
+  long*  pn;     // ptr to reference counter
+
+  template<class TT> friend class shared_ptr;
+
+  void dispose() { if (--*pn == 0) { delete px; delete pn; } }
+
+  void share(T* rpx, long* rpn)
+	 {
+		if (pn != rpn) {
+		  dispose();
+		  px = rpx;
+		  ++*(pn = rpn);
+		}
+	 }
+};
+
+template<class T, class U>
+inline bool operator==(const shared_ptr<T>& a, const shared_ptr<U>& b)
+{
+  return a.get() == b.get();
+}
+
+template<class T, class U>
+inline bool operator!=(const shared_ptr<T>& a, const shared_ptr<U>& b)
+{
+  return a.get() != b.get();
+}
 
 static const char vcid_pointers_h[] = "$Header$";
 #endif // !POINTERS_H_DEFINED
