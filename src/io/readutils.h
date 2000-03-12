d85b7244b4a949897b4259d1122049fa15496327
Robert J. Peters <rjpcal@gmail.com>
2000-03-12 07:22:20 +0000

    Added readSequenceCount().

--- src/io/readutils.h
+++ src/io/readutils.h
@@ -3,7 +3,7 @@
 // readutils.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Nov 16 14:25:40 1999
-// written: Mon Mar  6 12:38:12 2000
+// written: Sat Mar 11 23:21:33 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -26,58 +26,68 @@
 class ReadUtils {
 public:
 
+  /// Get the number of elements in the stored sequence \a seq_name.
+  static int readSequenceCount(Reader* reader, const char* seq_name)
+	 {
+		return reader->readInt(makeSeqCountString(name));
+	 }
+
   /** Provides a generic interface for handling containers, sequences,
 		etc. of value types. */
   template <class Inserter, class T>
   static void readValueSeq(Reader* reader,
-									const char* name, Inserter inserter, T* = 0) {
-	 int count = reader->readInt(makeSeqCountString(name));
+									const char* name, Inserter inserter, T* = 0)
+	 {
+		int count = readSequenceCount(reader, name);
 
-	 for (int i = 0; i < count; ++i) {
-		T temp;
-		reader->readValue(makeElementNameString(name, i), temp);
-		*inserter = temp;
-		++inserter;
+		for (int i = 0; i < count; ++i) {
+		  T temp;
+		  reader->readValue(makeElementNameString(name, i), temp);
+		  *inserter = temp;
+		  ++inserter;
+		}
 	 }
-  }
 
   /** Provides a generic interface for handling containers, sequences,
 		etc. of Value subtypes. */
   template <class Inserter, class T>
   static void readValueObjSeq(Reader* reader,
-										const char* name, Inserter inserter, T* = 0) {
-	 int count = reader->readInt(makeSeqCountString(name));
+										const char* name, Inserter inserter, T* = 0)
+	 {
+		int count = readSequenceCount(reader, name);
 
-	 for (int i = 0; i < count; ++i) {
-		T temp;
-		reader->readValueObj(makeElementNameString(name, i), temp);
-		*inserter = temp;
-		++inserter;
+		for (int i = 0; i < count; ++i) {
+		  T temp;
+		  reader->readValueObj(makeElementNameString(name, i), temp);
+		  *inserter = temp;
+		  ++inserter;
+		}
 	 }
-  }
 
   /** Provides a generic interface for handling containers, sequences,
       etc. of IO objects */
   template <class Inserter, class C>
   static void readObjectSeq(Reader* reader,
-									 const char* name, Inserter inserter, C* /*dummy*/) {
-	 int count = reader->readInt(makeSeqCountString(name));
+									 const char* name, Inserter inserter, C* /*dummy*/)
+	 {
+		int count = readSequenceCount(reader, name);
 
-	 for (int i = 0; i < count; ++i) {
-		IO* io = reader->readObject(makeElementNameString(name, i));
+		for (int i = 0; i < count; ++i) {
+		  IO* io = reader->readObject(makeElementNameString(name, i));
 
-		if (io == 0) {
-		  *inserter = static_cast<C*>(0);
+		  if (io == 0) {
+			 *inserter = static_cast<C*>(0);
+		  }
+
+		  else {
+			 C* obj = dynamic_cast<C*>(io);
+			 if (obj == 0) throw ReadError("failed cast in readObjectSeq");
+			 *inserter = obj;
+		  }
+
+		  ++inserter;
 		}
-
-		else {
-		  C* obj = dynamic_cast<C*>(io);
-		  if (obj == 0) throw ReadError("failed cast in readObjectSeq");
-		  *inserter = obj;
-		}
-
 	 }
-  }
 
 private:
   // Warning: the return value of these functions is only valid up
