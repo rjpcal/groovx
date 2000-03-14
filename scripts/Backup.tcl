19434834fb96f9b8ea6c416c83aa4364f3081b96
Robert J. Peters <rjpcal@gmail.com>
2000-03-14 21:21:40 +0000

    Initial revision

---
+++ scripts/Backup.tcl
@@ -0,0 +1,10 @@
+# $Id$
+set date_ext [clock format [clock seconds] -format %Y_%m_%d]
+set filename grsh_backup_${date_ext}.tar.gz
+set sources "../grsh/RCS ../grsh/testing/RCS"
+puts "generating archive \"$filename\" from \"$sources\"..."
+eval exec tar cvfz $filename $sources
+puts "moving to ftp directory..."
+exec mv $filename /cit/rjpeters/ftp/
+puts "sending email notification..."
+exec echo "made backup $filename" | mail rjpeters
