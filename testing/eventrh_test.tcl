cd3498524b70cf4f685bab1353ad96a2d71e50fc
Robert J. Peters <rjpcal@gmail.com>
2000-03-17 00:47:01 +0000

    Changed expected error string to match new error string from program.

--- testing/eventrh_test.tcl
+++ testing/eventrh_test.tcl
@@ -24,37 +24,37 @@ test "EventRhTcl-EventRh::useFeedback" "too few args" {
 test "EventRhTcl-EventRh::useFeedback" "too many args" {
 	 EventRh::useFeedback junk junk junk
 } {^wrong \# args: should be "EventRh::useFeedback item_id\(s\) \?new_value\(s\)\?"$}
-test "EventRhTcl-EventRh::useFeedback" "normal use" {
+test "EventRhTcl-EventRh::useFeedback" "normal use 1" {
 	 EventRh::useFeedback $::rhid true
 	 EventRh::useFeedback $::rhid
 } {^1$}
-test "EventRhTcl-EventRh::useFeedback" "normal use" {
+test "EventRhTcl-EventRh::useFeedback" "normal use 2" {
 	 EventRh::useFeedback $::rhid yes
 	 EventRh::useFeedback $::rhid
 } {^1$}
-test "EventRhTcl-EventRh::useFeedback" "normal use" {
+test "EventRhTcl-EventRh::useFeedback" "normal use 3" {
 	 EventRh::useFeedback $::rhid 1
 	 EventRh::useFeedback $::rhid
 } {^1$}
-test "EventRhTcl-EventRh::useFeedback" "normal use" {
+test "EventRhTcl-EventRh::useFeedback" "normal use 4" {
 	 EventRh::useFeedback $::rhid -1.5
 	 EventRh::useFeedback $::rhid
 } {^1$}
-test "EventRhTcl-EventRh::useFeedback" "normal use" {
+test "EventRhTcl-EventRh::useFeedback" "normal use 5" {
 	 EventRh::useFeedback $::rhid false
 	 EventRh::useFeedback $::rhid
 } {^0$}
-test "EventRhTcl-EventRh::useFeedback" "normal use" {
+test "EventRhTcl-EventRh::useFeedback" "normal use 6" {
 	 EventRh::useFeedback $::rhid no
 	 EventRh::useFeedback $::rhid
 } {^0$}
-test "EventRhTcl-EventRh::useFeedback" "normal use" {
+test "EventRhTcl-EventRh::useFeedback" "normal use 7" {
 	 EventRh::useFeedback $::rhid 0
 	 EventRh::useFeedback $::rhid
 } {^0$}
 test "EventRhTcl-EventRh::useFeedback" "error on non-boolean input" {
 	 EventRh::useFeedback $::rhid FLASE
-} {^expected boolean value but got "FLASE"$}
+} {expected boolean value but got "FLASE"}
 test "EventRhTcl-EventRh::useFeedback" "error on bad rhid" {
 	 EventRh::useFeedback -1
 } {^EventRh::useFeedback: .*$}
--- testing/kbdrh_test.tcl
+++ testing/kbdrh_test.tcl
@@ -24,37 +24,37 @@ test "KbdRhTcl-KbdRh::useFeedback" "too few args" {
 test "KbdRhTcl-KbdRh::useFeedback" "too many args" {
 	 KbdRh::useFeedback junk junk junk
 } {^wrong \# args: should be "EventRh::useFeedback item_id\(s\) \?new_value\(s\)\?"$}
-test "KbdRhTcl-KbdRh::useFeedback" "normal use" {
+test "KbdRhTcl-KbdRh::useFeedback" "normal use 1" {
 	 KbdRh::useFeedback $::rhid true
 	 KbdRh::useFeedback $::rhid
 } {^1$}
-test "KbdRhTcl-KbdRh::useFeedback" "normal use" {
+test "KbdRhTcl-KbdRh::useFeedback" "normal use 2" {
 	 KbdRh::useFeedback $::rhid yes
 	 KbdRh::useFeedback $::rhid
 } {^1$}
-test "KbdRhTcl-KbdRh::useFeedback" "normal use" {
+test "KbdRhTcl-KbdRh::useFeedback" "normal use 3" {
 	 KbdRh::useFeedback $::rhid 1
 	 KbdRh::useFeedback $::rhid
 } {^1$}
-test "KbdRhTcl-KbdRh::useFeedback" "normal use" {
+test "KbdRhTcl-KbdRh::useFeedback" "normal use 4" {
 	 KbdRh::useFeedback $::rhid -1.5
 	 KbdRh::useFeedback $::rhid
 } {^1$}
-test "KbdRhTcl-KbdRh::useFeedback" "normal use" {
+test "KbdRhTcl-KbdRh::useFeedback" "normal use 5" {
 	 KbdRh::useFeedback $::rhid false
 	 KbdRh::useFeedback $::rhid
 } {^0$}
-test "KbdRhTcl-KbdRh::useFeedback" "normal use" {
+test "KbdRhTcl-KbdRh::useFeedback" "normal use 6" {
 	 KbdRh::useFeedback $::rhid no
 	 KbdRh::useFeedback $::rhid
 } {^0$}
-test "KbdRhTcl-KbdRh::useFeedback" "normal use" {
+test "KbdRhTcl-KbdRh::useFeedback" "normal use 7" {
 	 KbdRh::useFeedback $::rhid 0
 	 KbdRh::useFeedback $::rhid
 } {^0$}
 test "KbdRhTcl-KbdRh::useFeedback" "error on non-boolean input" {
 	 KbdRh::useFeedback $::rhid FLASE
-} {^expected boolean value but got "FLASE"$}
+} {expected boolean value but got "FLASE"}
 test "KbdRhTcl-KbdRh::useFeedback" "error on bad rhid" {
 	 KbdRh::useFeedback -1
 } {^EventRh::useFeedback: .*$}
