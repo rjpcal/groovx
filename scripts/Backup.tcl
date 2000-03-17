29edea045d6f41fbf4d03593734d58886a8d77c4
Robert J. Peters <rjpcal@gmail.com>
2000-03-17 00:48:24 +0000

    Updated to reflect the new directory structure of the project.

--- scripts/Backup.tcl
+++ scripts/Backup.tcl
@@ -1,7 +1,15 @@
 # $Id$
 set date_ext [clock format [clock seconds] -format %Y_%m_%d]
 set filename grsh_backup_${date_ext}.tar.gz
-set sources "../grsh/RCS ../grsh/testing/RCS"
+set sources "../grsh/Makefile \
+		  ../grsh/RCS \
+		  ../grsh/doc \
+		  ../grsh/idep \
+		  ../grsh/logs \
+		  ../grsh/scripts \
+		  ../grsh/src \
+		  ../grsh/testing/ \
+		  "
 puts "generating archive \"$filename\" from \"$sources\"..."
 eval exec tar cvfz $filename $sources
 puts "moving to ftp directory..."
