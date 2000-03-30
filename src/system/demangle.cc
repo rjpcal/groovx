2b1489d8f14eab7e81cb4cd34cacf0beb3f356ac
Robert J. Peters <rjpcal@gmail.com>
2000-03-30 22:00:42 +0000

    Improved the demangle() function for g++ to handle namespace qualifiers, builtin types, arrays, pointers, references, etc. (It's still not perfect, though.)

--- src/system/demangle.cc
+++ src/system/demangle.cc
@@ -3,7 +3,7 @@
 // demangle.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Wed Oct 13 10:41:19 1999
-// written: Mon Mar 20 20:16:34 2000
+// written: Thu Mar 30 13:59:08 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -11,7 +11,7 @@
 #ifndef DEMANGLE_CC_DEFINED
 #define DEMANGLE_CC_DEFINED
 
-#include "demangle.h"
+#include "system/demangle.h"
 
 #ifdef ACC_COMPILER
 
@@ -22,14 +22,6 @@ const char* demangle_cstr(const char* in) { return in; }
 
 #ifdef GCC_COMPILER
 
-string demangle(const string& in) { return in; }
-
-const char* demangle_cstr(const char* in) {
-  static string result;
-  result = demangle(in);
-  return result.c_str();
-}
-
 #include <cctype>
 #include <cstdlib>
 #include <string>
@@ -38,8 +30,16 @@ const char* demangle_cstr(const char* in) {
 #define LOCAL_ASSERT
 #include "util/debug.h"
 
+string demangle(const string& in);
+
+const char* demangle_cstr(const char* in) {
+  static string result;
+  result = demangle(in);
+  return result.c_str();
+}
+
 namespace {
-  int munchInt(string& str, int pos) {
+  int munchInt(string& str, string::size_type pos) {
 	 int val = 0;
 	 while ( isdigit(str[pos]) ) {
 		val *= 10;
@@ -48,6 +48,91 @@ namespace {
 	 }
 
 	 return val;
+  }
+
+  void putInt(int val, string& str, string::size_type pos) {
+	 while (val != 0) {
+		str.insert(pos, 1, '0' + (val%10));
+		val /= 10;
+	 }
+  }
+
+  string readModifiers(string& str, string::size_type& pos) {
+	 string modifiers("");
+	 string array_dims("");
+
+	 bool have_array = false;
+
+	 while ( isupper(str[pos]) ) {
+		switch (str[pos]) {
+		case 'A':
+		  str.erase(pos, 1); // we need to also erase the '_'
+		  array_dims.append("[");
+		  if ( isdigit(str[pos]) )
+			 {
+				int dim_size = munchInt(str, pos);
+				putInt(dim_size+1,array_dims,array_dims.length());
+			 }
+		  array_dims.append("]");
+		  break;
+		case 'C':
+		  modifiers.insert(0, " const");
+		  break;
+		case 'P':
+		  modifiers.insert(0, "*");
+		  break;
+		case 'R':
+		  modifiers.insert(0, "&");
+		  break;
+		case 'U':
+		  modifiers.insert(0, " unsigned");
+		  break;
+		default:
+		  Assert(0);
+		}
+		str.erase(pos, 1);
+	 }
+	 DebugEvalNL(str);
+
+	 modifiers.append(array_dims);
+
+	 return modifiers;
+  }
+
+  void insertBuiltinTypename(string& str, string::size_type& pos) {
+
+	 char typecode = str[pos];
+	 str.erase(pos, 1);
+	 switch (typecode) {
+	 case 'b':
+		str.insert(pos, "bool"); pos += 4;
+		break;
+	 case 'c':
+		str.insert(pos, "char"); pos += 4;
+		break;
+	 case 'd':
+		str.insert(pos, "double"); pos += 6;
+		break;
+	 case 'f':
+		str.insert(pos, "float"); pos += 5;
+		break;
+	 case 'i':
+		str.insert(pos, "int"); pos += 3;
+		break;
+	 case 'l':
+		str.insert(pos, "long"); pos += 4;
+		break;
+	 case 's':
+		str.insert(pos, "short"); pos += 5;
+		break;
+	 case 'v':
+		str.insert(pos, "void"); pos += 4;
+		break;
+	 default:
+		DebugEvalNL(typecode);
+		Assert(0);
+		break;
+	 }
   }
 }
 
@@ -61,23 +146,52 @@ DOTRACE("demangle");
 
   if (out.length() == 0) return out;
 
-  if (out[0] == 't') {
-	 isTemplate = true;
+  string::size_type pos = 0;
+
+  // Check if we have namespace qualifiers...
+  if (out[0] == 'Q') {
 	 out.erase(0, 1);
+	 int num_levels = 0;
+	 // get the number of nesting levels...
+	 if (out[0] == '_') {
+		out.erase(0, 1);
+		num_levels = munchInt(out, 0);
+		DebugEvalNL(num_levels);
+		Assert(out[0] == '_');
+		out.erase(0, 1);
+	 }
+	 else {
+		num_levels = out[0] - '0';
+		out.erase(0, 1);
+	 }
+	 while (num_levels > 1) {
+		// Get the length of the current qualifier
+		int length = munchInt(out, pos);
+		// Skip over the qualifier itself
+		pos += length;
+		// Insert a scope resolution operator after the qualifier "::"
+		out.insert(pos, "::");
+		// Skip over the just-inserted "::"
+		pos += 2;
+		--num_levels;
+	 }
   }
 
   DebugEvalNL(out);
 
+  if (out[pos] == 't') {
+	 isTemplate = true;
+	 out.erase(pos, 1);
+  }
+
   if ( !isTemplate ) {
-	 while ( isdigit(out[0]) ) {
-		out.erase(0, 1);
+	 while ( isdigit(out[pos]) ) {
+		out.erase(pos, 1);
 	 }
 
 	 DebugEvalNL(out);
   }
   else {
-	 int pos = 0;
-
 	 // Read length of template base name and skip over it
 	 int base_length = munchInt(out, pos);
 	 pos += base_length;
@@ -97,9 +211,20 @@ DOTRACE("demangle");
 
 		DebugEvalNL(out);
 
-		// Read length of template parameter name and skip over it
-		int param_length = munchInt(out, pos);
-		pos += param_length;
+		// Get the parameter name:
+		string modifiers = readModifiers(out, pos);
+
+		if ( !isdigit(out[pos]) ) {
+		  insertBuiltinTypename(out, pos);
+		}
+		else {
+		  // Read length of template parameter name and skip over it
+		  int param_length = munchInt(out, pos);
+		  pos += param_length;
+		}
+
+		out.insert(pos, modifiers);
+		pos += modifiers.length();
 
 		// Insert a comma if this is not the last parameter
 		if (n < (num_parameters-1)) {
