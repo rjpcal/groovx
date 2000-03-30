fa71f1070fde77e70caa7b544892c04bbfe5e34c
Robert J. Peters <rjpcal@gmail.com>
2000-03-30 20:07:31 +0000

    Changed 'timingHandler' to 'SimpleTh'.

--- testing/th_test.tcl
+++ testing/th_test.tcl
@@ -11,9 +11,9 @@ source ${::TEST_DIR}/list_test.tcl
 List::testList ThListTcl ThList Th Th SimpleTh
 
 ### SimpleTh::timingHandlerCmd ###
-test "ThTcl-SimpleTh::timingHandler" "too many args" {
-    SimpleTh::timingHandler junk
-} {^wrong \# args: should be "SimpleTh::timingHandler"$}
-test "ThTcl-SimpleTh::timingHandler" "normal use" {
-	 catch {SimpleTh::timingHandler}
+test "ThTcl-SimpleTh::SimpleTh" "too many args" {
+    SimpleTh::SimpleTh junk
+} {^wrong \# args: should be "SimpleTh::SimpleTh"$}
+test "ThTcl-SimpleTh::SimpleTh" "normal use" {
+	 catch {SimpleTh::SimpleTh}
 } {^0$}
