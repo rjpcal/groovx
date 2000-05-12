ad81c4e42e20a1ba8dbd7b0f1e7beca47788c168
Robert J. Peters <rjpcal@gmail.com>
2000-05-12 03:27:41 +0000

    Initial revision

---
+++ src/visx/trialbase.cc
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////
+//
+// trialbase.cc
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Tue Jan 25 18:41:20 2000
+// written: Tue Jan 25 18:41:36 2000
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef TRIALBASE_CC_DEFINED
+#define TRIALBASE_CC_DEFINED
+
+#include "trialbase.h"
+
+TrialBase::~TrialBase () {}
+
+static const char vcid_trialbase_cc[] = "$Header$";
+#endif // !TRIALBASE_CC_DEFINED
---
+++ src/visx/trialbase.h
@@ -0,0 +1,62 @@
+///////////////////////////////////////////////////////////////////////
+//
+// trialbase.h
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Tue Jan 25 18:41:19 2000
+// written: Thu May 11 20:18:39 2000
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef TRIALBASE_H_DEFINED
+#define TRIALBASE_H_DEFINED
+
+namespace GWT {
+  class Canvas;
+  class Widget;
+}
+
+namespace Util { class ErrorHandler; }
+
+class Block;
+class Response;
+
+///////////////////////////////////////////////////////////////////////
+//
+// TrialBase class definition
+//
+///////////////////////////////////////////////////////////////////////
+
+class TrialBase {
+public:
+  virtual ~TrialBase();
+
+  // accessors
+  virtual int trialType() const = 0;
+  virtual int lastResponse() const = 0;
+  virtual const char* description() const = 0;
+
+  // manipulators
+  virtual void undoLastResponse() = 0;
+
+  // actions
+  virtual void trDoTrial(GWT::Widget& widget,
+								 Util::ErrorHandler& errhdlr, Block& block) = 0;
+
+  virtual int trElapsedMsec() = 0;
+
+  virtual void trAbortTrial() = 0;
+  virtual void trEndTrial() = 0;
+  virtual void trNextTrial() = 0;
+  virtual void trHaltExpt() = 0;
+  virtual void trResponseSeen() = 0;
+  virtual void trRecordResponse(const Response& response) = 0;
+  virtual void trDrawTrial() const = 0;
+  virtual void trUndrawTrial() const = 0;
+
+  virtual void trDraw(GWT::Canvas& canvas, bool flush) const = 0;
+  virtual void trUndraw(GWT::Canvas& canvas, bool flush) const = 0;
+};
+
+static const char vcid_trialbase_h[] = "$Header$";
+#endif // !TRIALBASE_H_DEFINED
