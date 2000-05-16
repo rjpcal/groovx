936c4e907cdada8cb48d777b6ba6fe873bcf0d04
Robert J. Peters <rjpcal@gmail.com>
2000-05-16 02:33:52 +0000

    Replaced Singleton mechanism (again) with a private static member of the class itself... this mechanism is faster than a function-scope static or a file-scope pointer.o

--- src/objlist.h
+++ src/objlist.h
@@ -3,7 +3,7 @@
 // objlist.h 
 // Rob Peters
 // Created: Nov-98
-// written: Wed Mar 29 14:07:36 2000
+// written: Mon May 15 19:33:50 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -28,6 +28,8 @@ class GrObj;
 class ObjList : public PtrList<GrObj> {
 private:
   typedef PtrList<GrObj> Base;
+
+  static ObjList theInstance;
 
 protected:
   /// Construct and reserve space for \a size objects.
