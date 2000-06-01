446c5398634945c2f3ec363d4d5e662eb0f1e3bb
Robert J. Peters <rjpcal@gmail.com>
2000-06-01 01:38:57 +0000

    Added explicit instantiations for the basic types.

--- src/tclitempkgbase.cc
+++ src/tclitempkgbase.cc
@@ -3,7 +3,7 @@
 // tclitempkgbase.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Wed May 31 18:32:51 2000
-// written: Wed May 31 18:35:44 2000
+// written: Wed May 31 18:38:45 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -13,6 +13,8 @@
 
 #include "tcl/tclitempkgbase.h"
 
+#include "util/strings.h"
+
 template <class T>
 Getter<T>::~Getter() {}
 
@@ -21,6 +23,24 @@ Setter<T>::~Setter() {}
 
 template <class T>
 Attrib<T>::~Attrib() {}
+
+template class Getter<int>;
+template class Getter<bool>;
+template class Getter<double>;
+template class Getter<const char*>;
+template class Getter<const fixed_string&>;
+
+template class Setter<int>;
+template class Setter<bool>;
+template class Setter<double>;
+template class Setter<const char*>;
+template class Setter<const fixed_string&>;
+
+template class Attrib<int>;
+template class Attrib<bool>;
+template class Attrib<double>;
+template class Attrib<const char*>;
+template class Attrib<const fixed_string&>;
 
 Tcl::TclItemPkgBase::TclItemPkgBase(Tcl_Interp* interp,
 												const char* name, const char* version) :
