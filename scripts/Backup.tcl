d214c50c4d074c9b4966141f4ef194599dc46e74
Robert J. Peters <rjpcal@gmail.com>
2000-09-14 17:03:17 +0000

    Added chmod -w to archive.

--- scripts/Backup.tcl
+++ scripts/Backup.tcl
@@ -1,6 +1,9 @@
 # $Id$
+
 set date_ext [clock format [clock seconds] -format %Y_%m_%d]
-set filename grsh_backup_${date_ext}.tar.gz
+
+set archive_name grsh_backup_${date_ext}.tar.gz
+
 set sources "../grsh/Makefile \
 		  ../grsh/RCS \
 		  ../grsh/doc \
@@ -10,9 +13,12 @@ set sources "../grsh/Makefile \
 		  ../grsh/src \
 		  ../grsh/testing/ \
 		  "
-puts "generating archive \"$filename\" from \"$sources\"..."
-eval exec tar cvfz $filename $sources
+puts "generating archive \"$archive_name\" from \"$sources\"..."
+eval exec tar cvfz $archive_name $sources
+exec chmod -w $archive_name
+
 puts "moving to ftp directory..."
-exec mv $filename /cit/rjpeters/ftp/
+exec mv $archive_name /cit/rjpeters/ftp/
+
 puts "sending email notification..."
-exec echo "made backup $filename" | mail rjpeters
+exec echo "made backup $archive_name" | mail rjpeters
