fce9f8e86fc85558fd01e44e8744f55446b88128
Robert J. Peters <rjpcal@gmail.com>
2000-09-20 00:15:24 +0000

    Added getLibraryDirectory().

--- src/visx/application.cc
+++ src/visx/application.cc
@@ -3,7 +3,7 @@
 // application.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Dec  7 11:05:52 1999
-// written: Thu Jun  1 13:41:51 2000
+// written: Tue Sep 19 17:14:23 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -12,6 +12,9 @@
 #define APPLICATION_CC_DEFINED
 
 #include "application.h"
+
+#include "system/system.h"
+#include "util/strings.h"
 
 #include "util/trace.h"
 
@@ -25,9 +28,26 @@ NoAppError::NoAppError(const char* msg) : ErrorWithMsg(msg) {}
 
 NoAppError::~NoAppError() {}
 
-Application::Application() 
+class Application::Impl {
+private:
+  Impl(const Impl&);
+  Impl& operator=(const Impl&);
+
+public:
+  Impl(const char* library_dir) : itsLibraryDir(library_dir) {}
+
+  fixed_string itsLibraryDir;
+};
+
+Application::Application(const char* library_env_var) :
+  itsImpl(new Impl(System::theSystem().getenv(library_env_var)))
 {
 DOTRACE("Application::Application");
+}
+
+Application::~Application() {
+DOTRACE("Application::~Application");
+  delete itsImpl; 
 }
 
 void Application::installApp(Application* theApp) {
@@ -37,14 +57,17 @@ DOTRACE("Application::installApp");
   }
 }
 
-Application::~Application() {}
-
 Application& Application::theApp() {
 DOTRACE("Application::theApp");
   if (theSingleton == 0) {
 	 throw NoAppError("the application has not yet been installed");
   }
   return *theSingleton;
+}
+
+const char* Application::getLibraryDirectory() const {
+DOTRACE("Application::getLibraryDirectory");
+  return itsImpl->itsLibraryDir.c_str();
 }
 
 static const char vcid_application_cc[] = "$Header$";
