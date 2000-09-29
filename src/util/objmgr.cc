57f5ace649335691b681af04502bd826c2440570
Robert J. Peters <rjpcal@gmail.com>
2000-09-29 17:32:57 +0000

    Removed the newIO() functions that take an istream& argument... this functionality has been supplanted by what LegacyReader can do.

--- src/util/objmgr.cc
+++ src/util/objmgr.cc
@@ -3,7 +3,7 @@
 // iomgr.cc
 // Rob Peters
 // created: Fri Apr 23 01:13:16 1999
-// written: Tue Sep 26 18:25:31 2000
+// written: Fri Sep 29 10:32:52 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -15,35 +15,12 @@
 
 #include "io/iofactory.h"
 
-#include "util/strings.h"
-
-#include <iostream.h>
-#include <typeinfo>
-
 #define NO_TRACE
 #include "util/trace.h"
-#define LOCAL_ASSERT
-#include "util/debug.h"
 
 IO::IoObject* IO::IoMgr::newIO(const char* type) {
 DOTRACE("IO::IoMgr::newIO(const char*)");
   return IO::IoFactory::theOne().newCheckedObject(type);
-}
-
-IO::IoObject* IO::IoMgr::newIO(STD_IO::istream& is, IO::IOFlag flag) {
-DOTRACE("IO::IoMgr::newIo(STD_IO::istream&, IO::IOFlag)");
-  fixed_string type;
-  is >> type;
-  // We must turn off the IO::TYPENAME flag since the typename has already
-  // been read from the input stream by the present function.
-  return newIO(type.c_str(), is, flag & ~IO::TYPENAME);
-}
-
-IO::IoObject* IO::IoMgr::newIO(const char* type, STD_IO::istream& is, IO::IOFlag flag) {
-DOTRACE("IO::IoMgr::newIo(const char*, STD_IO::istream&, IO::IOFlag)");
-  IO::IoObject* io = newIO(type);
-  io->ioDeserialize(is, flag);
-  return io;
 }
 
 static const char vcid_iomgr_cc[] = "$Header$";
--- src/util/objmgr.h
+++ src/util/objmgr.h
@@ -2,32 +2,20 @@
 // iomgr.h
 // Rob Peters
 // created: Fri Apr 23 01:12:37 1999
-// written: Sat Sep 23 15:37:51 2000
+// written: Fri Sep 29 10:32:06 2000
 // $Id$
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef IOMGR_H_DEFINED
 #define IOMGR_H_DEFINED
 
-#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(IO_H_DEFINED)
-#include "io/io.h"
-#endif
-
-#ifdef PRESTANDARD_IOSTREAMS
-class istream;
-class ostream;
-#else
-#  if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(IOSFWD_DEFINED)
-#    include <iosfwd>
-#    define IOSFWD_DEFINED
-#  endif
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(IODECLS_H_DEFINED)
+#include "io/iodecls.h"
 #endif
 
 namespace IO {
   namespace IoMgr {
 	 IO::IoObject* newIO(const char* type);
-	 IO::IoObject* newIO(STD_IO::istream &is, IO::IOFlag flag);
-	 IO::IoObject* newIO(const char* type, STD_IO::istream &is, IO::IOFlag flag);
   }
 }
 
