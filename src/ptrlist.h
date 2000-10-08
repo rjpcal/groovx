7a119db9feb7178883ac5fff4d313a19fe881a4b
Robert J. Peters <rjpcal@gmail.com>
2000-10-08 23:58:12 +0000

    Added PtrHandle<T> and made this the return type of getPtr() and getCheckedPtr(). Removed release(), since the reference counting allows us to emulate this by first getting the object via getPtr(), then removing reference to it from the list using remove().

--- src/ptrlist.h
+++ src/ptrlist.h
@@ -3,17 +3,13 @@
 // ptrlist.h
 // Rob Peters
 // created: Fri Apr 23 00:35:31 1999
-// written: Sun Oct  8 15:55:58 2000
+// written: Sun Oct  8 16:54:00 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef PTRLIST_H_DEFINED
 #define PTRLIST_H_DEFINED
-
-#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(DUMBPTR_H_DEFINED)
-#include "dumbptr.h"
-#endif
 
 #if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(IOPTRLIST_H_DEFINED)
 #include "ioptrlist.h"
@@ -35,6 +31,50 @@ public:
   virtual bool isValid() const;
 
   virtual bool operator==(const MasterPtrBase& other);
+};
+
+template <class T>
+class PtrHandle {
+public:
+  explicit PtrHandle(MasterPtr<T>* master) : itsMaster(master)
+  {
+	 if (master == 0) throw ErrorWithMsg("MasterPtr<T> was null");
+	 itsMaster->incrRefCount();
+  }
+
+  ~PtrHandle()
+    { itsMaster->decrRefCount(); }
+
+  PtrHandle(const PtrHandle& other) : itsMaster(other.itsMaster)
+    { itsMaster->incrRefCount(); }
+
+  PtrHandle& operator=(const PtrHandle& other)
+    {
+		PtrHandle otherCopy(other);
+		this->swap(otherCopy);
+		return *this;
+	 }
+
+  MasterPtr<T>* masterPtr()
+    { return itsMaster; }
+
+        T* operator->()       { return (itsMaster->getPtr()); }
+  const T* operator->() const { return (itsMaster->getPtr()); }
+        T& operator*()        { return *(itsMaster->getPtr()); }
+  const T& operator*()  const { return *(itsMaster->getPtr()); }
+
+        T* get()              { return (itsMaster->getPtr()); }
+  const T* get()        const { return (itsMaster->getPtr()); }
+
+private:
+  void swap(PtrHandle& other)
+    {
+		MasterPtr<T>* otherMaster = other.itsMaster;
+		other.itsMaster = this->itsMaster;
+		this->itsMaster = otherMaster;
+	 }
+
+  MasterPtr<T>* itsMaster;
 };
 
 ///////////////////////////////////////////////////////////////////////
@@ -71,7 +111,7 @@ public:
 
   typedef T* Ptr;
 
-  typedef DumbPtr<T> SharedPtr;
+  typedef PtrHandle<T> SharedPtr;
 
   ///////////////
   // accessors //
@@ -83,34 +123,28 @@ public:
       index into the list. If \a id is out of range, a segmentation
       fault may occur. If \a is in range, but is not an index for a
       valid object, the pointer returned may be null. */
-  SharedPtr getPtr(int id) const throw () 
+  SharedPtr getPtr(int id) const
     {
 		MasterPtrBase* voidPtr = VoidPtrList::getVoidPtr(id);
+		// cast as reference to force an exception on error
 		MasterPtr<T>& tPtr = dynamic_cast<MasterPtr<T>&>(*voidPtr);
-		return SharedPtr(tPtr.getPtr());
+		return SharedPtr(&tPtr);
 	 }
 
   /** Returns the object at index \a id, after a check is performed to
       ensure that \a id is in range, and refers to a valid object. If
       the check fails, an \c InvalidIdError exception is thrown. */
-  SharedPtr getCheckedPtr(int id) const throw (InvalidIdError)
+  SharedPtr getCheckedPtr(int id) const
 	 {
 		MasterPtrBase* voidPtr = VoidPtrList::getCheckedVoidPtr(id);
+		// cast as reference to force an exception on error
 		MasterPtr<T>& tPtr = dynamic_cast<MasterPtr<T>&>(*voidPtr);
-		return SharedPtr(tPtr.getPtr());
+		return SharedPtr(&tPtr);
 	 }
 
   //////////////////
   // manipulators //
   //////////////////
-
-#if 0
-  /** Releases the object at index \a id. Ownership of the pointed-to
-      object is transferred to the caller, and the list site at \a id
-      becomes vacant. */
-  Ptr release(int id) throw (InvalidIdError)
-	 { return Ptr(castToT(VoidPtrList::releaseVoidPtr(id))); }
-#endif
 
   /// Insert \a ptr into the list, and return its id.
   int insert(T* ptr)
