76a52610faf4a7db940e367ae561ed410dce8cd0
Robert J. Peters <rjpcal@gmail.com>
2000-10-09 15:35:24 +0000

    Moved the definitions of MasterPtr<T> virtual functions into "masterptrt.cc".

--- src/ptrlist.cc
+++ src/ptrlist.cc
@@ -3,7 +3,7 @@
 // ptrlist.cc
 // Rob Peters
 // created: Fri Apr 23 00:35:32 1999
-// written: Sun Oct  8 18:42:06 2000
+// written: Mon Oct  9 08:33:05 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -18,39 +18,6 @@
 #include "util/strings.h"
 
 #include <typeinfo>
-
-template <class T>
-MasterPtr<T>::MasterPtr(T* ptr) :
-  MasterIoPtr(),
-  itsPtr(ptr)
-{}
-
-template <class T>
-MasterPtr<T>::~MasterPtr()
-{
-  delete itsPtr;
-}
-
-template <class T>
-IO::IoObject* MasterPtr<T>::ioPtr() const
-{
-  return dynamic_cast<IO::IoObject*>(itsPtr);
-}
-
-template <class T>
-bool MasterPtr<T>::isValid() const
-{
-  return (itsPtr != 0);
-}
-
-template <class T>
-bool MasterPtr<T>::operator==(const MasterPtrBase& other)
-{
-  const MasterPtr<T>* otherPtr = dynamic_cast<const MasterPtr<T>*>(&other);
-  if (otherPtr == 0) return false;
-
-  return (otherPtr->itsPtr == this->itsPtr);
-}
 
 template <class T>
 PtrList<T>::PtrList(int size) :
