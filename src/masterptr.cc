5aa2d25cba6b840d0161e9c32d1a27492bb6b8ff
Robert J. Peters <rjpcal@gmail.com>
2000-10-09 16:15:38 +0000

    Initial revision

---
+++ src/masterptr.cc
@@ -0,0 +1,106 @@
+///////////////////////////////////////////////////////////////////////
+//
+// masterptr.cc
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Mon Oct  9 08:19:46 2000
+// written: Mon Oct  9 08:52:14 2000
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef MASTERPTR_CC_DEFINED
+#define MASTERPTR_CC_DEFINED
+
+#include "masterptr.h"
+
+#include "util/error.h"
+
+#define NO_TRACE
+#include "util/trace.h"
+#define LOCAL_ASSERT
+#include "util/debug.h"
+
+///////////////////////////////////////////////////////////////////////
+//
+// MasterPtrBase member definitions
+//
+///////////////////////////////////////////////////////////////////////
+
+void MasterPtrBase::throwErrorWithMsg(const char* msg) {
+DOTRACE("MasterPtrBase::throwErrorWithMsg");
+  throw ErrorWithMsg(msg);
+}
+
+MasterPtrBase::MasterPtrBase() :
+  itsRefCount(0)
+{
+DOTRACE("MasterPtrBase::MasterPtrBase");
+}
+
+MasterPtrBase::~MasterPtrBase()
+{
+DOTRACE("MasterPtrBase::~MasterPtrBase");
+}
+
+void MasterPtrBase::incrRefCount() {
+DOTRACE("MasterPtrBase::incrRefCount");
+  ++itsRefCount;
+}
+
+void MasterPtrBase::decrRefCount() {
+DOTRACE("MasterPtrBase::decrRefCount");
+  --itsRefCount;
+  if (itsRefCount <= 0)
+	 delete this;
+}
+
+int MasterPtrBase::refCount() const {
+DOTRACE("MasterPtrBase::refCount");
+  return itsRefCount;
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+// NullMasterPtr member definitions
+//
+///////////////////////////////////////////////////////////////////////
+
+NullMasterPtr::NullMasterPtr() :
+	 MasterPtrBase()
+{
+DOTRACE("NullMasterPtr::NullMasterPtr");
+}
+
+NullMasterPtr::~NullMasterPtr() {
+DOTRACE("NullMasterPtr::~NullMasterPtr");
+}
+
+bool NullMasterPtr::isValid() const {
+DOTRACE("NullMasterPtr::isValid");
+  return false;
+}
+
+bool NullMasterPtr::operator==(const MasterPtrBase& other) { 
+DOTRACE("NullMasterPtr::operator==");
+  bool otherIsNull = (dynamic_cast<const NullMasterPtr*>(&other) != 0);
+  return otherIsNull;
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+// MasterIoPtr member definitions
+//
+///////////////////////////////////////////////////////////////////////
+
+MasterIoPtr::MasterIoPtr()
+{
+DOTRACE("MasterIoPtr::MasterIoPtr");
+}
+
+MasterIoPtr::~MasterIoPtr()
+{
+DOTRACE("MasterIoPtr::~MasterIoPtr");
+}
+
+static const char vcid_masterptr_cc[] = "$Header$";
+#endif // !MASTERPTR_CC_DEFINED
---
+++ src/masterptr.h
@@ -0,0 +1,169 @@
+///////////////////////////////////////////////////////////////////////
+//
+// masterptr.h
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Mon Oct  9 08:18:28 2000
+// written: Mon Oct  9 08:56:16 2000
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef MASTERPTR_H_DEFINED
+#define MASTERPTR_H_DEFINED
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * MasterPtrBase
+ *
+ **/
+///////////////////////////////////////////////////////////////////////
+
+class MasterPtrBase {
+private:
+  int itsRefCount;
+
+  // These are disallowed since MasterPtrBase's should always be in
+  // one-to-one correspondence with their pointee's.
+  MasterPtrBase(const MasterPtrBase& other);
+  MasterPtrBase& operator=(const MasterPtrBase& other);
+
+public:
+  /// Utility function to throw an \c ErrorWithMsg exception.
+  static void throwErrorWithMsg(const char* msg);
+
+  MasterPtrBase();
+
+  virtual ~MasterPtrBase();
+
+  void incrRefCount();
+  void decrRefCount();
+
+  int refCount() const;
+
+  virtual bool isValid() const = 0;
+
+  virtual bool operator==(const MasterPtrBase& other) = 0;
+};
+
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * NullMasterPtr
+ *
+ **/
+///////////////////////////////////////////////////////////////////////
+
+class NullMasterPtr : public MasterPtrBase {
+public:
+  /// Default constructor.
+  NullMasterPtr();
+
+  /// Virtual destructor.
+  virtual ~NullMasterPtr();
+
+  /// Overridden from \c MasterPtrBase to always returns false.
+  virtual bool isValid() const;
+
+  /// Returns true if and only if \a other is also a \c NullMasterPtr.
+  virtual bool operator==(const MasterPtrBase& other);
+};
+
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * MasterIoPtr
+ *
+ **/
+///////////////////////////////////////////////////////////////////////
+
+namespace IO { class IoObject; }
+
+class MasterIoPtr : public MasterPtrBase {
+public:
+  MasterIoPtr();
+  virtual ~MasterIoPtr();
+
+  virtual IO::IoObject* ioPtr() const = 0;
+
+  virtual bool isValid() const = 0;
+
+  virtual bool operator==(const MasterPtrBase& other) = 0;
+};
+
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * MasterPtr<T> template
+ *
+ **/
+///////////////////////////////////////////////////////////////////////
+
+template <class T>
+class MasterPtr : public MasterIoPtr {
+private:
+  T* itsPtr;
+
+public:
+  explicit MasterPtr(T* ptr);
+  virtual ~MasterPtr();
+
+  T* getPtr() { return itsPtr; }
+
+  virtual IO::IoObject* ioPtr() const;
+
+  virtual bool isValid() const;
+
+  virtual bool operator==(const MasterPtrBase& other);
+};
+
+template <class T>
+class PtrHandle {
+public:
+  explicit PtrHandle(MasterPtr<T>* master) : itsMaster(master)
+  {
+	 if (master == 0) MasterPtrBase::throwErrorWithMsg(
+								"MasterPtr<T>* was null in PtrHandle<T>()");
+	 itsMaster->incrRefCount();
+  }
+
+  ~PtrHandle()
+    { itsMaster->decrRefCount(); }
+
+  PtrHandle(const PtrHandle& other) : itsMaster(other.itsMaster)
+    { itsMaster->incrRefCount(); }
+
+  PtrHandle& operator=(const PtrHandle& other)
+    {
+		PtrHandle otherCopy(other);
+		this->swap(otherCopy);
+		return *this;
+	 }
+
+  MasterPtr<T>* masterPtr()
+    { return itsMaster; }
+
+        T* operator->()       { return (itsMaster->getPtr()); }
+  const T* operator->() const { return (itsMaster->getPtr()); }
+        T& operator*()        { return *(itsMaster->getPtr()); }
+  const T& operator*()  const { return *(itsMaster->getPtr()); }
+
+        T* get()              { return (itsMaster->getPtr()); }
+  const T* get()        const { return (itsMaster->getPtr()); }
+
+private:
+  void swap(PtrHandle& other)
+    {
+		MasterPtr<T>* otherMaster = other.itsMaster;
+		other.itsMaster = this->itsMaster;
+		this->itsMaster = otherMaster;
+	 }
+
+  MasterPtr<T>* itsMaster;
+};
+
+static const char vcid_masterptr_h[] = "$Header$";
+#endif // !MASTERPTR_H_DEFINED
