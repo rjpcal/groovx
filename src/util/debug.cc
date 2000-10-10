fc995402c0f200907248544284068bf2534c913d
Robert J. Peters <rjpcal@gmail.com>
2000-10-10 02:28:01 +0000

    Added calls to Util::Trace::printStackTrace() before we abort().

--- src/util/debug.cc
+++ src/util/debug.cc
@@ -3,7 +3,7 @@
 // debug.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Mon Oct  9 18:48:38 2000
-// written: Mon Oct  9 19:13:33 2000
+// written: Mon Oct  9 19:27:40 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -16,27 +16,33 @@
 #include <cstdlib>
 #include <iostream.h>
 
+#include "util/trace.h"
+
 void Debug::AssertImpl(const char* what, const char* where, int line_no) {
   cerr << "Assertion failed: '" << what << "' in "
 		 << where << " line " << line_no << '\n';
+  Util::Trace::printStackTrace(cerr);
   abort();
 }
 
 void Debug::PreconditionImpl(const char* what, const char* where, int line_no) {
   cerr << "Precondition failed: '" << what << "' in "
 		 << where << " line " << line_no << '\n';
+  Util::Trace::printStackTrace(cerr);
   abort();
 }
 
 void Debug::PostconditionImpl(const char* what, const char* where, int line_no) {
   cerr << "Postcondition failed: '" << what << "' in "
 		 << where << " line " << line_no << '\n';
+  Util::Trace::printStackTrace(cerr);
   abort();
 }
 
 void Debug::InvariantImpl(const char* what, const char* where, int line_no) {
   cerr << "Invariant failed: '" << what << "' in "
 		 << where << " line " << line_no << '\n';
+  Util::Trace::printStackTrace(cerr);
   abort();
 }
 
