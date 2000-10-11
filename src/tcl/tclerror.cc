68cacf905e9819586be3ee4bd3e40b704db7f0aa
Robert J. Peters <rjpcal@gmail.com>
2000-10-11 18:36:30 +0000

    Added Tcl::BkdErrorHandler, which handles error messages by calling Tcl_BackgroundError() on a Tcl_Interp*.

--- src/tcl/tclerror.cc
+++ src/tcl/tclerror.cc
@@ -3,7 +3,7 @@
 // tclerror.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Sun Jun 20 15:10:13 1999
-// written: Wed May 31 18:50:55 2000
+// written: Wed Oct 11 11:04:09 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -13,8 +13,12 @@
 
 #include "tcl/tclerror.h"
 
+#include <tcl.h>
+
 #define NO_TRACE
 #include "util/trace.h"
+#define LOCAL_ASSERT
+#include "util/debug.h"
 
 namespace Tcl {
 
@@ -32,6 +36,38 @@ DOTRACE("TclError::TclError");
 
 TclError::~TclError() {}
 
+}
+
+
+void Tcl::BkdErrorHandler::raiseBackgroundError(const char* msg) {
+DOTRACE("Tcl::BkdErrorHandler::raiseBackgroundError");
+  Tcl_AppendResult(itsInterp, msg, (char*) 0);
+  Tcl_BackgroundError(itsInterp);
+}
+
+Tcl::BkdErrorHandler::BkdErrorHandler(Tcl_Interp* interp) :
+  itsInterp(interp)
+{
+DOTRACE("Tcl::BkdErrorHandler::BkdErrorHandler");
+  Invariant(itsInterp != 0);
+}
+
+Tcl::BkdErrorHandler::~BkdErrorHandler() {}
+
+void Tcl::BkdErrorHandler::handleUnknownError() {
+  raiseBackgroundError("an error of unknown type occurred");
+}
+
+void Tcl::BkdErrorHandler::handleError(Error&) {
+  raiseBackgroundError("an Error of unknown type occurred");
+}
+
+void Tcl::BkdErrorHandler::handleErrorWithMsg(ErrorWithMsg& err) {
+  raiseBackgroundError(err.msg_cstr());
+}
+
+void Tcl::BkdErrorHandler::handleMsg(const char* msg) {
+  raiseBackgroundError(msg);
 }
 
 static const char vcid_tclerror_cc[] = "$Header$";
