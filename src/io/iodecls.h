cdedbf0c80f69a1d4e10e5673a581d80c6d7c87f
Robert J. Peters <rjpcal@gmail.com>
2000-10-19 22:41:40 +0000

    Removed legacy IOFlag declarations and moved them into "iolegacy.h".

--- src/io/iodecls.h
+++ src/io/iodecls.h
@@ -3,7 +3,7 @@
 // iodecls.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Wed Sep 27 14:44:42 2000
-// written: Thu Oct 19 15:39:48 2000
+// written: Thu Oct 19 15:41:27 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -23,20 +23,6 @@ namespace IO {
   class LogicError;
   class ValueError;
   class FilenameError;
-
-  /** The symbolic constants of type \c IOFlag flags may be OR'ed
-   * together and passed to the flag argument of \c legacySrlz() or \c
-   * legacyDesrlz() to control different aspects of the formatting used
-   * to read and write objects. In general, the same flags must be
-   * used to read an object as were used to write it. */
-  typedef int IOFlag;
-
-  /// Neither the class's name or its bases will be written/read
-  const IOFlag NO_FLAGS   = 0;
-  /// The class's name is written/read
-  const IOFlag TYPENAME   = 1 << 0;
-  /// The class's bases are written/read
-  const IOFlag BASES      = 1 << 1;
 
   /// This type is used for verion ids during the read+write process.
   typedef signed long VersionId;
