93403b12569110f624b1c6aa85dbe8875e8ef304
Robert J. Peters <rjpcal@gmail.com>
2000-10-20 00:48:54 +0000

    Removed legacy IO stuff.

--- src/util/objdb.cc
+++ src/util/objdb.cc
@@ -3,7 +3,7 @@
 // ioptrlist.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Sun Nov 21 00:26:29 1999
-// written: Tue Oct 10 09:19:48 2000
+// written: Thu Oct 19 17:48:43 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -15,8 +15,6 @@
 
 #include "masterptr.h"
 
-#include "io/iolegacy.h"
-#include "io/iomgr.h"
 #include "io/readutils.h"
 #include "io/writeutils.h"
 
@@ -38,107 +36,8 @@ IoPtrList::~IoPtrList() {
 DOTRACE("IoPtrList::~IoPtrList");
 }
 
-void IoPtrList::legacySrlz(IO::LegacyWriter* lwriter) const {
-DOTRACE("IoPtrList::legacySrlz");
-
-  fixed_string ioTag = IO::IoObject::ioTypename();
-
-  // itsVec: we will legacySrlz only the non-null T*'s in
-  // itsVec. In order to correctly legacyDesrlz the object later, we
-  // must write both the size of itsVec (in order to correctly
-  // resize later), as well as the number of non-null objects that we
-  // legacySrlz (so that legacyDesrlz knows when to stop reading).
-
-  lwriter->writeValue("voidVecSize", baseVecSize());
-
-  int num_non_null = PtrListBase::count();
-  lwriter->setFieldSeparator('\n');
-  lwriter->writeValue("num_non_null", num_non_null);
-  lwriter->resetFieldSeparator();
-
-  // Serialize all non-null ptr's.
-  int c = 0;
-  for (size_t i = 0, end = baseVecSize();
-		 i < end;
-		 ++i) {
-	 if (getPtrBase(i)->isValid()) {
-		lwriter->writeValue("i", i);
-
-		// we must legacySrlz the typename since legacyDesrlz requires a
-		// typename in order to call the virtual constructor
-		MasterIoPtr* ioPtr = dynamic_cast<MasterIoPtr*>(getPtrBase(i));
-		IO::IoObject* obj = ioPtr ? ioPtr->ioPtr() : 0;
-
-		IO::LWFlagJanitor jtr_(*lwriter, lwriter->flags() | IO::TYPENAME);
-		lwriter->writeObject("ptrListItem", obj);
-
-		++c;
-	 }
-  }
-
-  if (c != num_non_null) {
-	 throw IO::LogicError(ioTag.c_str());
-  }
-
-  lwriter->setFieldSeparator('\n');
-  lwriter->writeInt("itsFirstVacant", firstVacant());
-}
-
-fixed_string IoPtrList::legacyValidTypenames() const {
-DOTRACE("IoPtrList::legacyValidTypenames");
-  fixed_string ioTag = IO::IoObject::ioTypename();
-
-  dynamic_string typename_list = ioTag;
-  typename_list += " ";
-  typename_list += alternateIoTags();
-  return typename_list.c_str();
-}
-
-void IoPtrList::legacyDesrlz(IO::LegacyReader* lreader) {
-DOTRACE("IoPtrList::legacyDesrlz");
-
-  fixed_string ioTag = IO::IoObject::ioTypename();
-
-  int size, num_non_null;
-  lreader->readValue("voidVecSize", size);
-  lreader->readValue("num_non_null", num_non_null);
-
-  if ( size < 0 || num_non_null < 0 || num_non_null > size ) {
-	 throw IO::ValueError(ioTag.c_str());
-  }
-
-  PtrListBase::clear();
-  baseVecResize(size);
-
-  int ptrid;
-
-  for (int i = 0; i < num_non_null; ++i) {
-	 lreader->readValue("i", ptrid);
-
-	 if (ptrid < 0 || ptrid >= size) {
-		throw IO::ValueError(ioTag.c_str());
-	 }
-
-	 IO::IoObject* obj = lreader->readObject("ptrListItem");
-
-	 insertPtrBaseAt(ptrid, makeMasterIoPtr(obj));
-  }
-
-  firstVacant() = lreader->readInt("itsFirstVacant");
-
-  // The next character after itsFirstVacant had better be a newline,
-  // and we need to remove it from the stream.
-  lreader->grabNewline();
-}
-
 void IoPtrList::readFrom(IO::Reader* reader) {
 DOTRACE("IoPtrList::readFrom");
-
-  IO::LegacyReader* lreader = dynamic_cast<IO::LegacyReader*>(reader); 
-  if (lreader != 0) {
-	 legacyDesrlz(lreader);
-	 return;
-  }
 
   firstVacant() = reader->readInt("itsFirstVacant");
 
@@ -161,15 +60,8 @@ DOTRACE("IoPtrList::readFrom");
 	 }
 }
 
-
 void IoPtrList::writeTo(IO::Writer* writer) const {
 DOTRACE("IoPtrList::writeTo");
-
-  IO::LegacyWriter* lwriter = dynamic_cast<IO::LegacyWriter*>(writer);
-  if (lwriter != 0) {
-	 legacySrlz(lwriter);
-	 return;
-  }
 
   writer->writeInt("itsFirstVacant", firstVacant());
 
@@ -190,12 +82,6 @@ DOTRACE("IoPtrList::writeTo");
 	 }
 
   IO::WriteUtils::writeObjectSeq(writer, "itsVec", ioBlock.begin(), ioBlock.end());
-}
-
-const char* IoPtrList::alternateIoTags() const {
-DOTRACE("IoPtrList::alternateIoTags");
-  static const char* result = ""; 
-  return result;
 }
 
 static const char vcid_ioptrlist_cc[] = "$Header$";
