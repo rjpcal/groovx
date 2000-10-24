d559cb81b444c919cfa313e355cb422770954c4b
Robert J. Peters <rjpcal@gmail.com>
2000-10-24 20:03:33 +0000

    Added get/set Ok/ErrSound.

--- src/visx/sound.cc
+++ src/visx/sound.cc
@@ -3,7 +3,7 @@
 // sound.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Thu Jul  8 11:43:07 1999
-// written: Thu Jun  1 13:57:41 2000
+// written: Tue Oct 24 13:01:42 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -21,11 +21,32 @@
 #include "irixsound.cc"
 #endif
 
+namespace {
+  NullableItemWithId<Sound> OK_SOUND(0);
+  NullableItemWithId<Sound> ERR_SOUND(0);
+}
+
 SoundError::SoundError() : ErrorWithMsg() {}
 SoundError::SoundError(const char* msg) : ErrorWithMsg(msg) {}
 SoundError::~SoundError() {}
 
 Sound::~Sound () {}
+
+void Sound::setOkSound(ItemWithId<Sound> ok_sound) {
+  OK_SOUND = ok_sound;
+}
+
+void Sound::setErrSound(ItemWithId<Sound> err_sound) {
+  ERR_SOUND = err_sound;
+}
+
+ItemWithId<Sound> Sound::getOkSound() {
+  return ItemWithId<Sound>(OK_SOUND.get(), OK_SOUND.id());
+}
+
+ItemWithId<Sound> Sound::getErrSound() {
+  return ItemWithId<Sound>(ERR_SOUND.get(), ERR_SOUND.id());
+}
 
 static const char vcid_sound_cc[] = "$Header$";
 #endif // !SOUND_CC_DEFINED
