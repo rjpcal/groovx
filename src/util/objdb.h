10cdbf70707cff65c303accf469a2100e2b2a55b
Robert J. Peters <rjpcal@gmail.com>
2000-10-26 17:43:54 +0000

    Removed the PtrListBase base class, moving its functionality up into IoPtrListBase, since PtrListBase was now working only with IO::IoObject's anyway.

--- src/util/objdb.h
+++ src/util/objdb.h
@@ -3,7 +3,7 @@
 // ioptrlist.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Sun Nov 21 00:26:29 1999
-// written: Wed Oct 25 16:47:54 2000
+// written: Thu Oct 26 10:43:28 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -11,40 +11,176 @@
 #ifndef IOPTRLIST_H_DEFINED
 #define IOPTRLIST_H_DEFINED
 
-#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(PTRLISTBASE_H_DEFINED)
-#include "ptrlistbase.h"
-#endif
-
 #if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(IO_H_DEFINED)
 #include "io/io.h"
 #endif
 
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(ERROR_H_DEFINED)
+#include "util/error.h"
+#endif
+
+namespace IO { class IoObject; }
+
+/**
+ *
+ * InvalidIdError is an exception class that will be thrown from
+ * IoPtrList if an attempt to use an invalid id is made in a checked
+ * function.
+ *
+ **/
+
+class InvalidIdError : public ErrorWithMsg {
+public:
+  /// Default constructor.
+  InvalidIdError();
+  /// Construct with an informative message \a msg.
+  InvalidIdError(const char* msg);
+  /// Virtual destructor.
+  virtual ~InvalidIdError();
+};
+
 ///////////////////////////////////////////////////////////////////////
 /**
  *
- * \c IoPtrList extends \c PtrListBase by adding and implementing the
- * \c IO interface. This class remains abstract, however, because the
- * ensureCorrectType() function from PtrListBase is not implemented
- * here.
+ * IoPtrList.
  *
  **/
 ///////////////////////////////////////////////////////////////////////
 
-class IoPtrList : public PtrListBase, public virtual IO::IoObject {
-public:
+class IoPtrList : public virtual IO::IoObject {
+protected:
   /// Default constructor makes an empty list.
   IoPtrList();
 
+public:
   /// Virtual destructor.
   virtual ~IoPtrList();
+
+  //
+  // IO interface
+  //
 
   virtual IO::VersionId serialVersionId() const;
   virtual void readFrom(IO::Reader* reader);
   virtual void writeTo(IO::Writer* writer) const;
 
+  class Impl;
+  class ItrImpl;
+
+  //
+  // Iterators
+  //
+
+  class Iterator {
+  private:
+	 ItrImpl* itsImpl;
+  public:
+	 Iterator(ItrImpl* impl);
+	 ~Iterator();
+	 Iterator(const Iterator& other);
+	 Iterator& operator=(const Iterator& other);
+
+	 bool operator==(const Iterator& other) const;
+
+	 bool operator!=(const Iterator& other) const
+		{ return !(this->operator==(other)); }
+
+	 Iterator& operator++();
+
+	 int getId() const;
+
+	 IO::IoObject* getObject() const;
+  };
+
+  class IdIterator : public Iterator {
+	 IdIterator();
+  public:
+	 IdIterator(const Iterator& other) : Iterator(other) {}
+
+	 int operator*() const { return getId(); }
+  };
+
+  class PtrIterator : public Iterator {
+	 PtrIterator();
+  public:
+	 PtrIterator(const Iterator& other) : Iterator(other) {}
+
+	 IO::IoObject* operator*() const { return getObject(); }
+  };
+
+  Iterator begin() const;
+  Iterator end() const;
+
+  IdIterator beginIds() const { return begin(); }
+  IdIterator endIds() const { return end(); }
+
+  PtrIterator beginPtrs() const { return begin(); }
+  PtrIterator endPtrs() const { return end(); }
+
+  class Inserter;
+  friend class Inserter;
+
+  class Inserter {
+	 IoPtrList* itsList;
+  public:
+	 Inserter(IoPtrList* list_) : itsList(list_) {}
+
+	 Inserter& operator=(IO::IoObject* obj)
+		{ itsList->insertPtrBase(obj); return *this; }
+
+	 Inserter& operator*() { return *this; }
+	 Inserter& operator++() { return *this; }
+	 Inserter& operator++(int) { return *this; }
+  };
+
+  Inserter inserter() { return Inserter(this); }
+
+  //
+  // Collection interface
+  //
+
+  /** Returns the number of filled sites in the PtrList. */
+  int count() const;
+
+  /** Returns true if 'id' is a valid index into a non-NULL T* in
+		the PtrList, given its current size. */
+  bool isValidId(int id) const;
+
+  /** If the object at \a id is unshared, removes reference to the
+      object at index \a id, causing the object to be destroyed since
+      it was unshared. If the object is shared, this operation throws
+      an exception. */
+  void remove(int id);
+
+  /** Removes reference to the object at \a id. */
+  void release(int id);
+
+  /** Releases references to all unshared objects held by the
+      list. Since the objects are unshared, they will be destroyed in
+      the process. */
+  void clear();
+
+protected:
+  /** Return the \c IO::IoObject* at the index given by \a id. Checks
+		first if \a id is a valid index, and throws an \c InvalidIdError
+		if it is not. */
+  IO::IoObject* getCheckedPtrBase(int id) const throw (InvalidIdError);
+
+  /** Add ptr at the next available location, and return the index
+		where it was inserted. If necessary, the list will be expanded
+		to make room for the ptr. The PtrList now assumes control of the
+		memory management for the object *ptr. */
+  int insertPtrBase(IO::IoObject* ptr);
+
+  /** Subclasses override to throw an exception if \a ptr is of the
+      wrong type to be in the list. */
+  virtual void ensureCorrectType(const IO::IoObject* ptr) const = 0;
+
 private:
   IoPtrList(const IoPtrList&);
   IoPtrList& operator=(const IoPtrList&);
+
+  Impl* const itsImpl;
 };
 
 static const char vcid_ioptrlist_h[] = "$Header$";
