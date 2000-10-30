42643ce45a6f455b1c0a1535843ce3cd13a6ecc1
Robert J. Peters <rjpcal@gmail.com>
2000-10-30 18:38:01 +0000

    Replaced 'GrObj::type' with 'IO::type'.

--- testing/objlist_test.tcl
+++ testing/objlist_test.tcl
@@ -23,28 +23,28 @@ test "ObjList-ObjList::loadObjects" "normal file read with no comments" {
 	 ObjList::reset
 	 set objids [ObjList::loadObjects $::TEST_DIR/faces_file_no_comments]
 	 set num_read [llength $objids]
-	 set result "$num_read [GrObj::type [lindex $objids 0]]"
+	 set result "$num_read [IO::type [lindex $objids 0]]"
 	 return $result
 } {^10 Face$}
 test "ObjList-ObjList::loadObjects" "normal file read" {
 	 ObjList::reset
 	 set objids [ObjList::loadObjects $::TEST_DIR/faces_file]
 	 set num_read [llength $objids]
-	 set result "$num_read [GrObj::type [lindex $objids 0]]"
+	 set result "$num_read [IO::type [lindex $objids 0]]"
 	 return $result
 } {^10 Face$}
 test "ObjList-ObjList::loadObjects" "normal file read with limit on # to read" {
 	 ObjList::reset
 	 set objids [ObjList::loadObjects $::TEST_DIR/faces_file 5]
 	 set num_read [llength $objids]
-	 set result "$num_read [GrObj::type [lindex $objids 0]]"
+	 set result "$num_read [IO::type [lindex $objids 0]]"
 	 return $result
 } {^5 Face$}
 test "ObjList-ObjList::loadObjects" "file read with virtual constructor" {
 	 ObjList::reset
 	 set objids [ObjList::loadObjects $::TEST_DIR/nos_faces_s50_c3]
 	 return "[llength $objids] \
-				[GrObj::type [lindex $objids 0]] [GrObj::type [lindex $objids 10]]"
+				[IO::type [lindex $objids 0]] [IO::type [lindex $objids 10]]"
 } {^20 *Face *CloneFace$}
 test "ObjList-ObjList::loadObjects" "empty file read" {
 	 ObjList::reset
