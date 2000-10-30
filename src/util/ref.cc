15feda3de48decb6af807979750725cdc8f680b7
Robert J. Peters <rjpcal@gmail.com>
2000-10-30 20:00:21 +0000

    Added #include "io/io.h".

--- src/util/ref.cc
+++ src/util/ref.cc
@@ -3,13 +3,15 @@
 // iditemutils.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Fri Oct 27 17:07:31 2000
-// written: Mon Oct 30 11:53:50 2000
+// written: Mon Oct 30 12:00:19 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef IDITEMUTILS_CC_DEFINED
 #define IDITEMUTILS_CC_DEFINED
+
+#include "io/io.h"
 
 #include "io/iditemutils.h"
 
