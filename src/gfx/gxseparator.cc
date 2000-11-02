16087038e2cd6e5692cc19276b2bd200ff44c287
Robert J. Peters <rjpcal@gmail.com>
2000-11-02 19:48:53 +0000

    Initial revision

---
+++ src/gfx/gxseparator.cc
@@ -0,0 +1,137 @@
+///////////////////////////////////////////////////////////////////////
+//
+// gxseparator.cc
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Thu Nov  2 11:24:04 2000
+// written: Thu Nov  2 11:47:36 2000
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef GXSEPARATOR_CC_DEFINED
+#define GXSEPARATOR_CC_DEFINED
+
+#include "gx/gxseparator.h"
+
+#include "gwt/canvas.h"
+#include "io/iditem.h"
+
+#include "util/error.h"
+#include "util/minivec.h"
+
+#include "util/trace.h"
+
+class GxSeparator::Impl {
+  Impl(const Impl&);
+  Impl& operator=(const Impl&);
+
+public:
+  Impl() {}
+  ~Impl() {}
+
+  typedef minivec<IdItem<GxNode> > VecType;
+  VecType itsChildren;
+};
+
+GxSeparator::GxSeparator() :
+  itsImpl(new Impl)
+{
+DOTRACE("GxSeparator::GxSeparator");
+}
+
+GxSeparator::~GxSeparator() {
+DOTRACE("GxSeparator::~GxSeparator");
+  delete itsImpl;
+}
+
+GxSeparator::ChildId GxSeparator::addChild(int ioId) {
+DOTRACE("GxSeparator::addChild");
+  itsImpl->itsChildren.push_back(IdItem<GxNode>(ioId)); 
+  return (itsImpl->itsChildren.size() - 1);
+}
+
+void GxSeparator::insertChild(int ioId, ChildId at_index) {
+DOTRACE("GxSeparator::insertChild");
+  if (at_index < 0)
+	 at_index = 0;
+
+  if (at_index > itsImpl->itsChildren.size())
+	 at_index = itsImpl->itsChildren.size();
+
+  itsImpl->itsChildren.insert(itsImpl->itsChildren.begin()+at_index,
+										IdItem<GxNode>(ioId));
+}
+
+IdItem<GxNode> GxSeparator::getChild(ChildId index) const {
+DOTRACE("GxSeparator::getChild");
+  if (index < 0 || index >= itsImpl->itsChildren.size())
+	 {
+		ErrorWithMsg err("GxSeparator has no child with index '");
+		err.appendNumber(index);
+		err.appendMsg("'");
+		throw err;
+	 }
+
+  return itsImpl->itsChildren[index];
+}
+
+void GxSeparator::removeChildId(ChildId index) {
+DOTRACE("GxSeparator::removeChildId");
+  if (index >= 0 && index < itsImpl->itsChildren.size())
+	 itsImpl->itsChildren.erase(itsImpl->itsChildren.begin()+index);
+}
+
+void GxSeparator::removeChildItem(int ioId) {
+DOTRACE("GxSeparator::removeChildItem");
+  for(Impl::VecType::iterator
+		  itr = itsImpl->itsChildren.begin(),
+		  end = itsImpl->itsChildren.end();
+		itr != end;
+		++itr)
+	 {
+		if ( (*itr)->id() == ioId )
+		  {
+			 itsImpl->itsChildren.erase(itr);
+			 break;
+		  }
+	 }   
+}
+
+GxSeparator::ChildItr GxSeparator::beginChildren() const {
+DOTRACE("GxSeparator::beginChildren");
+  return itsImpl->itsChildren.begin();
+}
+
+GxSeparator::ChildItr GxSeparator::endChildren() const {
+DOTRACE("GxSeparator::endChildren");
+  return itsImpl->itsChildren.end();
+}
+
+void GxSeparator::draw(GWT::Canvas& canvas) const {
+DOTRACE("GxSeparator::draw");
+
+  GWT::Canvas::StateSaver state(canvas);
+
+  for(ChildItr itr = beginChildren(), end = endChildren();
+		itr != end;
+		++itr)
+	 {
+		(*itr)->draw(canvas);
+	 }
+}
+
+void GxSeparator::undraw(GWT::Canvas& canvas) const {
+DOTRACE("GxSeparator::undraw");
+
+  GWT::Canvas::StateSaver state(canvas);
+
+  for(ChildItr itr = beginChildren(), end = endChildren();
+		itr != end;
+		++itr)
+	 {
+		(*itr)->undraw(canvas);
+	 }
+}
+
+static const char vcid_gxseparator_cc[] = "$Header$";
+#endif // !GXSEPARATOR_CC_DEFINED
---
+++ src/gfx/gxseparator.h
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////////////////
+//
+// gxseparator.h
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Thu Nov  2 11:20:15 2000
+// written: Thu Nov  2 11:47:58 2000
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef GXSEPARATOR_H_DEFINED
+#define GXSEPARATOR_H_DEFINED
+
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(GXNODE_H_DEFINED)
+#include "gx/gxnode.h"
+#endif
+
+template <class T> class IdItem;
+
+class GxSeparator : public GxNode {
+public:
+  GxSeparator();
+
+  virtual ~GxSeparator();
+
+  typedef int ChildId;
+
+  ChildId addChild(int ioId);
+  void insertChild(int ioId, ChildId at_index);
+  IdItem<GxNode> getChild(ChildId index) const;
+  void removeChildId(ChildId index);
+  void removeChildItem(int ioId);
+
+  typedef const IdItem<GxNode>* ChildItr;
+
+  ChildItr beginChildren() const;
+  ChildItr endChildren() const;
+
+  /// Draw all of the contained objects on \a canvas.
+  virtual void draw(GWT::Canvas& canvas) const;
+
+  /// Undraw all of the contained objects from \a canvas.
+  virtual void undraw(GWT::Canvas& canvas) const;
+
+private:
+  GxSeparator(const GxSeparator&);
+  GxSeparator& operator=(const GxSeparator&);
+
+  class Impl;
+  Impl* const itsImpl;
+};
+
+static const char vcid_gxseparator_h[] = "$Header$";
+#endif // !GXSEPARATOR_H_DEFINED
