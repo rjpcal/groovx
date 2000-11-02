7e4c7327cde7981995a8b7526161e5c25f768957
Robert J. Peters <rjpcal@gmail.com>
2000-11-02 22:48:01 +0000

    Initial revision

---
+++ src/gfx/gxtcl.cc
@@ -0,0 +1,105 @@
+///////////////////////////////////////////////////////////////////////
+//
+// gxtcl.cc
+// Rob Peters rjpeters@klab.caltech.edu
+// created: Thu Nov  2 14:39:14 2000
+// written: Thu Nov  2 14:44:16 2000
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef GXTCL_CC_DEFINED
+#define GXTCL_CC_DEFINED
+
+#include "gx/gxnode.h"
+#include "gx/gxseparator.h"
+
+#include "io/iofactory.h"
+
+#include "tcl/listitempkg.h"
+
+#include "util/trace.h"
+
+///////////////////////////////////////////////////////////////////////
+//
+// GxNodePkg
+//
+///////////////////////////////////////////////////////////////////////
+
+namespace GxTcl {
+  class ContainsCmd;
+  class GxNodePkg;
+}
+
+class GxTcl::ContainsCmd : public Tcl::TclItemCmd<GxNode> {
+public:
+  ContainsCmd(Tcl::CTclItemPkg<GxNode>* pkg, const char* cmd_name) :
+	 Tcl::TclItemCmd<GxNode>(pkg, cmd_name, "item_id other_id", 3, 3) {}
+
+protected:
+  virtual void invoke() {
+	 IdItem<GxNode> other(getIntFromArg(2));
+	 GxNode* sep = getItem();
+	 returnBool(sep->contains(other.get()));
+  }
+};
+
+class GxTcl::GxNodePkg : public Tcl::ItemPkg<GxNode> {
+public:
+  GxNodePkg(Tcl_Interp* interp) :
+	 Tcl::ItemPkg<GxNode>(interp, "GxNode", "$Revision$")
+	 {
+		addCommand( new ContainsCmd(this, makePkgCmdName("contains")) );
+	 }
+};
+
+///////////////////////////////////////////////////////////////////////
+//
+// GxSeparatorPkg
+//
+///////////////////////////////////////////////////////////////////////
+
+namespace GxTcl {
+  class AddChildCmd;
+  class GxSeparatorPkg;
+}
+
+class GxTcl::AddChildCmd : public Tcl::TclItemCmd<GxSeparator> {
+public:
+  AddChildCmd(Tcl::CTclItemPkg<GxSeparator>* pkg, const char* cmd_name) :
+	 Tcl::TclItemCmd<GxSeparator>(pkg, cmd_name,
+											"item_id child_item_id", 3, 3) {}
+
+protected:
+  virtual void invoke() {
+	 int child = getIntFromArg(2);
+	 GxSeparator* sep = getItem();
+	 int child_id = sep->addChild(child);
+	 returnInt(child_id);
+  }
+};
+
+class GxTcl::GxSeparatorPkg : public Tcl::ItemPkg<GxSeparator> {
+public:
+  GxSeparatorPkg(Tcl_Interp* interp) :
+	 Tcl::ItemPkg<GxSeparator>(interp, "GxSeparator", "$Revision$")
+	 {
+		addCommand( new AddChildCmd(this, makePkgCmdName("addChild")) );
+		declareCGetter("numChildren", &GxSeparator::numChildren);
+		declareCSetter("removeChildId", &GxSeparator::removeChildId);
+		declareCSetter("removeChildUid", &GxSeparator::removeChildUid);
+		IO::IoFactory::theOne().registerCreatorFunc(&GxSeparator::make);
+	 }
+};
+
+extern "C"
+int Gx_Init(Tcl_Interp* interp) {
+DOTRACE("Gx_Init");
+  Tcl::TclPkg* pkg1 = new GxTcl::GxNodePkg(interp);
+  Tcl::TclPkg* pkg2 = new GxTcl::GxSeparatorPkg(interp);
+
+  return pkg1->combineStatus(pkg2->initStatus());
+}
+
+static const char vcid_gxtcl_cc[] = "$Header$";
+#endif // !GXTCL_CC_DEFINED
---
+++ testing/gxsep_test.tcl
@@ -0,0 +1,180 @@
+##############################################################################
+###
+### GxSepTcl
+### Rob Peters
+### 2-Nov-1999
+### $Id$
+###
+##############################################################################
+
+package require Gxseparator
+package require Face
+
+### IO::new GxSeparator ###
+test "GxSepTcl-IO::new GxSeparator" "normal use" {
+	 set gxsep [IO::new GxSeparator]
+	 set is_gxnode [GxNode::is $gxsep]
+	 set is_gxsep [GxSeparator::is $gxsep]
+	 return "$gxsep $is_gxnode $is_gxsep"
+} "^${INT} 1 1$"
+
+### GxSeparator::addChild ###
+test "GxSepTcl-GxSeparator::addChild" "normal use" {
+	 set gxsep [IO::new GxSeparator]
+	 set face [IO::new Face]
+	 set child_id1 [GxSeparator::addChild $gxsep $face]
+	 set child_id2 [GxSeparator::addChild $gxsep $face]
+	 return "$child_id1 $child_id2"
+} {^0 1$}
+
+test "GxSepTcl-GxSeparator::addChild" "try to add self" {
+	 set gxsep [IO::new GxSeparator]
+	 set face [IO::new Face]
+	 GxSeparator::addChild $gxsep $face
+	 set code [catch {GxSeparator::addChild $gxsep $gxsep} result]
+	 return "$code $result"
+} {^1 GxSeparator::addChild}
+
+test "GxSepTcl-GxSeparator::addChild" "try to add node containing self" {
+	 set gxsep1 [IO::new GxSeparator]
+	 set face [IO::new Face]
+	 GxSeparator::addChild $gxsep1 $face
+
+	 set gxsep2 [IO::new GxSeparator]
+
+	 GxSeparator::addChild $gxsep2 $gxsep1
+
+	 set gxsep3 [IO::new GxSeparator]
+
+	 GxSeparator::addChild $gxsep3 $gxsep2
+
+	 set code [catch {GxSeparator::addChild $gxsep1 $gxsep3} result]
+	 return "$code $result"
+} {^1 GxSeparator::addChild}
+
+### GxSeparator::contains ###
+test "GxSepTcl-GxSeparator::contains" "test unrelated object" {
+	 set gxsep [IO::new GxSeparator]
+	 set face [IO::new Face]
+	 GxNode::contains $gxsep $face
+} {^0$}
+
+test "GxSepTcl-GxSeparator::contains" "test self" {
+	 set gxsep [IO::new GxSeparator]
+	 GxNode::contains $gxsep $gxsep
+} {^1$}
+
+test "GxSepTcl-GxSeparator::contains" "check contains containee" {
+	 set gxsep1 [IO::new GxSeparator]
+	 set gxsep2 [IO::new GxSeparator]
+	 GxSeparator::addChild $gxsep1 $gxsep2
+
+	 set 1_contains_2 [GxNode::contains $gxsep1 $gxsep2]
+	 set 2_contains_1 [GxNode::contains $gxsep2 $gxsep1]
+
+	 return "$1_contains_2 $2_contains_1"
+} {^1 0$}
+
+test "GxSepTcl-GxSeparator::contains" "check recursive contains" {
+	 set gxsep1 [IO::new GxSeparator]
+
+	 set gxsep2 [IO::new GxSeparator]
+
+	 GxSeparator::addChild $gxsep1 $gxsep2
+
+	 set gxsep3 [IO::new GxSeparator]
+
+	 GxSeparator::addChild $gxsep2 $gxsep3
+
+	 set face [IO::new Face]
+	 GxSeparator::addChild $gxsep3 $face
+
+	 GxNode::contains $gxsep1 $face
+} {^1$}
+
+### GxSeparator::numChildren ###
+test "GxSepTcl-GxSeparator::numChildren" "use when empty" {
+	 set gxsep [IO::new GxSeparator]
+	 GxSeparator::numChildren $gxsep
+} {^0$}
+
+test "GxSepTcl-GxSeparator::numChildren" "use with some children" {
+	 set gxsep [IO::new GxSeparator]
+	 set face [IO::new Face]
+	 set child_id1 [GxSeparator::addChild $gxsep $face]
+	 set child_id2 [GxSeparator::addChild $gxsep $face]
+	 GxSeparator::numChildren $gxsep
+} {^2$}
+
+test "GxSepTcl-GxSeparator::numChildren" "use after a remove" {
+	 set gxsep [IO::new GxSeparator]
+	 set face [IO::new Face]
+	 set child_id1 [GxSeparator::addChild $gxsep $face]
+	 set child_id2 [GxSeparator::addChild $gxsep $face]
+	 GxSeparator::removeChildId $gxsep $child_id1
+	 GxSeparator::numChildren $gxsep
+} {^1$}
+
+### GxSeparator::removeChildId ###
+test "GxSepTcl-GxSeparator::removeChildId" "id too small" {
+	 set gxsep [IO::new GxSeparator]
+	 GxSeparator::removeChildId $gxsep -1
+	 GxSeparator::numChildren $gxsep
+} {^0$}
+
+test "GxSepTcl-GxSeparator::removeChildId" "use when empty" {
+	 set gxsep [IO::new GxSeparator]
+	 GxSeparator::removeChildId $gxsep 0
+	 GxSeparator::numChildren $gxsep
+} {^0$}
+
+test "GxSepTcl-GxSeparator::removeChildId" "use when filled" {
+	 set gxsep [IO::new GxSeparator]
+	 set face [IO::new Face]
+	 GxSeparator::addChild $gxsep $face
+	 GxSeparator::addChild $gxsep $face
+	 GxSeparator::removeChildId $gxsep 0
+	 GxSeparator::numChildren $gxsep
+} {^1$}
+
+test "GxSepTcl-GxSeparator::removeChildId" "id too large" {
+	 set gxsep [IO::new GxSeparator]
+	 set face [IO::new Face]
+	 GxSeparator::addChild $gxsep $face
+	 GxSeparator::addChild $gxsep $face
+	 GxSeparator::removeChildId $gxsep 2
+	 GxSeparator::numChildren $gxsep
+} {^2$}
+
+
+### GxSeparator::removeChildUid ###
+test "GxSepTcl-GxSeparator::removeChildUid" "try non-contained item" {
+	 set gxsep [IO::new GxSeparator]
+	 set face [IO::new Face]
+	 GxSeparator::removeChildUid $gxsep $face
+	 GxSeparator::numChildren $gxsep
+} {^0$}
+
+test "GxSepTcl-GxSeparator::removeChildUid" "use when filled" {
+	 set gxsep [IO::new GxSeparator]
+	 set pos [IO::new Position]
+	 set face [IO::new Face]
+	 GxSeparator::addChild $gxsep $pos
+	 GxSeparator::addChild $gxsep $face
+	 GxSeparator::removeChildUid $gxsep $face
+	 return "[GxSeparator::numChildren $gxsep] [GxNode::contains $gxsep $face]"
+} {^1 0$}
+
+source ${::TEST_DIR}/io_test.tcl
+
+set ::GXSEP [IO::new GxSeparator]
+set ::FACE [IO::new Face]
+set ::POS [IO::new Position]
+
+GxSeparator::addChild $::GXSEP $::POS
+GxSeparator::addChild $::GXSEP $::FACE
+
+IO::testStringifyCmd GxSepTcl IO 1 $::GXSEP
+IO::testDestringifyCmd GxSepTcl IO 1 $::GXSEP
+IO::testWriteCmd GxSepTcl IO 1 $::GXSEP
+IO::testReadCmd GxSepTcl IO 1 $::GXSEP
