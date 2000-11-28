ba32f3134480743464b1ed4f58981a4408620fc8
Robert J. Peters <rjpcal@gmail.com>
2000-11-28 22:04:04 +0000

    Merged former derived class TlistWidget into the GWT::Widget base class. Once this works, we'll change the interface to use GxNode's instead of Trial's.

--- src/gwt/widget.h
+++ src/gwt/widget.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2000 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Thu Dec  2 15:05:17 1999
-// written: Fri Nov 10 17:03:51 2000
+// written: Tue Nov 28 14:03:29 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -18,6 +18,10 @@ namespace GWT {
   class Widget;
 }
 
+template <class T> class MaybeIdItem;
+
+class TrialBase;
+
 class GWT::Widget {
 public:
   virtual ~Widget();
@@ -27,10 +31,27 @@ public:
   virtual void bind(const char* event_sequence, const char* script) = 0;
   virtual void takeFocus() = 0;
   virtual void swapBuffers() = 0;
-  virtual void display() = 0;
-  virtual void clearscreen() = 0;
-  virtual void refresh() = 0;
-  virtual void undraw() = 0;
+
+  virtual void display();
+  virtual void clearscreen();
+  virtual void refresh();
+  virtual void undraw();
+
+  // Change the global visibility, which determines whether anything
+  // will be displayed by a "redraw" command, or by remap events sent
+  // to the screen window.
+  void setVisibility(bool vis);
+  
+  void setCurTrial(const MaybeIdItem<TrialBase>& item);
+
+  void setHold(bool hold_on);
+
+private:
+  Widget(const Widget&);
+  Widget& operator=(const Widget&);
+
+  class Impl;
+  Impl* const itsImpl;
 };
 
 static const char vcid_widget_h[] = "$Header$";
