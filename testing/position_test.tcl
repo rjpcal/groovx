57bd6b9a2a3dbe65c650b7c0f443adbcf65c0310
Robert J. Peters <rjpcal@gmail.com>
2000-11-29 21:27:14 +0000

    Updated tests for new FieldContainer implementation of Position.

--- testing/position_test.tcl
+++ testing/position_test.tcl
@@ -30,62 +30,56 @@ test "PositionTcl-IO::new Position" "normal create" {
 
 set pos [IO::new Position]
 
-### Pos::rotateCmd ###
-test "PositionTcl-Pos::rotate" "too few args" {
-    Pos::rotate
-} {wrong \# args: should be "Pos::rotate posid angle \?rot_axis_x rot_axis_y rot_axis_z\?"}
-test "PositionTcl-Pos::rotate" "illegal arg count" {
-    Pos::rotate posid angle junk
-} {wrong \# args: should be "Pos::rotate posid angle \?rot_axis_x rot_axis_y rot_axis_z\?"}
-test "PositionTcl-Pos::rotate" "too many" {
-    Pos::rotate posid angle j u n k
-} {wrong \# args: should be "Pos::rotate posid angle \?rot_axis_x rot_axis_y rot_axis_z\?"}
-test "PositionTcl-Pos::rotate" "normal use with angle only" {
-	 catch {Pos::rotate $::pos 3.0}
+### Pos::rotationAxis ###
+test "PositionTcl-Pos::rotationAxis" "too few args" {
+    Pos::rotationAxis
+} {wrong \# args: should be "Pos::rotationAxis item_id\(s\) \?new_value\(s\)\?"}
+test "PositionTcl-Pos::rotationAxis" "too many args" {
+    Pos::rotationAxis posid angle j u n k
+} {wrong \# args: should be "Pos::rotationAxis item_id\(s\) \?new_value\(s\)\?"}
+test "PositionTcl-Pos::rotationAxis" "normal use with angle and axis of rotation" {
+	 catch {Pos::rotationAxis $::pos {1.5 0.0 -2.2}}
 } {^0$}
-test "PositionTcl-Pos::rotate" "normal use with angle and axis of rotation" {
-	 catch {Pos::rotate $::pos 3.0 1.5 0.0 -2.2}
-} {^0$}
-test "PositionTcl-Pos::rotate" "error on non-numeric input" {
-    Pos::rotate $::pos junk
-} {expected floating-point number but got "junk"}
-test "PositionTcl-Pos::rotate" "error on bad posid" {
-    Pos::rotate -1 1 1 1 1
-} {Pos::rotate: attempt to access invalid id '.*' in}
+test "PositionTcl-Pos::rotationAxis" "error on non-numeric input" {
+    Pos::rotationAxis $::pos junk
+} {Pos::rotationAxis: }
+test "PositionTcl-Pos::rotationAxis" "error on bad posid" {
+    Pos::rotationAxis -1 {1 1 1}
+} {Pos::rotationAxis: attempt to access invalid id '.*' in}
 
-### scaleCmd ###
-test "PositionTcl-Pos::scale" "too few args" {
-    Pos::scale posid
-} {wrong \# args: should be "Pos::scale posid scale_x \?scale_y\? \?scale_z\?"}
-test "PositionTcl-Pos::scale" "too many args" {
-    Pos::scale posid j u n k
-} {wrong \# args: should be "Pos::scale posid scale_x \?scale_y\? \?scale_z\?"}
-test "PositionTcl-Pos::scale" "normal use" {
-	 catch {Pos::scale $::pos 1.5 3.0 4.5}
+### Pos::scaling ###
+test "PositionTcl-Pos::scaling" "too few args" {
+    Pos::scaling
+} {wrong \# args: should be "Pos::scaling item_id\(s\) \?new_value\(s\)\?"}
+test "PositionTcl-Pos::scaling" "too many args" {
+    Pos::scaling posid j u n k
+} {wrong \# args: should be "Pos::scaling item_id\(s\) \?new_value\(s\)\?"}
+test "PositionTcl-Pos::scaling" "normal use" {
+	 catch {Pos::scaling $::pos {1.5 3.0 4.5}}
 } {^0$}
-test "PositionTcl-Pos::scale" "error on non-numeric input" {
-    Pos::scale $::pos junk
-} {expected floating-point number but got "junk"}
-test "PositionTcl-Pos::scale" "error on bad posid" {
-    Pos::scale -1 -1.2 1.0 1.0
-} {Pos::scale: attempt to access invalid id '.*' in}
+test "PositionTcl-Pos::scaling" "error on non-numeric input" {
+    Pos::scaling $::pos junk
+} {Pos::scaling: }
+test "PositionTcl-Pos::scaling" "error on bad posid" {
+    Pos::scaling -1 {-1.2 1.0 1.0}
+} {Pos::scaling: attempt to access invalid id '.*' in}
 
-### Pos::translateCmd ###
-test "PositionTcl-Pos::translate" "too few args" {
-    Pos::translate posid x
-} {wrong \# args: should be "Pos::translate posid new_x new_y \?new_z\?"}
-test "PositionTcl-Pos::translate" "too many args" {
-    Pos::translate posid x y z junk
-} {wrong \# args: should be "Pos::translate posid new_x new_y \?new_z\?"}
-test "PositionTcl-Pos::translate" "normal use" {
-	 catch {Pos::translate $::pos 10.0 -3.6 9.4}
+### Pos::translation ###
+test "PositionTcl-Pos::translation" "too few args" {
+    Pos::translation
+} {wrong \# args: should be "Pos::translation item_id\(s\) \?new_value\(s\)\?"}
+test "PositionTcl-Pos::translation" "too many args" {
+    Pos::translation posid x y z junk
+} {wrong \# args: should be "Pos::translation item_id\(s\) \?new_value\(s\)\?"}
+test "PositionTcl-Pos::translation" "normal use" {
+	 catch {Pos::translation $::pos {10.0 -3.6 9.4}}
 } {^0$}
-test "PositionTcl-Pos::translate" "error on non-numeric input" {
-    Pos::translate $::pos junk junk junk
-} {expected floating-point number but got "junk"}
-test "PositionTcl-Pos::translate" "error on bad posid" {
-    Pos::translate -1 1 1 1
-} {Pos::translate: attempt to access invalid id '.*' in}
+test "PositionTcl-Pos::translation" "error on non-numeric input" {
+    Pos::translation $::pos junk
+} {Pos::translation: }
+test "PositionTcl-Pos::translation" "error on bad posid" {
+    Pos::translation -1 {1 1 1}
+} {Pos::translation: attempt to access invalid id '.*' in}
 
 ### Pos::stringifyCmd ###
 test "PositionTcl-Pos::stringify" "too few args" {
@@ -95,9 +89,8 @@ test "PositionTcl-IO::stringify" "too many args" {
     IO::stringify posid junk
 } {wrong \# args: should be "IO::stringify item_id"}
 test "PositionTcl-IO::stringify" "normal use" {
-	 Pos::rotate $::pos 3.0 1.5 0.0 -2.2
-	 Pos::scale $::pos 1.0 2.5 4.0
-	 Pos::translate $::pos 5.3 10.6 15.9
+	 Pos::scaling $::pos {1.0 2.5 4.0}
+	 Pos::translation $::pos {5.3 10.6 15.9}
 	 IO::stringify $::pos
 } "Position @$::INT"
 test "PositionTcl-IO::stringify" "error on bad posid" { 
