9d2eca3c7cf69a62491ebd93e33d48202f83205c
Robert J. Peters <rjpcal@gmail.com>
2000-12-11 23:05:48 +0000

    Replaced IO::count with IO::countAll.

--- testing/grshtest.tcl
+++ testing/grshtest.tcl
@@ -82,7 +82,7 @@ foreach file $permuted_files {
 		  testfile ${TEST_DIR}/[lindex $file 0]
 	 }
 
-	 puts "IO::count [IO::count]"
+	 puts "IO::count [IO::countAll]"
 }
 
 ### Compute test statistics and exit
