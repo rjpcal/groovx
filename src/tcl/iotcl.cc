02f4edbd318781d694531f2e3cfec48d4bdb3670
Robert J. Peters <rjpcal@gmail.com>
2000-12-12 01:36:51 +0000

    Moved LoadObjectsCmd and SaveObjectCmd here from the now-defunct ObjlistTcl package.

--- src/tcl/iotcl.cc
+++ src/tcl/iotcl.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2000 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Mon Oct 30 10:00:39 2000
-// written: Mon Dec 11 15:58:42 2000
+// written: Mon Dec 11 17:22:49 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -16,15 +16,134 @@
 #include "tcl/ioitempkg.h"
 
 #include "io/io.h"
+#include "io/iolegacy.h"
 #include "io/iomgr.h"
 #include "io/ioptrlist.h"
 
 #include "system/demangle.h"
 
+#include "tcl/stringifycmd.h"
+#include "tcl/tclpkg.h"
+
+#include <fstream.h>
 #include <typeinfo>
 
 #define NO_TRACE
 #include "util/trace.h"
+#define LOCAL_ASSERT
+#include "util/debug.h"
+
+namespace IoTcl {
+  class LoadObjectsCmd;
+  class SaveObjectsCmd;
+
+  template <class ReaderType, class Inserter>
+  void readBatch(STD_IO::istream& is, int num_to_read,
+					  Inserter result_inserter)
+	 {
+		const int ALL = -1; // indicates to read all objects until eof
+
+		int num_read = 0;
+
+		is >> ws;
+
+		while ( (num_to_read == ALL || num_read < num_to_read)
+				  && (is.peek() != EOF) ) {
+
+		  // allow for whole-line comments between objects beginning with '#'
+		  if (is.peek() == '#') {
+			 is.ignore(10000000, '\n');
+			 continue;
+		  }
+
+		  ReaderType reader(is);
+		  IO::IoObject* io = reader.readRoot(0);
+
+		  IdItem<IO::IoObject> obj(io, IdItem<IO::IoObject>::Insert());
+
+		  *result_inserter = obj.id();
+		  ++result_inserter;
+
+		  ++num_read;
+
+		  is >> ws;
+		}
+	 }
+
+  template <class WriterType, class Iterator>
+  void writeBatch(WriterType& writer, Iterator obj_itr, Iterator end)
+	 {
+		while (obj_itr != end)
+		  {
+			 IdItem<IO::IoObject> item(*obj_itr);
+			 writer.writeRoot(item.get());
+
+			 ++obj_itr;
+		  }
+	 }
+}
+
+//---------------------------------------------------------------------
+//
+// LoadObjectsCmd --
+//
+//---------------------------------------------------------------------
+
+class IoTcl::LoadObjectsCmd : public Tcl::TclCmd {
+public:
+  LoadObjectsCmd(Tcl_Interp* interp, const char* cmd_name) :
+	 Tcl::TclCmd(interp, cmd_name,
+					 "filename ?num_to_read=-1?", 2, 3, false)
+  {}
+
+protected:
+  virtual void invoke();
+};
+
+void IoTcl::LoadObjectsCmd::invoke() {
+DOTRACE("IoTcl::LoadObjectsCmd::invoke");
+  static const int ALL = -1; // indicates to read all objects until eof
+
+  const char* file        =                             getCstringFromArg(1);
+  int         num_to_read =      (objc() < 3) ? ALL   : getIntFromArg(2);
+
+  STD_IO::ifstream ifs(file);
+  if (ifs.fail()) { throw Tcl::TclError("unable to open file"); }
+
+  readBatch<IO::LegacyReader>(ifs, num_to_read, resultAppender((int*)0));
+}
+
+//---------------------------------------------------------------------
+//
+// SaveObjectsCmd --
+//
+//---------------------------------------------------------------------
+
+class IoTcl::SaveObjectsCmd : public Tcl::TclCmd {
+public:
+  SaveObjectsCmd(Tcl_Interp* interp, const char* cmd_name) :
+	 Tcl::TclCmd(interp, cmd_name,
+					 "objids filename ?use_bases=yes?", 3, 4)
+  {}
+protected:
+  virtual void invoke() {
+
+	 const char* filename = arg(2).getCstring();
+
+	 bool use_bases    = objc() < 4 ? true : arg(3).getBool();
+
+	 STD_IO::ofstream ofs(filename);
+	 if (ofs.fail()) {
+		Tcl::TclError err("error opening file: ");
+		err.appendMsg(filename);
+		throw err;
+	 }
+
+	 IO::LegacyWriter writer(ofs, use_bases);
+	 writer.usePrettyPrint(false);
+	 writeBatch(writer, beginOfArg(1, (int*)0), endOfArg(1, (int*)0));
+  }
+};
 
 namespace Tcl {
 
@@ -182,6 +301,8 @@ public:
 	 TclItemPkg::addIoCommands(this);
 	 declareCAction("clear", &IoPtrList::clear);
 	 declareCAction("purge", &IoPtrList::purge);
+	 addCommand( new IoTcl::LoadObjectsCmd(interp, "IoDb::loadObjects") );
+	 addCommand( new IoTcl::SaveObjectsCmd(interp, "IoDb::saveObjects") );
   }
 
   virtual ~IoDbPkg()
