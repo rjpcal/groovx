ae0c369d7fee0f5709da1fdc68484babb44ab4b3
Robert J. Peters <rjpcal@gmail.com>
2000-12-12 01:16:26 +0000

    Added countAll, findAll, and removeAll.

--- src/genericobjpkg.h
+++ src/genericobjpkg.h
@@ -13,35 +13,88 @@
 #ifndef IOITEMPKG_H_DEFINED
 #define IOITEMPKG_H_DEFINED
 
-#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(TCLITEMPKG_H_DEFINED)
-#include "tcl/tclitempkg.h"
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(IO_H_DEFINED)
+#include "io/io.h"
 #endif
 
 #if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(IDITEM_H_DEFINED)
 #include "io/iditem.h"
 #endif
 
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(TCLITEMPKG_H_DEFINED)
+#include "tcl/tclitempkg.h"
+#endif
+
 namespace Tcl {
 
-///////////////////////////////////////////////////////////////////////
-/**
- *
- * IsCmd
- *
- **/
-///////////////////////////////////////////////////////////////////////
+/** IoCaster **/
 
-template <class C>
-class IsCmd : public TclItemCmd<C> {
-public:
-  IsCmd(CTclItemPkg<C>* pkg, const char* cmd_name) :
-	 TclItemCmd<C>(pkg, cmd_name, "item_id", 2, 2) {}
+class IoCaster {
 protected:
-  virtual void invoke() {
-	 int id = TclCmd::getIntFromArg(1);
-	 MaybeIdItem<IO::IoObject> item(id);
-	 returnBool(item.isValid() && dynamic_cast<C*>(item.get()) != 0);
-  }
+  IoCaster();
+
+public:
+  virtual ~IoCaster();
+
+  virtual bool isMyType(const IO::IoObject* obj) = 0;
+
+  bool isMyType(int id);
+};
+
+/** CountAllCmd **/
+
+class CountAllCmd : public TclCmd {
+public:
+  CountAllCmd(Tcl_Interp* interp, IoCaster* caster, const char* cmd_name);
+  virtual ~CountAllCmd();
+
+protected:
+  virtual void invoke();
+
+private:
+  IoCaster* itsCaster;
+};
+
+/** FindAllCmd **/
+
+class FindAllCmd : public TclCmd {
+public:
+  FindAllCmd(Tcl_Interp* interp, IoCaster* caster, const char* cmd_name);
+  virtual ~FindAllCmd();
+
+protected:
+  virtual void invoke();
+
+private:
+  IoCaster* itsCaster;
+};
+
+/** RemoveAllCmd **/
+
+class RemoveAllCmd : public TclCmd {
+public:
+  RemoveAllCmd(Tcl_Interp* interp, IoCaster* caster, const char* cmd_name);
+  virtual ~RemoveAllCmd();
+
+protected:
+  virtual void invoke();
+
+private:
+  IoCaster* itsCaster;
+};
+
+/** IsCmd **/
+
+class IsCmd : public TclCmd {
+public:
+  IsCmd(Tcl_Interp* interp, IoCaster* caster, const char* cmd_name);
+  virtual ~IsCmd();
+
+protected:
+  virtual void invoke();
+
+private:
+  IoCaster* itsCaster;
 };
 
 ///////////////////////////////////////////////////////////////////////
@@ -53,19 +106,28 @@ protected:
 ///////////////////////////////////////////////////////////////////////
 
 template <class C>
-class IoItemPkg : public CTclItemPkg<C>
-{
+class IoItemPkg : public CTclItemPkg<C>, public IoCaster {
 public:
   IoItemPkg(Tcl_Interp* interp, const char* name, const char* version) :
 	 CTclItemPkg<C>(interp, name, version, 1)
   {
-	 addCommand( new IsCmd<C>(this, TclPkg::makePkgCmdName("is")));
+	 addCommand( new IsCmd(interp, this, TclPkg::makePkgCmdName("is")));
+	 addCommand( new CountAllCmd(interp, this,
+										  TclPkg::makePkgCmdName("countAll")));
+	 addCommand( new FindAllCmd(interp, this,
+										 TclPkg::makePkgCmdName("findAll")));
+	 addCommand( new RemoveAllCmd(interp, this,
+											TclPkg::makePkgCmdName("removeAll")));
   }
 
-  virtual C* getCItemFromId(int id) {
-	 IdItem<C> item(id);
-	 return item.get();
-  }
+  virtual C* getCItemFromId(int id)
+    {
+		IdItem<C> item(id);
+		return item.get();
+	 }
+
+  virtual bool isMyType(const IO::IoObject* obj)
+    { return (obj != 0 && dynamic_cast<const C*>(obj) != 0); }
 };
 
 } // end namespace Tcl
