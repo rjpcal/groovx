b953317b0d6716eb8219b7c19663be8f43a51a6f
Robert J. Peters <rjpcal@gmail.com>
2001-03-09 00:32:42 +0000

    Initial revision

---
+++ src/visx/num.cc
@@ -0,0 +1,64 @@
+///////////////////////////////////////////////////////////////////////
+//
+// num.cc
+//
+// Copyright (c) 1998-2000 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Thu Mar  8 16:28:26 2001
+// written: Thu Mar  8 16:32:07 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef NUM_CC_DEFINED
+#define NUM_CC_DEFINED
+
+#include "num.h"
+
+#include "trace.h"
+
+bool Num::filled = false;
+double Num::lookup[TABLE_SIZE] = { 0.0 };
+
+void Num::linearCombo(int nelems, const double* w, int w_stride,
+							 const double* const* elems, int elems_stride, int dim,
+							 double* result)
+{
+DOTRACE("Num::linearCombo");
+
+  // e.g nelems == 3   dim == 4
+  //
+  //               | e11  e12  e13  e14 |
+  // [w1 w2 w3] *  | e21  e22  e23  e24 | = 
+  //               | e31  e32  e33  e34 |
+  //
+  //
+  // [ w1*e11+w2*e21+w3*e31  w1*e12+w2*e22+w3*e32  ... ]
+  for (int d = 0; d < dim; ++d)
+	 {
+		result[d] = 0.0;
+		for (int elem = 0; elem < nelems; ++elem)
+		  result[d] += w[elem*w_stride] * elems[elem][d*elems_stride];
+	 }
+}
+
+double Num::gammalnEngine(double xx)
+{
+DOTRACE("Num::gammalnEngine");
+
+  static double cof[6] = {76.18009172947146,     -86.50532032941677,
+								  24.01409824083091,     -1.231738572450155,
+								  0.1208650973866179e-2, -0.5395239384953e-5};
+  double x,y,tmp,ser;
+  int j;
+
+  y = x = xx;
+  tmp = x+5.5;
+  tmp -= (x+0.5)*log(tmp);
+  ser=1.000000000190015;
+  for (j=0;j<=5;j++) ser += cof[j]/++y;
+  return -tmp+log(2.5066282746310005*ser/x);
+}
+
+static const char vcid_num_cc[] = "$Header$";
+#endif // !NUM_CC_DEFINED
---
+++ src/visx/num.h
@@ -0,0 +1,74 @@
+///////////////////////////////////////////////////////////////////////
+//
+// num.h
+//
+// Copyright (c) 1998-2000 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Thu Mar  8 16:27:36 2001
+// written: Thu Mar  8 16:31:01 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef NUM_H_DEFINED
+#define NUM_H_DEFINED
+
+#include <cmath>
+
+class Num {
+public:
+  static double erfc(double x) {
+	 double z = fabs(x);
+
+	 double t = 1.0/(1.0+0.5*fabs(x));
+
+	 double ans =
+      t*exp(-z*z-1.26551223+t*(1.00002368+t*(0.37409196+t*(0.09678418+
+      t*(-0.18628806+t*(0.27886807+t*(-1.13520398+t*(1.48851587+
+      t*(-0.82215223+t*0.17087277)))))))))
+		;
+
+	 return x >= 0.0 ? ans : 2.0-ans;
+  }
+
+  static double gammaln(double xx)
+  {
+	 double tol = 1e-50;
+
+	 int ival = int(xx);
+
+	 if ( (ival < TABLE_SIZE) && (xx - double(ival) < tol) )
+		{
+		  if (!filled) fillTable();
+		  return lookup[ival];
+		}
+	 else 
+		{
+		  return gammalnEngine(xx);
+		}
+  }
+
+  static const double SQRT_2 = 1.41421356237;
+
+  static void linearCombo(int nelems, const double* w, int w_stride,
+								  const double* const* elems, int elems_stride, int dim,
+								  double* result);
+
+private:
+  static bool filled;
+  static const int TABLE_SIZE = 101;
+  static double lookup[TABLE_SIZE];
+
+  static void fillTable()
+  {
+	 for (int i = 0; i < TABLE_SIZE; ++i)
+		lookup[i] = gammalnEngine(double(i));
+
+	 filled = true;
+  }
+
+  static double gammalnEngine(double xx);
+};
+
+static const char vcid_num_h[] = "$Header$";
+#endif // !NUM_H_DEFINED
