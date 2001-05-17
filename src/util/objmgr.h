92589894e6d83a98f10c134470ac859003bafd95
Robert J. Peters <rjpcal@gmail.com>
2001-05-17 14:03:34 +0000

    Added newTypedIO() template.

--- src/util/objmgr.h
+++ src/util/objmgr.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Fri Apr 23 01:12:37 1999
-// written: Thu May 10 12:04:37 2001
+// written: Thu May 17 06:57:15 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -17,12 +17,26 @@
 #include "io/iodecls.h"
 #endif
 
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(PTRHANDLE_H_DEFINED)
+#include "util/ptrhandle.h"
+#endif
+
 class fixed_string;
 
 namespace IO {
   namespace IoMgr {
+	 /// Will not return 0; will throw an exception on failure.
 	 IO::IoObject* newIO(const char* type);
+
+	 /// Will not return 0; will throw an exception on failure.
 	 IO::IoObject* newIO(const fixed_string& type);
+
+	 template <class T, class S>
+	 PtrHandle<T> newTypedIO(S type)
+	 {
+		PtrHandle<IO::IoObject> io(newIO(type));
+  		return dynamicCast<T>(io);
+	 }
   }
 }
 
