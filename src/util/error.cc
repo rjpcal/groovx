ff3783037a6a8d16f42123b9a80d3546578740ff
Robert J. Peters <rjpcal@gmail.com>
2001-06-05 21:07:18 +0000

    Put a conditional printStackTrace() in the Error constructor.

--- src/util/error.cc
+++ src/util/error.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Tue Jun 22 14:59:48 1999
-// written: Sat May 12 09:27:10 2001
+// written: Tue Jun  5 14:07:01 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -24,11 +24,14 @@
 #include "util/trace.h"
 #include "util/debug.h"
 
-Error::Error () {
+Error::Error() {
 DOTRACE("Error::Error ");
+#ifdef LOCAL_DEBUG
+  Util::Trace::printStackTrace();
+#endif 
 }
 
-Error::~Error () {
+Error::~Error() {
 DOTRACE("Error::~Error ");
 }
 
