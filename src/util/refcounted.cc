a9c14bd7ee84aff96f5caa64363df1564b019129
Robert J. Peters <rjpcal@gmail.com>
2001-06-13 19:26:12 +0000

    Added some debugging, and moved RefCounts constructor out of line.

--- src/util/refcounted.cc
+++ src/util/refcounted.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Sun Oct 22 14:40:28 2000
-// written: Wed Jun 13 10:37:26 2001
+// written: Wed Jun 13 12:25:36 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -35,68 +35,71 @@
 //
 ///////////////////////////////////////////////////////////////////////
 
+Util::RefCounts::RefCounts() : itsStrong(0), itsWeak(0)
+{
+DOTRACE("Util::RefCounts::RefCounts");
+}
+
 Util::RefCounts::~RefCounts() {
 DOTRACE("Util::RefCounts::~RefCounts");
 
-  Assert(strong == 0 && weak == 0);
+  Assert(itsStrong == 0 && itsWeak == 0);
 }
 
 void Util::RefCounts::acquireWeak() {
 DOTRACE("Util::RefCounts::acquireWeak");
 
 #ifndef NO_CPP_LIMITS
-  Assert(weak < std::numeric_limits<Count>::max());
+  Assert(itsWeak < std::numeric_limits<Count>::max());
 #else
-  Assert(weak < USHRT_MAX);
+  Assert(itsWeak < USHRT_MAX);
 #endif
 
-  ++weak;
+  ++itsWeak;
 }
 
 Util::RefCounts::Count Util::RefCounts::releaseWeak() {
 DOTRACE("Util::RefCounts::releaseWeak");
 
-  Assert(weak > 0);
+  Assert(itsWeak > 0);
 
-  --weak;
+  --itsWeak;
 
-  if (strong == 0 && weak == 0)
+  if (itsStrong == 0 && itsWeak == 0)
     {
-      Count c = weak;
       delete this;
-      return c;
+      return 0;
     }
 
-  return weak;
+  return itsWeak;
 }
 
 void Util::RefCounts::acquireStrong() {
 DOTRACE("Util::RefCounts::acquireStrong");
 
 #ifndef NO_CPP_LIMITS
-  Assert(strong < std::numeric_limits<Count>::max());
+  Assert(itsStrong < std::numeric_limits<Count>::max());
 #else
-  Assert(strong < USHRT_MAX);
+  Assert(itsStrong < USHRT_MAX);
 #endif
 
-  ++strong;
+  ++itsStrong;
 }
 
 Util::RefCounts::Count Util::RefCounts::releaseStrong() {
 DOTRACE("Util::RefCounts::releaseStrong");
 
-  Assert(strong > 0);
+  Assert(itsStrong > 0);
 
-  --strong;
+  --itsStrong;
 
-  if (strong == 0 && weak == 0)
+  if (itsStrong == 0 && itsWeak == 0)
     {
-      Count c = strong;
       delete this;
-      return c;
+      return 0;
     }
 
-  return strong;
+  return itsStrong;
 }
 
 
@@ -133,14 +136,14 @@ DOTRACE("Util::RefCounted::~RefCounted");
 
 void Util::RefCounted::incrRefCount() const {
 DOTRACE("Util::RefCounted::incrRefCount");
-  DebugEval((void*)this);
+  DebugEvalNL((void*)this);
 
   itsRefCounts->acquireStrong();
 }
 
 void Util::RefCounted::decrRefCount() const {
 DOTRACE("Util::RefCounted::decrRefCount");
-  DebugEval((void*)this);
+  DebugEvalNL((void*)this);
 
   if (itsRefCounts->releaseStrong() == 0)
     delete this;
@@ -163,6 +166,10 @@ DOTRACE("Util::RefCounted::isVolatile");
 
 int Util::RefCounted::refCount() const {
 DOTRACE("Util::RefCounted::refCount");
+
+  DebugEval(itsRefCounts->weakCount());
+  DebugEvalNL(itsRefCounts->strongCount());
+
   return itsRefCounts->strongCount();
 }
 
