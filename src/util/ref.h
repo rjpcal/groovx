962d58f9964ab81063b3d86f2b1f842785ee3958
Robert J. Peters <rjpcal@gmail.com>
2001-06-13 16:08:28 +0000

    Removed the "itsId" member of MaybeRef... this wasn't ever being used for its original intended purpose, which was to allow a MaybeRef to be constructed with an id that referred to a currently non-existent object, but then to later automagically try to retrieve that object when the MaybeRef was dereferenced. But, in practice, this usage pattern never occurred, and itsId was only used to indicate "no object" if its value was 0. This lightens the implementation of MaybeRef.

--- src/util/ref.h
+++ src/util/ref.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Thu Oct 26 17:50:59 2000
-// written: Mon Jun 11 15:08:15 2001
+// written: Wed Jun 13 09:07:03 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -77,9 +77,10 @@ namespace Util { namespace RefHelper {
  * Util::Ref<T> is a ref-counted smart pointer for holding RefCounted
  * objects. A Util::Ref<T> is guaranteed to always point to a valid
  * RefCounted object, and uses RefCounted's strong ref counts to
- * achieve this. Therefore, a Util::Ref<T> cannot be constructed for a
- * volatlie RefCounted object for which only weak references are
- * available.
+ * achieve this. In order to provide this guarantee, it is possible
+ * that construction of a Util::Ref<T> may throw an exception. For
+ * example, a Util::Ref<T> cannot be constructed for a volatlie
+ * RefCounted object for which only weak references are available.
  *
  **/
 ///////////////////////////////////////////////////////////////////////
@@ -175,13 +176,17 @@ Ref<To> dynamicCast(Ref<Fr> p)
   return Ref<To>(&t);
 }
 
+
+
 ///////////////////////////////////////////////////////////////////////
 /**
  *
- * Util::MaybeRef<T> is a ref-counted smart pointer for holding
- * RefCounted objects. However, a Util::MaybeRef<T> is not guaranteed
- * to always point to a valid object (this must be tested with
- * isValid() before dereferencing). Therefore, Util::MaybeRef<T> can
+ * Util::MaybeRef<T> is a ref-counted smart pointer (like
+ * Util::Ref<T>) for holding RefCounted objects. Construction of a
+ * Util::MaybeRef<T> is guaranteed not to fail. Because of this,
+ * however, a Util::MaybeRef<T> is not guaranteed to always point to a
+ * valid object (this must be tested with isValid() before
+ * dereferencing). With these characteristics, a Util::MaybeRef<T> can
  * be used with volatile RefCounted objects for which only weak
  * references are available.
  *
@@ -199,40 +204,40 @@ private:
   // get() (an exception will be raised if this would fail)
   class WeakHandle {
   public:
-	 explicit WeakHandle(T* master) : itsMaster(master),
-												 itsCounts(0),
-												 itsIsVolatile(isVolatile(master))
-	 {
-		if (itsMaster)
-		  itsCounts = itsMaster->refCounts();
+    explicit WeakHandle(T* master) : itsMaster(master),
+                                     itsCounts(0),
+                                     itsIsVolatile(isVolatile(master))
+    {
+      if (itsMaster)
+        itsCounts = itsMaster->refCounts();
 
-		acquire();
-	 }
+      acquire();
+    }
 
-	 ~WeakHandle()
-	 { release(); }
+    ~WeakHandle()
+    { release(); }
 
-	 WeakHandle(const WeakHandle& other) : itsMaster(other.itsMaster),
-														itsCounts(other.itsCounts),
-														itsIsVolatile(other.itsIsVolatile)
-	 { acquire(); }
+    WeakHandle(const WeakHandle& other) : itsMaster(other.itsMaster),
+                                          itsCounts(other.itsCounts),
+                                          itsIsVolatile(other.itsIsVolatile)
+    { acquire(); }
 
-	 template <class U> friend class WeakHandle;
+    template <class U> friend class WeakHandle;
 
-	 template <class U>
-	 WeakHandle(const WeakHandle<U>& other) : itsMaster(other.itsMaster),
-															itsCounts(other.itsCounts),
-															itsIsVolatile(other.itsIsVolatile)
-	 { acquire(); }
+    template <class U>
+    WeakHandle(const WeakHandle<U>& other) : itsMaster(other.itsMaster),
+                                             itsCounts(other.itsCounts),
+                                             itsIsVolatile(other.itsIsVolatile)
+    { acquire(); }
 
-	 WeakHandle& operator=(const WeakHandle& other)
+    WeakHandle& operator=(const WeakHandle& other)
     {
       WeakHandle otherCopy(other);
       this->swap(otherCopy);
       return *this;
     }
 
-	 bool isValid() const
+    bool isValid() const
     {
       if (itsMaster == 0)                             return false;
       if (itsCounts == 0)                             return false;
@@ -241,10 +246,10 @@ private:
       return true;
     }
 
-	 T* get()        const { ensureValid(); return itsMaster; }
+    T* get()        const { ensureValid(); return itsMaster; }
 
   private:
-	 void acquire()
+    void acquire()
     {
       if (itsCounts)
         {
@@ -255,7 +260,7 @@ private:
         }
     }
 
-	 void release() const
+    void release() const
     {
       if (itsCounts)
         {
@@ -268,14 +273,14 @@ private:
       itsCounts = 0; itsMaster = 0; itsIsVolatile = true;
     }
 
-	 void ensureValid() const
+    void ensureValid() const
     {
       if (!isValid())
         Util::RefHelper::throwErrorWithMsg(
-								 "attempt to dereference invalid WeakHandle");
+                         "attempt to dereference invalid WeakHandle");
     }
 
-	 void swap(WeakHandle& other)
+    void swap(WeakHandle& other)
     {
       T* otherMaster = other.itsMaster;
       other.itsMaster = this->itsMaster;
@@ -290,21 +295,20 @@ private:
       this->itsIsVolatile = otherIsVolatile;
     }
 
-	 bool isVolatile(T* master)
-	 {
-		if (master == 0) return true;
-		return master->isVolatile();
-	 }
+    bool isVolatile(T* master)
+    {
+      if (master == 0) return true;
+      return master->isVolatile();
+    }
 
-	 mutable T* itsMaster;
-	 mutable Util::RefCounts* itsCounts;
-	 mutable bool itsIsVolatile;
+    mutable T* itsMaster;
+    mutable Util::RefCounts* itsCounts;
+    mutable bool itsIsVolatile;
 
   }; // end helper class WeakHandle
 
 
   mutable WeakHandle itsHandle;
-  Util::UID itsId;
 
   void insertItem()
   {
@@ -312,54 +316,40 @@ private:
       RefHelper::insertItem(itsHandle.get());
   }
 
-  // This will try to refresh the handle from the id, but will just
-  // leave the object with an invalid handle if refreshing is not
-  // possible (thus a future dereference will fail).
-  void refresh() const {
-    if ( !itsHandle.isValid() )
-      {
-        if (RefHelper::isValidId(itsId)) {
-			 Ref<T> p(itsId);
-			 itsHandle = WeakHandle(p.get());
-			 if (itsId != itsHandle.get()->id())
-				Util::RefHelper::throwErrorWithMsg("assertion failed in refresh");
-		  }
-		}
-  }
-
 public:
-  MaybeRef() : itsHandle(0), itsId(0) {}
+  MaybeRef() : itsHandle(0) {}
 
-  explicit MaybeRef(Util::UID id_) : itsHandle(0), itsId(id_) {}
+  explicit MaybeRef(Util::UID id) :
+    itsHandle(RefHelper::isValidId(id) ?
+              RefHelper::getCastedItem<T>(id) : 0)
+  {}
 
-  explicit MaybeRef(T* master) : itsHandle(master),
-											itsId(master ? master->id() : 0)
+  explicit MaybeRef(T* master) : itsHandle(master)
   { insertItem(); }
 
-  MaybeRef(T* master, bool /*noInsert*/) : itsHandle(master),
-														 itsId(master ? master->id() : 0)
-  {}
+  MaybeRef(T* master, bool /*noInsert*/) : itsHandle(master) {}
 
 
   template <class U>
   MaybeRef(const MaybeRef<U>& other) :
-    itsHandle(other.isValid() ? other.get() : 0), itsId(other.id()) {}
+    itsHandle(other.isValid() ? other.get() : 0) {}
 
   template <class U>
   MaybeRef(const Ref<U>& other) :
-    itsHandle(other.get()), itsId(other.id()) {}
+    itsHandle(other.get()) {}
 
   // Default destructor, copy constructor, operator=() are fine
 
-  T* operator->() const { refresh(); return itsHandle.get(); }
-  T& operator*()  const { refresh(); return *(itsHandle.get()); }
+  T* operator->() const { return itsHandle.get(); }
+  T& operator*()  const { return *(itsHandle.get()); }
 
-  T* get()        const { refresh(); return itsHandle.get(); }
+  T* get()        const { return itsHandle.get(); }
 
 
-  bool isValid() const { refresh(); return itsHandle.isValid(); }
+  bool isValid() const { return itsHandle.isValid(); }
 
-  Util::UID id() const { return itsId; }
+  Util::UID id() const
+    { return itsHandle.isValid() ? itsHandle.get()->id() : 0; }
 };
 
 // TypeTraits specialization for MaybeRef smart pointer
