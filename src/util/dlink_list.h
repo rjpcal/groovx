b413679cd1a05e27150d6e2497f69710a93c6d16
Robert J. Peters <rjpcal@gmail.com>
2001-06-13 23:24:01 +0000

    Added initialization of the_node in constructors to placate compiler.

--- src/util/dlink_list.h
+++ src/util/dlink_list.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Wed May 31 14:24:31 2000
-// written: Tue May 22 12:18:28 2001
+// written: Wed Jun 13 16:23:47 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -51,145 +51,145 @@ public:
 
   class node {
   private:
-	 ~node(); // not defined
-	 node(const node& other); // not defined
-	 node& operator=(const node& other); // not defined
+    ~node(); // not defined
+    node(const node& other); // not defined
+    node& operator=(const node& other); // not defined
 
-	 friend class dlink_list;
-	 friend class dlink_list::iterator;
-	 friend class dlink_list::const_iterator;
+    friend class dlink_list;
+    friend class dlink_list::iterator;
+    friend class dlink_list::const_iterator;
 
-	 void construct(const T& new_val) { new (&val) T(new_val); }
-	 void destroy() { (&val)->~T(); }
+    void construct(const T& new_val) { new (&val) T(new_val); }
+    void destroy() { (&val)->~T(); }
 
-	 static node* allocate()
-		{ return static_cast<node*>(::operator new(sizeof(node))); }
+    static node* allocate()
+      { return static_cast<node*>(::operator new(sizeof(node))); }
 
-	 static node* allocate(node* p, node* n)
-		{ node* x = allocate(); x->prev = p; x->next = n; return x; }
+    static node* allocate(node* p, node* n)
+      { node* x = allocate(); x->prev = p; x->next = n; return x; }
 
-	 static node* allocate(const T& val, node* p, node* n)
-		{
-		  node* x = allocate(p,n);
-		  x->construct(val);
-		  return x;
-		}
+    static node* allocate(const T& val, node* p, node* n)
+      {
+        node* x = allocate(p,n);
+        x->construct(val);
+        return x;
+      }
 
-	 static void deallocate(node* n)
-		{ ::operator delete(static_cast<void*>(n)); }
+    static void deallocate(node* n)
+      { ::operator delete(static_cast<void*>(n)); }
 
-	 T val;
-	 node* prev;
-	 node* next;
+    T val;
+    node* prev;
+    node* next;
   };
 
   class iterator {
   private:
-	 node* nn;
+    node* nn;
 
   public:
-	 friend class dlink_list;
-	 friend class const_iterator;
+    friend class dlink_list;
+    friend class const_iterator;
 
-	 typedef T value_type;
-	 typedef T* pointer;
-	 typedef T& reference;
+    typedef T value_type;
+    typedef T* pointer;
+    typedef T& reference;
 
   private:
-	 iterator(node* n) : nn(n) {}
+    iterator(node* n) : nn(n) {}
 
   public:
-	 iterator(const iterator& other) : nn(other.nn) {}
+    iterator(const iterator& other) : nn(other.nn) {}
 
-	 iterator& operator=(const iterator& other)
-	   { nn = other.nn; return *this; }
+    iterator& operator=(const iterator& other)
+      { nn = other.nn; return *this; }
 
-	 reference operator*() { return nn->val; }
-	 pointer operator->() { return &(nn->val); }
+    reference operator*() { return nn->val; }
+    pointer operator->() { return &(nn->val); }
 
-	 iterator& operator++() { nn = nn->next; return *this; }
-	 iterator operator++(int) { iterator temp(*this); ++*this; return temp; }
+    iterator& operator++() { nn = nn->next; return *this; }
+    iterator operator++(int) { iterator temp(*this); ++*this; return temp; }
 
-	 iterator& operator--() { nn = nn->prev; return *this; }
-	 iterator operator--(int) { iterator temp(*this); ++*this; return temp; }
+    iterator& operator--() { nn = nn->prev; return *this; }
+    iterator operator--(int) { iterator temp(*this); ++*this; return temp; }
 
-	 bool operator==(const iterator& other) const { return nn == other.nn; }
-	 bool operator!=(const iterator& other) const { return nn != other.nn; }
+    bool operator==(const iterator& other) const { return nn == other.nn; }
+    bool operator!=(const iterator& other) const { return nn != other.nn; }
   };
 
   class const_iterator {
   private:
-	 node* nn;
+    node* nn;
 
   public:
-	 friend class dlink_list;
+    friend class dlink_list;
 
-	 typedef const T value_type;
-	 typedef const T* pointer;
-	 typedef const T& reference;
+    typedef const T value_type;
+    typedef const T* pointer;
+    typedef const T& reference;
 
   private:
-	 const_iterator(node* n) : nn(n) {}
+    const_iterator(node* n) : nn(n) {}
 
   public:
-	 const_iterator(const iterator& other) : nn (other.nn) {}
+    const_iterator(const iterator& other) : nn (other.nn) {}
 
-	 const_iterator(const const_iterator& other) : nn(other.nn) {}
-	 const_iterator& operator=(const const_iterator& other)
-	   { nn = other.nn; return *this; }
+    const_iterator(const const_iterator& other) : nn(other.nn) {}
+    const_iterator& operator=(const const_iterator& other)
+      { nn = other.nn; return *this; }
 
-	 const_reference operator*() { return nn->val; }
-	 const_pointer operator->() { return &(nn->val); }
+    const_reference operator*() { return nn->val; }
+    const_pointer operator->() { return &(nn->val); }
 
-	 const_iterator& operator++() { nn = nn->next; return *this; }
-	 const_iterator operator++(int)
-		{ const_iterator temp(*this); ++*this; return temp; }
+    const_iterator& operator++() { nn = nn->next; return *this; }
+    const_iterator operator++(int)
+      { const_iterator temp(*this); ++*this; return temp; }
 
-	 const_iterator& operator--() { nn = nn->prev; return *this; }
-	 const_iterator operator--(int)
-		{ const_iterator temp(*this); ++*this; return temp; }
+    const_iterator& operator--() { nn = nn->prev; return *this; }
+    const_iterator operator--(int)
+      { const_iterator temp(*this); ++*this; return temp; }
 
-	 bool operator==(const const_iterator& other) { return nn == other.nn; }
-	 bool operator!=(const const_iterator& other) { return nn != other.nn; }
+    bool operator==(const const_iterator& other) { return nn == other.nn; }
+    bool operator!=(const const_iterator& other) { return nn != other.nn; }
   };
 
 private:
   node* the_node;
 
   void init()
-	 {
+    {
 #ifdef LOCAL_TRACE
-	 DOTRACE("dlink_list::init");
+    DOTRACE("dlink_list::init");
 #endif
-		the_node = node::allocate();
-		the_node->prev = the_node->next = the_node;
-	 }
+      the_node = node::allocate();
+      the_node->prev = the_node->next = the_node;
+    }
 
   //
   // Member functions
   //
 
 public:
-  dlink_list() { init(); }
+  dlink_list() : the_node(0) { init(); }
 
-  dlink_list(const dlink_list& other)
-	 {
-		init();
+  dlink_list(const dlink_list& other) : the_node(0)
+    {
+      init();
 
-		insert_range(begin(), other.begin(), other.end());
-	 }
+      insert_range(begin(), other.begin(), other.end());
+    }
 
   ~dlink_list() { clear(); node::deallocate(the_node); }
 
   dlink_list& operator=(const dlink_list& other)
-	 {
-		if (this != &other)
-		  {
-			 dlink_list other_copy(other);
-			 this->swap(other_copy);
-		  }
-		return *this;
-	 }
+    {
+      if (this != &other)
+        {
+          dlink_list other_copy(other);
+          this->swap(other_copy);
+        }
+      return *this;
+    }
 
   void push_front(const T& val)   { insert(begin(), val); }
   void push_back(const T& val)    { insert(end(), val); }
@@ -199,26 +199,26 @@ public:
 
   template <class Itr>
   void insert_range(iterator pos, Itr itr, Itr stop)
-	 {
-		while (itr != stop)
-		  {   insert(pos, *itr);   ++itr; }
-	 }
+    {
+      while (itr != stop)
+        {   insert(pos, *itr);   ++itr; }
+    }
 
   /// Insert a new link containing \a val at a position just before \a pos.
   void insert(iterator pos, const T& val)
-	 {
+    {
 #ifdef LOCAL_TRACE
-	 DOTRACE("dlink_list::insert");
+    DOTRACE("dlink_list::insert");
 #endif
-		// Before the function:
+      // Before the function:
 
-		//               itr
-		//                |
-		//        ----->  |
-		//  before      after
-		//        <-----
+      //               itr
+      //                |
+      //        ----->  |
+      //  before      after
+      //        <-----
 
-		// After the function:
+      // After the function:
 
       //                             itr
       //                              |
@@ -226,24 +226,24 @@ public:
       //  before      new_node      after
       //        <-----        <-----
 
-		node* after = pos.nn;
-		node* before = after->prev;
+      node* after = pos.nn;
+      node* before = after->prev;
 
-		node* new_node = node::allocate(val, before, after);
+      node* new_node = node::allocate(val, before, after);
 
-		before->next = new_node;
-		after->prev = new_node;
-	 }
+      before->next = new_node;
+      after->prev = new_node;
+    }
 
   /// Erase the link at \a pos.
   void erase(iterator pos)
-	 {
+    {
 #ifdef LOCAL_TRACE
-	 DOTRACE("dlink_list::erase");
+    DOTRACE("dlink_list::erase");
 #endif
-		if (pos == end()) return;
+      if (pos == end()) return;
 
-		// Before the function:
+      // Before the function:
 
       //               itr
       //                |
@@ -251,31 +251,31 @@ public:
       //  before      target      after
       //        <-----      <-----
 
-		// After the function:
+      // After the function:
 
-		//
-		//
-		//        ----->
-		//  before      after
-		//        <-----
+      //
+      //
+      //        ----->
+      //  before      after
+      //        <-----
 
-		node* target = pos.nn;
-		node* before = target->prev;
-		node* after = target->next;
+      node* target = pos.nn;
+      node* before = target->prev;
+      node* after = target->next;
 
-		before->next = after;
-		after->prev = before;
+      before->next = after;
+      after->prev = before;
 
-		target->destroy();
-		node::deallocate(target);
-	 }
+      target->destroy();
+      node::deallocate(target);
+    }
 
   /// Removes the first link (if any) with the value \a val.
   void remove(const T& val)
     {
-		iterator ii = find(val);
-		erase(ii);
-	 }
+      iterator ii = find(val);
+      erase(ii);
+    }
 
   reference back() { return *(--end()); }
   const_reference back() const { return *(--end()); }
@@ -291,39 +291,39 @@ public:
   bool empty() const { return begin() == end(); }
 
   size_type size() const
-	 {
-		const_iterator itr(begin()), stop(end());
-		size_type result = 0;
-		while ( itr != stop )
-		  {
-			 ++result;
-			 ++itr;
-		  }
-		return result;
-	 }
+    {
+      const_iterator itr(begin()), stop(end());
+      size_type result = 0;
+      while ( itr != stop )
+        {
+          ++result;
+          ++itr;
+        }
+      return result;
+    }
 
   iterator find(const T& val)
-	 {
-		iterator itr(begin()), stop(end());
-		while (itr != stop)
-		  {
-			 if (*itr == val) return itr;
-			 ++itr;
-		  }
-		return end();
-	 }
+    {
+      iterator itr(begin()), stop(end());
+      while (itr != stop)
+        {
+          if (*itr == val) return itr;
+          ++itr;
+        }
+      return end();
+    }
 
   const_iterator find(const T& val) const
-	 { return const_cast<dlink_list*>(this)->find(val); }
+    { return const_cast<dlink_list*>(this)->find(val); }
 
   void clear() { while ( !empty() ) pop_front(); }
 
   void swap(dlink_list& other)
-	 {
-		node* other_the_node = other.the_node;
-		other.the_node = this->the_node;
-		this->the_node = other_the_node;
-	 }
+    {
+      node* other_the_node = other.the_node;
+      other.the_node = this->the_node;
+      this->the_node = other_the_node;
+    }
 };
 
 static const char vcid_dlink_list_h[] = "$Header$";
