089fa8f7e41e5cb95dd8328d737cf31657874332
Robert J. Peters <rjpcal@gmail.com>
2001-06-15 23:23:45 +0000

    Initial revision

---
+++ testing/obj_test.tcl
@@ -0,0 +1,16 @@
+##############################################################################
+###
+### ObjTcl
+### Rob Peters
+### Jun-2001
+### $Id$
+###
+##############################################################################
+
+source ${::TEST_DIR}/list_test.tcl
+List::testList Block Block Block
+List::testList GrObj Face Fish
+List::testList Pos Position Jitter
+List::testList Rh KbdResponseHdlr NullResponseHdlr
+List::testList Th TimingHdlr TimingHandler
+List::testList Trial Trial Trial
