bef0b4a2df767a1bd8acbdf9a7a6825aca404691
Robert J. Peters <rjpcal@gmail.com>
2001-06-15 13:58:25 +0000

    Added a specialization of CGetter and CSetter for Util::WeakRef argument types.

--- src/tcl/tclpkg.h
+++ src/tcl/tclpkg.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Tue Jun 15 12:33:59 1999
-// written: Mon Jun 11 15:08:15 2001
+// written: Fri Jun 15 06:46:03 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -23,7 +23,11 @@
 
 namespace IO { class IoObject; }
 
-namespace Util { template <class T> class Ref; };
+namespace Util
+{
+  template <class T> class Ref;
+  template <class T> class WeakRef;
+};
 
 ///////////////////////////////////////////////////////////////////////
 //
@@ -48,8 +52,8 @@ public:
 
   /// Casts \a item to type \c C* and calls the stored member function.
   virtual T get(void *item) const {
-	 const C* p = static_cast<C*>(item);
-	 return (p->*itsGetter_f)();
+    const C* p = static_cast<C*>(item);
+    return (p->*itsGetter_f)();
   }
 
 private:
@@ -71,13 +75,36 @@ public:
 
   /// Casts \a item to type \c C* and calls the stored member function.
   virtual int get(void *item) const {
-	 const C* p = static_cast<C*>(item);
-	 return (p->*itsGetter_f)()->id();
+    const C* p = static_cast<C*>(item);
+    return (p->*itsGetter_f)()->id();
   }
 
 private:
   CGetter<C, Util::Ref<T> >(const CGetter<C, Util::Ref<T> >&);
   CGetter<C, Util::Ref<T> >& operator=(const CGetter<C, Util::Ref<T> >&);
+
+  Getter_f itsGetter_f;
+};
+
+// Partial specialization of CGetter for T == Util::WeakRef<T>
+template <class C, class T>
+class CGetter<C, Util::WeakRef<T> > : public Getter<int> {
+public:
+  /// The type of member function pointer that will be called in \c get().
+  typedef Util::WeakRef<T> (C::* Getter_f) () const;
+
+  /// Construct with a member function pointer.
+  CGetter<C, Util::WeakRef<T> >(Getter_f getter) : itsGetter_f(getter) {}
+
+  /// Casts \a item to type \c C* and calls the stored member function.
+  virtual int get(void *item) const {
+    const C* p = static_cast<C*>(item);
+    return (p->*itsGetter_f)().id();
+  }
+
+private:
+  CGetter<C, Util::WeakRef<T> >(const CGetter<C, Util::WeakRef<T> >&);
+  CGetter<C, Util::WeakRef<T> >& operator=(const CGetter<C, Util::WeakRef<T> >&);
 
   Getter_f itsGetter_f;
 };
@@ -99,8 +126,8 @@ public:
 
   /// Casts \a item to type \c C* and calls the stored member function.
   virtual void set(void* item, T val) {
-	 C* p = static_cast<C*>(item);
-	 (p->*itsSetter_f)(val);
+    C* p = static_cast<C*>(item);
+    (p->*itsSetter_f)(val);
   }
 
 private:
@@ -123,13 +150,36 @@ public:
 
   /// Casts \a item to type \c C* and calls the stored member function.
   virtual void set(void* item, int val) {
-	 C* p = static_cast<C*>(item);
-	 (p->*itsSetter_f)(Util::Ref<T>(val));
+    C* p = static_cast<C*>(item);
+    (p->*itsSetter_f)(Util::Ref<T>(val));
   }
 
 private:
   CSetter<C, Util::Ref<T> >(const CSetter<C, Util::Ref<T> >&);
   CSetter<C, Util::Ref<T> >& operator=(const CSetter<C, Util::Ref<T> >&);
+
+  Setter_f itsSetter_f;
+};
+
+// Specialization of CSetter for T == Util::WeakRef<T>
+template <class C, class T>
+class CSetter<C, Util::WeakRef<T> > : public Setter<int> {
+public:
+  /// The type of member function pointer that will be called in \c set().
+  typedef void (C::* Setter_f) (Util::WeakRef<T>);
+
+  /// Construct with a member function pointer.
+  CSetter<C, Util::WeakRef<T> >(Setter_f setter) : itsSetter_f(setter) {}
+
+  /// Casts \a item to type \c C* and calls the stored member function.
+  virtual void set(void* item, int val) {
+    C* p = static_cast<C*>(item);
+    (p->*itsSetter_f)(Util::WeakRef<T>(val));
+  }
+
+private:
+  CSetter<C, Util::WeakRef<T> >(const CSetter<C, Util::WeakRef<T> >&);
+  CSetter<C, Util::WeakRef<T> >& operator=(const CSetter<C, Util::WeakRef<T> >&);
 
   Setter_f itsSetter_f;
 };
@@ -151,8 +201,8 @@ public:
 
   /// Casts \a item to type \c C* and calls the stored member function.
   virtual void action(void* item) {
-	 C* p = static_cast<C*>(item);
-	 (p->*itsAction_f)();
+    C* p = static_cast<C*>(item);
+    (p->*itsAction_f)();
   }
 
 private:
@@ -179,8 +229,8 @@ public:
 
   /// Casts \a item to type \c const \c C* and calls the stored member function.
   virtual void action(void* item) {
-	 const C* p = static_cast<C*>(item);
-	 (p->*itsAction_f)();
+    const C* p = static_cast<C*>(item);
+    (p->*itsAction_f)();
   }
 
 private:
@@ -199,10 +249,10 @@ namespace Tcl {
 ///////////////////////////////////////////////////////////////////////
 /**
  *
- * IoFetcher class definition                                       
- *	                                                                    
- *	This class exists to provide an interface for retrieving
- *	IO::IoObject's from integer id's.
+ * IoFetcher class definition
+ *
+ * This class exists to provide an interface for retrieving
+ * IO::IoObject's from integer id's.
  *
  **/
 ///////////////////////////////////////////////////////////////////////
@@ -228,7 +278,7 @@ public:
 class TclItemPkg : public TclItemPkgBase {
 public:
   TclItemPkg(Tcl_Interp* interp, const char* name, const char* version,
-				 int item_argn=1);
+             int item_argn=1);
 
   virtual ~TclItemPkg();
 
@@ -239,19 +289,19 @@ public:
 protected:
   template <class T>
   void declareGetter(const char* cmd_name, Getter<T>* getter,
-							const char* usage = 0);
+                     const char* usage = 0);
 
   template <class T>
   void declareSetter(const char* cmd_name, Setter<T>* setter,
-							const char* usage = 0);
+                     const char* usage = 0);
 
   template <class T>
   void declareAttrib(const char* attrib_name,
-							Getter<T>* getter, Setter<T>* setter,
-							const char* usage = 0);
+                     Getter<T>* getter, Setter<T>* setter,
+                     const char* usage = 0);
 
   void declareAction(const char* action_name, Action* action,
-							const char* usage = 0);
+                     const char* usage = 0);
 
   void addIoCommands(IoFetcher* fetcher);
 
@@ -273,49 +323,49 @@ template <class C>
 class CTclItemPkg : public TclItemPkg {
 public:
   CTclItemPkg(Tcl_Interp* interp, const char* name, const char* version,
-				  int item_argn=1) :
-	 TclItemPkg(interp, name, version, item_argn) {}
+              int item_argn=1) :
+    TclItemPkg(interp, name, version, item_argn) {}
 
   void declareCAction(const char* cmd_name, void (C::* actionFunc) (),
-							 const char* usage = 0)
+                      const char* usage = 0)
     {
-		declareAction(cmd_name, new CAction<C>(actionFunc), usage);
-	 }
+      declareAction(cmd_name, new CAction<C>(actionFunc), usage);
+    }
 
   void declareCAction(const char* cmd_name, void (C::* actionFunc) () const,
-							 const char* usage = 0)
+                      const char* usage = 0)
     {
-		declareAction(cmd_name, new CConstAction<C>(actionFunc), usage);
-	 }
+      declareAction(cmd_name, new CConstAction<C>(actionFunc), usage);
+    }
 
   template <class CC, class T>
   void declareCGetter(const char* cmd_name, T (CC::* getterFunc) () const,
-							 const char* usage = 0)
+                      const char* usage = 0)
     {
-		declareGetter(cmd_name, new CGetter<C,T>(getterFunc), usage);
-	 }
+      declareGetter(cmd_name, new CGetter<C,T>(getterFunc), usage);
+    }
 
   template <class CC, class T>
   void declareCSetter(const char* cmd_name, void (CC::* setterFunc) (T),
-							 const char* usage = 0)
+                      const char* usage = 0)
     {
-		declareSetter(cmd_name, new CSetter<C,T>(setterFunc), usage);
-	 }
+      declareSetter(cmd_name, new CSetter<C,T>(setterFunc), usage);
+    }
 
   template <class CC, class T>
   void declareCAttrib(const char* cmd_name,
-							 T (CC::* getterFunc) () const,
-							 void (CC::* setterFunc) (T),
-							 const char* usage = 0)
+                      T (CC::* getterFunc) () const,
+                      void (CC::* setterFunc) (T),
+                      const char* usage = 0)
     {
-		declareAttrib(cmd_name,
-						  new CGetter<C,T>(getterFunc), new CSetter<C,T>(setterFunc),
-						  usage);
-	 }
+      declareAttrib(cmd_name,
+                    new CGetter<C,T>(getterFunc), new CSetter<C,T>(setterFunc),
+                    usage);
+    }
 
   virtual C* getCItemFromId(int id) = 0;
   virtual void* getItemFromId(int id) {
-	 return static_cast<void*>(getCItemFromId(id));
+    return static_cast<void*>(getCItemFromId(id));
   }
 };
 
@@ -330,16 +380,16 @@ public:
 template <class C>
 class TclItemCmd : public TclCmd {
 public:
-  TclItemCmd(CTclItemPkg<C>* pkg, const char* cmd_name, const char* usage, 
-				 int objc_min=0, int objc_max=-1, bool exact_objc=false) :
-	 TclCmd(pkg->interp(), cmd_name, usage, objc_min, objc_max, exact_objc),
-	 itsPkg(pkg) {}
+  TclItemCmd(CTclItemPkg<C>* pkg, const char* cmd_name, const char* usage,
+             int objc_min=0, int objc_max=-1, bool exact_objc=false) :
+    TclCmd(pkg->interp(), cmd_name, usage, objc_min, objc_max, exact_objc),
+    itsPkg(pkg) {}
 protected:
   virtual void invoke() = 0;
 
   C* getItem() {
-	 int id = itsPkg->itemArgn() ? getIntFromArg(itsPkg->itemArgn()) : -1;
-	 return itsPkg->getCItemFromId(id);
+    int id = itsPkg->itemArgn() ? getIntFromArg(itsPkg->itemArgn()) : -1;
+    return itsPkg->getCItemFromId(id);
   }
 
   int afterItemArg(int n) { return itsPkg->itemArgn() + n; }
