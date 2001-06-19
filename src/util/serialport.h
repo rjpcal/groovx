46b6d78e0ebfbad292d5a57ee84de6ce572592a5
Robert J. Peters <rjpcal@gmail.com>
2001-06-19 22:34:23 +0000

    Updated for g++-3.0.

--- src/util/serialport.h
+++ src/util/serialport.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Wed Mar 29 13:45:35 2000
-// written: Thu May 10 12:04:35 2001
+// written: Tue Jun 19 15:28:10 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -13,9 +13,13 @@
 #ifndef SERIALPORT_H_DEFINED
 #define SERIALPORT_H_DEFINED
 
-#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(FSTREAM_H_DEFINED)
-#include <fstream.h>
-#define FSTREAM_H_DEFINED
+#ifdef PRESTANDARD_IOSTREAMS
+#  if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(FSTREAM_H_DEFINED)
+#    include <fstream.h>
+#    define FSTREAM_H_DEFINED
+#  endif
+#else
+#  include <fstream>
 #endif
 
 namespace Util {
@@ -31,18 +35,18 @@ public:
   /** Returns the next character from the stream, or EOF (-1) if the
       stream is empty. */
   int get()
-	 {
-		if ( itsClosed ) return -1;
+    {
+      if ( isClosed() ) return -1;
 
-		itsStream.clear();
-		itsStream.sync();
-		return itsStream.get();
-	 }
+      itsStream->clear();
+      itsStream->sync();
+      return itsStream->get();
+    }
 
   /// Returns the exit status of closing its stream
   int close();
 
-  bool isClosed() const { return itsClosed; }
+  bool isClosed() const { return (itsStream != 0); }
 
   int exitStatus() const { return itsExitStatus; }
 
@@ -53,8 +57,13 @@ private:
   SerialPort& operator=(const SerialPort&);
 
   int itsFiledes;
-  fstream itsStream;
-  bool itsClosed;
+
+#if defined(GCC_COMPILER) && GCC_COMPILER >= 3
+  std::filebuf* itsFilebuf;
+  std::iostream* itsStream;
+#else
+  STD_IO::fstream* itsStream;
+#endif
   int itsExitStatus;
 };
 
