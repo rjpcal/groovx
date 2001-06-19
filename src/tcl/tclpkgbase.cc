0437ff2ff9d33a3764f1425107d238e0fdce3c71
Robert J. Peters <rjpcal@gmail.com>
2001-06-19 22:15:07 +0000

    Removed throw spec's to please g++-3.0.

--- src/tcl/tclpkgbase.cc
+++ src/tcl/tclpkgbase.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Mon Jun 14 12:55:27 1999
-// written: Mon Jun 11 15:08:16 2001
+// written: Tue Jun 19 15:15:00 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -67,7 +67,7 @@ public:
 };
 
 Tcl::TclPkg::Impl::Impl(Tcl_Interp* interp,
-								const char* name, const char* version) :
+                        const char* name, const char* version) :
   itsInterp(interp),
   itsCmds(),
   itsPkgName(name ? name : ""),
@@ -79,27 +79,27 @@ Tcl::TclPkg::Impl::Impl(Tcl_Interp* interp,
 DOTRACE("Tcl::TclPkg::Impl::Impl");
   if ( !itsPkgName.empty() && !itsVersion.empty() ) {
 
-	 // First we must construct a capitalization-correct version of
-	 // itsPkgName that is just how Tcl likes it: first character
-	 // uppercase, all others lowercase.
-	 std::string pkgname = itsPkgName;
+    // First we must construct a capitalization-correct version of
+    // itsPkgName that is just how Tcl likes it: first character
+    // uppercase, all others lowercase.
+    std::string pkgname = itsPkgName;
 
-	 pkgname[0] = char(toupper(pkgname[0]));
+    pkgname[0] = char(toupper(pkgname[0]));
 
-	 for (size_t i = 1; i < pkgname.length(); ++i) {
-		pkgname[i] = char(tolower(pkgname[i]));
-	 }
+    for (size_t i = 1; i < pkgname.length(); ++i) {
+      pkgname[i] = char(tolower(pkgname[i]));
+    }
 
-	 // Fix up itsVersion to remove any extraneous char's (such as
-	 // version-control info, where we'd get a string like
-	 //   '$Revision 2.8 $'
+    // Fix up itsVersion to remove any extraneous char's (such as
+    // version-control info, where we'd get a string like
+    //   '$Revision 2.8 $'
 
-	 itsVersion.erase(0, itsVersion.find_first_of("0123456789"));
-	 itsVersion.erase(itsVersion.find_last_of("0123456789")+1, std::string::npos);
+    itsVersion.erase(0, itsVersion.find_first_of("0123456789"));
+    itsVersion.erase(itsVersion.find_last_of("0123456789")+1, std::string::npos);
 
-	 Tcl_PkgProvide(interp, 
-						 const_cast<char *>(pkgname.c_str()),
-						 const_cast<char *>(itsVersion.c_str()));
+    Tcl_PkgProvide(interp,
+                   const_cast<char *>(pkgname.c_str()),
+                   const_cast<char *>(itsVersion.c_str()));
   }
 }
 
@@ -108,7 +108,7 @@ DOTRACE("Tcl::TclPkg::Impl::~Impl");
 }
 
 Tcl::TclPkg::TclPkg(Tcl_Interp* interp,
-						  const char* name, const char* version) :
+                    const char* name, const char* version) :
   itsImpl(new Impl(interp, name, version))
 {
 DOTRACE("TclPkg::TclPkg");
@@ -128,11 +128,11 @@ DOTRACE("Tcl::TclPkg::initStatus");
 int Tcl::TclPkg::combineStatus(int other_status) const {
 DOTRACE("Tcl::TclPkg::initStatus");
   if (TCL_ERROR == itsImpl->itsInitStatus || TCL_ERROR == other_status)
-	 return TCL_ERROR;
+    return TCL_ERROR;
   else if (TCL_OK != itsImpl->itsInitStatus)
-	 return itsImpl->itsInitStatus;
+    return itsImpl->itsInitStatus;
   else if (TCL_OK != other_status)
-	 return other_status;
+    return other_status;
 
   return TCL_OK;
 }
@@ -147,7 +147,7 @@ DOTRACE("Tcl::TclPkg::interp");
  return itsImpl->itsInterp;
 }
 
-const char* Tcl::TclPkg::pkgName() { 
+const char* Tcl::TclPkg::pkgName() {
   return itsImpl->itsPkgName.c_str();
 }
 
@@ -166,7 +166,7 @@ DOTRACE("Tcl::TclPkg::makePkgCmdName");
 
 void Tcl::TclPkg::eval(const char* script) {
 DOTRACE("Tcl::TclPkg::eval");
-  fixed_string script_copy(script); 
+  fixed_string script_copy(script);
   Tcl_Eval(itsImpl->itsInterp, script_copy.data());
 }
 
@@ -175,53 +175,53 @@ DOTRACE("Tcl::TclPkg::addCommand");
   itsImpl->itsCmds.push_front(shared_ptr<TclCmd>(cmd));
 }
 
-void Tcl::TclPkg::linkVar(const char* varName, int& var) throw (Tcl::TclError) {
+void Tcl::TclPkg::linkVar(const char* varName, int& var) {
 DOTRACE("Tcl::TclPkg::linkVar int");
   DebugEvalNL(varName);
   if (Tcl_LinkInt(itsImpl->itsInterp, varName, &var, 0) != TCL_OK)
-	 throw TclError();
+    throw TclError();
 }
 
-void Tcl::TclPkg::linkVar(const char* varName, double& var) throw (Tcl::TclError) {
+void Tcl::TclPkg::linkVar(const char* varName, double& var) {
 DOTRACE("Tcl::TclPkg::linkVar double");
   DebugEvalNL(varName);
   if (Tcl_LinkDouble(itsImpl->itsInterp, varName, &var, 0) != TCL_OK)
-	 throw TclError();
+    throw TclError();
 }
 
-void Tcl::TclPkg::linkVarCopy(const char* varName, int var) throw (Tcl::TclError) {
+void Tcl::TclPkg::linkVarCopy(const char* varName, int var) {
 DOTRACE("Tcl::TclPkg::linkVarCopy int");
   DebugEvalNL(varName);
   shared_ptr<int> copy(new int(var));
   itsImpl->ownedInts.push_front(copy);
   if (Tcl_LinkInt(itsImpl->itsInterp, varName,
-						copy.get(), TCL_LINK_READ_ONLY) != TCL_OK)
-	 throw TclError();
+                  copy.get(), TCL_LINK_READ_ONLY) != TCL_OK)
+    throw TclError();
 }
 
-void Tcl::TclPkg::linkVarCopy(const char* varName, double var) throw (Tcl::TclError) {
+void Tcl::TclPkg::linkVarCopy(const char* varName, double var) {
 DOTRACE("Tcl::TclPkg::linkVarCopy double");
   shared_ptr<double> copy(new double(var));
   itsImpl->ownedDoubles.push_front(copy);
   if (Tcl_LinkDouble(itsImpl->itsInterp, varName,
-							copy.get(), TCL_LINK_READ_ONLY) != TCL_OK)
-	 throw TclError();
+                     copy.get(), TCL_LINK_READ_ONLY) != TCL_OK)
+    throw TclError();
 }
 
-void Tcl::TclPkg::linkConstVar(const char* varName, int& var) throw (Tcl::TclError) {
+void Tcl::TclPkg::linkConstVar(const char* varName, int& var) {
 DOTRACE("Tcl::TclPkg::linkConstVar int");
   DebugEvalNL(varName);
   if (Tcl_LinkInt(itsImpl->itsInterp, varName, &var, TCL_LINK_READ_ONLY)
-		!= TCL_OK)
-	 throw TclError();
+      != TCL_OK)
+    throw TclError();
 }
 
-void Tcl::TclPkg::linkConstVar(const char* varName, double& var) throw (Tcl::TclError) {
+void Tcl::TclPkg::linkConstVar(const char* varName, double& var) {
 DOTRACE("Tcl::TclPkg::linkConstVar double");
   DebugEvalNL(varName);
   if (Tcl_LinkDouble(itsImpl->itsInterp, varName, &var, TCL_LINK_READ_ONLY)
-		!= TCL_OK)
-	 throw TclError();
+      != TCL_OK)
+    throw TclError();
 }
 
 void Tcl::TclPkg::setInitStatusOk() {
