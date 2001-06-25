ca3761dab6fbc3df6dd68fa62ab18b734a90ba37
Robert J. Peters <rjpcal@gmail.com>
2001-06-25 13:46:43 +0000

    Got rid of old Tcl_HashTable based implementation of SubjectPkg, now using Tcl::GenericObjPkg.

--- testing/subject_test.tcl
+++ testing/subject_test.tcl
@@ -7,88 +7,21 @@
 ###
 ##############################################################################
 
-### subjectCmd ###
-test "SubjectTcl-subject" "args" {
-    subject
-} {wrong \# args: should be "subject subcommand \?args\.\.\.\?"}
-test "SubjectTcl-subject" "normal use" {} $BLANK $no_test
-test "SubjectTcl-subject" "error" {
-    subject junk
-} {subject: invalid command} 
+### Obj::new Subject ###
+test "SubjectTcl-Obj::new Subject" "norm1" {
+	 new Subject
+} {^[0-9]+$}
 
-### newSubjectCmd ###
-test "SubjectTcl-newSubject" "args" {
-    subject new
-} {wrong \# args: should be "subject new subjectKey \?subjectName\? \?subjectDir\?"}
-test "SubjectTcl-newSubject" "norm1" {
-	 catch {subject new rjp1}
-} {^0$}
-test "SubjectTcl-newSubject" "norm2" {
-	 catch {subject new rjp2 Rob}
-} {^0$}
-test "SubjectTcl-newSubject" "norm3" {
-	 catch {subject new rjp3 Rob ~/mydir/}
-} {^0$}
-test "SubjectTcl-newSubject" "error" {
-    subject new rep
-	 subject new rep
-} {subject: subject already exists}
+### Subject::nameCmd ###
+test "SubjectTcl-Subject::name" "normal" {
+	 set s [new Subject]
+	 Subject::name $s foobar
+	 Subject::name $s
+} {foobar}
 
-### subjectCmd ###
-test "SubjectTcl-clear" "args" {
-    subject
-} {wrong \# args: should be "subject subcommand \?args\.\.\.\?"}
-test "SubjectTcl-clear" "normal use" { 
-	 subject new s1
-	 subject new s2
-	 subject clear
-	 subject list
-} {^$}
-test "SubjectTcl-clear" "no error" {} $BLANK $no_test
-
-### subjectNameCmd ###
-subject new nt name_test
-test "SubjectTcl-subjectName" "args" {
-    subject name
-} {wrong \# args: should be "subject name subjectKey \?subjectName\?"}
-test "SubjectTcl-subjectName" "norm1" {
-	 subject name nt
-} {name_test}
-test "SubjectTcl-subjectName" "norm2" {
-	 subject name nt new_name
-	 subject name nt
-} {new_name}
-test "SubjectTcl-subjectName" "error" {
-    subject name junk
-} {subject: no such subject exists}
-
-### subjectDirCmd ###
-subject new dt dt_name /dir_test/
-test "SubjectTcl-subjectDir" "args" {
-    subject dir
-} {wrong \# args: should be "subject dir subjectKey \?subjectDir\?"}
-test "SubjectTcl-subjectDir" "norm1" {
-	 subject dir dt
-} {/dir_test/}
-test "SubjectTcl-subjectDir" "norm2" {
-	 subject dir dt /new_dir/
-	 subject dir dt
-} {/new_dir/}
-test "SubjectTcl-subjectDir" "error" {
-	 subject dir junk
-} {subject: no such subject exists}
-
-### listSubjectsCmd ###
-test "SubjectTcl-listSubjects" "args" {
-    subject list junk
-} {wrong \# args: should be "subject list"}
-test "SubjectTcl-listSubjects" "normal use" {
-	 subject clear
-	 subject new s1 subj1 /dir1/
-	 subject new s2 subj2 /dir2/
-	 subject list
-} {s1[ \t]+subj1[ \t]+/dir1/
-s2[ \t]+subj2[ \t]+/dir2/}
-test "SubjectTcl-listSubjects" "no error" {} $BLANK $no_test
-	 
-
+### Subject::directoryCmd ###
+test "SubjectTcl-Subject::directory" "normal" {
+	 set s [new Subject]
+	 Subject::directory $s /foobar/
+	 Subject::directory $s
+} {/foobar/}
