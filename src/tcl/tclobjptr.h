56cc3ff5666dfa301ac0fde9e5ba5ccf1e127cad
Robert J. Peters <rjpcal@gmail.com>
2001-07-11 19:06:18 +0000

    Removed unused TclObjLock, renamed Tcl::TclObjPtr to Tcl::ObjPtr, and renamed the file from "tclobjlock.h" to "tclobjptr.h".

--- src/tcl/tclobjptr.h
+++ src/tcl/tclobjptr.h
@@ -1,17 +1,17 @@
 ///////////////////////////////////////////////////////////////////////
 //
-// tclobjlock.h
+// tclobjptr.h
 //
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Tue May 11 13:44:19 1999
-// written: Mon Jun 11 15:08:15 2001
+// written: Wed Jul 11 12:04:54 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
 
-#ifndef TCLOBJLOCK_H_DEFINED
-#define TCLOBJLOCK_H_DEFINED
+#ifndef TCLOBJPTR_H_DEFINED
+#define TCLOBJPTR_H_DEFINED
 
 #if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(TCL_H_DEFINED)
 #include <tcl.h>
@@ -25,57 +25,6 @@ namespace Tcl {
 ///////////////////////////////////////////////////////////////////////
 /**
  *
- * This utility class manages the reference count of a Tcl_Obj within
- * a lexical scope. It increments the ref count on construction of the
- * lock, and decrements the ref count on destruction of the lock. This
- * allows Tcl_Obj*'s to behave according to normal scoping rules.
- *
- **/
-///////////////////////////////////////////////////////////////////////
-
-class TclObjLock {
-public:
-  /// Default constructor.
-  TclObjLock(Tcl_Obj* obj) : itsObj(obj) { Tcl_IncrRefCount(itsObj); }
-
-  /// Destructor.
-  ~TclObjLock() { Tcl_DecrRefCount(itsObj); }
-
-  /// Copy constructor.
-  TclObjLock(const TclObjLock& x) :
-	 itsObj(const_cast<Tcl_Obj*>(x.itsObj))
-	 {
-		Tcl_IncrRefCount(itsObj);
-	 }
-
-  /// Assignment operator from a TclObjLock.
-  TclObjLock& operator=(const TclObjLock& x)
-	 {
-		assign(x.itsObj); return *this;
-	 }
-
-  /// Assignment operator from a Tcl_Obj*.
-  TclObjLock& operator=(Tcl_Obj* x)
-	 {
-		assign(x); return *this;
-	 }
-
-private:
-  void assign(Tcl_Obj* x)
-	 {
-		if (itsObj != x) {
-		  Tcl_DecrRefCount(itsObj);
-		  itsObj = x;
-		  Tcl_IncrRefCount(itsObj);
-		}
-	 }
-
-  Tcl_Obj* itsObj;
-};
-
-///////////////////////////////////////////////////////////////////////
-/**
- *
  * This class acts as a reference counted smart pointer to Tcl_Obj. It
  * is similar to TclObjLock in that it manages the reference count of
  * a held Tcl_Obj*, but it also defines a conversion operator back to
@@ -84,32 +33,32 @@ private:
  **/
 ///////////////////////////////////////////////////////////////////////
 
-class TclObjPtr {
+class ObjPtr {
 public:
   /// Default constructor.
-  TclObjPtr(Tcl_Obj* obj) : itsObj(obj) { Tcl_IncrRefCount(itsObj); }
+  ObjPtr(Tcl_Obj* obj) : itsObj(obj) { Tcl_IncrRefCount(itsObj); }
 
   /// Destructor.
-  ~TclObjPtr() { Tcl_DecrRefCount(itsObj); }
+  ~ObjPtr() { Tcl_DecrRefCount(itsObj); }
 
   /// Copy constructor.
-  TclObjPtr(const TclObjPtr& x) :
-	 itsObj(const_cast<Tcl_Obj*>(x.itsObj))
-	 {
-		Tcl_IncrRefCount(itsObj);
-	 }
+  ObjPtr(const ObjPtr& x) :
+    itsObj(const_cast<Tcl_Obj*>(x.itsObj))
+    {
+      Tcl_IncrRefCount(itsObj);
+    }
 
-  /// Assignment operator from TclObjPtr.
-  TclObjPtr& operator=(const TclObjPtr& x)
-	 {
-		assign(x.itsObj); return *this;
-	 }
+  /// Assignment operator from ObjPtr.
+  ObjPtr& operator=(const ObjPtr& x)
+    {
+      assign(x.itsObj); return *this;
+    }
 
   /// Assignment operator from Tcl_Obj*.
-  TclObjPtr& operator=(Tcl_Obj* x)
-	 {
-		assign(x); return *this;
-	 }
+  ObjPtr& operator=(Tcl_Obj* x)
+    {
+      assign(x); return *this;
+    }
 
   typedef Tcl_Obj* Tcl_ObjPtr;
 
@@ -118,18 +67,18 @@ public:
 
 private:
   void assign(Tcl_Obj* x)
-	 {
-		if (itsObj != x) {
-		  Tcl_DecrRefCount(itsObj);
-		  itsObj = x;
-		  Tcl_IncrRefCount(itsObj);
-		}
-	 }
+    {
+      if (itsObj != x) {
+        Tcl_DecrRefCount(itsObj);
+        itsObj = x;
+        Tcl_IncrRefCount(itsObj);
+      }
+    }
 
   Tcl_Obj* itsObj;
 };
 
 } // end namespace Tcl
 
-static const char vcid_tclobjlock_h[] = "$Header$";
-#endif // !TCLOBJLOCK_H_DEFINED
+static const char vcid_tclobjptr_h[] = "$Header$";
+#endif // !TCLOBJPTR_H_DEFINED
