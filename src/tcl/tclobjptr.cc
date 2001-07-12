1f2d4238c02a96e8692349823c10ca713f429d79
Robert J. Peters <rjpcal@gmail.com>
2001-07-12 01:34:58 +0000

    Initial revision

---
+++ src/tcl/tclobjptr.cc
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////
+//
+// tclobjptr.cc
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Wed Jul 11 18:30:47 2001
+// written: Wed Jul 11 18:34:35 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef TCLOBJPTR_CC_DEFINED
+#define TCLOBJPTR_CC_DEFINED
+
+#include "tcl/tclobjptr.h"
+
+#include <tcl.h>
+
+bool Tcl::ObjPtr::isShared() const
+{
+  return Tcl_IsShared(itsObj);
+}
+
+void Tcl::ObjPtr::ensureUnique()
+{
+  if (isShared())
+    {
+      Tcl_Obj* newObj = Tcl_DuplicateObj(itsObj);
+      assign(newObj);
+    }
+}
+
+void Tcl::ObjPtr::incrRef(Tcl_Obj* obj)
+{
+  Tcl_IncrRefCount(obj);
+}
+
+void Tcl::ObjPtr::decrRef(Tcl_Obj* obj)
+{
+  Tcl_DecrRefCount(obj);
+}
+
+static const char vcid_tclobjptr_cc[] = "$Header$";
+#endif // !TCLOBJPTR_CC_DEFINED
