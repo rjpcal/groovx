c4c0026698d192609987411e19afc76448c81787
Robert J. Peters <rjpcal@gmail.com>
2001-07-12 19:45:39 +0000

    Initial revision

---
+++ src/tcl/tclveccmd.cc
@@ -0,0 +1,123 @@
+///////////////////////////////////////////////////////////////////////
+//
+// tclveccmd.cc
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Thu Jul 12 12:15:46 2001
+// written: Thu Jul 12 12:41:56 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef TCLVECCMD_CC_DEFINED
+#define TCLVECCMD_CC_DEFINED
+
+#include "tcl/tclveccmd.h"
+
+#include "tcl/tclerror.h"
+
+#include "util/minivec.h"
+
+#include "util/trace.h"
+#define LOCAL_ASSERT
+#include "util/debug.h"
+
+///////////////////////////////////////////////////////////////////////
+//
+// Tcl::VecCmd::VecContext implements the Context interface in such a
+// way as to treat each of the arguments as lists, and provide access
+// to slices across those lists, thus allowing "vectorized" command
+// invocations.
+//
+///////////////////////////////////////////////////////////////////////
+
+class Tcl::VecCmd::VecContext : public Tcl::TclCmd::Context {
+public:
+  VecContext(Tcl_Interp* interp, int objc, Tcl_Obj* const objv[],
+             unsigned int num_calls) :
+    Tcl::TclCmd::Context(interp, objc, objv),
+    itsArgs(),
+    itsNumCalls(num_calls),
+    itsResult()
+  {
+    for (int i = 0; i < objc; ++i)
+      {
+        Tcl::List arg(objv[i]);
+        if (arg.length() == 0)
+          {
+            throw Tcl::TclError("argument was empty");
+          }
+        itsArgs.push_back( arg.begin<Tcl_Obj*>() );
+      }
+  }
+
+  virtual ~VecContext()
+  {
+    Tcl::TclCmd::Context::setObjResult(itsResult.asObj());
+  }
+
+  void next()
+  {
+    for (unsigned int i = 0; i < itsArgs.size(); ++i)
+      {
+        if (itsArgs[i].hasMore())
+          ++(itsArgs[i]);
+      }
+  }
+
+protected:
+  virtual Tcl_Obj* getObjv(int argn)
+  {
+    return *(itsArgs.at(argn));
+  }
+
+  virtual void setObjResult(Tcl_Obj* obj)
+  {
+    itsResult.append(obj);
+  }
+
+private:
+  typedef Tcl::List::Iterator<Tcl_Obj*> Iter;
+
+  minivec<Iter> itsArgs;
+  unsigned int itsNumCalls;
+  Tcl::List itsResult;
+};
+
+
+Tcl::VecCmd::VecCmd(Tcl_Interp* interp, const char* cmd_name, const char* usage,
+                    unsigned int key_argn,
+                    int objc_min=0, int objc_max=-1, bool exact_objc=false) :
+  Tcl::TclCmd(interp, cmd_name, usage, objc_min, objc_max, exact_objc),
+  itsKeyArgn(key_argn)
+{}
+
+void Tcl::VecCmd::rawInvoke(Tcl_Interp* interp, int objc, Tcl_Obj* const objv[])
+{
+DOTRACE("Tcl::VecCmd::rawInvoke");
+
+  unsigned int ncalls = Tcl::List::getLength(objv[itsKeyArgn]);
+
+  if (ncalls > 1)
+    {
+      VecContext cx(interp, objc, objv, ncalls);
+
+      for (unsigned int c = 0; c < ncalls; ++c)
+        {
+          invoke(cx);
+          cx.next();
+        }
+    }
+  else if (ncalls == 1)
+    {
+      Tcl::TclCmd::rawInvoke(interp, objc, objv);
+    }
+  else // (ncalls == 0)
+    {
+      throw Tcl::TclError("key argument was empty");
+    }
+}
+
+static const char vcid_tclveccmd_cc[] = "$Header$";
+#endif // !TCLVECCMD_CC_DEFINED
---
+++ src/tcl/tclveccmd.h
@@ -0,0 +1,56 @@
+///////////////////////////////////////////////////////////////////////
+//
+// tclveccmd.h
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Thu Jul 12 12:14:43 2001
+// written: Thu Jul 12 12:45:13 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef TCLVECCMD_H_DEFINED
+#define TCLVECCMD_H_DEFINED
+
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(TCLCMD_H_DEFINED)
+#include "tcl/tclcmd.h"
+#endif
+
+namespace Tcl
+{
+  class VecCmd;
+}
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * \c Tcl::VecCmd reimplements rawInvoke to use a specialized \c
+ * Context class that treats each of the arguments as lists, and
+ * provide access to slices across those lists, thus allowing
+ * "vectorized" command invocations. Subclasses of \c Tcl::VecCmd
+ * implement \a invoke() just as they would if inheriting from \c
+ * Tcl::TclCmd.
+ *
+ **/
+///////////////////////////////////////////////////////////////////////
+
+class Tcl::VecCmd : public Tcl::TclCmd {
+public:
+  VecCmd(Tcl_Interp* interp, const char* cmd_name, const char* usage,
+         unsigned int key_argn,
+         int objc_min=0, int objc_max=-1, bool exact_objc=false);
+
+  virtual void invoke(Context& cx) = 0;
+
+protected:
+  class VecContext;
+
+  virtual void rawInvoke(Tcl_Interp* interp, int objc, Tcl_Obj* const objv[]);
+
+private:
+  unsigned int itsKeyArgn;
+};
+
+static const char vcid_tclveccmd_h[] = "$Header$";
+#endif // !TCLVECCMD_H_DEFINED
