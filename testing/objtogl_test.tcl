6641d8e96611a4918c8cee9dc6ae8f818f19edda
Robert J. Peters <rjpcal@gmail.com>
2001-07-15 21:30:54 +0000

    Changed to use WeakRef's instead of Ref's to pass objects to member functions in order to allow volatile objects.

--- testing/objtogl_test.tcl
+++ testing/objtogl_test.tcl
@@ -158,7 +158,7 @@ test "ObjTogl-dumpCmap" "error from non-integral number arg2" {
 } {expected integer but got "1\.5"}
 test "ObjTogl-dumpCmap" "error from too small index" {
     Togl::dumpCmap -3 -1
-} {^Toglet::dumpCmap: colormap index out of range$}
+} {^Toglet::dumpCmap:}
 test "ObjTogl-dumpCmap" "error from too large index" {
     Togl::dumpCmap 100 900
 } {^Toglet::dumpCmap: colormap index out of range$}
