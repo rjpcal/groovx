4896b9ea84d2f8d42a5debc5fb594be1d8e8f4ea
Robert J. Peters <rjpcal@gmail.com>
2001-07-15 14:47:33 +0000

    Changed to use Tcl::def instead of addCommand().

--- src/tcl/dlisttcl.cc
+++ src/tcl/dlisttcl.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Dec-98
-// written: Fri Jul 13 10:24:34 2001
+// written: Sun Jul 15 07:47:13 2001
 // $Id$
 //
 // This package provides additional Tcl list manipulation functions
@@ -15,6 +15,7 @@
 #ifndef DLISTTCL_CC_DEFINED
 #define DLISTTCL_CC_DEFINED
 
+#include "tcl/objfunctor.h"
 #include "tcl/tclcmd.h"
 #include "tcl/tclerror.h"
 #include "tcl/tclpkg.h"
@@ -33,15 +34,15 @@
 
 namespace DlistTcl
 {
-  class ChooseCmd;
-  class NotCmd;
-  class OnesCmd;
-  class PickoneCmd;
-  class RangeCmd;
-  class RepeatCmd;
-  class SelectCmd;
-  class SumCmd;
-  class ZerosCmd;
+  Tcl::List choose(Tcl::List source_list, Tcl::List index_list);
+  Tcl::List not_(Tcl::List source_list);
+  Tcl::List ones(unsigned int num_ones);
+  Tcl::ObjPtr pickone(Tcl::List source_list);
+  Tcl::List range(int begin, int end);
+  Tcl::List repeat(Tcl::List source_list, Tcl::List times_list);
+  Tcl::List select(Tcl::List source_list, Tcl::List flags_list);
+  Tcl::ObjPtr sum(Tcl::List source_list);
+  Tcl::List zeros(unsigned int num_zeros);
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -63,31 +64,21 @@ namespace DlistTcl
 //
 //--------------------------------------------------------------------
 
-class DlistTcl::ChooseCmd : public Tcl::TclCmd {
-public:
-  ChooseCmd(Tcl_Interp* interp, const char* cmd_name) :
-    Tcl::TclCmd(interp, cmd_name, "source_list index_list", 3) {}
+Tcl::List DlistTcl::choose(Tcl::List source_list, Tcl::List index_list)
+{
+  Tcl::List result;
 
-protected:
-  virtual void invoke(Tcl::Context& ctx)
+  for (unsigned int i = 0; i < index_list.length(); ++i)
     {
-      Tcl::List source_list = ctx.getValFromArg(1, (Tcl::List*)0);
-      Tcl::List index_list = ctx.getValFromArg(2, (Tcl::List*)0);
+      unsigned int index = index_list.get<unsigned int>(i);
 
-      Tcl::List result;
-
-      for (unsigned int i = 0; i < index_list.length(); ++i)
-        {
-          unsigned int index = index_list.get<unsigned int>(i);
-
-          // use that int as an index into source list, getting the
-          // corresponding list element and appending it to the output list
-          result.append(source_list.at(index));
-        }
-
-      ctx.setResult(result);
+      // use that int as an index into source list, getting the
+      // corresponding list element and appending it to the output list
+      result.append(source_list.at(index));
     }
-};
+
+  return result;
+}
 
 //--------------------------------------------------------------------
 //
@@ -97,31 +88,22 @@ protected:
 //
 //--------------------------------------------------------------------
 
-class DlistTcl::NotCmd : public Tcl::TclCmd {
-public:
-  NotCmd(Tcl_Interp* interp, const char* cmd_name) :
-    Tcl::TclCmd(interp, cmd_name, "source_list", 2) {}
+Tcl::List DlistTcl::not_(Tcl::List source_list)
+{
+  Tcl::ObjPtr one(Tcl::Convert<int>::toTcl(1));
+  Tcl::ObjPtr zero(Tcl::Convert<int>::toTcl(0));
+  Tcl::List result;
 
-protected:
-  virtual void invoke(Tcl::Context& ctx)
+  for (unsigned int i = 0; i < source_list.length(); ++i)
     {
-      Tcl::List source_list = ctx.getValFromArg(1, (Tcl::List*)0);
-
-      Tcl::ObjPtr one(Tcl::Convert<int>::toTcl(1));
-      Tcl::ObjPtr zero(Tcl::Convert<int>::toTcl(0));
-      Tcl::List result;
-
-      for (unsigned int i = 0; i < source_list.length(); ++i)
-        {
-          if ( source_list.get<int>(i) == 0 )
-            result.append(one);
-          else
-            result.append(zero);
-        }
-
-      ctx.setResult(result);
+      if ( source_list.get<int>(i) == 0 )
+        result.append(one);
+      else
+        result.append(zero);
     }
-};
+
+  return result;
+}
 
 //--------------------------------------------------------------------
 //
@@ -130,22 +112,13 @@ protected:
 //
 //--------------------------------------------------------------------
 
-class DlistTcl::OnesCmd : public Tcl::TclCmd {
-public:
-  OnesCmd(Tcl_Interp* interp, const char* cmd_name) :
-    Tcl::TclCmd(interp, cmd_name, "num_ones", 2) {}
+Tcl::List DlistTcl::ones(unsigned int num_ones)
+{
+  Tcl::List result;
+  result.append(1, num_ones);
 
-protected:
-  virtual void invoke(Tcl::Context& ctx)
-    {
-      unsigned int num_ones = ctx.getValFromArg(1, (unsigned int*)0);
-
-      Tcl::List result;
-      result.append(1, num_ones);
-
-      ctx.setResult(result);
-    }
-};
+  return result;
+}
 
 //--------------------------------------------------------------------
 //
@@ -154,28 +127,19 @@ protected:
 //
 //--------------------------------------------------------------------
 
-class DlistTcl::PickoneCmd : public Tcl::TclCmd {
-public:
-  PickoneCmd(Tcl_Interp* interp, const char* cmd_name) :
-    Tcl::TclCmd(interp, cmd_name, "source_list", 2) {}
-
-protected:
-  virtual void invoke(Tcl::Context& ctx)
+Tcl::ObjPtr DlistTcl::pickone(Tcl::List source_list)
+{
+  if (source_list.length() == 0)
     {
-      Tcl::List source_list = ctx.getValFromArg(1, (Tcl::List*)0);
-
-      if (source_list.length() == 0)
-        {
-          throw Tcl::TclError("source_list is empty");
-        }
-
-      // pick a random int between 0 and (src_len-1), inclusive
-      unsigned int randnum = (unsigned int)
-        ( double(rand())/(double(RAND_MAX)+1.0) * source_list.length() );
-
-      ctx.setResult(source_list.at(randnum));
+      throw Tcl::TclError("source_list is empty");
     }
-};
+
+  // pick a random int between 0 and (src_len-1), inclusive
+  unsigned int randnum = (unsigned int)
+    ( double(rand())/(double(RAND_MAX)+1.0) * source_list.length() );
+
+  return source_list.at(randnum);
+}
 
 //--------------------------------------------------------------------
 //
@@ -184,27 +148,17 @@ protected:
 //
 //--------------------------------------------------------------------
 
-class DlistTcl::RangeCmd : public Tcl::TclCmd {
-public:
-  RangeCmd(Tcl_Interp* interp, const char* cmd_name) :
-    Tcl::TclCmd(interp, cmd_name, "begin end", 3) {}
+Tcl::List DlistTcl::range(int begin, int end)
+{
+  Tcl::List result;
 
-protected:
-  virtual void invoke(Tcl::Context& ctx)
+  for (int i = begin; i <= end; ++i)
     {
-      int begin = ctx.getValFromArg(1, (int*)0);
-      int end = ctx.getValFromArg(2, (int*)0);
-
-      Tcl::List result;
-
-      for (int i = begin; i <= end; ++i)
-        {
-          result.append(i);
-        }
-
-      ctx.setResult(result);
+      result.append(i);
     }
-};
+
+  return result;
+}
 
 //--------------------------------------------------------------------
 //
@@ -220,34 +174,24 @@ protected:
 //
 //--------------------------------------------------------------------
 
-class DlistTcl::RepeatCmd : public Tcl::TclCmd {
-public:
-  RepeatCmd(Tcl_Interp* interp, const char* cmd_name) :
-    Tcl::TclCmd(interp, cmd_name, "source_list times_list", 3) {}
+Tcl::List DlistTcl::repeat(Tcl::List source_list, Tcl::List times_list)
+{
+  unsigned int src_len = source_list.length();
+  unsigned int tim_len = times_list.length();
 
-protected:
-  virtual void invoke(Tcl::Context& ctx)
+  // find the minimum of the two lists' lengths
+  unsigned int min_len = (src_len < tim_len) ? src_len : tim_len;
+
+  Tcl::List result;
+
+  for (unsigned int t = 0; t < min_len; ++t)
     {
-      Tcl::List source_list = ctx.getValFromArg(1, (Tcl::List*)0);
-      Tcl::List times_list = ctx.getValFromArg(2, (Tcl::List*)0);
-
-      unsigned int src_len = source_list.length();
-      unsigned int tim_len = times_list.length();
-
-      // find the minimum of the two lists' lengths
-      unsigned int min_len = (src_len < tim_len) ? src_len : tim_len;
-
-      Tcl::List result;
-
-      for (unsigned int t = 0; t < min_len; ++t)
-        {
-          result.append(source_list.at(t),
-                        times_list.get<unsigned int>(t));
-        }
-
-      ctx.setResult(result);
+      result.append(source_list.at(t),
+                    times_list.get<unsigned int>(t));
     }
-};
+
+  return result;
+}
 
 //--------------------------------------------------------------------
 //
@@ -257,39 +201,30 @@ protected:
 //
 //--------------------------------------------------------------------
 
-class DlistTcl::SelectCmd : public Tcl::TclCmd {
-public:
-  SelectCmd(Tcl_Interp* interp, const char* cmd_name) :
-    Tcl::TclCmd(interp, cmd_name, "source_list flags_list", 3) {}
+Tcl::List DlistTcl::select(Tcl::List source_list, Tcl::List flags_list)
+{
+  unsigned int src_len = source_list.length();
+  unsigned int flg_len = flags_list.length();
 
-protected:
-  virtual void invoke(Tcl::Context& ctx)
+  if (flg_len < src_len)
     {
-      Tcl::List source_list = ctx.getValFromArg(1, (Tcl::List*)0);
-      Tcl::List flags_list = ctx.getValFromArg(2, (Tcl::List*)0);
-      unsigned int src_len = source_list.length();
-      unsigned int flg_len = flags_list.length();
-
-      if (flg_len < src_len)
-        {
-          throw Tcl::TclError("flags list must be as long as source_list");
-        }
-
-      Tcl::List result;
-
-      for (unsigned int i = 0; i < src_len; ++i)
-        {
-          // if the flag is true, add the corresponding source_list
-          // element to the result list
-          if ( flags_list.get<int>(i) )
-            {
-              result.append(source_list.at(i));
-            }
-        }
-
-      ctx.setResult(result);
+      throw Tcl::TclError("flags list must be as long as source_list");
     }
-};
+
+  Tcl::List result;
+
+  for (unsigned int i = 0; i < src_len; ++i)
+    {
+      // if the flag is true, add the corresponding source_list
+      // element to the result list
+      if ( flags_list.get<int>(i) )
+        {
+          result.append(source_list.at(i));
+        }
+    }
+
+  return result;
+}
 
 //--------------------------------------------------------------------
 //
@@ -299,49 +234,40 @@ protected:
 //
 //--------------------------------------------------------------------
 
-class DlistTcl::SumCmd : public Tcl::TclCmd {
-public:
-  SumCmd(Tcl_Interp* interp, const char* cmd_name) :
-    Tcl::TclCmd(interp, cmd_name, "source_list", 2) {}
+Tcl::ObjPtr DlistTcl::sum(Tcl::List source_list)
+{
+  int isum=0;
+  double dsum=0.0;
+  bool seen_double=false;
 
-protected:
-  virtual void invoke(Tcl::Context& ctx)
+  for (unsigned int i = 0; i < source_list.length(); /* incr in loop body*/)
     {
-      Tcl::List source_list = ctx.getValFromArg(1, (Tcl::List*)0);
-
-      int isum=0;
-      double dsum=0.0;
-      bool seen_double=false;
-
-      for (unsigned int i = 0; i < source_list.length(); /* incr in loop body*/)
-        {
-          if ( !seen_double )
-            {
-              try
-                {
-                  isum += source_list.get<int>(i);
-                }
-              catch(Tcl::TclError&)
-                {
-                  seen_double = true;
-                  dsum = isum;
-                  continue; // skip the increment
-                }
-            }
-          else
-            {
-              dsum += source_list.get<double>(i);
-            }
-
-          ++i; // here's the increment
-      }
-
       if ( !seen_double )
-        ctx.setResult( isum );
+        {
+          try
+            {
+              isum += source_list.get<int>(i);
+            }
+          catch(Tcl::TclError&)
+            {
+              seen_double = true;
+              dsum = isum;
+              continue; // skip the increment
+            }
+        }
       else
-        ctx.setResult( dsum );
-    }
-};
+        {
+          dsum += source_list.get<double>(i);
+        }
+
+      ++i; // here's the increment
+  }
+
+  if ( !seen_double )
+    return isum;
+  else
+    return dsum;
+}
 
 //--------------------------------------------------------------------
 //
@@ -350,22 +276,13 @@ protected:
 //
 //--------------------------------------------------------------------
 
-class DlistTcl::ZerosCmd : public Tcl::TclCmd {
-public:
-  ZerosCmd(Tcl_Interp* interp, const char* cmd_name) :
-    Tcl::TclCmd(interp, cmd_name, "num_zeros", 2) {}
+Tcl::List DlistTcl::zeros(unsigned int num_zeros)
+{
+  Tcl::List result;
+  result.append(0, num_zeros);
 
-protected:
-  virtual void invoke(Tcl::Context& ctx)
-    {
-      unsigned int num_zeros = ctx.getValFromArg(1, (unsigned int*)0);
-
-      Tcl::List result;
-      result.append(0, num_zeros);
-
-      ctx.setResult(result);
-    }
-};
+  return result;
+}
 
 //--------------------------------------------------------------------
 //
@@ -374,20 +291,24 @@ protected:
 //--------------------------------------------------------------------
 
 extern "C"
-int Dlist_Init(Tcl_Interp* interp) {
+int Dlist_Init(Tcl_Interp* interp)
+{
 DOTRACE("Dlist_Init");
 
   Tcl::TclPkg* pkg = new Tcl::TclPkg(interp, "Dlist", "$Revision$");
 
-  pkg->addCommand( new DlistTcl::ChooseCmd(interp, "dlist_choose") );
-  pkg->addCommand( new DlistTcl::OnesCmd(interp, "dlist_ones") );
-  pkg->addCommand( new DlistTcl::ZerosCmd(interp, "dlist_zeros") );
-  pkg->addCommand( new DlistTcl::SumCmd(interp, "dlist_sum") );
-  pkg->addCommand( new DlistTcl::RangeCmd(interp, "dlist_range") );
-  pkg->addCommand( new DlistTcl::RepeatCmd(interp, "dlist_repeat") );
-  pkg->addCommand( new DlistTcl::SelectCmd(interp, "dlist_select") );
-  pkg->addCommand( new DlistTcl::NotCmd(interp, "dlist_not") );
-  pkg->addCommand( new DlistTcl::PickoneCmd(interp, "dlist_pickone") );
+  Tcl::def( pkg, &DlistTcl::choose,
+            "dlist_choose", "source_list index_list" );
+  Tcl::def( pkg, &DlistTcl::not_, "dlist_not", "source_list" );
+  Tcl::def( pkg, &DlistTcl::ones, "dlist_ones", "num_ones" );
+  Tcl::def( pkg, &DlistTcl::pickone, "dlist_pickone", "source_list" );
+  Tcl::def( pkg, &DlistTcl::range, "dlist_range", "begin end" );
+  Tcl::def( pkg, &DlistTcl::repeat,
+            "dlist_repeat", "source_list times_list" );
+  Tcl::def( pkg, &DlistTcl::select,
+            "dlist_select", "source_list flags_list" );
+  Tcl::def( pkg, &DlistTcl::sum, "dlist_sum", "source_list" );
+  Tcl::def( pkg, &DlistTcl::zeros, "dlist_zeros", "num_zeros" );
 
   return pkg->initStatus();
 }
