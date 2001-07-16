de38b5e7f5db37de3d5668837144b5e9eced9125
Robert J. Peters <rjpcal@gmail.com>
2001-07-16 18:48:48 +0000

    Updated error messages.

--- testing/dlist_test.tcl
+++ testing/dlist_test.tcl
@@ -10,7 +10,7 @@
 ### dlist_chooseCmd ###
 test "DlistTcl-dlist_choose" "args" {
 	 dlist_choose dlist_choose
-} {wrong \# args: should be "dlist_choose source_list index_list"}
+} {wrong \# args: should be "::dlist_choose source_list index_list"}
 test "DlistTcl-dlist_choose" "norm1" { 
 	 catch {dlist_choose {} {}}
 } {^0$}
@@ -19,7 +19,7 @@ test "DlistTcl-dlist_choose" "norm2" {
 } {7 3\.1 5}
 test "DlistTcl-dlist_choose" "err1" {
      dlist_choose {1 2} {-1 0} 
-} {^dlist_choose: signed/unsigned conversion failed}
+} {dlist_choose: signed/unsigned conversion failed}
 test "DlistTcl-dlist_choose" "err2" {
      dlist_choose {1 2} {1.5 0} 
 } {expected integer but got "1\.5"}
@@ -27,7 +27,7 @@ test "DlistTcl-dlist_choose" "err2" {
 ### dlist_notCmd ###
 test "DlistTcl-dlist_not" "args" {
     dlist_not
-} {wrong \# args: should be "dlist_not source_list"}
+} {wrong \# args: should be "::dlist_not source_list"}
 test "DlistTcl-dlist_not" "norm1" { 
 	 catch {dlist_not {}}
 } {^0$}
@@ -41,7 +41,7 @@ test "DlistTcl-dlist_not" "error" {
 ### dlist_onesCmd ###
 test "DlistTcl-dlist_ones" "args" {
     dlist_ones
-} {wrong \# args: should be "dlist_ones num_ones"}
+} {wrong \# args: should be "::dlist_ones num_ones"}
 test "DlistTcl-dlist_ones" "norm1" { 
 	 catch {dlist_ones 0}
 } {^0$}
@@ -53,12 +53,12 @@ test "DlistTcl-dlist_ones" "err1" {
 } {expected integer but got "4\.5"}
 test "DlistTcl-dlist_ones" "err2" {
 	 dlist_ones -1
-} {^dlist_ones: signed/unsigned conversion failed}
+} {dlist_ones: signed/unsigned conversion failed}
 
 ### dlist_pickoneCmd ###
 test "DlistTcl-dlist_pickone" "args" {
     dlist_pickone
-} {wrong \# args: should be "dlist_pickone source_list"}
+} {wrong \# args: should be "::dlist_pickone source_list"}
 test "DlistTcl-dlist_pickone" "norm1" { 
 	 dlist_pickone {1 3 5 7 9}
 } {[13579]}
@@ -72,7 +72,7 @@ test "DlistTcl-dlist_pickone" "error" {
 ### dlist_rangeCmd ###
 test "DlistTcl-dlist_range" "args" {
     dlist_range
-} {wrong \# args: should be "dlist_range begin end"}
+} {wrong \# args: should be "::dlist_range begin end"}
 test "DlistTcl-dlist_range" "norm1" { 
 	 dlist_range 0 0
 } {^0$}
@@ -87,7 +87,7 @@ test "DlistTcl-dlist_range" "err1" {
 ### dlist_repeatCmd ###
 test "DlistTcl-dlist_repeat" "args" {
     dlist_repeat
-} {wrong \# args: should be "dlist_repeat source_list times_list"}
+} {wrong \# args: should be "::dlist_repeat source_list times_list"}
 test "DlistTcl-dlist_repeat" "norm1" {
 	 catch {dlist_repeat {} {}}
 } {^0$}
@@ -99,7 +99,7 @@ test "DlistTcl-dlist_repeat" "norm3" {
 } {4 5 5 6 6 6}
 test "DlistTcl-dlist_repeat" "err4" { 
 	 dlist_repeat { 4 5 6 } { -1 2 3 }
-} {^dlist_repeat: signed/unsigned conversion failed}
+} {dlist_repeat: signed/unsigned conversion failed}
 test "DlistTcl-dlist_repeat" "err2" {
      dlist_repeat { 4 5 6 } { 1.5 2 3 }
 } {expected integer but got "1\.5"}
@@ -107,7 +107,7 @@ test "DlistTcl-dlist_repeat" "err2" {
 ### dlist_selectCmd ###
 test "DlistTcl-dlist_select" "args" {
     dlist_select
-} {wrong \# args: should be "dlist_select source_list flags_list"}
+} {wrong \# args: should be "::dlist_select source_list flags_list"}
 test "DlistTcl-dlist_select" "norm1" { 
 	 dlist_select {1 2 3 4 5} {1 0 0 1 1}
 } {1 4 5}
@@ -121,7 +121,7 @@ test "DlistTcl-dlist_select" "error" {
 ### dlist_sumCmd ###
 test "DlistTcl-dlist_sum" "args" {
     dlist_sum
-} {wrong \# args: should be "dlist_sum source_list"}
+} {wrong \# args: should be "::dlist_sum source_list"}
 test "DlistTcl-dlist_sum" "norm int" {
 	 dlist_sum { -3 6 0 9 17} 
 } {^29$}
@@ -138,7 +138,7 @@ test "DlistTcl-dlist_sum" "error" {
 ### dlist_zerosCmd ###
 test "DlistTcl-dlist_zeros" "args" {
 	 dlist_zeros
-} {wrong \# args: should be "dlist_zeros num_zeros"}
+} {wrong \# args: should be "::dlist_zeros num_zeros"}
 test "DlistTcl-dlist_zeros" "norm1" {
 	 catch {dlist_zeros 0}
 } {^0$}
