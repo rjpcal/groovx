8fb7ce3650e42bf2663c2fca0fe1f93c75df66d1
Robert J. Peters <rjpcal@gmail.com>
2001-07-18 20:31:02 +0000

    Put the order of the arguments for all of the def...() functions in a uniform order, which conceptually follows the Tcl way of defining a proc: command name, then args list (usage), then implementation.

--- src/tcl/tclpkg.h
+++ src/tcl/tclpkg.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Tue Jun 15 12:33:59 1999
-// written: Wed Jul 18 11:25:50 2001
+// written: Wed Jul 18 13:09:37 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -25,122 +25,138 @@
 #include "tcl/tclpkgbase.h"
 #endif
 
+namespace Tcl
+{
+  class ObjCaster;
+  class Pkg;
+}
+
+///////////////////////////////////////////////////////////////////////
+/**
+ *
+ * Tcl::Pkg extends Tcl::PkgBase by providing a set of functions to
+ * define Tcl commands from C++ functors.
+ *
+ **/
+///////////////////////////////////////////////////////////////////////
+
+class Tcl::Pkg : public PkgBase {
+public:
+  Pkg(Tcl_Interp* interp, const char* pkg_name, const char* version);
+
+  virtual ~Pkg();
+
+  template <class Func>
+  inline void def(const char* cmd_name, const char* usage, Func f)
+  {
+    addCommand( makeCmd(interp(), f, makePkgCmdName(cmd_name), usage) );
+  }
+
+  template <class Func>
+  inline void defVec(const char* cmd_name, const char* usage, Func f,
+                     unsigned int keyarg=1)
+  {
+    addCommand( makeVecCmd(interp(), f, makePkgCmdName(cmd_name),
+                           usage, keyarg) );
+  }
+
+  template <class Func>
+  inline void defRaw(const char* cmd_name, unsigned int nargs,
+                     const char* usage, Func f)
+  {
+    addCommand( makeGenericCmd(interp(), f, makePkgCmdName(cmd_name),
+                               usage, nargs) );
+  }
+
+  template <class Func>
+  inline void defVecRaw(const char* cmd_name, unsigned int nargs,
+                        const char* usage, Func f,
+                        unsigned int keyarg=1)
+  {
+    addCommand( makeGenericVecCmd(interp(), f, makePkgCmdName(cmd_name),
+                                  usage, nargs, keyarg) );
+  }
+
+  template <class C>
+  void defAction(const char* cmd_name, void (C::* actionFunc) ())
+  {
+    defVec( cmd_name, actionUsage(""), actionFunc );
+  }
+
+  template <class C>
+  void defAction(const char* cmd_name, void (C::* actionFunc) () const)
+  {
+    defVec( cmd_name, actionUsage(""), actionFunc );
+  }
+
+  template <class C, class T>
+  void defGetter(const char* cmd_name, const char* usage,
+                 T (C::* getterFunc) () const)
+  {
+    defVec( cmd_name, getterUsage(usage), getterFunc );
+  }
+
+  template <class C, class T>
+  void defGetter(const char* cmd_name, T (C::* getterFunc) () const)
+  {
+    defVec( cmd_name, getterUsage(""), getterFunc );
+  }
+
+  template <class C, class T>
+  void defSetter(const char* cmd_name, const char* usage,
+                 void (C::* setterFunc) (T))
+  {
+    defVec( cmd_name, setterUsage(usage), setterFunc );
+  }
+
+  template <class C, class T>
+  void defSetter(const char* cmd_name, void (C::* setterFunc) (T))
+  {
+    defVec( cmd_name, setterUsage(""), setterFunc );
+  }
+
+  template <class C, class T>
+  void defAttrib(const char* cmd_name,
+                 T (C::* getterFunc) () const,
+                 void (C::* setterFunc) (T))
+  {
+    defGetter( cmd_name, getterFunc );
+    defSetter( cmd_name, setterFunc );
+  }
+
+  void defIoCommands();
+
+  void defGenericObjCmds(shared_ptr<ObjCaster> caster);
+
+protected:
+  static const char* actionUsage(const char* usage);
+  static const char* getterUsage(const char* usage);
+  static const char* setterUsage(const char* usage);
+};
+
+
 namespace Util { class Object; }
+
+/** ObjCaster **/
+
+class Tcl::ObjCaster {
+protected:
+  ObjCaster();
+
+public:
+  virtual ~ObjCaster();
+
+  virtual bool isMyType(const Util::Object* obj) = 0;
+
+  bool isNotMyType(const Util::Object* obj) { return !isMyType(obj); }
+
+  bool isIdMyType(Util::UID uid);
+
+  bool isIdNotMyType(Util::UID uid) { return !isIdMyType(uid); }
+};
 
 namespace Tcl
 {
-  /** ObjCaster **/
-
-  class ObjCaster {
-  protected:
-    ObjCaster();
-
-  public:
-    virtual ~ObjCaster();
-
-    virtual bool isMyType(const Util::Object* obj) = 0;
-
-    bool isNotMyType(const Util::Object* obj) { return !isMyType(obj); }
-
-    bool isIdMyType(Util::UID uid);
-
-    bool isIdNotMyType(Util::UID uid) { return !isIdMyType(uid); }
-  };
-
-  ///////////////////////////////////////////////////////////////////////
-  /**
-   *
-   * Tcl::Pkg extends Tcl::PkgBase by providing a set of functions to
-   * define Tcl commands from C++ functors.
-   *
-   **/
-  ///////////////////////////////////////////////////////////////////////
-
-  class Pkg : public PkgBase {
-  public:
-    Pkg(Tcl_Interp* interp, const char* name, const char* version);
-
-    virtual ~Pkg();
-
-    template <class Func>
-    inline void def(Func f, const char* cmd_name, const char* usage)
-    {
-      addCommand( makeCmd(interp(), f, makePkgCmdName(cmd_name), usage) );
-    }
-
-    template <class Func>
-    inline void defVec(Func f, const char* cmd_name,
-                       const char* usage, unsigned int keyarg=1)
-    {
-      addCommand( makeVecCmd(interp(), f, makePkgCmdName(cmd_name),
-                             usage, keyarg) );
-    }
-
-    template <class Func>
-    inline void defRaw(Func f, const char* cmd_name,
-                       const char* usage, unsigned int nargs)
-    {
-      addCommand( makeGenericCmd(interp(), f, makePkgCmdName(cmd_name),
-                                 usage, nargs) );
-    }
-
-    template <class Func>
-    inline void defVecRaw(Func f, const char* cmd_name,
-                          const char* usage, unsigned int nargs,
-                          unsigned int keyarg=1)
-    {
-      addCommand( makeGenericVecCmd(interp(), f, makePkgCmdName(cmd_name),
-                                    usage, nargs, keyarg) );
-    }
-
-    template <class C>
-    void defAction(const char* cmd_name, void (C::* actionFunc) (),
-                   const char* usage = 0)
-      {
-        defVec( actionFunc, cmd_name, actionUsage(usage) );
-      }
-
-    template <class C>
-    void defAction(const char* cmd_name, void (C::* actionFunc) () const,
-                   const char* usage = 0)
-      {
-        defVec( actionFunc, cmd_name, actionUsage(usage) );
-      }
-
-    template <class C, class T>
-    void defGetter(const char* cmd_name, T (C::* getterFunc) () const,
-                   const char* usage = 0)
-      {
-        defVec( getterFunc, cmd_name, getterUsage(usage) );
-      }
-
-    template <class C, class T>
-    void defSetter(const char* cmd_name, void (C::* setterFunc) (T),
-                   const char* usage = 0)
-      {
-        defVec( setterFunc, cmd_name, setterUsage(usage) );
-      }
-
-    template <class C, class T>
-    void defAttrib(const char* cmd_name,
-                   T (C::* getterFunc) () const,
-                   void (C::* setterFunc) (T),
-                   const char* usage = 0)
-      {
-        defGetter( cmd_name, getterFunc, usage );
-        defSetter( cmd_name, setterFunc, usage );
-      }
-
-    void addIoCommands();
-
-    void addGenericObjCmds(shared_ptr<ObjCaster> caster);
-
-  protected:
-    static const char* actionUsage(const char* usage);
-    static const char* getterUsage(const char* usage);
-    static const char* setterUsage(const char* usage);
-  };
-
   template <class C>
   class CObjCaster : public ObjCaster {
   public:
@@ -154,9 +170,8 @@ namespace Tcl
   void defGenericObjCmds(Pkg* pkg)
   {
     shared_ptr<ObjCaster> caster(new CObjCaster<C>);
-    pkg->addGenericObjCmds(caster);
+    pkg->defGenericObjCmds(caster);
   }
-
 }
 
 static const char vcid_tclpkg_h[] = "$Header$";
