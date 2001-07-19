270bc734793413e9b93ed6fb8621d513f03ba4a8
Robert J. Peters <rjpcal@gmail.com>
2001-07-19 21:14:11 +0000

    Made commands not dependent on Tcl anymore.

--- src/io/ioutil.h
+++ src/io/ioutil.h
@@ -5,25 +5,13 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Fri Jun 11 21:43:43 1999
-// written: Wed Jul 18 14:17:37 2001
+// written: Thu Jul 19 14:14:04 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef STRINGIFYCMD_H_DEFINED
 #define STRINGIFYCMD_H_DEFINED
-
-#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(TCLCMD_H_DEFINED)
-#include "tcl/tclcmd.h"
-#endif
-
-#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(TCLOBJPTR_H_DEFINED)
-#include "tcl/tclobjptr.h"
-#endif
-
-#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(IO_H_DEFINED)
-#include "io/io.h"
-#endif
 
 class fixed_string;
 
@@ -32,18 +20,19 @@ namespace Util
   template <class T> class Ref;
 }
 
-namespace Tcl
+namespace IO
 {
-  Tcl::ObjPtr   stringify(Util::Ref<IO::IoObject> obj);
+  class IoObject;
+
+  fixed_string  stringify(Util::Ref<IO::IoObject> obj);
   void        destringify(Util::Ref<IO::IoObject> obj, const char* buf);
 
-  Tcl::ObjPtr write(Util::Ref<IO::IoObject> obj);
-  void         read(Util::Ref<IO::IoObject> obj, const char* buf);
+  fixed_string write(Util::Ref<IO::IoObject> obj);
+  void          read(Util::Ref<IO::IoObject> obj, const char* buf);
 
   void saveASW(Util::Ref<IO::IoObject> obj, fixed_string filename);
   void loadASR(Util::Ref<IO::IoObject> obj, fixed_string filename);
-
-} // end namespace Tcl
+}
 
 static const char vcid_stringifycmd_h[] = "$Header$";
 #endif // !STRINGIFYCMD_H_DEFINED
