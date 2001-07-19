1343e7ea4dcd420ba487f45cca3c9a8d8ccf68c0
Robert J. Peters <rjpcal@gmail.com>
2001-07-19 19:02:17 +0000

    Initial revision

---
+++ src/visx/bitmapcachenode.cc
@@ -0,0 +1,240 @@
+///////////////////////////////////////////////////////////////////////
+//
+// grobjrenderer.cc
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Thu Jul 19 11:22:10 2001
+// written: Thu Jul 19 12:00:48 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef GROBJRENDERER_CC_DEFINED
+#define GROBJRENDERER_CC_DEFINED
+
+#include "grobjrenderer.h"
+
+#include "bitmaprep.h"
+#include "glbmaprenderer.h"
+#include "grobjimpl.h"
+#include "xbmaprenderer.h"
+
+#include "util/error.h"
+#include "util/janitor.h"
+
+#include <GL/gl.h>
+
+#include "util/debug.h"
+#include "util/trace.h"
+
+dynamic_string GrObjRenderer::BITMAP_CACHE_DIR(".");
+
+GrObjRenderer::GrObjRenderer() :
+#ifndef I686
+  itsMode(GrObj::GLCOMPILE),
+#else
+  itsMode(GrObj::DIRECT_RENDER),
+#endif
+  itsCacheFilename(""),
+  itsIsCurrent(false),
+  itsDisplayList(-1),
+  itsBmapRenderer(0),
+  itsBitmapCache(0)
+{
+DOTRACE("GrObjRenderer::GrObjRenderer");
+}
+
+GrObjRenderer::~GrObjRenderer() {
+DOTRACE("GrObjRenderer::~GrObjRenderer");
+  glDeleteLists(itsDisplayList, 1);
+}
+
+void GrObjRenderer::recompileIfNeeded(const GrObjImpl* obj,
+                                      GWT::Canvas& canvas) const {
+DOTRACE("GrObjRenderer::recompileIfNeeded");
+  if (itsIsCurrent) return;
+
+  newList();
+
+  glNewList(itsDisplayList, GL_COMPILE);
+  obj->grRender(canvas, GrObj::DRAW);
+  glEndList();
+
+  postUpdated();
+}
+
+bool GrObjRenderer::recacheBitmapIfNeeded(const GrObjImpl* obj,
+                                          GWT::Canvas& canvas) const {
+DOTRACE("GrObjRenderer::recacheBitmapIfNeeded");
+  if (itsIsCurrent) return false;
+
+  Assert(itsBitmapCache.get() != 0);
+
+  obj->undraw(canvas);
+
+  {
+    GWT::Canvas::StateSaver state(canvas);
+
+    glPushAttrib(GL_COLOR_BUFFER_BIT|GL_PIXEL_MODE_BIT);
+    {
+      glDrawBuffer(GL_FRONT);
+
+      obj->itsScaler.doScaling(canvas);
+      obj->itsAligner.doAlignment(canvas, obj->grGetBoundingBox());
+
+      obj->grRender(canvas, GrObj::DRAW);
+
+      glReadBuffer(GL_FRONT);
+      Rect<double> bmapbox_init = obj->grGetBoundingBox();
+      Rect<int> screen_rect = canvas.getScreenFromWorld(bmapbox_init);
+      screen_rect.widenByStep(2);
+      screen_rect.heightenByStep(2);
+      Rect<double> bmapbox = canvas.getWorldFromScreen(screen_rect);
+
+      DebugEval(bmapbox.left()); DebugEval(bmapbox.top());
+      DebugEval(bmapbox.right()); DebugEvalNL(bmapbox.bottom());
+      itsBitmapCache->grabWorldRect(bmapbox);
+      itsBitmapCache->setRasterX(bmapbox.left());
+      itsBitmapCache->setRasterY(bmapbox.bottom());
+    }
+    glPopAttrib();
+  }
+
+  DebugEvalNL(itsMode);
+
+  if (GrObj::X11_BITMAP_CACHE == itsMode) {
+    itsBitmapCache->flipVertical();
+    itsBitmapCache->flipContrast();
+  }
+
+  if (GrObj::GL_BITMAP_CACHE == itsMode) {
+    itsBitmapCache->flipContrast();
+  }
+
+  postUpdated();
+
+  return true;
+}
+
+void GrObjRenderer::callList() const {
+DOTRACE("GrObjRenderer::callList");
+  // We must explicitly check that the display list is valid,
+  // since it might be invalid if the object was recently
+  // constructed, for example.
+  if (glIsList(itsDisplayList) == GL_TRUE) {
+    glCallList(itsDisplayList);
+    DebugEvalNL(itsDisplayList);
+  }
+}
+
+void GrObjRenderer::newList() const {
+DOTRACE("GrObjRenderer::newList");
+  glDeleteLists(itsDisplayList, 1);
+  itsDisplayList = glGenLists(1);
+  if (itsDisplayList == 0)
+    {
+      throw ErrorWithMsg("GrObj::newList: couldn't allocate display list");
+    }
+}
+
+void GrObjRenderer::setMode(GrObj::RenderMode new_mode) {
+DOTRACE("GrObjRenderer::setMode");
+
+#ifdef I686
+  // display lists don't work at present with i686/linux/mesa
+  if (new_mode == GrObj::GLCOMPILE) new_mode = GrObj::DIRECT_RENDER;
+#endif
+
+  // If new_mode is the same as the current render mode, then return
+  // immediately (and don't send a state change message)
+  if (new_mode == itsMode) return;
+
+  itsMode = new_mode;
+
+  switch (new_mode)
+    {
+    case GrObj::GL_BITMAP_CACHE:
+      itsBmapRenderer.reset(new GLBmapRenderer());
+      itsBitmapCache.reset(new BitmapRep(itsBmapRenderer));
+      queueBitmapLoad();
+      break;
+
+    case GrObj::X11_BITMAP_CACHE:
+      itsBmapRenderer.reset(new XBmapRenderer());
+      itsBitmapCache.reset(new BitmapRep(itsBmapRenderer));
+      queueBitmapLoad();
+      break;
+    }
+}
+
+void GrObjRenderer::render(const GrObjImpl* obj, GWT::Canvas& canvas) const {
+DOTRACE("GrObjRenderer::render");
+  DebugEvalNL(itsMode);
+  switch (itsMode) {
+
+  case GrObj::DIRECT_RENDER:
+    obj->grRender(canvas, GrObj::DRAW);
+    break;
+
+  case GrObj::GLCOMPILE:
+    callList();
+    break;
+
+  case GrObj::GL_BITMAP_CACHE:
+  case GrObj::X11_BITMAP_CACHE:
+    Assert(itsBitmapCache.get() != 0);
+    itsBitmapCache->render(canvas);
+    break;
+
+  } // end switch
+}
+
+bool GrObjRenderer::update(const GrObjImpl* obj, GWT::Canvas& canvas) const {
+DOTRACE("GrObjRenderer::update");
+  canvas.throwIfError("before GrObj::update");
+
+  bool objectDrawn = false;
+
+  switch (itsMode) {
+  case GrObj::GLCOMPILE:
+    recompileIfNeeded(obj, canvas);
+    break;
+
+  case GrObj::GL_BITMAP_CACHE:
+  case GrObj::X11_BITMAP_CACHE:
+    objectDrawn = recacheBitmapIfNeeded(obj, canvas);
+    break;
+  }
+  canvas.throwIfError("during GrObj::update");
+
+  return objectDrawn;
+}
+
+void GrObjRenderer::saveBitmapCache(const GrObjImpl* obj, GWT::Canvas& canvas,
+                                    const char* filename)
+{
+  Util::Janitor<GrObjRenderer, int> rmj(*this,
+                                        &GrObjRenderer::getMode,
+                                        &GrObjRenderer::setMode);
+
+  setMode(GrObj::X11_BITMAP_CACHE);
+
+  setCacheFilename("");
+
+  recacheBitmapIfNeeded(obj, canvas);
+  itsCacheFilename = filename;
+
+  itsBitmapCache->writePbmFile(fullCacheFilename().c_str());
+
+  invalidate();
+}
+
+void GrObjRenderer::queueBitmapLoad() const
+{
+  if ( !itsCacheFilename.empty() && itsBitmapCache.get() != 0 )
+    itsBitmapCache->queuePbmFile(fullCacheFilename().c_str());
+}
+
+static const char vcid_grobjrenderer_cc[] = "$Header$";
+#endif // !GROBJRENDERER_CC_DEFINED
---
+++ src/visx/bitmapcachenode.h
@@ -0,0 +1,99 @@
+///////////////////////////////////////////////////////////////////////
+//
+// grobjrenderer.h
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Thu Jul 19 11:19:59 2001
+// written: Thu Jul 19 11:30:41 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef GROBJRENDERER_H_DEFINED
+#define GROBJRENDERER_H_DEFINED
+
+#include "grobj.h"
+
+#include "util/strings.h"
+#include "util/pointers.h"
+
+class BmapRenderer;
+class BitmapRep;
+
+class GrObjRenderer {
+public:
+  GrObjRenderer();
+  virtual ~GrObjRenderer();
+
+  static dynamic_string BITMAP_CACHE_DIR;
+
+  void callList() const;
+
+  void invalidate() const { itsIsCurrent = false; }
+
+  void setMode(GrObj::RenderMode new_mode);
+
+  GrObj::RenderMode getMode() const { return itsMode; }
+
+  const dynamic_string& getCacheFilename() const { return itsCacheFilename; }
+
+  void setCacheFilename(const dynamic_string& name)
+    {
+      itsCacheFilename = name;
+      queueBitmapLoad();
+    }
+
+  // Returns true if the object was rendered to the screen as part
+  // of the update, false otherwise
+  bool update(const GrObjImpl* obj, GWT::Canvas& canvas) const;
+
+  void render(const GrObjImpl* obj, GWT::Canvas& canvas) const;
+
+  void saveBitmapCache(const GrObjImpl* obj, GWT::Canvas& canvas,
+                       const char* filename);
+
+  void restoreBitmapCache() const
+    {
+      queueBitmapLoad();
+      if ( GrObj::X11_BITMAP_CACHE == itsMode ||
+           GrObj::GL_BITMAP_CACHE == itsMode )
+        postUpdated();
+    }
+
+private:
+  GrObj::RenderMode itsMode;
+
+  mutable dynamic_string itsCacheFilename;
+
+  mutable bool itsIsCurrent;    // true if displaylist is current
+  mutable int itsDisplayList;   // OpenGL display list that draws the object
+  shared_ptr<BmapRenderer> itsBmapRenderer;
+  shared_ptr<BitmapRep> itsBitmapCache;
+
+  dynamic_string fullCacheFilename() const
+    {
+      dynamic_string result = BITMAP_CACHE_DIR;
+      result += "/";
+      result += itsCacheFilename;
+      return result;
+    }
+
+  void queueBitmapLoad() const;
+
+  // This function updates the cached OpenGL display list.
+  void recompileIfNeeded(const GrObjImpl* obj, GWT::Canvas& canvas) const;
+
+  // This function updates the cached bitmap, and returns a true if
+  // the bitmap was actually recached, and false if nothing was done.
+  bool recacheBitmapIfNeeded(const GrObjImpl* obj, GWT::Canvas& canvas) const;
+
+  void postUpdated() const { itsIsCurrent = true; }
+
+  // This function deletes any previous display list, allocates a new
+  // display list, and checks that the allocation actually succeeded.
+  void newList() const;
+};
+
+static const char vcid_grobjrenderer_h[] = "$Header$";
+#endif // !GROBJRENDERER_H_DEFINED
