4cdfe4aa3e5c5530ce1036ed90ed3e21f6e7dd55
Robert J. Peters <rjpcal@gmail.com>
2001-07-23 06:51:00 +0000

    Initial revision

---
+++ src/util/algo.h
@@ -0,0 +1,39 @@
+///////////////////////////////////////////////////////////////////////
+//
+// algo.h
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Sun Jul 22 23:31:48 2001
+// written: Sun Jul 22 23:36:56 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef ALGO_H_DEFINED
+#define ALGO_H_DEFINED
+
+namespace Util
+{
+  template <class T>
+  inline const T& max(const T& a, const T& b)
+    { return (a > b) ? a : b; }
+
+  template <class T>
+  inline const T& min(const T& a, const T& b)
+    { return (a < b) ? a : b; }
+
+  template <class T>
+  inline void swap(T& t1, T& t2)
+  {
+    T t2_copy = t2;
+    t2 = t1;
+    t1 = t2_copy;
+  }
+}
+
+using Util::max;
+using Util::min;
+
+static const char vcid_algo_h[] = "$Header$";
+#endif // !ALGO_H_DEFINED
