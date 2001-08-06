b41c9e3a675384292b126b4ef9df84ca7166c169
Robert J. Peters <rjpcal@gmail.com>
2001-08-06 03:25:16 +0000

    Changed to do string conversions with Util::Convert<>::toString().

--- src/util/error.cc
+++ src/util/error.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Tue Jun 22 14:59:48 1999
-// written: Sat Jul 21 19:53:09 2001
+// written: Sun Aug  5 20:12:47 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -18,26 +18,28 @@
 #include "util/strings.h"
 
 #include <exception>
-#include <iostream.h>
-#include <strstream.h>
 
 #define NO_TRACE
 #include "util/trace.h"
+#define LOCAL_ASSERT
 #include "util/debug.h"
 
-Error::Error() {
-DOTRACE("Error::Error ");
+Error::Error()
+{
+DOTRACE("Error::Error");
 #ifdef LOCAL_DEBUG
   Util::Trace::printStackTrace();
 #endif
 
   if (std::uncaught_exception())
     {
-      std::cerr << "DOUBLE JEAPORDY!" << std::endl;
+      Util::Trace::printStackTrace();
+      Assert(false);
     }
 }
 
-Error::~Error() {
+Error::~Error()
+{
 DOTRACE("Error::~Error ");
 }
 
@@ -65,12 +67,14 @@ DOTRACE("ErrorWithMsg::ErrorWithMsg");
   DebugEvalNL(*itsInfo);
 }
 
-ErrorWithMsg::~ErrorWithMsg() {
+ErrorWithMsg::~ErrorWithMsg()
+{
 DOTRACE("ErrorWithMsg::~ErrorWithMsg");
   delete itsInfo;
 }
 
-ErrorWithMsg& ErrorWithMsg::operator=(const ErrorWithMsg& other) {
+ErrorWithMsg& ErrorWithMsg::operator=(const ErrorWithMsg& other)
+{
 DOTRACE("ErrorWithMsg::operator=");
 
   *itsInfo = *(other.itsInfo);    DebugEvalNL(*itsInfo);
@@ -78,19 +82,22 @@ DOTRACE("ErrorWithMsg::operator=");
   return *this;
 }
 
-const char* ErrorWithMsg::msg_cstr() const {
+const char* ErrorWithMsg::msg_cstr() const
+{
 DOTRACE("ErrorWithMsg::msg_cstr");
   return itsInfo->c_str();
 }
 
-ErrorWithMsg& ErrorWithMsg::appendMsg(const char* addMsg) {
+ErrorWithMsg& ErrorWithMsg::appendMsg(const char* addMsg)
+{
 DOTRACE("ErrorWithMsg::appendMsg");
   *itsInfo += addMsg;
   DebugEvalNL(*itsInfo);
   return *this;
 }
 
-ErrorWithMsg& ErrorWithMsg::appendMsg(const char* addMsg1, const char* addMsg2) {
+ErrorWithMsg& ErrorWithMsg::appendMsg(const char* addMsg1, const char* addMsg2)
+{
 DOTRACE("ErrorWithMsg::appendMsg");
   *itsInfo += addMsg1;
   *itsInfo += addMsg2;
@@ -99,7 +106,8 @@ DOTRACE("ErrorWithMsg::appendMsg");
 }
 
 ErrorWithMsg& ErrorWithMsg::appendMsg(const char* addMsg1, const char* addMsg2,
-                                      const char* addMsg3) {
+                                      const char* addMsg3)
+{
 DOTRACE("ErrorWithMsg::appendMsg");
   *itsInfo += addMsg1;
   *itsInfo += addMsg2;
@@ -108,40 +116,8 @@ DOTRACE("ErrorWithMsg::appendMsg");
   return *this;
 }
 
-namespace
+void ErrorWithMsg::setMsg(const char* str)
 {
-  static char num2str_buf[32];
-}
-
-template <class T>
-const char* ErrorWithMsg::num2str(T x) {
-DOTRACE("ErrorWithMsg::num2str");
-  ostrstream ost(num2str_buf, 30);
-  ost << x << '\0';
-  return &num2str_buf[0];
-}
-
-#ifndef ACC_COMPILER
-template const char* ErrorWithMsg::num2str<int>(int);
-template const char* ErrorWithMsg::num2str<unsigned int>(unsigned int);
-template const char* ErrorWithMsg::num2str<long>(long);
-template const char* ErrorWithMsg::num2str<unsigned long>(unsigned long);
-template const char* ErrorWithMsg::num2str<double>(double);
-#else
-class ErrorWithMsg::Impl {
-public:
-  void dummy() // just a hack since aCC won't do explicit instantiation
-  {
-    num2str((int)0);
-    num2str((unsigned int)0);
-    num2str((long)0);
-    num2str((unsigned long)0);
-    num2str((double)0);
-  }
-};
-#endif
-
-void ErrorWithMsg::setMsg(const char* str) {
 DOTRACE("ErrorWithMsg::setMsg");
   *itsInfo = str;
   DebugEvalNL(*itsInfo);
