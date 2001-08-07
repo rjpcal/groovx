8aa8b99a548782ea875c0bc6f23d5a81d5fd5d36
Robert J. Peters <rjpcal@gmail.com>
2001-08-07 18:18:55 +0000

    Changed implementation to use a Tcl::ObjPtr rather than a raw Tcl_Obj*; got rid of overloaded constructors since we can just use Tcl::ObjPtr's templated constructor.

--- src/tclvalue.cc
+++ src/tclvalue.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Tue Sep 28 11:23:55 1999
-// written: Tue Aug  7 10:32:33 2001
+// written: Tue Aug  7 11:18:33 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -15,11 +15,8 @@
 
 #include "tcl/tclvalue.h"
 
-#include "tcl/tclconvert.h"
-
 #include "util/strings.h"
 
-#include <tcl.h>
 #include <iostream.h>
 
 #define NO_TRACE
@@ -32,92 +29,43 @@
 //
 //---------------------------------------------------------------------
 
-Tcl::TclValue::TclValue(Tcl_Obj* obj) :
-  itsObj(obj)
-{
-DOTRACE("Tcl::TclValue::TclValue(Tcl_Obj*)");
-  Tcl_IncrRefCount(itsObj);
-}
-
-Tcl::TclValue::TclValue(int val) :
-  itsObj(Tcl::Convert<int>::toTcl(val))
-{
-DOTRACE("Tcl::TclValue::TclValue(int)");
-  Tcl_IncrRefCount(itsObj);
-}
-
-Tcl::TclValue::TclValue(long val) :
-  itsObj(Tcl::Convert<long>::toTcl(val))
-{
-DOTRACE("Tcl::TclValue::TclValue(long)");
-  Tcl_IncrRefCount(itsObj);
-}
-
-Tcl::TclValue::TclValue(bool val) :
-  itsObj(Tcl::Convert<bool>::toTcl(val))
-{
-DOTRACE("Tcl::TclValue::TclValue(bool)");
-  Tcl_IncrRefCount(itsObj);
-}
-
-Tcl::TclValue::TclValue(double val) :
-  itsObj(Tcl::Convert<double>::toTcl(val))
-{
-DOTRACE("Tcl::TclValue::TclValue(double)");
-  Tcl_IncrRefCount(itsObj);
-}
-
-Tcl::TclValue::TclValue(const char* val) :
-  itsObj(Tcl::Convert<const char*>::toTcl(val))
-{
-DOTRACE("Tcl::TclValue::TclValue(const char*)");
-  Tcl_IncrRefCount(itsObj);
-}
-
 Tcl::TclValue::TclValue(const Value& rhs) :
-  itsObj(0)
+  itsObjPtr()
 {
 DOTRACE("Tcl::TclValue::TclValue(const Value&)");
 
   Value::Type rhs_type = rhs.getNativeType();
   DebugEvalNL(rhs_type);
 
-  switch (rhs_type) {
-  case Value::INT:
-    itsObj = Tcl::Convert<int>::toTcl(rhs.get(Util::TypeCue<int>()));
-    break;
-  case Value::LONG:
-    itsObj = Tcl::Convert<long>::toTcl(rhs.get(Util::TypeCue<long>()));
-    break;
-  case Value::BOOL:
-    itsObj = Tcl::Convert<bool>::toTcl(rhs.get(Util::TypeCue<bool>()));
-    break;
-  case Value::DOUBLE:
-    itsObj = Tcl::Convert<double>::toTcl(rhs.get(Util::TypeCue<double>()));
-    break;
+  switch (rhs_type)
+    {
+    case Value::INT:
+      itsObjPtr = Tcl::Convert<int>::toTcl(rhs.get(Util::TypeCue<int>()));
+      break;
+    case Value::LONG:
+      itsObjPtr = Tcl::Convert<long>::toTcl(rhs.get(Util::TypeCue<long>()));
+      break;
+    case Value::BOOL:
+      itsObjPtr = Tcl::Convert<bool>::toTcl(rhs.get(Util::TypeCue<bool>()));
+      break;
+    case Value::DOUBLE:
+      itsObjPtr = Tcl::Convert<double>::toTcl(rhs.get(Util::TypeCue<double>()));
+      break;
 
-  case Value::CSTRING:
-  case Value::NONE:
-  case Value::UNKNOWN:
-  default:
-    itsObj = Tcl::Convert<const char*>::toTcl(rhs.get(Util::TypeCue<const char*>()));
-    break;
-  }
-
-  DebugEval((void*) itsObj);
-  DebugEvalNL(itsObj->refCount);
-
-  Tcl_IncrRefCount(itsObj);
-
-  DebugEvalNL(itsObj->refCount);
+    case Value::CSTRING:
+    case Value::NONE:
+    case Value::UNKNOWN:
+    default:
+      itsObjPtr = Tcl::Convert<const char*>::toTcl(rhs.get(Util::TypeCue<const char*>()));
+      break;
+    }
 }
 
 Tcl::TclValue::TclValue(const TclValue& rhs) :
   Value(rhs),
-  itsObj(rhs.itsObj)
+  itsObjPtr(rhs.itsObjPtr)
 {
 DOTRACE("Tcl::TclValue::TclValue");
-  Tcl_IncrRefCount(itsObj);
 }
 
 //---------------------------------------------------------------------
@@ -126,32 +74,31 @@ DOTRACE("Tcl::TclValue::TclValue");
 //
 //---------------------------------------------------------------------
 
-Tcl::TclValue::~TclValue() {
+Tcl::TclValue::~TclValue()
+{
 DOTRACE("Tcl::TclValue::~TclValue");
-  Tcl_DecrRefCount(itsObj);
 }
 
-Tcl_Obj* Tcl::TclValue::getObj() const {
+Tcl_Obj* Tcl::TclValue::getObj() const
+{
 DOTRACE("Tcl::TclValue::getObj");
-  return itsObj;
+  return itsObjPtr;
 }
 
-void Tcl::TclValue::setObj(Tcl_Obj* obj) {
+void Tcl::TclValue::setObj(Tcl_Obj* obj)
+{
 DOTRACE("Tcl::TclValue::setObj");
-  if (itsObj == obj) return;
-
-  Tcl_DecrRefCount(itsObj);
-
-  itsObj = obj;
-  Tcl_IncrRefCount(itsObj);
+  itsObjPtr = obj;
 }
 
-Value* Tcl::TclValue::clone() const {
+Value* Tcl::TclValue::clone() const
+{
 DOTRACE("Tcl::TclValue::clone");
-  return new TclValue(itsObj);
+  return new TclValue(itsObjPtr);
 }
 
-Value::Type Tcl::TclValue::getNativeType() const {
+Value::Type Tcl::TclValue::getNativeType() const
+{
 DOTRACE("Tcl::TclValue::getNativeType");
 
   string_literal type_name(getNativeTypeName());
@@ -163,28 +110,25 @@ DOTRACE("Tcl::TclValue::getNativeType");
   else return Value::UNKNOWN;
 }
 
-const char* Tcl::TclValue::getNativeTypeName() const {
+const char* Tcl::TclValue::getNativeTypeName() const
+{
 DOTRACE("Tcl::TclValue::getNativeTypeName");
-  Tcl_ObjType* type = itsObj->typePtr;
-
-  if (type == 0) return "cstring";
-
-  DebugEvalNL(type->name);
-
-  return type->name;
+  return itsObjPtr.typeName();
 }
 
-void Tcl::TclValue::printTo(STD_IO::ostream& os) const {
+void Tcl::TclValue::printTo(STD_IO::ostream& os) const
+{
 DOTRACE("Tcl::TclValue::printTo");
-  os << Tcl_GetString(itsObj);
+  os << get(Util::TypeCue<const char*>());
 }
 
-void Tcl::TclValue::scanFrom(STD_IO::istream& is) {
+void Tcl::TclValue::scanFrom(STD_IO::istream& is)
+{
 DOTRACE("Tcl::TclValue::scanFrom");
   char buf[256];
   is >> ws;
   is.get(buf, 256, '\n');
-  Tcl_SetStringObj(itsObj, buf, -1);
+  itsObjPtr = Tcl::ObjPtr(buf);
 }
 
 //---------------------------------------------------------------------
@@ -193,29 +137,34 @@ DOTRACE("Tcl::TclValue::scanFrom");
 //
 //---------------------------------------------------------------------
 
-int Tcl::TclValue::get(Util::TypeCue<int>) const {
+int Tcl::TclValue::get(Util::TypeCue<int>) const
+{
 DOTRACE("Tcl::TclValue::get(Util::TypeCue<int>)");
-  return Tcl::Convert<int>::fromTcl(itsObj);
+  return Tcl::Convert<int>::fromTcl(itsObjPtr);
 }
 
-long Tcl::TclValue::get(Util::TypeCue<long>) const {
+long Tcl::TclValue::get(Util::TypeCue<long>) const
+{
 DOTRACE("Tcl::TclValue::get(Util::TypeCue<long>)");
-  return Tcl::Convert<long>::fromTcl(itsObj);
+  return Tcl::Convert<long>::fromTcl(itsObjPtr);
 }
 
-bool Tcl::TclValue::get(Util::TypeCue<bool>) const {
+bool Tcl::TclValue::get(Util::TypeCue<bool>) const
+{
 DOTRACE("Tcl::TclValue::get(Util::TypeCue<bool>)");
-  return Tcl::Convert<bool>::fromTcl(itsObj);
+  return Tcl::Convert<bool>::fromTcl(itsObjPtr);
 }
 
-double Tcl::TclValue::get(Util::TypeCue<double>) const {
+double Tcl::TclValue::get(Util::TypeCue<double>) const
+{
 DOTRACE("Tcl::TclValue::get(Util::TypeCue<double>)");
-  return Tcl::Convert<double>::fromTcl(itsObj);
+  return Tcl::Convert<double>::fromTcl(itsObjPtr);
 }
 
-const char* Tcl::TclValue::get(Util::TypeCue<const char*>) const {
+const char* Tcl::TclValue::get(Util::TypeCue<const char*>) const
+{
 DOTRACE("Tcl::TclValue::get(Util::TypeCue<const char*>)");
-  return Tcl::Convert<const char*>::fromTcl(itsObj);
+  return Tcl::Convert<const char*>::fromTcl(itsObjPtr);
 }
 
 static const char vcid_tclvalue_cc[] = "$Header$";
--- src/tclvalue.h
+++ src/tclvalue.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Tue Sep 28 11:23:55 1999
-// written: Tue Aug  7 10:32:26 2001
+// written: Tue Aug  7 11:11:44 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -13,11 +13,13 @@
 #ifndef TCLVALUE_H_DEFINED
 #define TCLVALUE_H_DEFINED
 
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(TCLOBJPTR_H_DEFINED)
+#include "tcl/tclobjptr.h"
+#endif
+
 #if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(VALUE_H_DEFINED)
 #include "util/value.h"
 #endif
-
-struct Tcl_Obj;
 
 namespace Tcl
 {
@@ -36,19 +38,8 @@ namespace Tcl
 
 class Tcl::TclValue : public Value {
 public:
-  /// Construct from a Tcl object \a obj.
-  TclValue(Tcl_Obj* obj);
-
-  /// Construct with initial value \a val.
-  TclValue(int val);
-  /// Construct with initial value \a val.
-  TclValue(long val);
-  /// Construct with initial value \a val.
-  TclValue(bool val);
-  /// Construct with initial value \a val.
-  TclValue(double val);
-  /// Construct with initial value \a val.
-  TclValue(const char* val);
+  template <class T>
+  TclValue(T t) : itsObjPtr(t) {}
 
   /// Construct with initial value \a val.
   TclValue(const Value& val);
@@ -67,7 +58,7 @@ public:
 
   /// Assignment operator.
   TclValue& operator=(const TclValue& other)
-    { setObj(other.itsObj); return *this; }
+    { setObj(other.itsObjPtr); return *this; }
 
   /// Assignment from raw \c Tcl_Obj*.
   TclValue& operator=(Tcl_Obj* obj)
@@ -89,7 +80,7 @@ public:
   virtual const char* get(Util::TypeCue<const char*>) const;
 
 private:
-  mutable Tcl_Obj* itsObj;
+  mutable Tcl::ObjPtr itsObjPtr;
 };
 
 static const char vcid_tclvalue_h[] = "$Header$";
