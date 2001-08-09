3723a25fd492accd373f78e35a4a71876128cd0f
Robert J. Peters <rjpcal@gmail.com>
2001-08-09 22:44:38 +0000

    Initial revision

---
+++ src/gx/vec2.cc
@@ -0,0 +1,108 @@
+///////////////////////////////////////////////////////////////////////
+//
+// point.cc
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Thu Aug  9 14:38:13 2001
+// written: Thu Aug  9 15:18:31 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef POINT_CC_DEFINED
+#define POINT_CC_DEFINED
+
+#include "point.h"
+
+#include "util/algo.h"
+
+#include <cmath>
+
+namespace PointAlgo
+{
+  double deg2rad(double degrees)
+  {
+    return degrees * PI/180.0;
+  }
+
+  double rad2deg(double radians)
+  {
+    return radians * 180.0/PI;
+  }
+
+  void normRad(double& radians)
+  {
+    while (radians > PI) { radians -= 2*PI; }
+    while (radians <= -PI) { radians += 2*PI; }
+  }
+
+  void normDeg(double& degrees)
+  {
+    while (degrees > 180.0) { degrees -= 360.0; }
+    while (degrees <= -180.0) { degrees += 360.0; }
+  }
+
+  const char X_POS = 1;
+  const char Y_POS = 2;
+
+  const char QUAD_1 = X_POS | Y_POS;
+  const char QUAD_2 = Y_POS;
+  const char QUAD_3 = 0;
+  const char QUAD_4 = X_POS;
+
+  char quadrantDeg(double degrees)
+  {
+    if (degrees <= -180.0)
+      {
+        return quadrantDeg(degrees+360.0);
+      }
+    else if (degrees <= -90.0)
+      {
+        return QUAD_3;
+      }
+    else if (degrees <= 0.0)
+      {
+        return QUAD_4;
+      }
+    else if (degrees <= 90.0)
+      {
+        return QUAD_1;
+      }
+    else if (degrees <= 180.0)
+      {
+        return QUAD_2;
+      }
+
+    return quadrantDeg(degrees-360.0);
+  }
+}
+
+double PointAlgo::thetaDeg(double y, double x)
+{
+  return rad2deg(atan2(y,x));
+}
+
+void PointAlgo::setPolarPoint(Point<double>& point,
+                              double length, double degrees)
+{
+  normDeg(degrees);
+
+  if (degrees == 0.0) { point.set(length, 0.0); return; }
+  if (degrees == 90.0) { point.set(0.0, length); return; }
+  if (degrees == 180.0) { point.set(-length, 0.0); return; }
+  if (degrees == -90.0) { point.set(0.0, -length); return; }
+
+  point.x() = 1.0;
+  point.y() = Util::abs(tan(deg2rad(degrees)));
+
+  point.setLength(length);
+
+  char quadrant = quadrantDeg(degrees);
+
+  if ( !(quadrant & X_POS) ) { point.x() *= -1.0; }
+  if ( !(quadrant & Y_POS) ) { point.y() *= -1.0; }
+}
+
+static const char vcid_point_cc[] = "$Header$";
+#endif // !POINT_CC_DEFINED
