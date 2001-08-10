3f763e0d3f9f28416d75ec35b4153f78e6c47df9
Robert J. Peters <rjpcal@gmail.com>
2001-08-10 00:48:33 +0000

    Initial revision

---
+++ src/visx/pointtcl.h
@@ -0,0 +1,66 @@
+///////////////////////////////////////////////////////////////////////
+//
+// pointtcl.h
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Thu Aug  9 17:47:29 2001
+// written: Thu Aug  9 17:48:22 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef POINTTCL_H_DEFINED
+#define POINTTCL_H_DEFINED
+
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(POINT_H_DEFINED)
+#include "point.h"
+#endif
+
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(TCLCONVERT_H_DEFINED)
+#include "tcl/tclconvert.h"
+#endif
+
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(TCLLISTOBJ_H_DEFINED)
+#include "tcl/tcllistobj.h"
+#endif
+
+namespace Tcl
+{
+  template <class T>
+  struct Convert<const Point<T>& >
+  {
+    typedef T Type;
+    static Point<T> fromTcl( Tcl_Obj* obj )
+    {
+      Tcl::List listObj(obj);
+      return Point<T>(listObj.get(0, (T*)0), listObj.get(1, (T*)0));
+    }
+
+    static Tcl_Obj* toTcl( const Point<T>& point )
+    {
+      Tcl::List listObj;
+      listObj.append(point.x());
+      listObj.append(point.y());
+      return listObj.asObj();
+    }
+  };
+
+  template <class T>
+  struct Convert<Point<T> >
+  {
+    typedef T Type;
+    static Point<T> fromTcl( Tcl_Obj* obj )
+    {
+      return Convert<const Point<T>&>::fromTcl(obj);
+    }
+
+    static Tcl_Obj* toTcl( Point<T> point )
+    {
+      return Convert<const Point<T>&>::toTcl(point);
+    }
+  };
+}
+
+static const char vcid_pointtcl_h[] = "$Header$";
+#endif // !POINTTCL_H_DEFINED
