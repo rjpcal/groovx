da6f70c5dc2d53d966a8a2106506b219b2fe72f3
Robert J. Peters <rjpcal@gmail.com>
2001-08-10 21:10:19 +0000

    Initial revision

---
+++ src/visx/gnode.cc
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////
+//
+// gnode.cc
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Fri Aug 10 13:36:54 2001
+// written: Fri Aug 10 13:37:59 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef GNODE_CC_DEFINED
+#define GNODE_CC_DEFINED
+
+#include "gnode.h"
+
+Gnode::~Gnode() {}
+
+static const char vcid_gnode_cc[] = "$Header$";
+#endif // !GNODE_CC_DEFINED
---
+++ src/visx/gnode.h
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////
+//
+// gnode.h
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Fri Aug 10 13:36:59 2001
+// written: Fri Aug 10 13:59:35 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef GNODE_H_DEFINED
+#define GNODE_H_DEFINED
+
+#include "util/pointers.h"
+
+namespace Gfx
+{
+  class Canvas;
+}
+
+template <class V> class Rect;
+
+class Gnode {
+private:
+  shared_ptr<Gnode> itsChild;
+
+protected:
+  Gnode* child() const { return itsChild.get(); }
+
+public:
+  Gnode() : itsChild() {}
+
+  virtual ~Gnode();
+
+  void setChild(shared_ptr<Gnode> child) { itsChild = child; }
+
+  virtual void gnodeDraw(Gfx::Canvas& canvas) const = 0;
+
+  virtual void gnodeUndraw(Gfx::Canvas& canvas) const = 0;
+
+  virtual Rect<double> gnodeBoundingBox(Gfx::Canvas& canvas) const = 0;
+};
+
+static const char vcid_gnode_h[] = "$Header$";
+#endif // !GNODE_H_DEFINED
