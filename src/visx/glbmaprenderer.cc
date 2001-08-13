9dd26cb829683f3697c52976c99fd841a053da1b
Robert J. Peters <rjpcal@gmail.com>
2001-08-13 21:41:05 +0000

    Moved actual rendering code into GLCanvas.

--- src/visx/glbmaprenderer.cc
+++ src/visx/glbmaprenderer.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Wed Dec  1 17:52:41 1999
-// written: Mon Aug 13 12:13:11 2001
+// written: Mon Aug 13 14:38:14 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -17,14 +17,11 @@
 
 #include "gfx/bmapdata.h"
 #include "gfx/canvas.h"
-#include "gfx/vec2.h"
-
-#include <GL/gl.h>
 
 #include "util/trace.h"
 
 GLBmapRenderer::GLBmapRenderer () :
-  itsUsingGlBitmap(true)
+  itsUsingGlBitmap(false)
 {
 DOTRACE("GLBmapRenderer::GLBmapRenderer ");
 }
@@ -40,43 +37,13 @@ void GLBmapRenderer::doRender(Gfx::Canvas& canvas,
                               const Gfx::Vec2<double>& zoom) const
 {
 DOTRACE("GLBmapRenderer::doRender");
-  glRasterPos2d(world_pos.x(), world_pos.y());
-  glPixelZoom(zoom.x(), zoom.y());
-
-  glPixelStorei(GL_UNPACK_ALIGNMENT, data.byteAlignment());
-
-  if (data.bitsPerPixel() == 24)
+  if (data.bitsPerPixel() == 1 && itsUsingGlBitmap)
     {
-      glDrawPixels(data.width(), data.height(), GL_RGB, GL_UNSIGNED_BYTE,
-                   static_cast<GLvoid*>(data.bytesPtr()));
+      canvas.drawBitmap(data, world_pos);
     }
-  else if (data.bitsPerPixel() == 8)
+  else
     {
-      glDrawPixels(data.width(), data.height(), GL_COLOR_INDEX, GL_UNSIGNED_BYTE,
-                   static_cast<GLvoid*>(data.bytesPtr()));
-    }
-  else if (data.bitsPerPixel() == 1)
-    {
-      if (itsUsingGlBitmap)
-        {
-          glBitmap(data.width(), data.height(), 0.0, 0.0, 0.0, 0.0,
-                   static_cast<GLubyte*>(data.bytesPtr()));
-        }
-      else
-        {
-          if (canvas.isRgba())
-            {
-              GLfloat simplemap[] = {0.0, 1.0};
-
-              glPixelMapfv(GL_PIXEL_MAP_I_TO_R, 2, simplemap);
-              glPixelMapfv(GL_PIXEL_MAP_I_TO_G, 2, simplemap);
-              glPixelMapfv(GL_PIXEL_MAP_I_TO_B, 2, simplemap);
-              glPixelMapfv(GL_PIXEL_MAP_I_TO_A, 2, simplemap);
-            }
-
-          glDrawPixels(data.width(), data.height(), GL_COLOR_INDEX, GL_BITMAP,
-                       static_cast<GLvoid*>(data.bytesPtr()));
-        }
+      canvas.drawPixels(data, world_pos, zoom);
     }
 }
 
