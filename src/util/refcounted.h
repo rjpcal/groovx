62d48b80216674a7f8fac854e67407523b6843e3
Robert J. Peters <rjpcal@gmail.com>
2001-08-19 23:33:35 +0000

    Removed isShared() and isUnshared() from Util::RefCounts.

--- src/util/refcounted.h
+++ src/util/refcounted.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Sun Oct 22 14:40:19 2000
-// written: Sun Jul 22 23:29:12 2001
+// written: Sun Aug 19 16:31:26 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -63,9 +63,6 @@ protected:
 public:
   Count strongCount() { return itsStrong; }
   Count weakCount() { return itsWeak; }
-
-  bool isShared() { return (itsStrong > 1) || (itsWeak > 2); }
-  bool isUnshared() { return !isShared(); }
 
 private:
   RefCounts(const RefCounts&);
