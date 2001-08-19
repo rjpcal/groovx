cd9481e3d248bc012c6d0c4f70b8ce474bc57078
Robert J. Peters <rjpcal@gmail.com>
2001-08-19 22:12:10 +0000

    Moved implementation details relating to iterators back into the iter.h file.

--- src/utilfwd.h
+++ src/utilfwd.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Tue Jun  5 10:47:29 2001
-// written: Fri Aug 17 14:11:11 2001
+// written: Sun Aug 19 15:11:49 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -32,19 +32,9 @@ namespace Util
 
   const IterEnd_t IterEnd = IterEnd_t();
 
-  template <class T> class FwdIterIfx;
-  template <class T> class BidirIterIfx;
-  template <class T> class RxsIterIfx;
-
-  template <class Iter, class T> class FwdIterAdapter;
-  template <class Iter, class T> class BidirIterAdapter;
-  template <class Iter, class T> class RxsIterAdapter;
-
   template <class T> class FwdIter;
   template <class T> class BidirIter;
   template <class T> class RxsIter;
-
-  template <class T, class Ifx> class ConcreteIter;
 }
 
 static const char vcid_utilfwd_h[] = "$Header$";
