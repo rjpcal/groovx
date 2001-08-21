a841be9fe955a85b760ce7daea35c83e086d5169
Robert J. Peters <rjpcal@gmail.com>
2001-08-21 22:29:09 +0000

    Initial revision

---
+++ src/util/uid.h
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////////////////
+//
+// uid.h
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Tue Aug 21 15:28:54 2001
+// written: Tue Aug 21 15:29:06 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef UID_H_DEFINED
+#define UID_H_DEFINED
+
+namespace Util
+{
+  /// This type is used for IoObject unique identifiers
+  typedef unsigned long UID;
+}
+
+static const char vcid_uid_h[] = "$Header$";
+#endif // !UID_H_DEFINED
