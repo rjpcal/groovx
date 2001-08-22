18aeaa8c4b30d37f9d75da62a7be762fb872f366
Robert J. Peters <rjpcal@gmail.com>
2001-08-22 00:19:36 +0000

    Initial revision

---
+++ src/util/volatileobject.cc
@@ -0,0 +1,42 @@
+///////////////////////////////////////////////////////////////////////
+//
+// volatileobject.cc
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Tue Aug 21 17:17:51 2001
+// written: Tue Aug 21 17:19:28 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef VOLATILEOBJECT_CC_DEFINED
+#define VOLATILEOBJECT_CC_DEFINED
+
+#include "util/volatileobject.h"
+
+#define LOCAL_ASSERT
+#include "util/debug.h"
+
+Util::VolatileObject::VolatileObject()
+{
+  this->incrRefCount();
+}
+
+Util::VolatileObject::~VolatileObject()
+{}
+
+void Util::VolatileObject::destroy()
+{
+  Assert(refCount() == 1);
+
+  this->decrRefCount();
+}
+
+bool Util::VolatileObject::isVolatile()
+{
+  return true;
+}
+
+static const char vcid_volatileobject_cc[] = "$Header$";
+#endif // !VOLATILEOBJECT_CC_DEFINED
---
+++ src/util/volatileobject.h
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////
+//
+// volatileobject.h
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Tue Aug 21 17:16:15 2001
+// written: Tue Aug 21 17:19:30 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef VOLATILEOBJECT_H_DEFINED
+#define VOLATILEOBJECT_H_DEFINED
+
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(OBJECT_H_DEFINED)
+#include "util/object.h"
+#endif
+
+namespace Util
+{
+  class VolatileObject;
+}
+
+class Util::VolatileObject : public virtual Util::Object {
+public:
+  VolatileObject();
+  virtual ~VolatileObject();
+
+  void destroy();
+
+  virtual bool isVolatile() const;
+};
+
+static const char vcid_volatileobject_h[] = "$Header$";
+#endif // !VOLATILEOBJECT_H_DEFINED
