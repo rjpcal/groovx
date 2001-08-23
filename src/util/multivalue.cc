c02150a3ac601a0f4ee286665f3b32b94bf511bb
Robert J. Peters <rjpcal@gmail.com>
2001-08-23 00:20:46 +0000

    Initial revision

---
+++ src/util/multivalue.cc
@@ -0,0 +1,83 @@
+///////////////////////////////////////////////////////////////////////
+//
+// multivalue.cc
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Wed Aug 22 16:58:56 2001
+// written: Wed Aug 22 17:04:46 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef MULTIVALUE_CC_DEFINED
+#define MULTIVALUE_CC_DEFINED
+
+#include "util/multivalue.h"
+
+#include <strstream.h>
+
+#include "util/debug.h"
+
+template <class T>
+void TMultiValue<T>::printTo(STD_IO::ostream& os) const
+{
+  const T* dat = constBegin();
+  const T* end = constEnd();
+
+  while (dat < end)
+    {
+      os << *dat;
+      if (++dat < end)
+        os << ' ';
+    }
+}
+
+template <class T>
+void TMultiValue<T>::scanFrom(STD_IO::istream& is)
+{
+  T* dat = mutableBegin();
+  T* end = mutableEnd();
+
+  while (dat < end)
+    {
+      is >> *dat++;
+      if (dat < end)
+        {
+          if (is.fail() || is.eof())
+            throw ValueError("error while scanning a multi-value");
+        }
+      else
+        {
+          if (is.fail() && !is.eof())
+            throw ValueError("error while scanning a multi-value");
+        }
+    }
+}
+
+template <class T>
+const char* TMultiValue<T>::get(Util::TypeCue<const char*>) const
+{
+  static char buf[256];
+  ostrstream ost(buf, 256);
+  printTo(ost);
+  return buf;
+}
+
+template <class T>
+void TMultiValue<T>::assignTo(Value& other) const
+{ other.set(this->get(Util::TypeCue<const char*>())); }
+
+template <class T>
+void TMultiValue<T>::assignFrom(const Value& other)
+{
+  istrstream ist(other.get(Util::TypeCue<const char*>()));
+  DebugEvalNL(other.get(Util::TypeCue<const char*>()));
+  scanFrom(ist);
+}
+
+template class TMultiValue<int>;
+template class TMultiValue<double>;
+
+static const char vcid_multivalue_cc[] = "$Header$";
+#endif // !MULTIVALUE_CC_DEFINED
---
+++ src/util/multivalue.h
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////
+//
+// multivalue.h
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Wed Aug 22 16:45:38 2001
+// written: Wed Aug 22 17:05:06 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef MULTIVALUE_H_DEFINED
+#define MULTIVALUE_H_DEFINED
+
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(VALUE_H_DEFINED)
+#include "util/value.h"
+#endif
+
+template <class T>
+class TMultiValue : public Value {
+public:
+  virtual Value* clone() const = 0;
+  virtual fstring getNativeTypeName() const = 0;
+
+  virtual void printTo(STD_IO::ostream& os) const;
+  virtual void scanFrom(STD_IO::istream& is);
+
+  virtual const char* get(Util::TypeCue<const char*>) const;
+
+  virtual void assignTo(Value& other) const;
+  virtual void assignFrom(const Value& other);
+
+protected:
+  virtual unsigned int numValues() const = 0;
+  virtual const T*   constBegin() const = 0;
+          const T*   constEnd()   const { return constBegin() + numValues(); }
+                T* mutableBegin()       { return const_cast<T*>(constBegin()); }
+                T* mutableEnd()         { return const_cast<T*>(constEnd()); }
+};
+
+static const char vcid_multivalue_h[] = "$Header$";
+#endif // !MULTIVALUE_H_DEFINED
