3fac180027c8e4afa88a1f198eefab1a98d6d4a2
Robert J. Peters <rjpcal@gmail.com>
2001-08-29 17:42:08 +0000

    Initial revision

---
+++ src/gfx/gxdrawstyle.cc
@@ -0,0 +1,92 @@
+///////////////////////////////////////////////////////////////////////
+//
+// gxdrawstyle.cc
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Wed Aug 29 10:19:31 2001
+// written: Wed Aug 29 10:34:00 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef GXDRAWSTYLE_CC_DEFINED
+#define GXDRAWSTYLE_CC_DEFINED
+
+#include "gfx/gxdrawstyle.h"
+
+#include "gfx/canvas.h"
+
+#include "io/reader.h"
+#include "io/writer.h"
+
+namespace
+{
+  const IO::VersionId GXDRAWSTYLE_SERIAL_VERSION_ID = 1;
+}
+
+GxDrawStyle::GxDrawStyle() :
+  FieldContainer(&sigNodeChanged),
+  polygonFill(false),
+  pointSize(1.0),
+  lineWidth(1.0),
+  linePattern(0xFFFF)
+{}
+
+GxDrawStyle::~GxDrawStyle() {}
+
+IO::VersionId GxDrawStyle::serialVersionId() const
+{
+  return GXDRAWSTYLE_SERIAL_VERSION_ID;
+}
+
+void GxDrawStyle::readFrom(IO::Reader* reader)
+{
+  reader->ensureReadVersionId("GxDrawStyle", 1,
+                              "Try grsh0.8a7");
+
+  readFieldsFrom(reader, classFields());
+}
+
+void GxDrawStyle::writeTo(IO::Writer* writer) const
+{
+  writer->ensureWriteVersionId("GxDrawStyle", GXDRAWSTYLE_SERIAL_VERSION_ID, 1,
+                               "Try grsh0.8a7");
+
+  writeFieldsTo(writer, classFields());
+}
+
+const FieldMap& GxDrawStyle::classFields()
+{
+  static const Field FIELD_ARRAY[] =
+  {
+    Field("polygonFill", &GxDrawStyle::polygonFill, false, false, true, true,
+          true),
+    Field("pointSize", &GxDrawStyle::pointSize, 1.0, 0.1, 1.0, 0.1),
+    Field("lineWidth", &GxDrawStyle::lineWidth, 1.0, 0.1, 1.0, 0.1),
+    Field("linePattern", &GxDrawStyle::linePattern, 0xFFFF, 0, 0xFFFF, 1)
+  };
+
+  static FieldMap GXDRAWSTYLE_FIELDS(FIELD_ARRAY);
+
+  return GXDRAWSTYLE_FIELDS;
+}
+
+void GxDrawStyle::draw(Gfx::Canvas& canvas) const
+{
+  canvas.setPolygonFill(polygonFill);
+  canvas.setPointSize(pointSize);
+  canvas.setLineWidth(lineWidth);
+  canvas.setLineStipple((unsigned short)linePattern);
+}
+
+void GxDrawStyle::undraw(Gfx::Canvas& canvas) const
+{
+  canvas.setPolygonFill(polygonFill);
+  canvas.setPointSize(pointSize);
+  canvas.setLineWidth(lineWidth);
+  canvas.setLineStipple((unsigned short)linePattern);
+}
+
+static const char vcid_gxdrawstyle_cc[] = "$Header$";
+#endif // !GXDRAWSTYLE_CC_DEFINED
---
+++ src/gfx/gxdrawstyle.h
@@ -0,0 +1,57 @@
+///////////////////////////////////////////////////////////////////////
+//
+// gxdrawstyle.h
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Wed Aug 29 10:16:30 2001
+// written: Wed Aug 29 10:33:35 2001
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef GXDRAWSTYLE_H_DEFINED
+#define GXDRAWSTYLE_H_DEFINED
+
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(GXNODE_H_DEFINED)
+#include "gfx/gxnode.h"
+#endif
+
+#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(FIELDS_H_DEFINED)
+#include "io/fields.h"
+#endif
+
+class GxDrawStyle : public GxNode, public FieldContainer
+{
+private:
+  GxDrawStyle(const GxDrawStyle&);
+  GxDrawStyle& operator=(const GxDrawStyle&);
+
+protected:
+  /// Default constructor.
+  GxDrawStyle();
+
+public:
+  virtual ~GxDrawStyle();
+
+  static GxDrawStyle* make() { return new GxDrawStyle; }
+
+private:
+  bool polygonFill;
+  double pointSize;
+  double lineWidth;
+  int linePattern;
+
+public:
+  virtual IO::VersionId serialVersionId() const;
+  virtual void readFrom(IO::Reader* reader);
+  virtual void writeTo(IO::Writer* writer) const;
+
+  static const FieldMap& classFields();
+
+  virtual void draw(Gfx::Canvas& canvas) const;
+  virtual void undraw(Gfx::Canvas& canvas) const;
+};
+
+static const char vcid_gxdrawstyle_h[] = "$Header$";
+#endif // !GXDRAWSTYLE_H_DEFINED
