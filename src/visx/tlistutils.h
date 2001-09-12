1e6acef9173ea79ff27fe7c4ecc35026ed842f3d
Robert J. Peters <rjpcal@gmail.com>
2001-09-12 23:44:04 +0000

    Moved createPreview to "tlisttcl.cc".

--- src/visx/tlistutils.h
+++ src/visx/tlistutils.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Sat Dec  4 02:58:20 1999
-// written: Sun Aug 26 08:35:12 2001
+// written: Wed Sep 12 16:43:57 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -13,23 +13,9 @@
 #ifndef TLISTUTILS_H_DEFINED
 #define TLISTUTILS_H_DEFINED
 
-#if defined(NO_EXTERNAL_INCLUDE_GUARDS) || !defined(UID_H_DEFINED)
-#include "util/uid.h"
-#endif
-
-namespace Gfx
-{
-  class Canvas;
-}
-
-template <class T> class dynamic_block;
-
 class TlistUtils
 {
 public:
-  static Util::UID createPreview(Gfx::Canvas& canvas,
-                                 Util::UID* objids, unsigned int objids_size);
-
   /** Write a file containing a summary of the responses to each Trial
       in the Tlist. */
   static void writeResponses(const char* filename);
