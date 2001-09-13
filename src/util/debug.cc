8872b09e3d3cacc328dcedc2ac220404aada6afb
Robert J. Peters <rjpcal@gmail.com>
2001-09-13 18:26:47 +0000

    Added STD_IO:: qualifier on cerr.

--- src/util/debug.cc
+++ src/util/debug.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Mon Oct  9 18:48:38 2000
-// written: Sun Aug 26 08:53:52 2001
+// written: Thu Sep 13 11:26:27 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -23,33 +23,33 @@
 
 void Debug::AssertImpl(const char* what, const char* where, int line_no)
 {
-  cerr << "Assertion failed: '" << what << "' in "
-       << where << " line " << line_no << '\n';
-  Util::Trace::printStackTrace(cerr);
+  STD_IO::cerr << "Assertion failed: '" << what << "' in "
+               << where << " line " << line_no << '\n';
+  Util::Trace::printStackTrace(STD_IO::cerr);
   abort();
 }
 
 void Debug::PreconditionImpl(const char* what, const char* where, int line_no)
 {
-  cerr << "Precondition failed: '" << what << "' in "
-       << where << " line " << line_no << '\n';
-  Util::Trace::printStackTrace(cerr);
+  STD_IO::cerr << "Precondition failed: '" << what << "' in "
+               << where << " line " << line_no << '\n';
+  Util::Trace::printStackTrace(STD_IO::cerr);
   abort();
 }
 
 void Debug::PostconditionImpl(const char* what, const char* where, int line_no)
 {
-  cerr << "Postcondition failed: '" << what << "' in "
-       << where << " line " << line_no << '\n';
-  Util::Trace::printStackTrace(cerr);
+  STD_IO::cerr << "Postcondition failed: '" << what << "' in "
+               << where << " line " << line_no << '\n';
+  Util::Trace::printStackTrace(STD_IO::cerr);
   abort();
 }
 
 void Debug::InvariantImpl(const char* what, const char* where, int line_no)
 {
-  cerr << "Invariant failed: '" << what << "' in "
-       << where << " line " << line_no << '\n';
-  Util::Trace::printStackTrace(cerr);
+  STD_IO::cerr << "Invariant failed: '" << what << "' in "
+               << where << " line " << line_no << '\n';
+  Util::Trace::printStackTrace(STD_IO::cerr);
   abort();
 }
 
