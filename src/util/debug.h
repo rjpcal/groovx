d9cee11a0dfabcc292e2ed3176cc1faf82f26355
Robert J. Peters <rjpcal@gmail.com>
2001-09-13 18:24:09 +0000

    Fixed things up so that "util/debug.h" can be included multiple times.

--- src/util/debug.h
+++ src/util/debug.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Mon Jan  4 08:00:00 1999
-// written: Sun Aug 26 08:38:29 2001
+// written: Thu Sep 13 11:20:52 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -13,16 +13,39 @@
 #ifndef DEBUG_H_DEFINED
 #define DEBUG_H_DEFINED
 
+static const char vcid_debug_h[] = "$Id$";
+
+#else // DEBUG_H_DEFINED
+
+#undef DebugEval
+#undef DebugEvalNL
+#undef DebugPrint
+#undef DebugPrintNL
+
+#undef Assert
+#undef Invariant
+#undef Precondition
+#undef Postcondition
+
+#endif
+
 #ifdef DEBUG
 #  ifndef LOCAL_DEBUG
 #    define LOCAL_DEBUG
 #  endif
 #endif
 
-#if defined(ASSERT) || defined(LOCAL_DEBUG)
-#  ifndef LOCAL_ASSERT
-#    define LOCAL_ASSERT
-#  endif
+#ifdef LOCAL_DEBUG
+#  include <iostream.h>
+#  define DebugEval(expr) STD_IO::cerr << #expr << " == " << (expr) << ", ";
+#  define DebugEvalNL(expr) STD_IO::cerr << #expr << " == " << (expr) << STD_IO::endl;
+#  define DebugPrint(expr) STD_IO::cerr << expr << " ";
+#  define DebugPrintNL(expr) STD_IO::cerr << expr << " " << STD_IO::endl;
+#else
+#  define DebugEval(expr) {}
+#  define DebugEvalNL(expr) {}
+#  define DebugPrint(str) {}
+#  define DebugPrintNL(str) {}
 #endif
 
 namespace Debug
@@ -33,20 +56,8 @@ namespace Debug
   void InvariantImpl     (const char* what, const char* where, int line_no);
 }
 
-#ifdef LOCAL_DEBUG
-#  include <iostream.h>
-#  define DebugEval(expr) cerr << #expr << " == " << (expr) << ", ";
-#  define DebugEvalNL(expr) cerr << #expr << " == " << (expr) << endl;
-#  define DebugPrint(expr) cerr << expr << " ";
-#  define DebugPrintNL(expr) cerr << expr << " " << endl;
-#else
-#  define DebugEval(expr) {}
-#  define DebugEvalNL(expr) {}
-#  define DebugPrint(str) {}
-#  define DebugPrintNL(str) {}
-#endif
+#if !defined(NO_ASSERT) || defined(LOCAL_DEBUG)
 
-#ifdef LOCAL_ASSERT
 #  define Assert(expr) \
         if ( !(expr) ) { Debug::AssertImpl(#expr, __FILE__, __LINE__); }
 #  define Invariant(expr) \
@@ -55,12 +66,12 @@ namespace Debug
         if ( !(expr) ) { Debug::PreconditionImpl(#expr, __FILE__, __LINE__); }
 #  define Postcondition(expr) \
         if ( !(expr) ) { Debug::PostconditionImpl(#expr, __FILE__, __LINE__); }
-#else // !LOCAL_ASSERT
+
+#else // NO_ASSERT
+
 #  define Assert(x) {}
 #  define Invariant(x) {}
 #  define Precondition(x) {}
 #  define Postcondition(x) {}
-#endif // !LOCAL_ASSERT
 
-static const char vcid_debug_h[] = "$Id$";
-#endif // !DEBUG_H_DEFINED
+#endif
