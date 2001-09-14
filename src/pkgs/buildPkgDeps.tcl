6175d18d5f09d4bf5677bcb5876cc7e22d87997f
Robert J. Peters <rjpcal@gmail.com>
2001-09-14 21:06:08 +0000

    Updated to automatically figure out what other packages are depended upon, and to "package require" those in the target's "package ifneeded" statement.

--- src/pkgs/buildPkgDeps.tcl
+++ src/pkgs/buildPkgDeps.tcl
@@ -1,17 +1,62 @@
 #!/usr/bin/env tclsh
 
-set indexfile [open $::env(HOME)/local/$::env(ARCH)/lib/visx/pkgIndex.tcl w]
 
-set depfile [open $::env(HOME)/sorcery/grsh/dep/$::env(ARCH)/pkgdepends w]
+
+proc getPkgNameFromPath { path } {
+
+	 set code [regexp {pkgs/([a-z]*)/} $path fullmatch pkgname]
+
+	 if { $code == 0 } {
+		  error "couldn't extract package name from path '$path'"
+	 }
+
+	 return $pkgname
+}
+
+proc extractPkgReqs { contents } {
+	 set lines [regexp -line -inline -all {include "pkgs/.*/.*"} $contents]
+
+	 set pkgs [list]
+
+	 foreach line $lines {
+		  lappend pkgs [string totitle [getPkgNameFromPath $line]]
+	 }
+
+	 return $pkgs
+}
+
+proc extractPkgRevision { contents } {
+
+	 set code [regexp {Revision: ([0-9\.]*)} $contents fullmatch revision]
+
+	 return $revision
+}
+
+proc getFileContents { filename } {
+	 set f [open $filename r]
+	 set contents [read $f]
+	 close $f
+	 return $contents
+}
 
 proc print { chan txt } {
 #	 puts -nonewline $txt
 	 puts -nonewline $chan $txt
 }
 
+
+
+#
+# Main code
+#
+
+set indexfile [open $::env(HOME)/local/$::env(ARCH)/lib/visx/pkgIndex.tcl w]
+
+set depfile [open $::env(HOME)/sorcery/grsh/dep/$::env(ARCH)/pkgdepends w]
+
 set libdir $::env(HOME)/local/$::env(ARCH)/lib/visx
 
-set objdir $::env(HOME)/sorcery/grsh/obj/$::env(ARCH)/pkgs
+set objdir obj/$::env(ARCH)/pkgs
 
 cd $::env(HOME)/sorcery/grsh/src/pkgs
 
@@ -37,26 +82,29 @@ foreach dir [glob \[a-z\]*/] {
 	 print $::depfile "\n\n"
 
 	 set ccfiles [glob ${dir}*.cc]
-	 set code [catch {eval exec grep -l _Init $ccfiles} initfiles]
+	 set code [catch {eval exec grep -l _Init $ccfiles} initfile]
 
 	 if { $code == 0 } {
 
-		  if { [llength $initfiles] > 1 } {
-				error "package $pkg has more than one Tcl_PkgInit function"
+		  if { [llength $initfile] > 1 } {
+				error "package $pkg has more than one Tcl_PkgInit: $initfile"
 		  }
-
-		  set initfile [open $initfiles r]
-
-		  set contents [read $initfile]
-
-		  close $initfile
-
-		  set code [regexp {Revision: ([0-9\.]*)} $contents fullmatch revision]
 
 		  set pkgname [string totitle $pkg]
 
-		  print $::indexfile \
-					 "package ifneeded $pkgname $revision { load $shlib }\n"
+		  set contents [getFileContents $initfile]
+
+		  set revision [extractPkgRevision $contents]
+
+		  set pkgreqs [extractPkgReqs $contents]
+
+		  print $::indexfile "package ifneeded $pkgname $revision \{\n" 
+
+		  foreach req $pkgreqs {
+				print $::indexfile "\tpackage require $req\n"
+		  } 
+		  
+		  print $::indexfile "\tload $shlib\n\}\n"
 	 }
 }
 
