9d2c583c3687441754ee9bf7aa5cc45fec507be2
Robert J. Peters <rjpcal@gmail.com>
2001-09-17 19:32:30 +0000

    Profiling the performance differences between hand-written Tcl_ObjCmdProc's and my templated functor versions.

--- src/visx/hooktcl.cc
+++ src/visx/hooktcl.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Thu Oct  5 13:51:43 2000
-// written: Fri Sep 14 14:48:36 2001
+// written: Mon Sep 17 12:21:02 2001
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -55,10 +55,55 @@ namespace HookTcl
   size_t memUsage() { return TOTAL; }
 }
 
+#include "visx/grobj.h"
+#include "util/objdb.h"
+#include <tcl.h>
+
+static int categoryCmd(ClientData clientData, Tcl_Interp* interp,
+                       int objc, Tcl_Obj *const objv[])
+{
+  try
+    {
+      if (objc < 2 || objc > 3)
+        {
+          return TCL_ERROR;
+        }
+
+      int id;
+      if (Tcl_GetIntFromObj(interp, objv[1], &id) != TCL_OK)
+        {
+          return TCL_ERROR;
+        }
+
+      Ref<FieldContainer> fc(id);
+
+      static const Field& categoryfield = GrObj::classFields().field("category");
+
+      if (objc == 2)
+        {
+          Tcl::ObjPtr category = categoryfield.getValue(fc.get());
+          Tcl_SetObjResult(interp, category.obj());
+        }
+      else if (objc == 3)
+        {
+          categoryfield.setValue(fc.get(), Tcl::ObjPtr(objv[2]));
+        }
+    }
+  catch (...)
+    {
+      return TCL_ERROR;
+    }
+
+  return TCL_OK;
+}
+
 extern "C"
 int Hook_Init(Tcl_Interp* interp)
 {
   Tcl::Pkg* pkg = new Tcl::Pkg(interp, "Hook", "$Revision$");
+
+  Tcl_CreateObjCommand(interp, "hook::category", categoryCmd, 0, 0);
+
   pkg->def( "::hook", "", HookTcl::hook );
   pkg->def( "::memUsage", 0, HookTcl::memUsage );
 
