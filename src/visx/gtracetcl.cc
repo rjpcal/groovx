a1b20e7702d5009b5e694115c2964655563675f0
Robert J. Peters <rjpcal@gmail.com>
2002-01-21 21:01:16 +0000

    Initial revision

---
+++ src/visx/gtracetcl.cc
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////
+//
+// gtrace.cc
+//
+// Copyright (c) 1998-2001 Rob Peters rjpeters@klab.caltech.edu
+//
+// created: Mon Jan 21 12:52:51 2002
+// written: Mon Jan 21 12:55:57 2002
+// $Id$
+//
+///////////////////////////////////////////////////////////////////////
+
+#ifndef GTRACE_CC_DEFINED
+#define GTRACE_CC_DEFINED
+
+#include "tcl/tclpkg.h"
+
+#include "util/trace.h"
+
+extern "C"
+int Gtrace_Init(Tcl_Interp* interp)
+{
+DOTRACE("Gtrace_Init");
+
+  Tcl::Pkg* pkg = new Tcl::Pkg(interp, "Gtrace", "$Revision$");
+  pkg->def("::gtrace", "on_off", &Util::Trace::setGlobalTrace);
+  pkg->def("::gtrace", "", &Util::Trace::getGlobalTrace);
+
+  return pkg->initStatus();
+}
+
+static const char vcid_gtrace_cc[] = "$Header$";
+#endif // !GTRACE_CC_DEFINED
