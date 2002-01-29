2c4d658552387d23c9dad2bd947ecdfd1e6ef80d
Robert J. Peters <rjpcal@gmail.com>
2002-01-29 19:34:09 +0000

    Whitespace.

--- testing/list_test.tcl
+++ testing/list_test.tcl
@@ -18,9 +18,9 @@ namespace eval List {
 variable TEST_DEFINED 1
 
 proc purgeAll {} {
-	 Togl::setVisible 0
-	 Expt::clear
-	 ObjDb::clear
+    Togl::setVisible 0
+    Expt::clear
+    ObjDb::clear
 }
 
 proc testList { baseclass subclass1 subclass2 } {
@@ -29,13 +29,13 @@ proc testList { baseclass subclass1 subclass2 } {
     set testObj(subclass1) $subclass1
     set testObj(subclass2) $subclass2
 
-	 purgeAll
+    purgeAll
 
     testResetCmd testObj
     testCountAllCmd testObj
-	 testDeleteCmd testObj
-	 testFindAllCmd testObj
-	 testIsCmd testObj
+    testDeleteCmd testObj
+    testFindAllCmd testObj
+    testIsCmd testObj
 }
 
 proc testResetCmd { objname } {
@@ -55,7 +55,7 @@ proc testResetCmd { objname } {
         Obj::new ${this(subclass2)}
         $cmdname
         set after_count \[${this(baseclass)}::countAll\]
-		  return \[expr \$before_count - \$after_count\]
+        return \[expr \$before_count - \$after_count\]
     "} {"^0$"}
 }
 
@@ -70,11 +70,11 @@ proc testCountAllCmd { objname } {
         $cmdname junk
     "} {$usage}
     eval ::test $testname {"normal use"} {"
-	     set before_count \[$cmdname\]
+        set before_count \[$cmdname\]
         Obj::new ${this(subclass1)}
         Obj::new ${this(subclass2)}
         set after_count \[$cmdname\]
-		  return \[expr \$after_count - \$before_count\]
+        return \[expr \$after_count - \$before_count\]
     "} {"^2$"}
 } 
 
@@ -85,19 +85,19 @@ proc testDeleteCmd { objname } {
     set usage "wrong \# args: should be \"$cmdname item_id\\(s\\)\""
     set testname "${this(packagename)}-${cmdname}"
 
-	 eval ::test $testname {"too few args"} {"
-	     $cmdname
-	 "} {$usage}
+    eval ::test $testname {"too few args"} {"
+        $cmdname
+    "} {$usage}
 
-	 eval ::test $testname {"too many args"} {"
-	     $cmdname junk junk
-	 "} {$usage}
+    eval ::test $testname {"too many args"} {"
+        $cmdname junk junk
+    "} {$usage}
 
-	 eval ::test $testname {"normal use"} {"
+    eval ::test $testname {"normal use"} {"
         set id \[Obj::new ${this(subclass1)}\]
-	     $cmdname \$id
-	     IO::is \$id
- 	 "} {"^0$"}
+        $cmdname \$id
+        IO::is \$id
+     "} {"^0$"}
 }
 
 proc testFindAllCmd { objname } {
@@ -107,20 +107,20 @@ proc testFindAllCmd { objname } {
     set usage "wrong \# args: should be \"$cmdname\""
     set testname "${this(packagename)}-${cmdname}"
 
-	 eval ::test $testname {"too many args"} {"
-	     $cmdname junk
-	 "} {$usage}
+    eval ::test $testname {"too many args"} {"
+        $cmdname junk
+    "} {$usage}
 
-	 eval ::test $testname {"normal use on filled list"} {"
+    eval ::test $testname {"normal use on filled list"} {"
         Obj::new ${this(subclass1)}
         set remove_me \[Obj::new ${this(subclass1)}\]
         Obj::new ${this(subclass2)}
-		  Obj::delete \$remove_me
-		  set count \[${this(baseclass)}::countAll\]
-		  set num_ids \[llength \[$cmdname\]\]
-		  set removed_id \[lsearch -exact \[$cmdname\] \$remove_me\]
-		  return \"\[expr \$count - \$num_ids\] \$removed_id\"
-	 "} {"^0 -1$"}
+        Obj::delete \$remove_me
+        set count \[${this(baseclass)}::countAll\]
+        set num_ids \[llength \[$cmdname\]\]
+        set removed_id \[lsearch -exact \[$cmdname\] \$remove_me\]
+        return \"\[expr \$count - \$num_ids\] \$removed_id\"
+    "} {"^0 -1$"}
 }
 
 proc testIsCmd { objname } {
@@ -130,24 +130,24 @@ proc testIsCmd { objname } {
     set usage "wrong \# args: should be "
     set testname "${this(packagename)}-${cmdname}"
 
-	 eval ::test $testname {"too few args"} {"
-	     $cmdname
-	 "} {$usage}
+    eval ::test $testname {"too few args"} {"
+        $cmdname
+    "} {$usage}
 
-	 eval ::test $testname {"too many args"} {"
-	     $cmdname 0 junk
-	 "} {$usage}
+    eval ::test $testname {"too many args"} {"
+        $cmdname 0 junk
+    "} {$usage}
 
-	 eval ::test $testname {"normal use on valid id"} {"
+    eval ::test $testname {"normal use on valid id"} {"
         set id \[Obj::new ${this(subclass1)}\]
-	     $cmdname \$id
-	 "} {"^1$"}
+        $cmdname \$id
+    "} {"^1$"}
 
-	 eval ::test $testname {"normal use on valid id"} {"
+    eval ::test $testname {"normal use on valid id"} {"
         set id \[Obj::new ${this(subclass1)}\]
-	     Obj::delete \$id
-	     $cmdname \$id
-	 "} {"^0$"}
+        Obj::delete \$id
+        $cmdname \$id
+    "} {"^0$"}
 }
 
 namespace export testList
