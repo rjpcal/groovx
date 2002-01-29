9aa8ed066bf308685c4ded4d70e6c36df48f1795
Robert J. Peters <rjpcal@gmail.com>
2002-01-29 20:48:04 +0000

    Whitespace.

--- testing/test.tcl
+++ testing/test.tcl
@@ -15,7 +15,7 @@ set START_CLICKS [clock clicks]
 
 if { ![info exists ::VERBOSE] } { set ::VERBOSE 0 }
 if { ![info exists ::TEST_DIR] } {
-	 set ::TEST_DIR $env(HOME)/sorcery/grsh/testing
+    set ::TEST_DIR $env(HOME)/sorcery/grsh/testing
 }
 
 set DATE [clock format [clock seconds] -format %H%M%d%b%Y]
@@ -37,114 +37,114 @@ set intermittent_bug 2
 
 # If the 'flags' argument is 0, the test is skipped.
 proc test {package_name test_name script expected_result_regexp {flags 1}} {
-	 if { $flags == $::must_implement } {
-		  lappend ::TO_IMPLEMENT "$package_name $test_name"
-	 }
-	 if { $flags == $::skip_known_bug } {
-		  lappend ::KNOWN_BUGS "$package_name $test_name"
-	 }
-	 if { $flags == $::intermittent_bug } {
-		  lappend ::INTERMITTENT_BUGS "$package_name $test_name"
-	 }
-	 if {$flags <= 0} { return }
-	 if {$::VERBOSE > 0} {
-		  puts "================================================================"
-		  puts "==== $package_name $test_name"
-	 }
-	 if {$::VERBOSE > 1} {
-		  puts "==== Contents of test case:"
-		  puts $script
-	 }
-	 set ::errorInfo ""
-	 catch {eval $script} result
-	 incr ::num_tests
-	 if { [regexp -- $expected_result_regexp $result] == 1 } {
-		  if {$::VERBOSE != 0} { 
-				puts "---- $package_name $test_name succeeded\n"
-		  }
-		  incr ::num_success
-	 } else {
-		  if {$::VERBOSE <= 0} {
-				puts "================================================================"
-				puts "==== $package_name $test_name"
-		  }
-		  if {$::VERBOSE <= 1} {
-				puts "==== Contents of test case:"
-				puts $script
-		  }
-		  puts "==== Result was:"
-		  puts $result
-		  puts "---- Result should have been:"
-		  puts $expected_result_regexp
-		  if { [string length $::errorInfo] > 0 } {
-				puts "---- ErrorInfo:"
-				puts $::errorInfo
-		  }
-		  puts "---- $package_name $test_name FAILED\n"
-#		  finish
-		  lappend ::FAILED "$package_name $test_name"
-	 }
+    if { $flags == $::must_implement } {
+        lappend ::TO_IMPLEMENT "$package_name $test_name"
+    }
+    if { $flags == $::skip_known_bug } {
+        lappend ::KNOWN_BUGS "$package_name $test_name"
+    }
+    if { $flags == $::intermittent_bug } {
+        lappend ::INTERMITTENT_BUGS "$package_name $test_name"
+    }
+    if {$flags <= 0} { return }
+    if {$::VERBOSE > 0} {
+        puts "================================================================"
+        puts "==== $package_name $test_name"
+    }
+    if {$::VERBOSE > 1} {
+        puts "==== Contents of test case:"
+        puts $script
+    }
+    set ::errorInfo ""
+    catch {eval $script} result
+    incr ::num_tests
+    if { [regexp -- $expected_result_regexp $result] == 1 } {
+        if {$::VERBOSE != 0} { 
+            puts "---- $package_name $test_name succeeded\n"
+        }
+        incr ::num_success
+    } else {
+        if {$::VERBOSE <= 0} {
+            puts "================================================================"
+            puts "==== $package_name $test_name"
+        }
+        if {$::VERBOSE <= 1} {
+            puts "==== Contents of test case:"
+            puts $script
+        }
+        puts "==== Result was:"
+        puts $result
+        puts "---- Result should have been:"
+        puts $expected_result_regexp
+        if { [string length $::errorInfo] > 0 } {
+            puts "---- ErrorInfo:"
+            puts $::errorInfo
+        }
+        puts "---- $package_name $test_name FAILED\n"
+#        finish
+        lappend ::FAILED "$package_name $test_name"
+    }
 }
 
 proc finish {} {
-	 set clicks_per_ms 1000
-	 set ::END_CLICKS [clock clicks]
-	 set elapsed_ms [expr ($::END_CLICKS-$::START_CLICKS) / $clicks_per_ms]
-	 set per_test [expr $::num_tests ? ($elapsed_ms/$::num_tests) : 0]
-	 set msg ""
-	 if { $::num_success < $::num_tests } {
-		  append msg "-- WARNING -- BUGGY SOFTWARE --\n"
-	 }
-	 append msg "$::num_success tests succeeded of $::num_tests tests attempted.\n"
-	 append msg "Testing lasted $elapsed_ms msec, or $per_test msec per test.\n"
+    set clicks_per_ms 1000
+    set ::END_CLICKS [clock clicks]
+    set elapsed_ms [expr ($::END_CLICKS-$::START_CLICKS) / $clicks_per_ms]
+    set per_test [expr $::num_tests ? ($elapsed_ms/$::num_tests) : 0]
+    set msg ""
+    if { $::num_success < $::num_tests } {
+        append msg "-- WARNING -- BUGGY SOFTWARE --\n"
+    }
+    append msg "$::num_success tests succeeded of $::num_tests tests attempted.\n"
+    append msg "Testing lasted $elapsed_ms msec, or $per_test msec per test.\n"
 
-	 if { [llength $::FAILED] > 0 } {
-		  append msg "[llength $::FAILED] tests FAILED:\n"
-		  foreach item $::FAILED {
-				append msg "\t${item}\n"
-		  }
-	 }
+    if { [llength $::FAILED] > 0 } {
+        append msg "[llength $::FAILED] tests FAILED:\n"
+        foreach item $::FAILED {
+            append msg "\t${item}\n"
+        }
+    }
 
-	 if { [llength $::TO_IMPLEMENT] > 0 } {
-		  append msg "[llength $::TO_IMPLEMENT] tests remain to be implemented:\n"
-		  foreach item $::TO_IMPLEMENT {
-				append msg "\t${item}\n"
-		  }
-	 }
+    if { [llength $::TO_IMPLEMENT] > 0 } {
+        append msg "[llength $::TO_IMPLEMENT] tests remain to be implemented:\n"
+        foreach item $::TO_IMPLEMENT {
+            append msg "\t${item}\n"
+        }
+    }
 
     if { [llength $::KNOWN_BUGS] > 0 } {
-		  append msg "[llength $::KNOWN_BUGS] known bugs were reported:\n"
-		  foreach bug $::KNOWN_BUGS {
-				append msg "\t${bug}\n"
-		  }
-	 }
+        append msg "[llength $::KNOWN_BUGS] known bugs were reported:\n"
+        foreach bug $::KNOWN_BUGS {
+            append msg "\t${bug}\n"
+        }
+    }
 
-	 if { [llength $::INTERMITTENT_BUGS] > 0 } {
-		  append msg "[llength $::INTERMITTENT_BUGS] intermittent bugs were reported:\n"
-		  foreach bug $::INTERMITTENT_BUGS {
-				append msg "\t${bug}\n"
-		  }
-	 }
+    if { [llength $::INTERMITTENT_BUGS] > 0 } {
+        append msg "[llength $::INTERMITTENT_BUGS] intermittent bugs were reported:\n"
+        foreach bug $::INTERMITTENT_BUGS {
+            append msg "\t${bug}\n"
+        }
+    }
 
-	 append msg "Testing complete."
+    append msg "Testing complete."
     puts $msg
     after 10000 { event generate . <KeyPress> -keysym Return; exit }
-	 tk_messageBox -icon info -type ok -message $msg
-	 exit
+    tk_messageBox -icon info -type ok -message $msg
+    exit
 }
 
 proc testfile {file_arg} {
-	 set ::file_arg $file_arg
-	 namespace eval :: {
-		  
-		  puts "running test file ${file_arg}..."
+    set ::file_arg $file_arg
+    namespace eval :: {
+        
+        puts "running test file ${file_arg}..."
 
-		  set result [ catch {source $file_arg} errmsg ]
-		  if { $result != 0 } {
-				puts "...an error occurred while evaluating ${file_arg}:"
-				puts $errmsg
-		  }
-	 }
+        set result [ catch {source $file_arg} errmsg ]
+        if { $result != 0 } {
+            puts "...an error occurred while evaluating ${file_arg}:"
+            puts $errmsg
+        }
+    }
 }
 
 # test the test procedure
