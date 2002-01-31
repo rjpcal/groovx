4197581fa81dec35f49ddf3414f7b001956b2723
Robert J. Peters <rjpcal@gmail.com>
2002-01-31 22:03:36 +0000

    Changed namespace name "Rh" to "ResponseHandler".

--- testing/obj_test.tcl
+++ testing/obj_test.tcl
@@ -11,6 +11,6 @@ source ${::TEST_DIR}/list_test.tcl
 List::testList Block Block Block
 List::testList GrObj Face Fish
 List::testList Position Position Jitter
-List::testList Rh KbdResponseHdlr NullResponseHdlr
+List::testList ResponseHandler KbdResponseHdlr NullResponseHdlr
 List::testList Th TimingHdlr TimingHandler
 List::testList Trial Trial Trial
