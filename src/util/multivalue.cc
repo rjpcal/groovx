3db958dc46d1c4b6d9705f0ef08aef409fb90c49
Robert J. Peters <rjpcal@gmail.com>
2002-02-22 02:56:07 +0000

    Added ugly workaround for broken explicit instantiation with g++-2.96 under Mac OS X.

--- src/util/multivalue.cc
+++ src/util/multivalue.cc
@@ -21,41 +21,49 @@
 
 #include "util/debug.h"
 
-template <class T>
-void TMultiValue<T>::printTo(STD_IO::ostream& os) const
-{
-  const T* dat = constBegin();
-  const T* end = constEnd();
+//  template <class T>
+//  TMultiValue<T>::~TMultiValue() {}
 
-  while (dat < end)
-    {
-      os << *dat;
-      if (++dat < end)
-        os << ' ';
-    }
+#define INSTANTIATE(T) \
+ \
+template <> \
+void TMultiValue<T>::doPrintTo(STD_IO::ostream& os) const \
+{ \
+  const T* dat = constBegin(); \
+  const T* end = constEnd(); \
+ \
+  while (dat < end) \
+    { \
+      os << *dat; \
+      if (++dat < end) \
+        os << ' '; \
+    } \
+} \
+ \
+template <> \
+void TMultiValue<T>::doScanFrom(STD_IO::istream& is) \
+{ \
+  T* dat = mutableBegin(); \
+  T* end = mutableEnd(); \
+ \
+  while (dat < end) \
+    { \
+      is >> *dat++; \
+      if (dat < end) \
+        { \
+          if (is.fail() || is.eof()) \
+            throw Util::Error("error while scanning a multi-value"); \
+        } \
+      else \
+        { \
+          if (is.fail() && !is.eof()) \
+            throw Util::Error("error while scanning a multi-value"); \
+        } \
+    } \
 }
 
-template <class T>
-void TMultiValue<T>::scanFrom(STD_IO::istream& is)
-{
-  T* dat = mutableBegin();
-  T* end = mutableEnd();
-
-  while (dat < end)
-    {
-      is >> *dat++;
-      if (dat < end)
-        {
-          if (is.fail() || is.eof())
-            throw Util::Error("error while scanning a multi-value");
-        }
-      else
-        {
-          if (is.fail() && !is.eof())
-            throw Util::Error("error while scanning a multi-value");
-        }
-    }
-}
+INSTANTIATE(double);
+INSTANTIATE(int);
 
 template class TMultiValue<int>;
 template class TMultiValue<double>;
