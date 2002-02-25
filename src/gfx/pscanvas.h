7e27e495fe25c1bcda54e381efd604597e945cfc
Robert J. Peters <rjpcal@gmail.com>
2002-02-25 18:39:49 +0000

    Added drawBezierFill4().

--- src/gfx/pscanvas.h
+++ src/gfx/pscanvas.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Mon Aug 27 17:18:49 2001
-// written: Fri Jan 18 16:06:58 2002
+// written: Mon Feb 25 10:39:45 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -105,6 +105,13 @@ public:
                            const Gfx::Vec3<double>& p3,
                            const Gfx::Vec3<double>& p4,
                            unsigned int subdivisions);
+
+  virtual void drawBezierFill4(const Gfx::Vec3<double>& center,
+                               const Gfx::Vec3<double>& p1,
+                               const Gfx::Vec3<double>& p2,
+                               const Gfx::Vec3<double>& p3,
+                               const Gfx::Vec3<double>& p4,
+                               unsigned int subdivisions);
 
   virtual void beginPoints();
   virtual void beginLines();
