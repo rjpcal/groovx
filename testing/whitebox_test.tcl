8fca94d2b7183602882910cae57a0f6b9e92435a
Robert J. Peters <rjpcal@gmail.com>
2002-06-24 23:47:38 +0000

    A home for all blackbox tests to be "package require"d and then run.

---
+++ testing/whitebox_test.tcl
@@ -0,0 +1,21 @@
+##############################################################################
+###
+### blackbox tests
+### Rob Peters
+### 24-Jun-2002
+### $Id$
+###
+##############################################################################
+
+set pkgs {
+    Dlinktest
+}
+
+foreach pkg $pkgs {
+
+    package require $pkg
+
+    foreach cmd [info commands ${pkg}::test*] {
+	::test "$pkg" "$cmd" $cmd {^$}
+    }
+}
