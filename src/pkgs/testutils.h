8dbcddc0a1f0cae2fde14981f4c1ddbe3b82bcee
Robert J. Peters <rjpcal@gmail.com>
2002-06-25 00:36:40 +0000

    Moved the TEST_REQUIRE() macro implementation into a separate function to conserve object code size.

--- src/pkgs/testutils.h
+++ src/pkgs/testutils.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Mon Sep 10 18:57:59 2001
-// written: Fri Jan 18 16:06:56 2002
+// written: Mon Jun 24 17:35:13 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -17,9 +17,19 @@
 
 #include "tcl/tclpkg.h"
 
+namespace
+{
+  void testRequireImpl(bool expr, const char* exprString,
+                       const char* file, int line)
+  {
+    if (!expr)
+      throw Util::Error(fstring(file, "@", line,
+                                ": failed test of: ",exprString));
+  }
+}
+
 #define TEST_REQUIRE(expr) \
-    if (!(expr)) \
-      throw Util::Error(fstring("test for '", #expr, "' failed"));
+  testRequireImpl(bool(expr), #expr, __FILE__, __LINE__);
 
 #define DEF_TEST(pkg, func) pkg->def(#func, "", &func)
 
