e8cead77d2ca8210140c039dae03762804ff51a8
Robert J. Peters <rjpcal@gmail.com>
2002-09-06 19:16:15 +0000

    Added setResult().

--- src/tcl/tclsafeinterp.h
+++ src/tcl/tclsafeinterp.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Wed Oct 11 10:25:36 2000
-// written: Mon Jul 22 18:37:46 2002
+// written: Fri Sep  6 12:15:07 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -79,6 +79,12 @@ public:
     return Tcl::Convert<T>::fromTcl(getObjResult());
   }
 
+  template <class T>
+  void setResult(const T& x)
+  {
+    setObjResult(Tcl::Convert<T>::toTcl(x).obj());
+  }
+
   // Variables
   void setGlobalVar(const char* var_name, const Tcl::ObjPtr& var) const;
   void unsetGlobalVar(const char* var_name) const;
@@ -108,6 +114,7 @@ public:
 private:
   Tcl_Obj* getObjResult() const;
   Tcl_Obj* getObjGlobalVar(const char* name1, const char* name2) const;
+  void setObjResult(Tcl_Obj* obj);
 
   void handleError(const char* msg) const;
 
