dced5a1c1799682bdb7b0e7654291088b7b54a4b
Robert J. Peters <rjpcal@gmail.com>
2002-09-10 00:09:38 +0000

    Simple proc for parsing command-line options and setting the corresponding args to similarly-named Tcl global variables.

---
+++ scripts/parse_cmdline.tcl
@@ -0,0 +1,19 @@
+# Tcl functions for parsing command-line options
+#
+# Copyright (c) 2002-2002 Rob Peters rjpeters@klab.caltech.edu
+#
+# $Revision$
+
+proc parse_cmdline { argv } {
+
+    for {set i 0} {$i < [llength $argv]} {incr i} {
+
+	set arg [lindex $argv $i]
+
+	if { [string equal "--" [string range $arg 0 1]] } {
+	    set name [string toupper [string range $arg 2 end]]
+
+	    set ::${name} [lindex $argv [incr i]]
+	}
+    }
+}
