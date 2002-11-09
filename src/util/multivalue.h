2115d907f61dc9f59e70a41e7fc4ab6ba32bc721
Robert J. Peters <rjpcal@gmail.com>
2002-11-09 20:21:20 +0000

    * Replaced TMultiValue::numValues() with a data member.
    * Make TMultiValue just implement printTo() and scanFrom(), rather than
      having subclasses forward to doPrintTo() and doScanFrom().

--- src/util/multivalue.h
+++ src/util/multivalue.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Wed Aug 22 16:45:38 2001
-// written: Wed Sep 25 18:59:01 2002
+// written: Tue Nov  5 06:39:44 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -15,24 +15,27 @@
 
 #include "util/value.h"
 
-/// A Value subclass representing a (small) set of homogeneous values.
+/// A Value subclass representing a fixed-size set of homogeneous values.
 template <class T>
 class TMultiValue : public Value
 {
 public:
+  TMultiValue(int num) : itsNumValues(num) {}
   virtual ~TMultiValue() {}
 
   virtual fstring getNativeTypeName() const = 0;
 
-  void doPrintTo(STD_IO::ostream& os) const;
-  void doScanFrom(STD_IO::istream& is);
+  virtual void printTo(STD_IO::ostream& os) const;
+  virtual void scanFrom(STD_IO::istream& is);
 
 protected:
-  virtual unsigned int numValues() const = 0;
   virtual const T*   constBegin() const = 0;
-          const T*   constEnd()   const { return constBegin() + numValues(); }
+          const T*   constEnd()   const { return constBegin() + itsNumValues; }
                 T* mutableBegin()       { return const_cast<T*>(constBegin()); }
                 T* mutableEnd()         { return const_cast<T*>(constEnd()); }
+
+private:
+  int itsNumValues;
 };
 
 static const char vcid_multivalue_h[] = "$Header$";
