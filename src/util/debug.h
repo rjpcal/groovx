5dd9e82446f0ba9efed21c4760b979fdb4683632
Robert J. Peters <rjpcal@gmail.com>
2002-11-09 20:27:08 +0000

    * Changed debugging to be dynamically turn-on-off-able.
    * Allow multiple debug levels.
    
    * Rename DebugEval* to dbgEval*.
    
    * Add dbgDump().

--- src/util/debug.h
+++ src/util/debug.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Mon Jan  4 08:00:00 1999
-// written: Tue Aug  6 15:23:13 2002
+// written: Sun Nov  3 13:49:59 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -13,54 +13,64 @@
 #ifndef DEBUG_H_DEFINED
 #define DEBUG_H_DEFINED
 
-static const char vcid_debug_h[] = "$Id$";
-
-#else // DEBUG_H_DEFINED
-
-#undef DebugEval
-#undef DebugEvalNL
-#undef DebugPrint
-#undef DebugPrintNL
-
-#undef Assert
-#undef Invariant
-#undef Precondition
-#undef Postcondition
-
-#endif
-
-#ifdef DEBUG
-#  ifndef LOCAL_DEBUG
-#    define LOCAL_DEBUG
-#  endif
-#endif
-
-#ifdef LOCAL_DEBUG
-#  include <iostream>
-#  define DebugEval(expr) STD_IO::cerr << #expr << " == " << (expr) << ", ";
-#  define DebugEvalNL(expr) STD_IO::cerr << #expr << " == " << (expr) << STD_IO::endl;
-#  define DebugPrint(expr) STD_IO::cerr << expr << " ";
-#  define DebugPrintNL(expr) STD_IO::cerr << expr << " " << STD_IO::endl;
-#else
-#  define DebugEval(expr) {}
-#  define DebugEvalNL(expr) {}
-#  define DebugPrint(str) {}
-#  define DebugPrintNL(str) {}
-#endif
+class fstring;
 
 namespace Debug
 {
+  void Eval (const char* what, int level, const char* where, int line_no, bool expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, char expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, unsigned char expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, short expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, unsigned short expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, int expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, unsigned int expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, long expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, unsigned long expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, float expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, double expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, const char* expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, void* expr, bool nl = false);
+  void Eval (const char* what, int level, const char* where, int line_no, fstring expr, bool nl = false);
+
   void AssertImpl        (const char* what, const char* where, int line_no);
   void PreconditionImpl  (const char* what, const char* where, int line_no);
   void PostconditionImpl (const char* what, const char* where, int line_no);
   void InvariantImpl     (const char* what, const char* where, int line_no);
+
+  extern int level;
 }
 
 // Like Assert(), but can't ever be turned off.
 #define AbortIf(expr) \
       if ( expr ) { Debug::AssertImpl(#expr, __FILE__, __LINE__); }
 
-#if !defined(NO_ASSERT) || defined(LOCAL_DEBUG)
+static const char vcid_debug_h[] = "$Id$";
+
+#else // DEBUG_H_DEFINED
+
+//
+// Everything below here might get processed multiple times if the file is
+// #include'd more than once
+//
+
+#undef dbgEval
+#undef dbgEvalNL
+#undef dbgPrint
+#undef dbgPrintNL
+
+#undef Assert
+#undef Invariant
+#undef Precondition
+#undef Postcondition
+
+#endif // DEBUG_H_DEFINED
+
+#if !defined(NO_DEBUG)
+#  define dbgEval(lev, x)    do { if (Debug::level >= lev) Debug::Eval(#x, lev, __FILE__, __LINE__, x, false); } while (0)
+#  define dbgEvalNL(lev, x)  do { if (Debug::level >= lev) Debug::Eval(#x, lev, __FILE__, __LINE__, x, true); } while (0)
+#  define dbgPrint(lev, x)   do { if (Debug::level >= lev) Debug::Eval(0, lev, __FILE__, __LINE__, x, false); } while (0)
+#  define dbgPrintNL(lev, x) do { if (Debug::level >= lev) Debug::Eval(0, lev, __FILE__, __LINE__, x, true); } while (0)
+#  define dbgDump(lev, x)    do { if (Debug::level >= lev) { Debug::Eval(#x, lev, __FILE__, __LINE__, "...", true); (x).debugDump(); } } while (0)
 
 #  define Assert(expr) \
         if ( !(expr) ) { Debug::AssertImpl(#expr, __FILE__, __LINE__); }
@@ -71,11 +81,17 @@ namespace Debug
 #  define Postcondition(expr) \
         if ( !(expr) ) { Debug::PostconditionImpl(#expr, __FILE__, __LINE__); }
 
-#else // NO_ASSERT
+#else // defined(NO_DEBUG)
 
-#  define Assert(x) {}
-#  define Invariant(x) {}
-#  define Precondition(x) {}
-#  define Postcondition(x) {}
+#  define dbgEval(lev, x)     do {} while (0)
+#  define dbgEvalNL(lev, x)   do {} while (0)
+#  define dbgPrint(lev, x)    do {} while (0)
+#  define dbgPrintNL(lev, x)  do {} while (0)
+#  define dbgDump(lev, expr)  do {} while (0)
+
+#  define Assert(x)        do {} while (0)
+#  define Invariant(x)     do {} while (0)
+#  define Precondition(x)  do {} while (0)
+#  define Postcondition(x) do {} while (0)
 
 #endif
