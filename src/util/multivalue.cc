17fcd83e48a6bab8124855d651d03874e1fe923a
Robert J. Peters <rjpcal@gmail.com>
2002-11-09 20:21:37 +0000

    * Replaced TMultiValue::numValues() with a data member.
    * Make TMultiValue just implement printTo() and scanFrom(), rather than
      having subclasses forward to doPrintTo() and doScanFrom().
    
    * New debug interface.

--- src/util/multivalue.cc
+++ src/util/multivalue.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Wed Aug 22 16:58:56 2001
-// written: Tue May 14 20:07:41 2002
+// written: Tue Nov  5 06:40:19 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -20,46 +20,59 @@
 #include <iostream>
 
 #include "util/debug.h"
+#include "util/trace.h"
 
 //  template <class T>
 //  TMultiValue<T>::~TMultiValue() {}
 
-#define INSTANTIATE(T) \
- \
-template <> \
-void TMultiValue<T>::doPrintTo(STD_IO::ostream& os) const \
-{ \
-  const T* dat = constBegin(); \
-  const T* end = constEnd(); \
- \
-  while (dat < end) \
-    { \
-      os << *dat; \
-      if (++dat < end) \
-        os << ' '; \
-    } \
-} \
- \
-template <> \
-void TMultiValue<T>::doScanFrom(STD_IO::istream& is) \
-{ \
-  T* dat = mutableBegin(); \
-  T* end = mutableEnd(); \
- \
-  while (dat < end) \
-    { \
-      is >> *dat++; \
-      if (dat < end) \
-        { \
-          if (is.fail() || is.eof()) \
-            throw Util::Error("error while scanning a multi-value"); \
-        } \
-      else \
-        { \
-          if (is.fail() && !is.eof()) \
-            throw Util::Error("error while scanning a multi-value"); \
-        } \
-    } \
+#define INSTANTIATE(T)                                          \
+                                                                \
+template <>                                                     \
+void TMultiValue<T>::printTo(STD_IO::ostream& os) const         \
+{                                                               \
+DOTRACE("TMultiValue<T>::printTo");                             \
+  const T* dat = constBegin();                                  \
+  const T* end = constEnd();                                    \
+                                                                \
+  while (dat < end)                                             \
+    {                                                           \
+      os << *dat;                                               \
+      if (++dat < end)                                          \
+        os << ' ';                                              \
+    }                                                           \
+}                                                               \
+                                                                \
+template <>                                                     \
+void TMultiValue<T>::scanFrom(STD_IO::istream& is)              \
+{                                                               \
+DOTRACE("TMultiValue<T>::scanFrom");                            \
+                                                                \
+  if (is.fail())                                                \
+    throw Util::Error("istream started out in fail state");     \
+  if (is.eof())                                                 \
+    throw Util::Error("istream started out in eof state");      \
+                                                                \
+  T* dat = mutableBegin();                                      \
+  T* end = mutableEnd();                                        \
+                                                                \
+  while (dat < end)                                             \
+    {                                                           \
+      is >> *dat;                                               \
+      dbgEvalNL(3, *dat);                                       \
+      ++dat;                                                    \
+      if (dat < end)                                            \
+        {                                                       \
+          if (is.fail() || is.eof())                            \
+            throw Util::Error("stream underflow "               \
+                              "while scanning a multi-value");  \
+        }                                                       \
+      else                                                      \
+        {                                                       \
+          if (is.fail() && !is.eof())                           \
+            throw Util::Error("stream error "                   \
+                              "while scanning a multi-value");  \
+        }                                                       \
+    }                                                           \
 }
 
 INSTANTIATE(double);
