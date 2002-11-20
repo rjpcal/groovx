2b548ebf4f1a8ac95a63b9e276674c4623eb9c26
Robert J. Peters <rjpcal@gmail.com>
2002-11-20 20:12:53 +0000

    Optimized Bezier4 a bit.

--- src/visx/bezier.h
+++ src/visx/bezier.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Tue Sep 21 09:51:40 1999
-// written: Wed Sep 25 19:00:54 2002
+// written: Wed Nov 20 12:12:02 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -226,21 +226,22 @@ double Bezier::evalDeriv(double u)
 class Bezier4
 {
 private:
-  fixed_block<double> R;        // control points
+  double R[4];        // control points
 
   // coefficients of the Bezier polynomial
   // p(u) = c0[0] + c0[1]*u + c0[2]*u^2 + c0[3]*u^3
-  fixed_block<double> c0;
+  double c0[4];
 
   // coefficients of the derivative polynomial
   // p'(u) = c1[0] + c1[1]*u + c1[2]*u^2
-  fixed_block<double> c1;
-
-  double extremum1;
-  double extremum2;
+  double c1[4];
 
   double valMin;
   double valMax;
+
+  bool haveMinMax;
+
+  void computeMinMax();
 
 public:
   /// Construct from four control points.
@@ -263,57 +264,63 @@ public:
 };
 
 Bezier4::Bezier4(double R0, double R1, double R2, double R3) :
-  R(4),
-  c0(4),
-  c1(3),
-  extremum1(0.0),
-  extremum2(0.0),
+  R(),
+  c0(),
+  c1(),
   valMin(0.0),
-  valMax(0.0)
+  valMax(0.0),
+  haveMinMax(false)
 {
   setCtrlPnts(R0, R1, R2, R3);
 }
 
-void Bezier4::setCtrlPnts(double R0, double R1, double R2, double R3)
+void Bezier4::computeMinMax()
 {
-  R[0] = R0;
-  R[1] = R1;
-  R[2] = R2;
-  R[3] = R3;
-
-  c0[0] =    R[0];
-  c0[1] = -3*R[0] + 3*R[1];
-  c0[2] =  3*R[0] - 6*R[1] + 3*R[2];
-  c0[3] =   -R[0] + 3*R[1] - 3*R[2] + R[3];
-
-  for (size_t i = 0; i < c1.size(); ++i)
-    {
-      c1[i] = (i+1) * c0[i+1];
-    }
-
   double b2_4ac = c1[1]*c1[1] - 4*c1[0]*c1[2];
+
+  using Util::max;
+  using Util::min;
+
+  valMin = min(eval(0.0), eval(1.0));
+  valMax = max(eval(0.0), eval(1.0));
 
   if (b2_4ac >= 0.0)
     {
       double part1 = -c1[1] / (2*c1[2]);
       double part2 = sqrt(b2_4ac) / (2*c1[2]);
 
-      extremum1 = Util::clamp(part1 + part2, 0.0, 1.0);
-      extremum2 = Util::clamp(part1 - part2, 0.0, 1.0);
-    }
-  else
-    {
-      extremum1 = 0.0;
-      extremum2 = 1.0;
+      double extremum1 = Util::clamp(part1 + part2, 0.0, 1.0);
+      double extremum2 = Util::clamp(part1 - part2, 0.0, 1.0);
+
+      double v1 = eval(extremum1);
+      double v2 = eval(extremum2);
+
+      if (v1 < valMin) valMin = v1; else if (v1 > valMax) valMax = v1;
+      if (v2 < valMin) valMin = v2; else if (v2 > valMax) valMax = v2;
     }
 
-  using Util::max;
-  using Util::min;
+  haveMinMax = true;
+}
 
-  valMin = min( min(eval(0.0), eval(1.0)),
-                min(eval(extremum1), eval(extremum2)) );
-  valMax = max( max(eval(0.0), eval(1.0)),
-                max(eval(extremum1), eval(extremum2)) );
+void Bezier4::setCtrlPnts(double R0, double R1, double R2, double R3)
+{
+  haveMinMax = false;
+
+  R[0] = R0;
+  R[1] = R1;
+  R[2] = R2;
+  R[3] = R3;
+
+  // Compute polynomial coefficients
+  c0[0] =    R[0];
+  c0[1] = -3*R[0] + 3*R[1];
+  c0[2] =  3*R[0] - 6*R[1] + 3*R[2];
+  c0[3] =   -R[0] + 3*R[1] - 3*R[2] + R[3];
+
+  // Compute polynomial coefficients of derivative
+  c1[0] = 1 * c0[1];
+  c1[1] = 2 * c0[2];
+  c1[2] = 3 * c0[3];
 }
 
 double Bezier4::eval(double u)
