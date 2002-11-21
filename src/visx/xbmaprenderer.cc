344aac35e6981f77280cb35afb97403d0811f349
Robert J. Peters <rjpcal@gmail.com>
2002-11-21 17:11:09 +0000

    Cleaned out XBmapRenderer interface.

--- src/visx/xbmaprenderer.cc
+++ src/visx/xbmaprenderer.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Wed Dec  1 17:22:34 1999
-// written: Sun Nov  3 13:41:11 2002
+// written: Thu Nov 21 09:09:34 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -22,98 +22,32 @@
 
 #include "util/error.h"
 
-#include <tk.h>
-#include <X11/Xlib.h>
-#include <GL/gl.h>
 #include <GL/glx.h>
 
 #include "util/trace.h"
 #include "util/debug.h"
 
-namespace
-{
-  bool x11_ready = false;
-
-  GC gfx_context_white, gfx_context_black;
-  Display* display;
-  Window win;
-  int screen;
-  Visual* visual;
-
-  Tk_Window theTkWin = 0;
-
-  void setupX11Stuff()
-  {
-  DOTRACE("{xbmaprenderer.cc}::setupX11Stuff");
-    if (x11_ready) return;
-
-    if (theTkWin == 0)
-      throw Util::Error(
-          "must call XBitmap::initClass before using an XBmapRenderer");
-
-    if (!Tk_IsMapped(reinterpret_cast<Tk_FakeWin *>(theTkWin)))
-      throw Util::Error(
-          "the main window must be mapped before using an XBmapRenderer");
-
-    display = Tk_Display(reinterpret_cast<Tk_FakeWin *>(theTkWin));
-    win = Tk_WindowId(reinterpret_cast<Tk_FakeWin *>(theTkWin));
-    screen = Tk_ScreenNumber(reinterpret_cast<Tk_FakeWin *>(theTkWin));
-
-    XGCValues gc_values;
-    gc_values.foreground = XWhitePixel(display,screen);
-    gc_values.background = XBlackPixel(display,screen);
-
-    unsigned long gc_valuemask = GCForeground | GCBackground;
-
-    gfx_context_white = XCreateGC(display,win,gc_valuemask,&gc_values);
-
-    gc_values.foreground = XBlackPixel(display,screen);
-    gc_values.background = XWhitePixel(display,screen);
-
-    gfx_context_black = XCreateGC(display,win,gc_valuemask,&gc_values);
-
-    XWindowAttributes xwa;
-    Status status = XGetWindowAttributes(display, win, &xwa);
-    if (status == 0)
-        throw Util::Error("couldn't get X11 window attributes");
-    visual = xwa.visual;
-
-    x11_ready = true;
-  }
-}
-
-void XBmapRenderer::initClass(Tk_Window tkwin)
-{
-DOTRACE("XBmapRenderer::initClass");
-  theTkWin = tkwin;
-}
-
-XBmapRenderer::XBmapRenderer() :
-  itsImage(0)
-{
-DOTRACE("XBmapRenderer::XBmapRenderer");
-}
-
-XBmapRenderer::~XBmapRenderer()
-{
-DOTRACE("XBmapRenderer::~XBmapRenderer");
-  if (itsImage) XFree(itsImage);
-}
-
-void XBmapRenderer::doRender(Gfx::Canvas& canvas,
+void XBmapRenderer::doRender(Display* display,
+                             Window win,
+                             int screen,
+                             Gfx::Canvas& canvas,
                              const Gfx::BmapData& data,
                              const Gfx::Vec2<double>& world_pos,
-                             const Gfx::Vec2<double>& /* zoom */) const
+                             const Gfx::Vec2<double>& /* zoom */)
 {
 DOTRACE("XBmapRenderer::doRender");
 
-  setupX11Stuff();
+  XWindowAttributes xwa;
+  Status status = XGetWindowAttributes(display, win, &xwa);
+  if (status == 0)
+    {
+      throw Util::Error("couldn't get X11 window attributes");
+    }
+  Visual* visual = xwa.visual;
 
-  // Update the cached image if necessary
-  update(data);
+  int format = (data.bitsPerPixel() == 1) ? XYBitmap : ZPixmap;
 
-  // Check if we have an image to display
-  if (itsImage == NULL) return;
+  data.setRowOrder(Gfx::BmapData::TOP_FIRST);
 
   // Calculate GL window coordinates of lower left corner of image
   Gfx::Vec2<int> screen_pos = canvas.screenFromWorld(world_pos);
@@ -127,42 +61,42 @@ DOTRACE("XBmapRenderer::doRender");
   // Calculate X11 window coordinates for upper left corner of image
   // (X11 coordinates put 0 at the top of the window, while GL
   // coordinates put 0 at the bottom of the window)
-  XWindowAttributes xwa;
-  XGetWindowAttributes(display, win, &xwa);
   screen_pos.y() = xwa.height - screen_pos.y();
+
+  // Create the XImage
+  XImage* image = XCreateImage(display, visual,
+                               data.bitsPerPixel(), /* bit depth */
+                               format, /* format = XYBitmap, XYPixmap, ZPixmap */
+                               0, /* offset */
+                               reinterpret_cast<char*>(data.bytesPtr()),
+                               data.width(), data.height(),
+                               data.byteAlignment()*8, /* bitmap_pad */
+                               0); /* bytes_per_line */
+
+  if (image == NULL)
+    {
+      throw Util::Error("couldn't create an XImage");
+    }
+
+  // Create a graphics context
+  XGCValues gc_values;
+
+  gc_values.foreground = XBlackPixel(display, screen);
+  gc_values.background = XWhitePixel(display, screen);
+
+  unsigned long gc_valuemask = GCForeground | GCBackground;
+
+  GC gfx_context_black = XCreateGC(display, win, gc_valuemask, &gc_values);
 
   // Draw the image
   glXWaitGL();
-  XPutImage(display, win, gfx_context_black, itsImage,
+  XPutImage(display, win, gfx_context_black, image,
             0, 0, screen_pos.x(), screen_pos.y(),
             data.width(), data.height());
   glXWaitX();
-}
 
-void XBmapRenderer::update(const Gfx::BmapData& data) const
-{
-DOTRACE("XBmapRenderer::update");
-
-  setupX11Stuff();
-
-  if (itsImage)
-    {
-      XFree(itsImage);
-      itsImage = 0;
-    }
-
-  int format = (data.bitsPerPixel() == 1) ? XYBitmap : ZPixmap;
-
-  data.setRowOrder(Gfx::BmapData::TOP_FIRST);
-
-  itsImage = XCreateImage(display, visual,
-                          data.bitsPerPixel(), /* bit depth */
-                          format, /* format = XYBitmap, XYPixmap, ZPixmap */
-                          0, /* offset */
-                          reinterpret_cast<char*>(data.bytesPtr()),
-                          data.width(), data.height(),
-                          data.byteAlignment()*8, /* bitmap_pad */
-                          0); /* bytes_per_line */
+  XFreeGC(display, gfx_context_black);
+  XFree(image);
 }
 
 static const char vcid_xbmaprenderer_cc[] = "$Header$";
--- src/visx/xbmaprenderer.h
+++ src/visx/xbmaprenderer.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Wed Dec  1 17:19:23 1999
-// written: Wed Nov 20 17:19:44 2002
+// written: Thu Nov 21 09:05:22 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -13,46 +13,25 @@
 #ifndef XBMAPRENDERER_H_DEFINED
 #define XBMAPRENDERER_H_DEFINED
 
-struct _XImage;
-typedef _XImage XImage;
-
-struct Tk_Window_;
-typedef Tk_Window_* Tk_Window;
+#include <X11/Xlib.h>
 
 namespace Gfx
 {
   class BmapData;
   class Canvas;
-  template <class V> class Rect;
   template <class V> class Vec2;
 }
 
 /// XBmapRenderer is "in between jobs"... needs to be merged into GLCanvas.
-class XBmapRenderer
+namespace XBmapRenderer
 {
-public:
-  /** Initializes some X11 structures. Must be called before any \c
-      XBmapRenderer objects are rendered. */
-  static void initClass(Tk_Window win);
-
-  /// Default constructor.
-  XBmapRenderer();
-
-  /// Virtual destructor.
-  virtual ~XBmapRenderer();
-
-  virtual void doRender(Gfx::Canvas& canvas,
-                        const Gfx::BmapData& data,
-                        const Gfx::Vec2<double>& world_pos,
-                        const Gfx::Vec2<double>& zoom) const;
-
-private:
-  void update(const Gfx::BmapData& data) const;
-
-  XBmapRenderer(const XBmapRenderer&);
-  XBmapRenderer& operator=(const XBmapRenderer&);
-
-  mutable XImage* itsImage;
+   void doRender(Display* display,
+                 Window win,
+                 int screen,
+                 Gfx::Canvas& canvas,
+                 const Gfx::BmapData& data,
+                 const Gfx::Vec2<double>& world_pos,
+                 const Gfx::Vec2<double>& zoom);
 };
 
 static const char vcid_xbmaprenderer_h[] = "$Header$";
