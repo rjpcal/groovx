bd38e9d6efba94d0684ceef0868db9a704e75e57
Robert J. Peters <rjpcal@gmail.com>
2002-11-25 19:28:42 +0000

    Added isItDestroyed flag to allow VolatileObject to be used correctly as a stack object in addition to a heap object.

--- src/util/volatileobject.cc
+++ src/util/volatileobject.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Tue Aug 21 17:17:51 2001
-// written: Fri Jan 18 16:07:03 2002
+// written: Mon Nov 25 11:16:22 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -18,7 +18,8 @@
 #include "util/trace.h"
 #include "util/debug.h"
 
-Util::VolatileObject::VolatileObject()
+Util::VolatileObject::VolatileObject() :
+  isItDestroyed(false)
 {
 DOTRACE("Util::VolatileObject::VolatileObject");
   this->incrRefCount();
@@ -27,12 +28,24 @@ DOTRACE("Util::VolatileObject::VolatileObject");
 Util::VolatileObject::~VolatileObject()
 {
 DOTRACE("Util::VolatileObject::~VolatileObject");
+
+  // If this destructor was not called via destroy(), then it means we
+  // probably have a stack object here. In order to keep weak references
+  // doing the right thing, we need to make sure that we decrement the ref
+  // count here.
+  if (!isItDestroyed)
+    {
+      Assert(refCount() == 1);
+      this->decrRefCountNoDelete();
+    }
 }
 
 void Util::VolatileObject::destroy()
 {
 DOTRACE("Util::VolatileObject::destroy");
   Assert(refCount() == 1);
+
+  isItDestroyed = true;
 
   this->decrRefCount();
 }
--- src/util/volatileobject.h
+++ src/util/volatileobject.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Tue Aug 21 17:16:15 2001
-// written: Wed Sep 25 19:00:42 2002
+// written: Mon Nov 25 11:10:58 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -26,6 +26,8 @@ namespace Util
     windowing system, for example). */
 class Util::VolatileObject : public virtual Util::Object
 {
+  bool isItDestroyed; // To keep track of whether destroy() was called yet.
+
 public:
   VolatileObject();
   virtual ~VolatileObject();
