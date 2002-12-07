a62e76ebd8762d7df6f05f880e8818f423dffbc3
Robert J. Peters <rjpcal@gmail.com>
2002-12-07 06:23:30 +0000

    Reimplemented using snprintf().

--- src/util/tostring.cc
+++ src/util/tostring.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Sun Aug  5 20:12:30 2001
-// written: Fri Feb  1 10:57:49 2002
+// written: Fri Dec  6 22:23:11 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -15,34 +15,25 @@
 
 #include "util/tostring.h"
 
+#include <cstdio>
 #include <cstring>
-#include <strstream.h>
-
-#include "util/trace.h"
 
 namespace
 {
-  static char num2str_buf[64];
-
-  template <class T>
-  const char* num2str_impl(const T& x)
-  {
-    ostrstream ost(num2str_buf, 30);
-    ost << x << '\0';
-    return &num2str_buf[0];
-  }
+  const int SZ = 64;
+  static char buf[SZ];
 }
 
 Util::CharData::CharData(const char* t) :
   text(t), len(t ? strlen(t) : 0)
 {}
 
-const char* Util::num2str(bool x) { return num2str_impl(x); }
-const char* Util::num2str(int x) { return num2str_impl(x); }
-const char* Util::num2str(unsigned int x) { return num2str_impl(x); }
-const char* Util::num2str(long x) { return num2str_impl(x); }
-const char* Util::num2str(unsigned long x) { return num2str_impl(x); }
-const char* Util::num2str(double x) { return num2str_impl(x); }
+const char* Util::num2str(bool x)         { snprintf(buf, SZ, "%d", int(x)); return buf; }
+const char* Util::num2str(int x)          { snprintf(buf, SZ, "%d", x); return buf; }
+const char* Util::num2str(unsigned int x) { snprintf(buf, SZ, "%u", x); return buf; }
+const char* Util::num2str(long x)         { snprintf(buf, SZ, "%ld", x); return buf; }
+const char* Util::num2str(unsigned long x){ snprintf(buf, SZ, "%lu", x); return buf; }
+const char* Util::num2str(double x)       { snprintf(buf, SZ, "%g", x); return buf; }
 
 static const char vcid_tostring_cc[] = "$Header$";
 #endif // !TOSTRING_CC_DEFINED
