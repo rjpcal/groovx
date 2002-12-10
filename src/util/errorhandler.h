65eff8fcd60d38cc638ca9a38cadb43cdf68dc30
Robert J. Peters <rjpcal@gmail.com>
2002-12-10 21:15:58 +0000

    Added ThrowingErrorHandler.

--- src/util/errorhandler.h
+++ src/util/errorhandler.h
@@ -5,7 +5,7 @@
 // Copyright (c) 1998-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Thu May 11 10:15:41 2000
-// written: Wed Jun 26 11:23:21 2002
+// written: Tue Dec 10 12:40:31 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -18,15 +18,23 @@ namespace Util
   class Error;
   class ErrorHandler;
   class NullErrorHandler;
+  class ThrowingErrorHandler;
 }
 
 /// Abstract interface for error-handlers.
 class Util::ErrorHandler
 {
-public:
+  ErrorHandler(const ErrorHandler&);
+  ErrorHandler& operator=(const ErrorHandler&);
+
+protected:
+  /// Default constructor.
+  ErrorHandler();
+
   /// Virtual destructor.
   virtual ~ErrorHandler();
 
+public:
   /// Handle an error about which no information is known.
   virtual void handleUnknownError() = 0;
 
@@ -40,9 +48,40 @@ public:
 /// Null error handler simply ignores all errors.
 class Util::NullErrorHandler : public Util::ErrorHandler
 {
-public:
+protected:
+  /// Default constructor.
+  NullErrorHandler();
+
   /// Virtual destructor.
   virtual ~NullErrorHandler();
+
+public:
+  /// Singleton accessor.
+  static NullErrorHandler* get();
+
+  /// Ignore the error.
+  virtual void handleUnknownError();
+
+  /// Ignore the error.
+  virtual void handleError(Util::Error& err);
+
+  /// Ignore the error.
+  virtual void handleMsg(const char* msg);
+};
+
+/// Throwing error handler raises an exception (Util::Error) for all errors.
+class Util::ThrowingErrorHandler : public Util::ErrorHandler
+{
+protected:
+  /// Default constructor.
+  ThrowingErrorHandler();
+
+  /// Virtual destructor.
+  virtual ~ThrowingErrorHandler();
+
+public:
+  /// Singleton accessor.
+  static ThrowingErrorHandler* get();
 
   /// Ignore the error.
   virtual void handleUnknownError();
