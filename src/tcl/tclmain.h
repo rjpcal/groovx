81806998bfe1a18759afffaa6ac3d37f79750472
Robert J. Peters <rjpcal@gmail.com>
2002-12-10 21:26:57 +0000

    Pulled forward decl.

--- src/tcl/tclmain.h
+++ src/tcl/tclmain.h
@@ -5,15 +5,13 @@
 // Copyright (c) 2002-2002 Rob Peters rjpeters@klab.caltech.edu
 //
 // created: Mon Jul 22 16:32:01 2002
-// written: Tue Dec 10 13:25:42 2002
+// written: Tue Dec 10 13:27:02 2002
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
 
 #ifndef TCLMAIN_H_DEFINED
 #define TCLMAIN_H_DEFINED
-
-struct Tcl_Interp;
 
 namespace Tcl
 {
