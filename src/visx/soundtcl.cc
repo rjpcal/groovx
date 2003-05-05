669d5ad4c8d54e7dfa909713a73e341cb5e87754
Robert J. Peters <rjpcal@gmail.com>
2003-05-05 23:54:16 +0000

    Clean up a bit.

--- src/visx/soundtcl.cc
+++ src/visx/soundtcl.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1999-2003 Rob Peters rjpeters at klab dot caltech dot edu
 //
 // created: Tue Apr 13 14:09:59 1999
-// written: Wed Mar 19 18:00:52 2003
+// written: Mon May  5 16:43:43 2003
 // $Id$
 //
 // --------------------------------------------------------------------
@@ -46,27 +46,7 @@
 #include "util/trace.h"
 #include "util/debug.h"
 
-//---------------------------------------------------------------------
-//
-// SoundTcl namespace --
-//
-//---------------------------------------------------------------------
-
-namespace SoundTcl
-{
-  const char* ok_sound_file = "/audio/saw50_500Hz_300ms.au";
-  const char* err_sound_file = "/audio/saw50_350Hz_300ms.au";
-
-  class SoundPkg;
-}
-
-//---------------------------------------------------------------------
-//
-// SoundTcl::SoundPkg --
-//
-//---------------------------------------------------------------------
-
-class SoundTcl::SoundPkg : public Tcl::Pkg
+class SoundPkg : public Tcl::Pkg
 {
 public:
   SoundPkg(const Tcl::Interp& interp) :
@@ -86,23 +66,20 @@ public:
         fstring lib_dir(Grsh::libraryDirectory());
         dbgEvalNL(3, lib_dir);
 
-        fstring full_ok_file(lib_dir);
-        full_ok_file.append(ok_sound_file);     dbgEvalNL(3, full_ok_file);
-
-        fstring full_err_file(lib_dir);
-        full_err_file.append(err_sound_file);     dbgEvalNL(3, full_err_file);
+        const fstring ok_file(lib_dir, "/audio/saw50_500Hz_300ms.au");
+        const fstring err_file(lib_dir, "/audio/saw50_350Hz_300ms.au");
 
         static int OK = -1;
         static int ERR = -1;
 
         try
           {
-            Ref<Sound> ok_sound(Sound::newPlatformSound(full_ok_file.c_str()));
+            Ref<Sound> ok_sound(Sound::newPlatformSound(ok_file.c_str()));
             Sound::setOkSound(ok_sound);
             OK = ok_sound.id();
             linkConstVar("Sound::ok", OK);
 
-            Ref<Sound> err_sound(Sound::newPlatformSound(full_err_file.c_str()));
+            Ref<Sound> err_sound(Sound::newPlatformSound(err_file.c_str()));
             Sound::setErrSound(err_sound);
             ERR = err_sound.id();
             linkConstVar("Sound::err", ERR);
@@ -139,7 +116,7 @@ extern "C" int Sound_Init(Tcl_Interp* interp)
 DOTRACE("Sound_Init");
 
   Tcl::Interp intp(interp);
-  Tcl::Pkg* pkg = new SoundTcl::SoundPkg(intp);
+  Tcl::Pkg* pkg = new SoundPkg(intp);
 
   Util::ObjFactory::theOne().registerCreatorFunc(&Sound::make);
 
