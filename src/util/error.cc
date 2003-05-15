86c2fba7a42c199aa64ec8d2f6de622b3d2fb10c
Robert J. Peters <rjpcal@gmail.com>
2003-05-15 23:24:15 +0000

    Removed unneeded #include.

--- src/util/error.cc
+++ src/util/error.cc
@@ -5,7 +5,7 @@
 // Copyright (c) 1999-2003 Rob Peters rjpeters at klab dot caltech dot edu
 //
 // created: Tue Jun 22 14:59:48 1999
-// written: Wed Mar 19 17:58:55 2003
+// written: Thu May 15 16:24:06 2003
 // $Id$
 //
 // --------------------------------------------------------------------
@@ -36,7 +36,6 @@
 #include "util/strings.h"
 
 #include <cstdlib>
-#include <exception>
 
 #include "util/trace.h"
 #include "util/debug.h"
