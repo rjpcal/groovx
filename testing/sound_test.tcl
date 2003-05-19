d53ca4182d19aee13903f27cff5195f47a21bec0
Robert J. Peters <rjpcal@gmail.com>
2003-05-19 22:24:43 +0000

    Updated for overhauled Sound class.

--- testing/sound_test.tcl
+++ testing/sound_test.tcl
@@ -7,14 +7,14 @@
 ###
 ##############################################################################
 
-if { [catch {package require Sound}] != 0 } { return }
+package require Sound
 
 # Check if audio is connected; if not, we skip the sound tests.
 #set AUDIO [Sound::haveAudio]
 set AUDIO 0   ;# disable sound tests
 
 set TEST_SOUND [Obj::new Sound]
-Sound::file $TEST_SOUND $::TEST_DIR/sound1.au
+-> $TEST_SOUND file $::TEST_DIR/sound1.au
 
 source ${::TEST_DIR}/io_test.tcl
 
@@ -25,59 +25,77 @@ IO::testReadCmd SoundTcl IO 1 $::TEST_SOUND
 
 ### Obj::new Sound ###
 test "SoundTcl-Obj::new Sound" "normal sound create" {
-    set ::TEST_SOUND [Obj::new Sound]
-    Sound::file $::TEST_SOUND $::TEST_DIR/sound1.au
-    expr {$::TEST_SOUND >= 0}
-} {^1$}
+    set s [new Sound]
+    delete $s
+} {^$}
 
 
-### Sound::fileCmd ###
-test "SoundTcl-Sound::file" "too few args" {
+### Sound::file ###
+test "Sound::file" "too few args" {
     Sound::file
 } {wrong \# args: should be}
-test "SoundTcl-Sound::file" "too many args" {
+
+test "Sound::file" "too many args" {
     Sound::file j u n
 } {wrong \# args: should be}
-test "SoundTcl-Sound::file" "normal filename set" {
-    catch {Sound::file $::TEST_SOUND $::TEST_DIR/sound1.au}
+
+test "Sound::file" "normal use" {
+    set s [new Sound]
+    -> $s file $::TEST_DIR/sound1.au
+    set f [-> $s file]
+    delete $s
+    expr [string compare $f "$::TEST_DIR/sound1.au"]
 } {^0$} $AUDIO
-test "SoundTcl-Sound::file" "normal filename query" {
-    Sound::file $::TEST_SOUND $::TEST_DIR/sound1.au
-    set f [Sound::file $::TEST_SOUND]
-    expr [string compare $f "$::TEST_DIR/sound1.au"] == 0
-} {^1$} $AUDIO
-test "SoundTcl-Sound::file" "error on bad filename" {
-    exec rm -rf $::TEST_DIR/nonexistent_file
-    Sound::file $::TEST_SOUND $::TEST_DIR/nonexistent_file
-} "^Sound::file:"
-test "SoundTcl-Sound::file" "error on junk file" {
-    Sound::file $::TEST_SOUND $::TEST_DIR/junk_text_file
-} "^Sound::file:" \
+
+### Sound::forceLoad ###
+
+test "Sound::forceLoad" "error on bad filename" {
+    set s [new Sound]
+    file delete $::TEST_DIR/nonexistent_file
+    -> $s file $::TEST_DIR/nonexistent_file
+    catch {-> $s forceLoad} result
+    delete $s
+    return $result
+} {^Sound::forceLoad:}
+
+test "Sound::forceLoad" "error on junk file" {
+    set s [new Sound]
+    -> $s file $::TEST_DIR/junk_text_file
+    catch {-> $s forceLoad} result
+    delete $s
+    return $result
+} {^Sound::forceLoad:} \
   [expr [string equal $env(ARCH) "irix6"] ? $skip_known_bug : $normal_test]
 
 ### Sound::playCmd ###
-test "SoundTcl-Sound::play" "too few args" {
+
+test "Sound::play" "too few args" {
     Sound::play
 } {wrong \# args: should be "Sound::play item_id\(s\)"}
-test "SoundTcl-Sound::play" "too many args" {
+
+test "Sound::play" "too many args" {
     Sound::play item junk
 } {wrong \# args: should be "Sound::play item_id\(s\)"}
-test "SoundTcl-Sound::play" "normal play test sound" {
-    Sound::file $::TEST_SOUND $::TEST_DIR/sound1.au
-    catch {Sound::play $::TEST_SOUND}
-} {^0$} $AUDIO
-test "SoundTcl-Sound::play" "normal play ok sound" {
-    catch {Sound::play $Sound::ok}
-} {^0$} $AUDIO
-test "SoundTcl-Sound::play" "normal play err sound" {
-    catch {Sound::play $Sound::err}
-} {^0$} $AUDIO
-test "SoundTcl-Sound::play" "error on bad sound id" {
-    Sound::play -1
-} {Sound::play: signed/unsigned conversion failed}
+
+test "Sound::play" "normal play test sound" {
+    set s [new Sound]
+    -> $s file $::TEST_DIR/sound1.au
+    -> $s play
+} {^$} $AUDIO
+
+test "Sound::play" "normal play ok sound" {
+    -> $Sound::ok play
+} {^$} $AUDIO
+
+test "Sound::play" "normal play err sound" {
+    -> $Sound::err play
+} {^$} $AUDIO
+
+test "Sound::play" "error on bad sound id" {
+    Sound::play 0
+} {^Sound::play: attempted to access invalid object}
 
 
 ### cleanup
 unset AUDIO
 unset TEST_SOUND
-
