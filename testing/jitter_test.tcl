eec2f2e381b8caaed85b06e65db528d12c81c1be
Robert J. Peters <rjpcal@gmail.com>
2003-05-24 20:28:53 +0000

    Updated error messages.

--- testing/jitter_test.tcl
+++ testing/jitter_test.tcl
@@ -19,33 +19,29 @@ IO::testReadCmd JitterTcl IO 1 $::JITTER
 unset ::JITTER
 
 ### Obj::new Jitter ###
-test "JitterTcl-Obj::new Jitter" "too many args" {
-	 Obj::new Jitter junk junk
+test "Obj::new Jitter" "too many args" {
+    new Jitter junk junk
 } {wrong \# args: should be}
-test "JitterTcl-Obj::new Jitter" "normal create" {
-	 Obj::new Jitter
+test "Obj::new Jitter" "normal create" {
+    new Jitter
 } {[0-9]+}
 
-set jit [Obj::new Jitter]
-
-### Jitter::setJitterCmd ###
-test "JitterTcl-Jitter::setJitter" "too few args" {
+### Jitter::setJitter ###
+test "Jitter::setJitter" "too few args" {
     Jitter::setJitter
 } {wrong \# args: should be "Jitter::setJitter posid x_jitter y_jitter r_jitter"}
-test "JitterTcl-Jitter::setJitter" "too many args" {
+test "Jitter::setJitter" "too many args" {
     Jitter::setJitter j u n k y
 } {wrong \# args: should be "Jitter::setJitter posid x_jitter y_jitter r_jitter"}
-test "JitterTcl-Jitter::setJitter" "normal use" {
-	 catch {Jitter::setJitter $::jit 1.0 1.0 1.0}
-} {^0$}
-test "JitterTcl-Jitter::setJitter" "error from non-numeric input" {
-    Jitter::setJitter $::jit junk 1.0 1.0
+test "Jitter::setJitter" "normal use" {
+    set jit [new Jitter]
+    -> $jit setJitter 1.0 1.0 1.0
+} {^$}
+test "Jitter::setJitter" "error from non-numeric input" {
+    set jit [new Jitter]
+    -> $jit setJitter junk 1.0 1.0
 } {expected floating-point number but got "junk"}
-test "JitterTcl-Jitter::setJitter" "error from wrong type" {
-    set p [Obj::new GxTransform]
-	 Jitter::setJitter $p 1 1 1
-} {^Jitter::setJitter: an error of type (std::)?bad_cast occurred:.*$}
-
-unset jit
-
-
+test "Jitter::setJitter" "error from wrong type" {
+    set p [new GxTransform]
+    Jitter::setJitter $p 1 1 1
+} {failed cast}
