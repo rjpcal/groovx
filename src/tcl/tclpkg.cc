40341dba0fa52015c2ab23761e407699c8c82ea3
Robert J. Peters <rjpcal@gmail.com>
2003-05-28 22:55:26 +0000

    Merged Tcl::PkgBase into Tcl::Pkg, since all clients were using Tcl::Pkg anyway.

--- src/tcl/tclpkg.cc
+++ src/tcl/tclpkg.cc
@@ -32,21 +32,349 @@
 
 #include "tcl/tclpkg.h"
 
+#include "tcl/tclcmd.h"
+#include "tcl/tclsafeinterp.h"
+
+#include "util/pointers.h"
+#include "util/slink_list.h"
+#include "util/strings.h"
+
+#include <tcl.h>
+#include <cctype>
+#include <typeinfo>
+#include <string>
+
 #include "util/trace.h"
 #include "util/debug.h"
 DBG_REGISTER;
 
+namespace
+{
+  // Construct a capitalization-correct version of the given name that is
+  // just how Tcl likes it: first character uppercase, all others
+  // lowercase.
+  std::string makeCleanPkgName(const std::string& name)
+  {
+    std::string clean = name;
+
+    clean[0] = char(toupper(clean[0]));
+
+    for (size_t i = 1; i < clean.length(); ++i)
+      {
+        clean[i] = char(tolower(clean[i]));
+      }
+
+    return clean;
+  }
+
+  std::string makeCleanVersionString(const char* ver)
+  {
+    std::string result = ver ? ver : "";
+
+    // Fix up version to remove any extraneous char's (such as
+    // version-control info, where we'd get a string like
+    //   '$Revision 2.8 $'
+
+    result.erase(0, result.find_first_of("0123456789"));
+    result.erase(result.find_last_of("0123456789")+1, std::string::npos);
+
+    return result;
+  }
+}
+
 ///////////////////////////////////////////////////////////////////////
 //
-// Pkg member definitions
+// Helper functions that provide typesafe access to Tcl_LinkVar
 //
 ///////////////////////////////////////////////////////////////////////
 
-Tcl::Pkg::Pkg(Tcl_Interp* interp, const char* name, const char* version) :
-  Tcl::PkgBase(interp, name, version)
-{}
+struct Tcl::Pkg::Impl
+{
+private:
+  Impl(const Impl&);
+  Impl& operator=(const Impl&);
 
-Tcl::Pkg::~Pkg() throw() {}
+public:
+  Impl(Tcl_Interp* interp, const char* name, const char* version);
+
+  ~Impl() throw();
+
+  Tcl::Interp interp;
+  slink_list<shared_ptr<Command> > cmds;
+  const std::string namespName;
+  const std::string pkgName;
+  const std::string version;
+
+  int initStatus;
+
+  slink_list<shared_ptr<int> > ownedInts;
+  slink_list<shared_ptr<double> > ownedDoubles;
+
+  ExitCallback* exitCallback;
+
+  static void exitHandler(ClientData clientData)
+  {
+    DOTRACE("Tcl::Pkg-exitHandler");
+    Tcl::Pkg* pkg = static_cast<Tcl::Pkg*>(clientData);
+    dbgEvalNL(3, pkg->namespName());
+    delete pkg;
+  }
+};
+
+Tcl::Pkg::Impl::Impl(Tcl_Interp* intp,
+                     const char* name, const char* vers) :
+  interp(intp),
+  cmds(),
+  namespName(name ? name : ""),
+  pkgName(makeCleanPkgName(namespName)),
+  version(makeCleanVersionString(vers)),
+  initStatus(TCL_OK),
+  ownedInts(),
+  ownedDoubles(),
+  exitCallback(0)
+{
+DOTRACE("Tcl::Pkg::Impl::Impl");
+}
+
+Tcl::Pkg::Impl::~Impl() throw()
+{
+DOTRACE("Tcl::Pkg::Impl::~Impl");
+  if (exitCallback != 0)
+    exitCallback();
+}
+
+Tcl::Pkg::Pkg(Tcl_Interp* interp,
+              const char* name, const char* version) :
+  rep(new Impl(interp, name, version))
+{
+DOTRACE("Tcl::Pkg::Pkg");
+
+  if ( !rep->pkgName.empty() && !rep->version.empty() )
+    {
+      Tcl_PkgProvideEx(interp, rep->pkgName.c_str(), rep->version.c_str(),
+                       static_cast<ClientData>(this));
+    }
+
+  Tcl_CreateExitHandler(&Impl::exitHandler, static_cast<ClientData>(this));
+}
+
+Tcl::Pkg::~Pkg() throw()
+{
+DOTRACE("Tcl::Pkg::~Pkg");
+  delete rep;
+}
+
+void Tcl::Pkg::onExit(ExitCallback* callback)
+{
+DOTRACE("Tcl::Pkg::onExit");
+  rep->exitCallback = callback;
+}
+
+Tcl::Pkg* Tcl::Pkg::lookup(Tcl_Interp* interp, const char* name,
+                           const char* version) throw()
+{
+DOTRACE("Tcl::Pkg::lookup");
+
+  ClientData clientData = 0;
+
+  std::string cleanName = makeCleanPkgName(name);
+
+  const char* ver =
+    Tcl_PkgRequireEx(interp, cleanName.c_str(), version, 0, &clientData);
+
+  if (ver != 0)
+    {
+      Tcl::Pkg* result = static_cast<Tcl::Pkg*>(clientData);
+
+      result = dynamic_cast<Tcl::Pkg*>(result);
+
+      return result;
+    }
+
+  return 0;
+}
+
+int Tcl::Pkg::initStatus() const throw()
+{
+DOTRACE("Tcl::Pkg::initStatus");
+  if (rep->interp.getResult<const char*>()[0] != '\0')
+    {
+      rep->initStatus = TCL_ERROR;
+    }
+  return rep->initStatus;
+}
+
+Tcl::Interp& Tcl::Pkg::interp() throw()
+{
+DOTRACE("Tcl::Pkg::interp");
+  return rep->interp;
+}
+
+namespace
+{
+  void exportAll(Tcl::Interp& interp, const char* from)
+  {
+  DOTRACE("exportAll");
+    fstring cmd("namespace eval ", from, " { namespace export * }");
+
+    interp.eval(cmd);
+  }
+
+  void exportInto(Tcl::Interp& interp, const char* from, const char* to)
+  {
+  DOTRACE("exportInto");
+    fstring cmd("namespace eval ", to, " { namespace import ::");
+    cmd.append(from, "::* }");
+
+    interp.eval(cmd);
+  }
+}
+
+void Tcl::Pkg::namespaceAlias(const char* namesp) throw()
+{
+DOTRACE("Tcl::Pkg::namespaceAlias");
+
+  try
+    {
+      exportAll(rep->interp, rep->namespName.c_str());
+      exportInto(rep->interp, rep->namespName.c_str(), namesp);
+    }
+  catch (...)
+    {
+      rep->interp.handleLiveException("Tcl::Pkg::namespaceAlias", false);
+    }
+}
+
+void Tcl::Pkg::inherit(const char* namesp) throw()
+{
+DOTRACE("Tcl::Pkg::inherit");
+
+  try
+    {
+      exportAll(rep->interp, namesp);
+      exportInto(rep->interp, namesp, rep->namespName.c_str());
+    }
+  catch (...)
+    {
+      rep->interp.handleLiveException("Tcl::Pkg::inherit", false);
+    }
+}
+
+void Tcl::Pkg::inheritPkg(const char* name, const char* version) throw()
+{
+DOTRACE("Tcl::Pkg::inheritPkg");
+
+  Tcl::Pkg* other = lookup(rep->interp.intp(), name, version);
+
+  if (other != 0)
+    {
+      inherit(other->namespName());
+    }
+}
+
+const char* Tcl::Pkg::namespName() throw()
+{
+  return rep->namespName.c_str();
+}
+
+const char* Tcl::Pkg::pkgName() throw()
+{
+  return rep->pkgName.c_str();
+}
+
+const char* Tcl::Pkg::version() throw()
+{
+  return rep->version.c_str();
+}
+
+const char* Tcl::Pkg::makePkgCmdName(const char* cmd_name_cstr)
+{
+DOTRACE("Tcl::Pkg::makePkgCmdName");
+  std::string cmd_name(cmd_name_cstr);
+
+  // Look for a namespace qualifier "::" -- if there is already one,
+  // then we assume the caller has something special in mind -- if
+  // there is not one, then we do the default thing and prepend the
+  // package name as a namespace qualifier.
+  if (cmd_name.find("::") != std::string::npos)
+    {
+      return cmd_name_cstr;
+    }
+  else
+    {
+      static std::string name;
+      name = namespName();
+      name += "::";
+      name += cmd_name;
+      return name.c_str();
+    }
+}
+
+void Tcl::Pkg::eval(const char* script) throw()
+{
+DOTRACE("Tcl::Pkg::eval");
+
+  try
+    {
+      rep->interp.eval(script);
+    }
+  catch(...)
+    {
+      rep->interp.handleLiveException("Tcl::Pkg::eval", false);
+    }
+}
+
+void Tcl::Pkg::addCommand(Command* cmd)
+{
+DOTRACE("Tcl::Pkg::addCommand");
+  rep->cmds.push_front(shared_ptr<Command>(cmd));
+}
+
+void Tcl::Pkg::linkVar(const char* varName, int& var)
+{
+DOTRACE("Tcl::Pkg::linkVar int");
+  rep->interp.linkInt(varName, &var, false);
+}
+
+void Tcl::Pkg::linkVar(const char* varName, double& var)
+{
+DOTRACE("Tcl::Pkg::linkVar double");
+  rep->interp.linkDouble(varName, &var, false);
+}
+
+void Tcl::Pkg::linkVarCopy(const char* varName, int var)
+{
+DOTRACE("Tcl::Pkg::linkVarCopy int");
+  shared_ptr<int> copy(new int(var));
+  rep->ownedInts.push_front(copy);
+  rep->interp.linkInt(varName, copy.get(), true);
+}
+
+void Tcl::Pkg::linkVarCopy(const char* varName, double var)
+{
+DOTRACE("Tcl::Pkg::linkVarCopy double");
+  shared_ptr<double> copy(new double(var));
+  rep->ownedDoubles.push_front(copy);
+  rep->interp.linkDouble(varName, copy.get(), true);
+}
+
+void Tcl::Pkg::linkConstVar(const char* varName, int& var)
+{
+DOTRACE("Tcl::Pkg::linkConstVar int");
+  rep->interp.linkInt(varName, &var, true);
+}
+
+void Tcl::Pkg::linkConstVar(const char* varName, double& var)
+{
+DOTRACE("Tcl::Pkg::linkConstVar double");
+  rep->interp.linkDouble(varName, &var, true);
+}
+
+void Tcl::Pkg::setInitStatusError() throw()
+{
+DOTRACE("Tcl::Pkg::setInitStatusError");
+  rep->initStatus = TCL_ERROR;
+}
 
 const char* Tcl::Pkg::actionUsage(const char* usage)
 {
--- src/tcl/tclpkg.h
+++ src/tcl/tclpkg.h
@@ -31,28 +31,135 @@
 #define TCLPKG_H_DEFINED
 
 #include "tcl/tclfunctor.h"
-#include "tcl/tclpkgbase.h"
+
+struct Tcl_Interp;
 
 namespace Tcl
 {
+  class Command;
+  class Interp;
   class Pkg;
 }
 
 ///////////////////////////////////////////////////////////////////////
+//
+// Tcl::Pkg class definition
+//
+///////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////
 /**
  *
- * Tcl::Pkg extends Tcl::PkgBase by providing a set of functions to
- * define Tcl commands from C++ functors.
+ * \c Tcl::Pkg is a class more managing groups of related \c
+ * Tcl::Command's. It provides several facilities:
+ *
+ *   -- stores a list of \c Command's, and ensures that these are
+ *   properly destroyed upon exit from Tcl
+ *
+ *   -- ensures that the package is provided to Tcl so that other
+ *   packages can query for its presence
+ *
+ *   -- provides a set of functions to define Tcl commands from C++ functors
  *
  **/
 ///////////////////////////////////////////////////////////////////////
 
-class Tcl::Pkg : public PkgBase
+class Tcl::Pkg
 {
 public:
-  Pkg(Tcl_Interp* interp, const char* pkg_name, const char* version);
+  /** Construct a \c Tcl::Pkg with a Tcl interpreter, package
+      name, and package version. The version string should be in the
+      form MM.mm where MM is major version, and mm is minor
+      version. This constructor can also correctly parse a version
+      string such as given by the RCS revision tag. */
+  Pkg(Tcl_Interp* interp, const char* name, const char* version);
 
+private:
+  /** Virtual destructor ensures proper destruction of base
+      classes. This destructor will destroy all \c Command's owned by
+      the package. */
   virtual ~Pkg() throw();
+
+public:
+  typedef void (ExitCallback)();
+
+  /// Specify a function to be called when the package is destroyed.
+  /** (Package destruction typically occurs at application exit, when the
+      Tcl interpreter and all associated objects are destroyed.) */
+  void onExit(ExitCallback* callback);
+
+  /// Find a package given its name and version.
+  /** If the package is not already loaded, this function will attempt to
+      "require" the package. If a null pointer is passed to version (the
+      default), then any version will be acceptable. If no suitable package
+      cannot be found or loaded, a null pointer will be returned. */
+  static Pkg* lookup(Tcl_Interp* interp,
+                     const char* name, const char* version = 0) throw();
+
+  /** Returns a Tcl status code indicating whether the package
+      initialization was successful. */
+  int initStatus() const throw();
+
+  /// Mark the package as having failed its initialization.
+  void setInitStatusError() throw();
+
+  /// Returns the Tcl interpreter that was passed to the constructor.
+  Tcl::Interp& interp() throw();
+
+  /// Returns the package's "namespace name".
+  /** Note that the "namespace name" will be the same as the "package name"
+      except possibly for capitalization. The "namespace name" is the name
+      of the namespace that is used as the default prefix all commands
+      contained in the package. */
+  const char* namespName() throw();
+
+  /// Return the package's "package name".
+  /** Note that the "package name" will be the same as the "namespace name"
+      except possibly for capitalization. The "package name" is the name
+      that is passed to Tcl_PkgProvide() and Tcl_PkgProvide(), and has a
+      well-defined capitalization scheme: first character uppercase, all
+      remaining letters lowercase. */
+  const char* pkgName() throw();
+
+  /// Returns the package version string.
+  const char* version() throw();
+
+  /** Causes all of our package's currently defined commands and procedures to
+      be imported into the specified other namespace. */
+  void namespaceAlias(const char* namesp) throw();
+
+  /** Import all of the commands and procedures defined in the specified
+      namespace into our own package namespace. */
+  void inherit(const char* namesp) throw();
+
+  /// Import all of the commands and procedures defined in the named pkg.
+  /** If the named pkg has not yet been loaded, this function will attempt
+      to load it via loookup(). If a null pointer is passed to version (the
+      default), then any version will be acceptable. */
+  void inheritPkg(const char* name, const char* version = 0) throw();
+
+  /// Does a simple \c Tcl_Eval of \a script using the package's \c Tcl_Interp.
+  void eval(const char* script) throw();
+
+  /// Links the \a var with the Tcl variable \a varName.
+  void linkVar(const char* varName, int& var);
+  /// Links \a var with the Tcl variable \a varName.
+  void linkVar(const char* varName, double& var);
+
+  /** Links a copy of \a var with the Tcl variable \a varName. The Tcl
+      variable will be read-only.*/
+  void linkVarCopy(const char* varName, int var);
+  /** Links a copy of \a var with the Tcl variable \a varName. The Tcl
+      variable will be read-only.*/
+  void linkVarCopy(const char* varName, double var);
+
+  /** Links \a var with the Tcl variable \a varName. The Tcl variable
+      will be read_only. */
+  void linkConstVar(const char* varName, int& var);
+  /** Links \a var with the Tcl variable \a varName. The Tcl variable
+      will be read_only. */
+  void linkConstVar(const char* varName, double& var);
+
 
   template <class Func>
   inline void def(const char* cmd_name, const char* usage, Func f)
@@ -132,10 +239,26 @@ public:
     defSetter( cmd_name, setterFunc );
   }
 
-protected:
+private:
+  Pkg(const Pkg&); // not implemented
+  Pkg& operator=(const Pkg&); // not implemented
+
+  /** Returns a namespace'd command name in the form of
+      pkg_name::cmd_name. The result of this function is valid only
+      until the next time it is called, so callers should make a copy
+      of the result. */
+  const char* makePkgCmdName(const char* cmd_name);
+
+  /// Adds \a cmd to the commands managed by the package.
+  void addCommand(Command* cmd);
+
   static const char* actionUsage(const char* usage);
   static const char* getterUsage(const char* usage);
   static const char* setterUsage(const char* usage);
+
+  struct Impl;
+  friend struct Impl;
+  Impl* rep;
 };
 
 static const char vcid_tclpkg_h[] = "$Header$";
