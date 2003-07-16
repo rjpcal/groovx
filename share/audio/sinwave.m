63ae85a20b5e225e6832b8b107be44d0478ad6fe
Robert J. Peters <rjpcal@gmail.com>
2003-07-16 20:49:36 +0000

    Added phase and amp parameters.

--- share/audio/sinwave.m
+++ share/audio/sinwave.m
@@ -1,6 +1,15 @@
-function snd = sinwave(freq, secs, sample_freq)
+function snd = sinwave(freq, secs, sample_freq, phase, amp)
+
+if nargin < 4
+  phase = 0;
+end
+
+if nargin < 5
+  amp = 1;
+end
 
 num_samples = secs * sample_freq;
-x = 0:(num_samples-1);
-x = x./sample_freq;
-snd = sin(2*pi*freq*x);
+t = (0:(num_samples-1)) ./ sample_freq;
+snd = amp .* sin(phase + 2*pi*freq*t);
+snd(snd > 1) = 1;
+snd(snd < -1) = -1;
