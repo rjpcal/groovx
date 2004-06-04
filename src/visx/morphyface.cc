e2dfa2e97cf83d271507a47e62a60a3b7fb4454d
Robert J. Peters <rjpcal@gmail.com>
2004-06-04 00:11:55 +0000

    Moved LineStrip into separate source files.

--- src/visx/morphyface.cc
+++ src/visx/morphyface.cc
@@ -34,6 +34,7 @@
 
 #include "gfx/canvas.h"
 #include "gfx/gxscaler.h"
+#include "gfx/linestrip.h"
 
 #include "gx/bezier4.h"
 #include "gx/bbox.h"
@@ -45,8 +46,6 @@
 #include "io/writer.h"
 
 #include "util/algo.h"
-
-#include <vector>
 
 #include "util/trace.h"
 #include "util/debug.h"
@@ -130,123 +129,6 @@ namespace
         canvas.vertex2(Vec2d(vertices[4*i+2], vertices[4*i+3]));
       }
   }
-
-  class LineStrip
-  {
-  private:
-    Gfx::Canvas* canvas;
-    std::vector<Vec2d> pts;
-    double width;
-    bool join;
-
-  public:
-    LineStrip() :
-      canvas(0),
-      pts(0),
-      width(1.0),
-      join(false)
-    {}
-
-    void begin(Gfx::Canvas& c, double w, bool j)
-    {
-      Assert(pts.size() == 0);
-
-      canvas = &c;
-      width = w;
-      join = j;
-    }
-
-    void vertex(const Vec2d& pt)
-    {
-      pts.push_back(pt);
-    }
-
-    void drawBezier4(const Vec3d& p1,
-                     const Vec3d& p2,
-                     const Vec3d& p3,
-                     const Vec3d& p4,
-                     unsigned int subdivisions,
-                     unsigned int start = 0)
-    {
-      Bezier4 xb(p1.x(), p2.x(), p3.x(), p4.x());
-      Bezier4 yb(p1.y(), p2.y(), p3.y(), p4.y());
-      //     Bezier4 zb(p1.z(), p2.z(), p3.z(), p4.z());
-
-      for (unsigned int i = start; i < subdivisions; ++i)
-        {
-          double u = double(i) / double(subdivisions - 1);
-          this->vertex(Vec2d(xb.eval(u), yb.eval(u)));
-        }
-    }
-
-    void end()
-    {
-      if (pts.size() == 0) return;
-
-      Assert(canvas != 0);
-
-      if (join)
-        drawJoinedLineStrip();
-      else
-        drawSimpleLineStrip();
-
-      canvas = 0;
-      pts.clear();
-    }
-
-  private:
-    void drawSimpleLineStrip()
-    {
-      canvas->beginLineStrip();
-      for (unsigned int i = 0; i < pts.size(); ++i)
-        {
-          canvas->vertex2(pts[i]);
-        }
-      canvas->end();
-    }
-
-    void drawJoinedLineStrip()
-    {
-      canvas->beginQuadStrip();
-
-      Vec2d n1;
-      Vec2d d1;
-
-      for (unsigned int i = 1; i < pts.size(); ++i)
-        {
-          const Vec2d d2 = makeUnitLength(pts[i] - pts[i-1]);
-          const Vec2d n2 = normalTo(d2);
-
-          if (i == 1)
-            {
-              n1 = n2;
-              d1 = d2;
-            }
-
-          double c = 0.0;
-          if      (d1.x()+d2.x() != 0.0) { c = (n2.x() - n1.x()) / (d1.x() + d2.x()); }
-          else if (d1.y()+d2.y() != 0.0) { c = (n2.y() - n1.y()) / (d1.y() + d2.y()); }
-
-          const Vec2d use_n(n2 - d2 * c);
-
-          canvas->vertex2(pts[i-1] - use_n*width);
-          canvas->vertex2(pts[i-1] + use_n*width);
-
-          n1 = n2;
-          d1 = d2;
-        }
-
-      const Vec2d pt1(pts.back().x() - width*n1.x(),
-                      pts.back().y() - width*n1.y());
-      const Vec2d pt2(pts.back().x() + width*n1.x(),
-                      pts.back().y() + width*n1.y());
-
-      canvas->vertex2(pt1);
-      canvas->vertex2(pt2);
-
-      canvas->end();
-    }
-  };
 
 }
 
@@ -416,7 +298,7 @@ DOTRACE("MorphyFace::grRender");
 
   Gfx::AttribSaver attribSaver(canvas);
 
-//   canvas.enableAntialiasing();
+  //canvas.enableAntialiasing();
 
   LineStrip ls;
 
