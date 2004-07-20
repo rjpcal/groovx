f564fefaf571d941044d4ba2905d11d55f01dac7
Robert J. Peters <rjpcal@gmail.com>
2004-07-20 03:07:06 +0000

    Moved GlxAttribs into a private namespace in glxwrapper.cc as AttribList.

--- src/gfx/glxwrapper.cc
+++ src/gfx/glxwrapper.cc
@@ -32,7 +32,6 @@
 
 #include "glxwrapper.h"
 
-#include "gfx/glxattribs.h"
 #include "gfx/glxopts.h"
 
 #include "util/error.h"
@@ -46,6 +45,140 @@ namespace
 {
   // For caching the latest subject of GlxWrapper::makeCurrent()
   const GlxWrapper* currentGlxWrapper = 0;
+
+  // Builds a formatted data array for constructing a GLX context.
+  class AttribList
+  {
+  private:
+    enum { MAXSIZE = 1000 };
+
+    void push(int v)
+    {
+      Assert(next < MAXSIZE);
+      data[next++] = v;
+    }
+
+    void cap()
+    {
+      data[next] = None;
+    }
+
+    int data[MAXSIZE];
+    int next;
+
+  public:
+    /// Default constructor.
+    AttribList(const GlxOpts& opts) : next(0)
+    {
+      push( GLX_USE_GL );
+
+      if (opts.rgbaFlag)        this->rgba(opts.rgbaRed, opts.rgbaGreen, opts.rgbaBlue,
+                                           opts.alphaFlag ? opts.alphaSize : -1);
+
+      else                      this->colorIndex( opts.colorIndexSize );
+
+      if (opts.depthFlag)       this->depthBuffer( opts.depthSize );
+
+      if (opts.doubleFlag)      this->doubleBuffer();
+
+      if (opts.stencilFlag)     this->stencilBuffer( opts.stencilSize );
+
+      if (opts.accumFlag)       this->accum(opts.accumRed, opts.accumGreen, opts.accumBlue,
+                                            opts.alphaFlag ? opts.accumAlpha : -1);
+
+      if (opts.auxNumber > 0)   this->auxBuffers( opts.auxNumber );
+
+      if (opts.level != 0)      this->level( opts.level );
+
+      if (opts.transparent)     this->transparent();
+    }
+
+    /// Get the underlying data array to use to construct a GLXContext.
+    int* get() { cap(); return data; }
+
+    /// Set RGBA-mode minimum bit-depths.
+    void rgba(int rbits, int gbits, int bbits, int abits = -1)
+    {
+      push( GLX_RGBA );
+      push( GLX_RED_SIZE );
+      push( rbits );          dbgEval(3, rbits);
+      push( GLX_GREEN_SIZE );
+      push( gbits );          dbgEval(3, gbits);
+      push( GLX_BLUE_SIZE );
+      push( bbits );          dbgEvalNL(3, bbits);
+      if (abits > 0)
+        {
+          push( GLX_ALPHA_SIZE );
+          push( abits );      dbgEvalNL(3, abits);
+        }
+    }
+
+    /// Specify the stacking level for this context.
+    void level(int lev)
+    {
+      push( GLX_LEVEL );
+      push( lev );
+    }
+
+    /// Specify a minimum bit-depth for color-index mode.
+    void colorIndex(int bits)
+    {
+      push( GLX_BUFFER_SIZE );
+      push( bits );
+    }
+
+    /// Specify a minimum bit-depth for the depth-buffer (i.e. z-buffer).
+    void depthBuffer(int bits)
+    {
+      push( GLX_DEPTH_SIZE );
+      push( bits );
+    }
+
+    /// Request double-buffering mode.
+    void doubleBuffer()
+    {
+      push( GLX_DOUBLEBUFFER );
+    }
+
+    /// Specify a minimum bit-depth for the stencil buffer.
+    void stencilBuffer(int bits)
+    {
+      push( GLX_STENCIL_SIZE );
+      push( bits );
+    }
+
+    /// Specify minimum bit-depths for the accum buffer.
+    void accum(int rbits, int gbits, int bbits, int abits = -1)
+    {
+      push( GLX_ACCUM_RED_SIZE );
+      push( rbits );
+      push( GLX_ACCUM_GREEN_SIZE );
+      push( gbits );
+      push( GLX_ACCUM_BLUE_SIZE );
+      push( bbits );
+      if (abits > 0)
+        {
+          push( GLX_ACCUM_ALPHA_SIZE );
+          push( abits );
+        }
+    }
+
+    /// Request a number of aux buffers.
+    void auxBuffers(int n)
+    {
+      push( GLX_AUX_BUFFERS );
+      push( n );
+    }
+
+    /// Request transparency if available.
+    void transparent()
+    {
+#ifdef GLX_TRANSPARENT_TYPE_EXT
+      push( GLX_TRANSPARENT_TYPE_EXT );
+      push( GLX_TRANSPARENT_INDEX_EXT );
+#endif
+    }
+  };
 }
 
 GlxWrapper::GlxWrapper(Display* dpy, GlxOpts& opts, GlxWrapper* share) :
@@ -62,7 +195,7 @@ DOTRACE("GlxWrapper::GlxWrapper");
       throw Util::Error("GlxWrapper: X server has no OpenGL GLX extension");
     }
 
-  GlxAttribs attribs(opts);
+  AttribList attribs(opts);
 
   itsVisInfo = glXChooseVisual(itsDisplay,
                                DefaultScreen(itsDisplay),
