d42773b8c0c733bad988e51f0c9038d663c0cf16
Robert J. Peters <rjpcal@gmail.com>
2004-07-21 00:07:40 +0000

    Changed to sneak the pathname to the installation 'bin' directory into the Tcl code via an environment variable from /bin/sh.

--- share/scripts/visedit.tcl
+++ share/scripts/visedit.tcl
@@ -1,6 +1,21 @@
 #!/bin/sh
+
+# We sneak the pathname to the 'bin' directory into the tcl code via an
+# environment variable, GROOVX_BINDIR. We need this because it's possible
+# (e.g. on Mac OS X) that the groovx executable is itself a script that
+# execs a different file (in order to get at an executable nestled inside
+# Groovx.app/Contents/MacOS, for example). When that happens, we'll have
+# lost track of the original 'bin' directory, except that we've saved it in
+# the GROOVX_BINDIR environment variable.
+
+# The next two lines are seen first by /bin/sh as a comment followed by a
+# line of shell script. That line of shell script execs the groovx
+# executable with this file as its first argument. When groovx (i.e. Tcl)
+# re-reads this file, it ignores the next two lines since it sees them as a
+# single comment because of the backslash in the comment line.
+
 # \
-exec `dirname $0`/groovx "$0" "$@"
+    export GROOVX_BINDIR=`dirname $0`; exec ${GROOVX_BINDIR}/groovx "$0" "$@"
 
 # $Id$
 
@@ -11,4 +26,4 @@ exec `dirname $0`/groovx "$0" "$@"
 # installations on a given system, and each installation's visedit.tcl will
 # be run using the groovx from the same installation.)
 
-source [file dirname [info nameofexecutable]]/editor.tcl
+source $::env(GROOVX_BINDIR)/editor.tcl
