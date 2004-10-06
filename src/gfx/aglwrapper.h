a0dce28d929157d5258bb3091308e9cd03346cff
Robert J. Peters <rjpcal@gmail.com>
2004-10-06 01:32:49 +0000

    Rewrap GPL message to avoid long lines.

--- src/gfx/aglwrapper.h
+++ src/gfx/aglwrapper.h
@@ -2,7 +2,8 @@
 //
 // aglwrapper.h
 //
-// Copyright (c) 2004-2004 Rob Peters rjpeters at klab dot caltech dot edu
+// Copyright (c) 2004-2004
+// Rob Peters <rjpeters at klab dot caltech dot edu>
 //
 // created: Tue Jul 20 10:18:14 2004
 // commit: $Id$
@@ -10,20 +11,21 @@
 // --------------------------------------------------------------------
 //
 // This file is part of GroovX.
+//   [http://www.klab.caltech.edu/rjpeters/groovx/]
 //
-// GroovX is free software; you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free
-// Software Foundation; either version 2 of the License, or (at your
-// option) any later version.
+// GroovX is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
 //
-// GroovX is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-// for more details.
+// GroovX is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License along
-// with GroovX; if not, write to the Free Software Foundation, Inc., 59
-// Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+// You should have received a copy of the GNU General Public License
+// along with GroovX; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 //
 ///////////////////////////////////////////////////////////////////////
 
--- src/gfx/gxtext.h
+++ src/gfx/gxtext.h
@@ -2,7 +2,8 @@
 //
 // gxtext.h
 //
-// Copyright (c) 1999-2004 Rob Peters rjpeters at klab dot caltech dot edu
+// Copyright (c) 1999-2004
+// Rob Peters <rjpeters at klab dot caltech dot edu>
 //
 // created: Thu Jul  1 11:54:47 1999 (as gtext.h)
 // commit: $Id$
@@ -10,20 +11,21 @@
 // --------------------------------------------------------------------
 //
 // This file is part of GroovX.
+//   [http://www.klab.caltech.edu/rjpeters/groovx/]
 //
-// GroovX is free software; you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free
-// Software Foundation; either version 2 of the License, or (at your
-// option) any later version.
+// GroovX is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
 //
-// GroovX is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-// for more details.
+// GroovX is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License along
-// with GroovX; if not, write to the Free Software Foundation, Inc., 59
-// Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+// You should have received a copy of the GNU General Public License
+// along with GroovX; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 //
 ///////////////////////////////////////////////////////////////////////
 
