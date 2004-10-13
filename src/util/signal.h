f20d029a191e2fd6b003f06e35eab6cce5700a18
Robert J. Peters <rjpcal@gmail.com>
2004-10-13 17:33:42 +0000

    Re-indented.

--- src/util/signal.h
+++ src/util/signal.h
@@ -39,336 +39,337 @@
 namespace Util
 {
 #if 0
-template <class R>
-class Marshal
-{
-  R val;
-
-public:
-  Marshal() : val() {}
-
-  typedef R InType;
-  typedef R OutType;
-
-  bool marshal(const InType& in)
+  template <class R>
+  class Marshal
   {
-    val = in;
-  }
+    R val;
 
-  OutType value() const { return val; }
-};
+  public:
+    Marshal() : val() {}
+
+    typedef R InType;
+    typedef R OutType;
+
+    bool marshal(const InType& in)
+    {
+      val = in;
+    }
+
+    OutType value() const { return val; }
+  };
 #endif
 
-//  ###################################################################
-//  ===================================================================
+  //  ###################################################################
+  //  ===================================================================
 
-/// The base class for all slot classes.
+  /// The base class for all slot classes.
 
-/** Slots can be triggered from a Signal by calling connect() on that
-    Signal. Thereafter, the slot will be called whenever that signal
-    emits a message. */
+  /** Slots can be triggered from a Signal by calling connect() on
+      that Signal. Thereafter, the slot will be called whenever that
+      signal emits a message. */
 
-class SlotBase : public virtual Util::Object
-{
-public:
-  /// Default constructor.
-  SlotBase();
-
-  /// Virtual destructor.
-  virtual ~SlotBase() throw();
-
-  /// Answers whether the components of this Slot still exist.
-  /** This allows a slot adapter, for example, to indicate if its
-      target object has disappeared. */
-  virtual bool exists() const = 0;
-
-  /// Call the slot with the given arguments
-  virtual void doCall(void* params) = 0;
-};
-
-//  ###################################################################
-//  ===================================================================
-
-/// A zero-argument slot class.
-
-class Slot0 : public SlotBase
-{
-public:
-  /// Default constructor.
-  Slot0();
-
-  /// Virtual destructor.
-  virtual ~Slot0() throw();
-
-  template<class C, class MF>
-  static Util::SoftRef<Slot0> make(C* obj, MF mf);
-
-  static Util::SoftRef<Slot0> make(void (*freeFunc)());
-
-  virtual void call() = 0;
-
-  /// Unpacks any parameters and then calls the implementation.
-  virtual void doCall(void* /*params*/) { call(); }
-};
-
-
-//  ###################################################################
-//  ===================================================================
-
-/// A mem-func adapter for zero-argument slots.
-
-template <class C, class MF>
-class SlotAdapterMemFunc0 : public Slot0
-{
-  Util::SoftRef<C> itsObject;
-  MF itsMemFunc;
-
-  SlotAdapterMemFunc0(C* obj, MF mf) :
-    itsObject(obj, Util::WEAK, Util::PRIVATE), itsMemFunc(mf) {}
-
-  virtual ~SlotAdapterMemFunc0() throw() {}
-
-public:
-  static SlotAdapterMemFunc0<C, MF>* make(C* obj, MF mf)
-  { return new SlotAdapterMemFunc0<C, MF>(obj, mf); }
-
-  virtual bool exists() const { return itsObject.isValid(); }
-
-  virtual void call()
+  class SlotBase : public virtual Util::Object
   {
-    if (itsObject.isValid())
-      (itsObject.get()->*itsMemFunc)();
-  }
-};
+  public:
+    /// Default constructor.
+    SlotBase();
 
-template <class C, class MF>
-inline Util::SoftRef<Slot0> Slot0::make(C* obj, MF mf)
-{
-  return Util::SoftRef<Slot0>
-    (SlotAdapterMemFunc0<C, MF>::make(obj, mf),
-     Util::STRONG,
-     Util::PRIVATE);
-}
+    /// Virtual destructor.
+    virtual ~SlotBase() throw();
 
-//  ###################################################################
-//  ===================================================================
+    /// Answers whether the components of this Slot still exist.
+    /** This allows a slot adapter, for example, to indicate if its
+        target object has disappeared. */
+    virtual bool exists() const = 0;
 
-/// A mem-func adapter for zero-argument slots.
+    /// Call the slot with the given arguments
+    virtual void doCall(void* params) = 0;
+  };
 
-class SlotAdapterFreeFunc0 : public Slot0
-{
-public:
-  typedef void (FreeFunc)();
+  //  ###################################################################
+  //  ===================================================================
 
-private:
-  FreeFunc* itsFreeFunc;
+  /// A zero-argument slot class.
 
-  SlotAdapterFreeFunc0(FreeFunc* f);
-
-  virtual ~SlotAdapterFreeFunc0() throw();
-
-public:
-  static SlotAdapterFreeFunc0* make(FreeFunc* f);
-
-  virtual bool exists() const;
-
-  virtual void call();
-};
-
-//  ###################################################################
-//  ===================================================================
-
-/// A one-argument call data wrapper.
-
-template <class P1>
-struct CallData1
-{
-  CallData1(P1 i1) : p1(i1) {}
-  P1 p1;
-};
-
-//  ###################################################################
-//  ===================================================================
-
-/// A slot with one argument.
-
-template <class P1>
-class Slot1 : public SlotBase
-{
-public:
-  /// Default constructor.
-  Slot1() {}
-
-  /// Virtual destructor.
-  virtual ~Slot1() throw() {}
-
-  template<class C, class MF>
-  static Util::SoftRef<Slot1<P1> > make(C* obj, MF mf);
-
-  virtual void call(P1 p1) = 0;
-
-  /// Unpacks any parameters and then calls the implementation.
-  virtual void doCall(void* params)
+  class Slot0 : public SlotBase
   {
-    CallData1<P1>* data = static_cast<CallData1<P1>*>(params);
-    call(data->p1);
-  }
-};
+  public:
+    /// Default constructor.
+    Slot0();
 
-//  ###################################################################
-//  ===================================================================
+    /// Virtual destructor.
+    virtual ~Slot0() throw();
 
-/// A mem-func adapter for slots with one argument.
+    template<class C, class MF>
+    static Util::SoftRef<Slot0> make(C* obj, MF mf);
 
-template <class P1, class C, class MF>
-class SlotAdapterMemFunc1 : public Slot1<P1>
-{
-  Util::SoftRef<C> itsObject;
-  MF itsMemFunc;
+    static Util::SoftRef<Slot0> make(void (*freeFunc)());
 
-  SlotAdapterMemFunc1(C* obj, MF mf) :
-    itsObject(obj, Util::WEAK, Util::PRIVATE), itsMemFunc(mf) {}
+    virtual void call() = 0;
 
-  virtual ~SlotAdapterMemFunc1() throw() {}
-
-public:
-  static SlotAdapterMemFunc1<P1, C, MF>* make(C* obj, MF mf)
-  { return new SlotAdapterMemFunc1<P1, C, MF>(obj, mf); }
-
-  virtual bool exists() const { return itsObject.isValid(); }
-
-  virtual void call(P1 p1)
-  {
-    if (itsObject.isValid())
-      (itsObject.get()->*itsMemFunc)(p1);
-  }
-};
-
-template <class P1>
-template <class C, class MF>
-inline Util::SoftRef<Slot1<P1> > Slot1<P1>::make(C* obj, MF mf)
-{
-  return Util::SoftRef<Slot1<P1> >
-    (SlotAdapterMemFunc1<P1, C, MF>::make(obj, mf),
-     Util::STRONG,
-     Util::PRIVATE);
-}
+    /// Unpacks any parameters and then calls the implementation.
+    virtual void doCall(void* /*params*/) { call(); }
+  };
 
 
-//  ###################################################################
-//  ===================================================================
+  //  ###################################################################
+  //  ===================================================================
 
-/// SignalBase provides basic implementation for Signal.
-
-/** Classes that need to notify others of changes should hold an
-    appropriate signal object by value, and call emit() when it is
-    necessary to notify observers of the change. In turn, emit() will
-    \c call all of the Slot's that are observing this Signal. */
-
-class SignalBase : public Util::VolatileObject
-{
-protected:
-  SignalBase();
-  virtual ~SignalBase() throw();
-
-  void doEmit(void* params) const;
-
-  /// Add a Slot to the list of those watching this Signal.
-  void doConnect(Util::SoftRef<SlotBase> slot);
-
-  /// Remove a Slot from the list of those watching this Signal.
-  void doDisconnect(Util::SoftRef<SlotBase> slot);
-
-private:
-  SignalBase(const SignalBase&);
-  SignalBase& operator=(const SignalBase&);
-
-  class Impl;
-  Impl* rep;
-};
-
-
-//  ###################################################################
-//  ===================================================================
-
-/// A zero-argument signal.
-
-class Signal0 : public SignalBase
-{
-public:
-  /// Default constructor.
-  Signal0();
-
-  /// Virtual destructor.
-  virtual ~Signal0() throw();
-
-  /// Add a Slot to the list of those watching this Signal.
-  Util::SoftRef<Slot0> connect(Util::SoftRef<Slot0> slot)
-  { SignalBase::doConnect(slot); return slot; }
-
-  /// Connect a free function to this Signal0.
-  Util::SoftRef<Slot0> connect(void (*freeFunc)())
-  { return connect(Slot0::make(freeFunc)); }
-
-  /// Connect an object to this Signal0.
-  /** After connection, when the signal is triggered, \a mem_func will
-      be called on \a obj. \c connect() returns the Util::UID of the
-      connection object that is created. */
-  template <class C, class MF>
-  Util::SoftRef<Slot0> connect(C* obj, MF mem_func)
-  { return connect(Slot0::make(obj, mem_func)); }
-
-  /// Remove a Slot from the list of those watching this Signal0.
-  void disconnect(Util::SoftRef<Slot0> slot)
-  { SignalBase::doDisconnect(slot); }
-
-  /// Trigger all of this object's Slots.
-  void emit() const { SignalBase::doEmit((void*)0); }
-
-  /// Returns a slot which when called will cause this Signal to emit().
-  Util::SoftRef<Slot0> slot() const { return slotEmitSelf; }
-
-private:
-  Signal0(const Signal0&);
-  Signal0& operator=(const Signal0&);
-
-  Util::Ref<Util::Slot0> slotEmitSelf;
-};
-
-
-//  ###################################################################
-//  ===================================================================
-
-/// A one-argument signal.
-
-template <class P1>
-class Signal1 : public SignalBase
-{
-public:
-  Signal1() {}
-
-  virtual ~Signal1() throw() {}
-
-  Util::SoftRef<Slot1<P1> > connect(Util::SoftRef<Slot1<P1> > slot)
-  { SignalBase::doConnect(slot); return slot; }
+  /// A mem-func adapter for zero-argument slots.
 
   template <class C, class MF>
-  Util::SoftRef<Slot1<P1> > connect(C* obj, MF mem_func)
-  { return connect(Slot1<P1>::make(obj, mem_func)); }
-
-  void disconnect(Util::SoftRef<Slot1<P1> > slot)
-  { SignalBase::doDisconnect(slot); }
-
-  void emit(P1 p1) const
+  class SlotAdapterMemFunc0 : public Slot0
   {
-    CallData1<P1> dat( p1 );
-    SignalBase::doEmit(static_cast<void*>(&dat));
+    Util::SoftRef<C> itsObject;
+    MF itsMemFunc;
+
+    SlotAdapterMemFunc0(C* obj, MF mf) :
+      itsObject(obj, Util::WEAK, Util::PRIVATE), itsMemFunc(mf) {}
+
+    virtual ~SlotAdapterMemFunc0() throw() {}
+
+  public:
+    static SlotAdapterMemFunc0<C, MF>* make(C* obj, MF mf)
+    { return new SlotAdapterMemFunc0<C, MF>(obj, mf); }
+
+    virtual bool exists() const { return itsObject.isValid(); }
+
+    virtual void call()
+    {
+      if (itsObject.isValid())
+        (itsObject.get()->*itsMemFunc)();
+    }
+  };
+
+  template <class C, class MF>
+  inline Util::SoftRef<Slot0> Slot0::make(C* obj, MF mf)
+  {
+    return Util::SoftRef<Slot0>
+      (SlotAdapterMemFunc0<C, MF>::make(obj, mf),
+       Util::STRONG,
+       Util::PRIVATE);
   }
 
-private:
-  Signal1(const Signal1&);
-  Signal1& operator=(const Signal1&);
-};
+  //  ###################################################################
+  //  ===================================================================
+
+  /// A mem-func adapter for zero-argument slots.
+
+  class SlotAdapterFreeFunc0 : public Slot0
+  {
+  public:
+    typedef void (FreeFunc)();
+
+  private:
+    FreeFunc* itsFreeFunc;
+
+    SlotAdapterFreeFunc0(FreeFunc* f);
+
+    virtual ~SlotAdapterFreeFunc0() throw();
+
+  public:
+    static SlotAdapterFreeFunc0* make(FreeFunc* f);
+
+    virtual bool exists() const;
+
+    virtual void call();
+  };
+
+  //  ###################################################################
+  //  ===================================================================
+
+  /// A one-argument call data wrapper.
+
+  template <class P1>
+  struct CallData1
+  {
+    CallData1(P1 i1) : p1(i1) {}
+    P1 p1;
+  };
+
+  //  ###################################################################
+  //  ===================================================================
+
+  /// A slot with one argument.
+
+  template <class P1>
+  class Slot1 : public SlotBase
+  {
+  public:
+    /// Default constructor.
+    Slot1() {}
+
+    /// Virtual destructor.
+    virtual ~Slot1() throw() {}
+
+    template<class C, class MF>
+    static Util::SoftRef<Slot1<P1> > make(C* obj, MF mf);
+
+    virtual void call(P1 p1) = 0;
+
+    /// Unpacks any parameters and then calls the implementation.
+    virtual void doCall(void* params)
+    {
+      CallData1<P1>* data = static_cast<CallData1<P1>*>(params);
+      call(data->p1);
+    }
+  };
+
+  //  ###################################################################
+  //  ===================================================================
+
+  /// A mem-func adapter for slots with one argument.
+
+  template <class P1, class C, class MF>
+  class SlotAdapterMemFunc1 : public Slot1<P1>
+  {
+    Util::SoftRef<C> itsObject;
+    MF itsMemFunc;
+
+    SlotAdapterMemFunc1(C* obj, MF mf) :
+      itsObject(obj, Util::WEAK, Util::PRIVATE), itsMemFunc(mf) {}
+
+    virtual ~SlotAdapterMemFunc1() throw() {}
+
+  public:
+    static SlotAdapterMemFunc1<P1, C, MF>* make(C* obj, MF mf)
+    { return new SlotAdapterMemFunc1<P1, C, MF>(obj, mf); }
+
+    virtual bool exists() const { return itsObject.isValid(); }
+
+    virtual void call(P1 p1)
+    {
+      if (itsObject.isValid())
+        (itsObject.get()->*itsMemFunc)(p1);
+    }
+  };
+
+  template <class P1>
+  template <class C, class MF>
+  inline Util::SoftRef<Slot1<P1> > Slot1<P1>::make(C* obj, MF mf)
+  {
+    return Util::SoftRef<Slot1<P1> >
+      (SlotAdapterMemFunc1<P1, C, MF>::make(obj, mf),
+       Util::STRONG,
+       Util::PRIVATE);
+  }
+
+
+  //  ###################################################################
+  //  ===================================================================
+
+  /// SignalBase provides basic implementation for Signal.
+
+  /** Classes that need to notify others of changes should hold an
+      appropriate signal object by value, and call emit() when it is
+      necessary to notify observers of the change. In turn, emit()
+      will \c call all of the Slot's that are observing this
+      Signal. */
+
+  class SignalBase : public Util::VolatileObject
+  {
+  protected:
+    SignalBase();
+    virtual ~SignalBase() throw();
+
+    void doEmit(void* params) const;
+
+    /// Add a Slot to the list of those watching this Signal.
+    void doConnect(Util::SoftRef<SlotBase> slot);
+
+    /// Remove a Slot from the list of those watching this Signal.
+    void doDisconnect(Util::SoftRef<SlotBase> slot);
+
+  private:
+    SignalBase(const SignalBase&);
+    SignalBase& operator=(const SignalBase&);
+
+    class Impl;
+    Impl* rep;
+  };
+
+
+  //  ###################################################################
+  //  ===================================================================
+
+  /// A zero-argument signal.
+
+  class Signal0 : public SignalBase
+  {
+  public:
+    /// Default constructor.
+    Signal0();
+
+    /// Virtual destructor.
+    virtual ~Signal0() throw();
+
+    /// Add a Slot to the list of those watching this Signal.
+    Util::SoftRef<Slot0> connect(Util::SoftRef<Slot0> slot)
+    { SignalBase::doConnect(slot); return slot; }
+
+    /// Connect a free function to this Signal0.
+    Util::SoftRef<Slot0> connect(void (*freeFunc)())
+    { return connect(Slot0::make(freeFunc)); }
+
+    /// Connect an object to this Signal0.
+    /** After connection, when the signal is triggered, \a mem_func
+        will be called on \a obj. \c connect() returns the Util::UID
+        of the connection object that is created. */
+    template <class C, class MF>
+    Util::SoftRef<Slot0> connect(C* obj, MF mem_func)
+    { return connect(Slot0::make(obj, mem_func)); }
+
+    /// Remove a Slot from the list of those watching this Signal0.
+    void disconnect(Util::SoftRef<Slot0> slot)
+    { SignalBase::doDisconnect(slot); }
+
+    /// Trigger all of this object's Slots.
+    void emit() const { SignalBase::doEmit((void*)0); }
+
+    /// Returns a slot which when called will cause this Signal to emit().
+    Util::SoftRef<Slot0> slot() const { return slotEmitSelf; }
+
+  private:
+    Signal0(const Signal0&);
+    Signal0& operator=(const Signal0&);
+
+    Util::Ref<Util::Slot0> slotEmitSelf;
+  };
+
+
+  //  ###################################################################
+  //  ===================================================================
+
+  /// A one-argument signal.
+
+  template <class P1>
+  class Signal1 : public SignalBase
+  {
+  public:
+    Signal1() {}
+
+    virtual ~Signal1() throw() {}
+
+    Util::SoftRef<Slot1<P1> > connect(Util::SoftRef<Slot1<P1> > slot)
+    { SignalBase::doConnect(slot); return slot; }
+
+    template <class C, class MF>
+    Util::SoftRef<Slot1<P1> > connect(C* obj, MF mem_func)
+    { return connect(Slot1<P1>::make(obj, mem_func)); }
+
+    void disconnect(Util::SoftRef<Slot1<P1> > slot)
+    { SignalBase::doDisconnect(slot); }
+
+    void emit(P1 p1) const
+    {
+      CallData1<P1> dat( p1 );
+      SignalBase::doEmit(static_cast<void*>(&dat));
+    }
+
+  private:
+    Signal1(const Signal1&);
+    Signal1& operator=(const Signal1&);
+  };
 
 } // end namespace Util
 
