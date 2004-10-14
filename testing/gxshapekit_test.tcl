1bc553cbae8381478afa0fba4b4577d09f11024d
Robert J. Peters <rjpcal@gmail.com>
2004-10-14 20:18:19 +0000

    Cleanup -- remove superfluous conditional logic by splitting baseclass and subclass tests into separate procs.

--- testing/gxshapekit_test.tcl
+++ testing/gxshapekit_test.tcl
@@ -7,13 +7,14 @@
 ##############################################################################
 
 # Multiple inclusion guard
-if { [info exists GxShapeKit::TEST_DEFINED] } return;
+if { [info exists ::GXSHAPEKIT_TEST_DEFINED] } return;
+
+set ::GXSHAPEKIT_TEST_DEFINED 1
 
 package require Gxshapekit
 package require Toglet
 
 source ${::TEST_DIR}/io_test.tcl
-
 
 namespace eval GxShapeKit {
 
@@ -23,95 +24,81 @@ variable BASE_CLASS_TESTED 0
 namespace export testSubclass
 proc testSubclass { subclass {objid -1} } {
 
-    variable BASE_CLASS_TESTED
-
-    set testbase [expr !$BASE_CLASS_TESTED]
-    set testsubclass [expr ![string equal $subclass "GxShapeKit"]]
-
-    if { $testsubclass } {
-        if { $objid == -1 } {
-            set objid [Obj::new $subclass]
-        } else {
-            set objid $objid
-        }
-    } else {
-        set objid -1
+    if { $objid == -1 } {
+	set objid [Obj::new $subclass]
     }
 
-    if { $objid > 0 } {
-        ::testReadWrite $subclass $objid
-    }
+    ::testReadWrite $subclass $objid
 
     # class::type
-    if { $testbase } {
-        ::test ${subclass}::type "too few args" {
-            Obj::type
-	} {wrong \# args: should be}
-
-        ::test ${subclass}::type "too many args" {
-            Obj::type 0 junk
-	} {wrong \# args: should be}
-
-        ::test ${subclass}::type "error from bad objid" {
-            Obj::type -1
-        } {expected.*but got}
-    }
-
-    if { $testsubclass } {
-        ::test ${subclass}::type "normal use on $subclass" [format {
-            Obj::type %s
-        } $objid] $subclass
-    }
+    ::test ${subclass}::type "normal use on $subclass" [format {
+	Obj::type %s
+    } $objid] $subclass
 
 
-    if { $testbase } {
-	# classname::category
-        ::test ${subclass}::category "too few args" {
-            GxShapeKit::category
-	} {wrong \# args: should be}
-        ::test ${subclass}::category "too many args" {
-            GxShapeKit::category 0 0 junk
-	} {wrong \# args: should be}
-    }
+    # classname::category
+    ::test ${subclass}::category "normal use get" [format {
+	GxShapeKit::category %s
+    } $objid] "^$::INT$"
 
-    if { $testsubclass } {
+    ::test ${subclass}::category "normal use set" [format {
+	GxShapeKit::category %s 34
+	GxShapeKit::category %s
+    } $objid $objid] {^34$}
 
-	# classname::category
-        ::test ${subclass}::category "normal use get" [format {
-            GxShapeKit::category %s
-        } $objid] "^$::INT$"
+    ::test ${subclass}::category "normal use vector get" [format {
+	GxShapeKit::category "%s %s"
+    } $objid $objid] "^${::INT}${::SP}${::INT}$"
 
-        ::test ${subclass}::category "normal use set" [format {
-            GxShapeKit::category %s 34
-            GxShapeKit::category %s
-        } $objid $objid] {^34$}
+    ::test ${subclass}::category "normal use vector set with one value" [format {
+	GxShapeKit::category "%s %s" 49
+	GxShapeKit::category "%s %s"
+    } $objid $objid $objid $objid] "^${::INT}${::SP}${::INT}$"
 
-        ::test ${subclass}::category "normal use vector get" [format {
-            GxShapeKit::category "%s %s"
-        } $objid $objid] "^${::INT}${::SP}${::INT}$"
+    ::test ${subclass}::category "normal use vector set with many values" [format {
+	GxShapeKit::category "%s %s" "3 7 11"
+	GxShapeKit::category "%s %s"
+    } $objid $objid $objid $objid] "^${::INT}${::SP}${::INT}$"
 
-        ::test ${subclass}::category "normal use vector set with one value" [format {
-            GxShapeKit::category "%s %s" 49
-            GxShapeKit::category "%s %s"
-        } $objid $objid $objid $objid] "^${::INT}${::SP}${::INT}$"
 
-        ::test ${subclass}::category "normal use vector set with many values" [format {
-            GxShapeKit::category "%s %s" "3 7 11"
-            GxShapeKit::category "%s %s"
-        } $objid $objid $objid $objid] "^${::INT}${::SP}${::INT}$"
+    # class::draw
+    ::test ${subclass}::draw "normal use" [format {
+	glClearColor 0 0 0 0
+	glColor 1 1 1 1
+	clearscreen
+	set pix1 [pixelCheckSum]
+	see %s
+	set pix2 [pixelCheckSum]
+	-> [Toglet::current] setVisible false
+	return "[expr $pix1 == $pix2] $pix1 $pix2"
+    } $objid] {^0 }
+}
 
-	# class::draw
-        ::test ${subclass}::draw "normal use" [format {
-            glClearColor 0 0 0 0
-            glColor 1 1 1 1
-            clearscreen
-            set pix1 [pixelCheckSum]
-            see %s
-            set pix2 [pixelCheckSum]
-            -> [Toglet::current] setVisible false
-            return "[expr $pix1 == $pix2] $pix1 $pix2"
-	} $objid] {^0 }
-    }
+proc testSelf {} {
+    variable BASE_CLASS_TESTED
+
+    if { $BASE_CLASS_TESTED } { return }
+
+    # class::type
+    ::test GxShapeKit::type "too few args" {
+	Obj::type
+    } {wrong \# args: should be}
+
+    ::test GxShapeKit::type "too many args" {
+	Obj::type 0 junk
+    } {wrong \# args: should be}
+
+    ::test GxShapeKit::type "error from bad objid" {
+	Obj::type -1
+    } {expected.*but got}
+
+    # classname::category
+    ::test GxShapeKit::category "too few args" {
+	GxShapeKit::category
+    } {wrong \# args: should be}
+    ::test GxShapeKit::category "too many args" {
+	GxShapeKit::category 0 0 junk
+    } {wrong \# args: should be}
 
     set BASE_CLASS_TESTED 1
 }
@@ -119,4 +106,4 @@ proc testSubclass { subclass {objid -1} } {
 }
 # end namespace GxShapeKit
 
-GxShapeKit::testSubclass GxShapeKit
+GxShapeKit::testSelf
