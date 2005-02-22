284362279978ef0529a3a574313d89f2dde783a4
Robert J. Peters <rjpcal@gmail.com>
2005-02-22 20:15:14 +0000

    Make local variables 'const' in erfc().

--- src/pkgs/mtx/num.h
+++ src/pkgs/mtx/num.h
@@ -39,13 +39,13 @@ class Num
 {
 public:
   /// the error function
-  static double erfc(double x)
+  static double erfc(const double x)
   {
-    double z = fabs(x);
+    const double z = fabs(x);
 
-    double t = 1.0/(1.0+0.5*fabs(x));
+    const double t = 1.0/(1.0+0.5*fabs(x));
 
-    double ans =
+    const double ans =
       t*exp(-z*z-1.26551223+t*(1.00002368+t*(0.37409196+t*(0.09678418+
       t*(-0.18628806+t*(0.27886807+t*(-1.13520398+t*(1.48851587+
       t*(-0.82215223+t*0.17087277)))))))))
