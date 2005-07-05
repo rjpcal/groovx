9af656fa9c0b8fc0c150307c742f9a41e9c8019a
Robert J. Peters <rjpcal@gmail.com>
2005-07-05 18:24:30 +0000

    * devscripts/list-all-sources.sh -- lists not only actual source   files, but also any related text files (such as READMEs and other   documentation) that are likely to mention source code constructs by   name -- thus, if we do a global search-and-replace, we'd like to   also apply it to such auxiliary files

---
+++ devscripts/list-all-sources.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# $Id$
+
+# Just print a list of all source files to stdout, for use in other
+# scripts.
+
+flz=$(find `dirname $0`/../src \
+    -name \*.H \
+    -or -name \*.C \
+    -or -name \*.cc \
+    -or -name \*.hh \
+    -or -name \*.c \
+    -or -name \*.h \
+    -or -name \*.hpp \
+    -or -name \*.cpp)
+
+for f in $flz README TODO TUTORIAL doc/groovx.dxy; do
+    g=${f#`dirname $0`/../}
+    echo $g
+done
