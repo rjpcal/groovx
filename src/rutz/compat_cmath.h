1cdbfed2ed440565d956d34420915b437fd69faa
Robert J. Peters <rjpcal@gmail.com>
2006-11-07 17:54:46 +0000

    * Unified isnan() handling -- if the configure script finds   MISSING_ISNAN, we define a compatibility isnan() function in   rutz/compat_cmath.h that will call std::isnan() (other compatibility   options are possible in the future as well). So for now the right   way to call isnan is with a literal "isnan(xyz)", i.e. with no std::   prefix, since one way another we will have a global isnan() either   from <cmath> or else from rutz/compat_cmath.h.

--- src/rutz/compat_cmath.h
+++ src/rutz/compat_cmath.h
@@ -64,5 +64,12 @@ namespace rutz
 #define                cbrt(x)  (pow(x, 1.0/3.0))
 #endif
 
+#ifdef MISSING_ISNAN
+#  if defined(HAVE_STD_ISNAN)
+template <class T> inline
+int                    isnan(x) { return std::isnan(x); }
+#  endif
+#endif
+
 static const char __attribute__((used)) vcid_groovx_rutz_compat_cmath_h_utc20060117021640[] = "$Id$ $HeadURL$";
 #endif // !GROOVX_RUTZ_COMPAT_CMATH_H_UTC20060117021640DEFINED
